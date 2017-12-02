fcd128b070605f5ee3a7713667cf871c5476ac93
xtaci <daniel820313@gmail.com>
2017-12-02 16:11:37 +0800

    Add  ERC20 compatible interfaces

--- AElf.Kernel/IAccount.cs
+++ AElf.Kernel/IAccount.cs
@@ -1,5 +1,10 @@
-﻿namespace AElf.Kernel
+﻿using System.Numerics;
+
+namespace AElf.Kernel
 {
+    using AddressType = IHash<IAccount>;
+    using ValueType = System.Numerics.BigInteger;
+
     /// <summary>
     /// Every smart contract was an account
     /// </summary>
@@ -9,6 +14,58 @@
         /// Get Account's Address, the address is the id for a account
         /// </summary>
         /// <returns></returns>
-        IHash<IAccount> GetAddress();
+        AddressType GetAddress();
+
+        /// <summary>
+        /// Gets the balance of this account
+        /// </summary>
+        /// <returns>The balance.</returns>
+        ValueType GetBalance();
+
+        /// <summary>
+        /// Gets the total token supply.
+        /// </summary>
+        /// <returns>The total token supply.</returns>
+        ValueType GetTotalTokenSupply();
+
+        /// <summary>
+        /// Gets the token balance of.
+        /// </summary>
+        /// <returns>The token balance of.</returns>
+        /// <param name="owner">Owner Address.</param>
+        ValueType GetTokenBalanceOf(AddressType owner);
+
+        /// <summary>
+        /// Transfers the token.
+        /// </summary>
+        /// <returns><c>true</c>, if token was transfered, <c>false</c> otherwise.</returns>
+        /// <param name="to">To.</param>
+        /// <param name="value">Value.</param>
+        bool TransferToken(AddressType to, ValueType value);
+
+        /// <summary>
+        /// Transfers the token from one address to another if approved
+        /// </summary>
+        /// <returns><c>true</c>, if token from was transfered, <c>false</c> otherwise.</returns>
+        /// <param name="from">From.</param>
+        /// <param name="to">To.</param>
+        /// <param name="value">Value.</param>
+        bool TransferTokenFrom(AddressType from, AddressType to, ValueType value);
+
+        /// <summary>
+        /// Approve the specified spender of value.
+        /// </summary>
+        /// <returns>The approve.</returns>
+        /// <param name="spender">Spender.</param>
+        /// <param name="_value">Value.</param>
+        bool ApproveToken(AddressType spender, ValueType value);
+
+        /// <summary>
+        /// Gets the allowance token.
+        /// </summary>
+        /// <returns>The allowance token.</returns>
+        /// <param name="owner">Owner.</param>
+        /// <param name="spender">Spender.</param>
+        ValueType GetAllowanceToken(AddressType owner, AddressType spender);
     }
 }
