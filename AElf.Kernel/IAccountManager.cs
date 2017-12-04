8c2df128e14434e175da1518466085ad33591d64
Loning <aloning@gmail.com>
2017-12-04 11:20:54 +0800

    [Kernel] add some basic interfaces

---
+++ AElf.Kernel/IAccountManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace AElf.Kernel
+{
+    public interface IAccountManager
+    {
+        /// <summary>
+        /// send a transaction from an account
+        /// </summary>
+        /// <param name="fromAccount"></param>
+        /// <param name="tx"></param>
+        /// <returns></returns>
+        Task SendTransactionAsync(IAccount fromAccount, ITransaction tx);
+    }
+}
--- AElf.Kernel/IWorldState.cs
+++ AElf.Kernel/IWorldState.cs
@@ -5,6 +5,11 @@
     /// </summary>
     public interface IWorldState
     {
-        
+        /// <summary>
+        /// Get a data provider for an account to do further data operation 
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        IAccountDataProvider GetAccountDataProviderByAccount(IAccount account);
     }
 }
