f3787749fa67b0ffed1eea9fb5f1e712736a3df6
xtaci <daniel820313@gmail.com>
2017-12-07 18:37:22 +0800

    minor changes

--- AElf.Kernel.Tests/PipelineTest.cs
+++ AElf.Kernel.Tests/PipelineTest.cs
@@ -8,15 +8,12 @@ namespace AElf.Kernel.Tests
     public class PipelineTest
     {
         [Fact]
-        public void Test()
+        public void BasicPipeline()
         {
-            var tx = new Mock<ITransaction>();
-
             var blkheader = new Mock<IBlockHeader>();
             var blk = new Mock<IBlock>();
             blk.Setup(b => b.AddTransaction(It.IsAny<ITransaction>())).Returns(true);
             blk.Setup(b => b.GetHeader()).Returns(blkheader.Object);
-
 
             var hash = new Mock<IHash>();
             hash.Setup(p => p.GetBytes()).Returns(new byte[] { 1, 2, 3 });
@@ -32,12 +29,11 @@ namespace AElf.Kernel.Tests
 
             var chain = new Mock<IChain>();
 
-            // the pipeline
+            // basic pipeline
+            var tx = new Mock<ITransaction>();
             blk.Object.AddTransaction(tx.Object);
             miner.Object.Mine(blk.Object.GetHeader());
             chainmgr.Object.AddBlockAsync(chain.Object,blk.Object);
-
-
         }
     }
 }
