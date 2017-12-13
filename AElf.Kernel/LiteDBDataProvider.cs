826879d16faac86d08dd11410b7c7cb2bf5d5a82
xtaci <daniel820313@gmail.com>
2017-12-13 12:01:16 +0800

    xxx

---
+++ AElf.Kernel/LiteDBDataProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace AElf.Kernel
+{
+    public class LiteDBDataProvider:IAccountDataProvider
+    {
+        private struct Record
+        {
+            public IHash Key { get; set; }
+            public byte[] Value { get; set; }
+        }
+
+        private LiteDatabase db;
+
+
+        public LiteDBDataProvider(string path)
+        {
+            this.db = new LiteDatabase(@"path");
+        }
+
+        Task<ISerializable> IAccountDataProvider.GetAsync(IHash key)
+        {
+            var c = db.GetCollection<ISerializable>("accounts");
+            c.Query(x => x.Key.Contains(key));
+        }
+
+        IHash<IMerkleTree<ISerializable>> IAccountDataProvider.GetDataMerkleTreeRoot()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAccountDataProvider.SetAsync(IHash key, ISerializable obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
