33d725860948265f21d6a61cd662de967a98918b
xtaci <daniel820313@gmail.com>
2017-12-13 17:13:32 +0800

    upd

--- AElf.Kernel/LiteDBDataProvider.cs
+++ AElf.Kernel/LiteDBDataProvider.cs
@@ -6,10 +6,15 @@ namespace AElf.Kernel
 {
     public class LiteDBDataProvider:IAccountDataProvider
     {
-        private struct Record
+        class Record:ISerializable
         {
             public IHash Key { get; set; }
             public byte[] Value { get; set; }
+
+            public byte[] Serialize()
+            {
+                return Value;
+            }
         }
 
         private LiteDatabase db;
@@ -22,13 +27,16 @@ namespace AElf.Kernel
 
         Task<ISerializable> IAccountDataProvider.GetAsync(IHash key)
         {
-            var c = db.GetCollection<ISerializable>("accounts");
-            c.Query(x => x.Key.Contains(key));
+            var c = this.db.GetCollection<Record>("data");
+            var result = c.Find(x => x.Key.Equals(key));
+            return result;
         }
 
         IHash<IMerkleTree<ISerializable>> IAccountDataProvider.GetDataMerkleTreeRoot()
         {
-            throw new NotImplementedException();
+            var c = this.db.GetCollection<Record>("accounts");
+            var result = c.Find(x => x.Key.Equals("root"));
+            return null;        
         }
 
         Task IAccountDataProvider.SetAsync(IHash key, ISerializable obj)
