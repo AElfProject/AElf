751caa19924546c6c39004e75ef11a49eae7906d
xtaci <daniel820313@gmail.com>
2017-12-15 19:08:43 +0800

    upd

--- AElf.Kernel/TransactionExecutingManager.cs
+++ AElf.Kernel/TransactionExecutingManager.cs
@@ -11,6 +11,16 @@ namespace AElf.Kernel
         private Mutex mut = new Mutex();
         private Dictionary<IHash, List<ITransaction>> pending = new Dictionary<IHash, List<ITransaction>>();
 
+
+        /// <summary>
+        /// Node 
+        /// </summary>
+        private class Node
+        {
+            public ITransaction tx;
+            public Dictionary<IHash, ITransaction> neighbours; 
+        }
+
         public TransactionExecutingManager()
         {
         }
@@ -46,23 +56,31 @@ namespace AElf.Kernel
         /// <summary>
         /// Schedule execution of transaction
         /// </summary>
-        Task Scheduler() {
-            //  execution strategy(experimental)
+        void Scheduler() {
+            //  Execution strategy(experimental)
             //  1. tranform the dependency of Resource(R) into graph of related Transactions(T)
             //  2. find the T(ransaction) which owns the most neightbours
-            //  3. exeucte the T(ransaction)
-            //  4. check to see if this T(ransaction) leads to graph split
+            //  3. exeucte the T(ransaction), and removes this node from the graph
+            //  4. check to see if this removal leads to graph split
             //  5. if YES, we can parallel execute the transactions from the splitted graph
             //  6  if NO, goto step 2
 
-            Task task = new Task(() =>
-            {
-        
-            });
 
+            // step1:
+            var rootnodes = new Dictionary<IHash, ITransaction>();
+            this.mut.WaitOne();
+            foreach (var list in pending)
+            {
+                foreach(var tx in list.Value) {
+                    
+                }
+            }
+
+
+            this.mut.ReleaseMutex();
+                
             // reset pending 
             pending = new Dictionary<IHash, List<ITransaction>>();
-            return task;
         }
     }
 }
