bf358b224b3e96cccded3cc15caa3fae5eebdc6e
EanCuznaivy <1036682639@qq.com>
2018-01-04 23:46:25 +0800

    Reorg.

---
+++ AElf.Kernel/Consensus/DPoS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AElf.Kernel
+{
+    public class DPoS : IMiner
+    {
+        public byte[] Mine(IBlockHeader blockheader)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
