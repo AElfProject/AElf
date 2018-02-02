e7e318ce607abf797181dd0c345870c6da05d64a
samfrb <samos2x@outlook.com>
2018-02-02 14:58:31 +0800

    Added bootstrapper project

---
+++ AElf.Bootstrapper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AElf.Bootstrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing AElf :");
+            Console.WriteLine("Bootstrapping sequence started...");
+        }
+    }
+}
