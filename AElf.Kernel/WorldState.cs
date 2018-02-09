3fd2b10483745ed44c64d4ea8468b7830e42abb3
EanCusnaivy <EanCuznaivy@users.noreply.github.com>
2018-02-09 16:47:42 +0800

    Update WorldState.cs
    Make Wrold State to singleton model.

--- AElf.Kernel/WorldState.cs
+++ AElf.Kernel/WorldState.cs
@@ -13,11 +13,27 @@ namespace AElf.Kernel
         private Dictionary<IAccount, IAccountDataProvider> _accountDataProviders;
 
         private BinaryMerkleTree<IHash> _merkleTree;
+        private List<IDataProvider> _dataProviders;
 
-        public WorldState()
+        private static WorldState _instance = null;
+        private static readonly object SyncObject = new object();
+        
+        public static WorldState Instance
+        {
+            get
+            {
+                lock (SyncObject)
+                {
+                    return _instance ?? (_instance = new WorldState());
+                }
+            }
+        }
+
+        private WorldState()
         {
             _accountDataProviders = new Dictionary<IAccount, IAccountDataProvider>();
             _merkleTree = new BinaryMerkleTree<IHash>();
+            _dataProviders = new List<IDataProvider>();
         }
 
         /// <summary>
@@ -43,10 +59,25 @@ namespace AElf.Kernel
             var accountDataProvider = new AccountDataProvider(account);
             //Add the address to dict.
             _accountDataProviders[account] = accountDataProvider;
-            //Add the hash of account data provider to merkle tree as a node.
-            _merkleTree.AddNode(new Hash<IHash>(accountDataProvider.GetDataProvider().CalculateHash()));
-
+            
+            AddDataProvider(accountDataProvider.GetDataProvider());
+            
             return accountDataProvider;
+        }
+
+        public void AddDataProvider(IDataProvider dataProvider)
+        {
+            _dataProviders.Add(dataProvider);
+            //Add the hash of account data provider to merkle tree as a node.
+            _merkleTree.AddNode(new Hash<IHash>(dataProvider.CalculateHash()));
+        }
+
+        public void UpdateDataProvider(IDataProvider oldDataProvider, IDataProvider newDataProvider)
+        {
+            var order = _dataProviders.IndexOf(oldDataProvider);
+            _dataProviders[order] = newDataProvider;
+            _merkleTree.UpdateNode(new Hash<IHash>(oldDataProvider.CalculateHash()), 
+                new Hash<IHash>(newDataProvider.CalculateHash()));
         }
     }
 }
