d203f9dbfc86cb17b9a9a26b8a167eb5c0a85661
swk <shiwk@tju.edu.cn>
2018-02-28 14:45:40 +0800

    update Transaction scheduler test cases

--- AElf.Kernel.Tests/TransactionSchedulerTest.cs
+++ AElf.Kernel.Tests/TransactionSchedulerTest.cs
@@ -71,13 +71,15 @@ namespace AElf.Kernel.Tests
             var smartContractZero = new SmartContractZero();
             var accountZero = new AccountZero(smartContractZero);
             var accountManager = new AccountManager(worldState, accountZero);
-            var smartContractManager = new SmartContractManager(worldState, accountManager);
+            var smartContractManager = new SmartContractManager(accountManager);
             var transactionExecutingManager = new TransactionExecutingManager(smartContractManager);
             
             // simple demo cases
 
-
             var accounts = CreateAccountList(10);
+            // add all the accounts' account data providers to world state
+            accounts.ForEach(acc => worldState.AddAccountDataProvider(new AccountDataProvider(acc, worldState)));
+            
             // one tx
             // A
             var tx1 = CreateTransaction((byte) 'A', accounts[0], accounts[1]);
