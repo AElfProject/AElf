d883e7c7beabec1061db0dd8e59ba5c890008ec9
Loning <aloning@gmail.com>
2018-03-02 18:08:55 +0800

    world state manager

--- AElf.Kernel/WorldStateManager.cs
+++ AElf.Kernel/WorldStateManager.cs
@@ -1,9 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace AElf.Kernel
 {
     public class WorldStateManager: IWorldStateManager
     {
+        private readonly Func<IAccountDataProvider> _factory;
+
+        public WorldStateManager(Func<IAccountDataProvider> factory)
+        {
+            _factory = factory;
+        }
+
         public Task<IWorldState> GetWorldStateAsync(IChain chain)
         {
             throw new System.NotImplementedException();
@@ -11,6 +19,8 @@ namespace AElf.Kernel
 
         public IAccountDataProvider GetAccountDataProvider(IChain chain, IAccount account)
         {
+            var p = _factory();
+            
             throw new System.NotImplementedException();
         }
     }
