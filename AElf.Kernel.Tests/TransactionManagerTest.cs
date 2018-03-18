6f458e97158ea14ae8f9ef42a63153a5c14b46c2
Loning <aloning@gmail.com>
2018-03-18 17:04:20 +0800

    IoC integration

---
+++ AElf.Kernel.Tests/TransactionManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+
+namespace AElf.Kernel.Tests
+{
+    [UseAutofacTestFramework]
+    public class TransactionManagerTest
+    {
+        private ITransactionManager _manager;
+
+        public TransactionManagerTest(ITransactionManager manager)
+        {
+            _manager = manager;
+        }
+
+        [Fact]
+        public async Task TestInsert()
+        {
+            await _manager.AddTransactionAsync(new Transaction());
+        }
+    }
+}
--- AElf.Kernel/Storages/ITransactionStore.cs
+++ AElf.Kernel/Storages/ITransactionStore.cs
@@ -7,4 +7,17 @@ namespace AElf.Kernel.Storages
         Task InsertAsync(ITransaction tx);
         Task<ITransaction> GetAsync(IHash<ITransaction> hash);
     }
+    
+    public class TransactionStore: ITransactionStore
+    {
+        public Task InsertAsync(ITransaction tx)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ITransaction> GetAsync(IHash<ITransaction> hash)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
