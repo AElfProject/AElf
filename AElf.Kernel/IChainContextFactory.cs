d8e133fc4b1493ffffd8b440ab8d94f30c56fdaf
swk <shiwk@tju.edu.cn>
2018-03-26 18:17:20 +0800

    remove account manager

--- AElf.Kernel/IChainContextFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace AElf.Kernel
-{
-    public interface IChainContextFactory
-    {
-        Task<IChainContext> GetChainContext(IHash<IChain> chainId);
-    }
-}
