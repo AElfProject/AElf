c8322eda16b0f873bbe8d78ece2247d9a1e496f4
swk <shiwk@tju.edu.cn>
2018-03-29 14:36:29 +0800

    chain test cases

---
+++ AElf.Kernel.Tests/ChainTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using AElf.Kernel.KernelAccount;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+
+namespace AElf.Kernel.Tests
+{
+    [UseAutofacTestFramework]
+    public class ChainTest
+    {
+        private readonly IChainCreationService _chainCreationService;
+        private readonly ISmartContractZero _smartContractZero;
+
+        public ChainTest(ISmartContractZero smartContractZero, IChainCreationService chainCreationService)
+        {
+            _smartContractZero = smartContractZero;
+            _chainCreationService = chainCreationService;
+        }
+
+        [Fact]
+        public async Task CreateChain()
+        {
+            var chain = await _chainCreationService.CreateNewChainAsync(Hash.Generate(), _smartContractZero);
+            Assert.Equal(1, chain.CurrentBlockHeight);
+            //Console.WriteLine(_smartContractZero.GetHash().Value.ToByteArray().ToString());
+        }
+    }
+}
