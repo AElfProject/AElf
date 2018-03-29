f299c4dbcd4cf084328b966a2058627f76c0941f
swk <shiwk@tju.edu.cn>
2018-03-29 14:36:05 +0800

    autofac register

--- AElf.Kernel.Modules.AutofacModule/Module.cs
+++ AElf.Kernel.Modules.AutofacModule/Module.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reflection;
+using AElf.Kernel.KernelAccount;
+using AElf.Kernel.Storages;
 using Autofac;
 
 namespace AElf.Kernel.Modules.AutofacModule
@@ -7,12 +10,29 @@ namespace AElf.Kernel.Modules.AutofacModule
     {
         protected override void Load(ContainerBuilder builder)
         {
-            var assembly = typeof(AElf.Kernel.IAccount).Assembly;
+            var dataAccess = Assembly.GetExecutingAssembly();
             
-            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
+            //builder.RegisterAssemblyTypes(dataAccess).Where(t=> t.Name.EndsWith("Service")|| t.Name.EndsWith("Manager")).AsImplementedInterfaces();
 
             builder.RegisterGeneric(typeof(Serializer<>)).As(typeof(ISerializer<>));
-            
+            builder.RegisterType(typeof(SmartContractZero)).As(typeof(ISmartContractZero)).SingleInstance();
+            builder.RegisterType(typeof(SmartContractRunnerFactory)).As(typeof(ISmartContractRunnerFactory)).SingleInstance();
+            builder.RegisterType(typeof(ChainManager)).As(typeof(IChainManager)).SingleInstance();
+            builder.RegisterType(typeof(TransactionManager)).As(typeof(ITransactionManager)).SingleInstance();
+            builder.RegisterType(typeof(WorldStateManager)).As(typeof(IWorldStateManager)).SingleInstance();
+
+            builder.RegisterType(typeof(ChainStore)).As(typeof(IChainStore)).SingleInstance();            
+            builder.RegisterType(typeof(ChainBlockRelationStore)).As(typeof(IChainBlockRelationStore)).SingleInstance();
+            builder.RegisterType(typeof(KeyValueDatabase)).As(typeof(IKeyValueDatabase)).SingleInstance();
+            builder.RegisterType(typeof(TransactionStore)).As(typeof(ITransactionStore)).SingleInstance();            
+
+            builder.RegisterType(typeof(ChainCreationService)).As(typeof(IChainCreationService)).SingleInstance();
+            builder.RegisterType(typeof(AccountContextService)).As(typeof(IAccountContextService)).SingleInstance();
+            builder.RegisterType(typeof(AccountDataProvider)).As(typeof(IAccountDataProvider)).PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies).SingleInstance();
+            builder.RegisterType(typeof(AccountDataContext)).As(typeof(IAccountDataContext))
+                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies).InstancePerLifetimeScope();
+            builder.RegisterType(typeof(Hash)).As(typeof(IHash));
+
             base.Load(builder);
         }
     }
