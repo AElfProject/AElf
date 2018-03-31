640c38d5dadb93067654e02bb5ceb9f69df8e5f0
swk <shiwk@tju.edu.cn>
2018-03-31 10:46:30 +0800

    add smartcontract manager

--- AElf.Kernel/SmartContractService.cs
+++ AElf.Kernel/SmartContractService.cs
@@ -29,4 +29,17 @@ namespace AElf.Kernel
         Task<SmartContractRegistration> GetAsync(Hash account);
         Task<SmartContractRegistration> InsertAsync(SmartContractRegistration reg);
     }
+
+    public class SmartContractManager : ISmartContractManager
+    {
+        public Task<SmartContractRegistration> GetAsync(Hash account)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<SmartContractRegistration> InsertAsync(SmartContractRegistration reg)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
