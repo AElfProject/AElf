bd215cab335d58ddbba5f96a03f47e69bde7fc7a
swk <shiwk@tju.edu.cn>
2018-03-31 15:36:28 +0800

    add SmartContract implemention

--- AElf.Kernel/SmartContract.cs
+++ AElf.Kernel/SmartContract.cs
@@ -10,6 +10,7 @@ namespace AElf.Kernel
     {
         private IAccountDataProvider _accountDataProvider;
         private ISerializer<SmartContractRegistration> _serializer;
+        protected byte[] Data { get; set; }
 
         protected SmartContract(ISerializer<SmartContractRegistration> serializer)
         {
@@ -22,27 +23,30 @@ namespace AElf.Kernel
             await Task.CompletedTask;
         }
 
-        public async Task InvokeAsync(IHash caller, string methodname, params object[] objs)
+        protected void Resolve(SmartContractRegistration smartContractRegistration)
         {
-            throw new NotImplementedException();
-            /*
-            // get smartContractRegistration by accountDataProvider 
-            var smartContractRegistrationBytes = await _accountDataProvider.GetDataProvider()
-                .GetDataProvider("SmartContract")
-                .GetAsync(new Hash(_accountDataProvider.CalculateHashWith("SmartContract")));
-            var smartContractRegistration = _serializer.Deserialize(smartContractRegistrationBytes);
-            // load assembly with bytes
-            var assembly = Assembly.Load(smartContractRegistration.Bytes);
-            var type = assembly.GetTypes().ElementAt(0);
-            var method = type.GetMethod(methodname);
-            
-            if (type.GetConstructors().Length == 0)
-            {
-                // if contract is static, first param will be ignore
-                await (Task) method.Invoke(null, objs);
-            }*/
+            Data = _serializer.Serialize(smartContractRegistration);
         }
 
+        public abstract Task InvokeAsync(IHash caller, string methodname, params object[] objs);
         public abstract IHash GetHash();
+    }
+
+    public class CSharpSmartContract : SmartContract
+    {
+        public CSharpSmartContract(ISerializer<SmartContractRegistration> serializer, SmartContractRegistration smartContractRegistration) : base(serializer)
+        {
+            Resolve(smartContractRegistration);
+        }
+        
+        public override Task InvokeAsync(IHash caller, string methodname, params object[] objs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IHash GetHash()        
+        {
+            throw new NotImplementedException();
+        }
     }
 }
