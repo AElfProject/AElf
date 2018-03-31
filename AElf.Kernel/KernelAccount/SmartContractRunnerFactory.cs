84f1402d2406c62eb503720f90695f689c51a785
swk <shiwk@tju.edu.cn>
2018-03-31 15:38:10 +0800

    implement SmartContractFactory

--- AElf.Kernel/KernelAccount/SmartContractRunnerFactory.cs
+++ AElf.Kernel/KernelAccount/SmartContractRunnerFactory.cs
@@ -1,10 +1,32 @@
+using System;
+using System.Threading.Tasks;
+
 namespace AElf.Kernel.KernelAccount
 {
     public class SmartContractRunnerFactory : ISmartContractRunnerFactory
     {
+        
+        private CSharpSmartContractRunner CSharpSmartContractRunner { get; set; }
         public ISmartContractRunner GetRunner(int category)
         {
-            throw new System.NotImplementedException();
+            if(category == 1)
+                return CSharpSmartContractRunner;
+            throw new NotImplementedException();
+        }
+    }
+    
+    public class CSharpSmartContractRunner : ISmartContractRunner
+    {
+        private ISerializer<SmartContractRegistration> _serializer;
+
+        public CSharpSmartContractRunner(ISerializer<SmartContractRegistration> serializer)
+        {
+            _serializer = serializer;
+        }
+
+        public async Task<ISmartContract> RunAsync(SmartContractRegistration reg)
+        {
+            return await Task.FromResult(new CSharpSmartContract(_serializer, reg));
         }
     }
 }
