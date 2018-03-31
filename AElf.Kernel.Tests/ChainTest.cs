c74a10d4456745c346705aa18f0f9257a4031970
swk <shiwk@tju.edu.cn>
2018-03-31 10:48:44 +0800

    chain test cases

--- AElf.Kernel.Tests/ChainTest.cs
+++ AElf.Kernel.Tests/ChainTest.cs
@@ -11,19 +11,60 @@ namespace AElf.Kernel.Tests
     {
         private readonly IChainCreationService _chainCreationService;
         private readonly ISmartContractZero _smartContractZero;
+        private readonly IChainManager _chainManager;
+        private readonly IChainContextService _chainContextService;
 
-        public ChainTest(ISmartContractZero smartContractZero, IChainCreationService chainCreationService)
+        public ChainTest(ISmartContractZero smartContractZero, IChainCreationService chainCreationService,
+            IChainManager chainManager, IChainContextService chainContextService)
         {
             _smartContractZero = smartContractZero;
             _chainCreationService = chainCreationService;
+            _chainManager = chainManager;
+            _chainContextService = chainContextService;
         }
 
         [Fact]
-        public async Task CreateChain()
+        public async Task<IChain> CreateChain()
         {
-            var chain = await _chainCreationService.CreateNewChainAsync(Hash.Generate(), _smartContractZero);
+            var chainId = Hash.Generate();
+            var chain = await _chainCreationService.CreateNewChainAsync(chainId, _smartContractZero);
+            var adp = new AccountDataProvider
+            {
+                Context =
+                {
+                    Address = new Hash(new byte[] {1, 2, 3}),
+                    ChainId = chainId
+                }
+            };
+            await _smartContractZero.InititalizeAsync(adp);
             Assert.Equal(1, chain.CurrentBlockHeight);
-            //Console.WriteLine(_smartContractZero.GetHash().Value.ToByteArray().ToString());
+            return chain;
+        }
+
+        public async Task ChainStore(Hash chainId)
+        {
+            await _chainManager.AddChainAsync(chainId);
+            Assert.NotNull(_chainManager.GetChainAsync(chainId).Result);
+        }
+
+        public async Task ChainContext()
+        {
+            var chain = await CreateChain();
+            await _chainManager.AddChainAsync(chain.Id);
+            chain = await _chainManager.GetChainAsync(chain.Id);
+            var context = _chainContextService.GetChainContext(chain.Id);
+            Assert.NotNull(context);
+            Assert.Equal(context.SmartContractZero, _smartContractZero);
+        }
+
+        public async Task AppendBlock()
+        {
+            var chain = await CreateChain();
+            Assert.Equal(chain.CurrentBlockHeight, 1);
+            var block = new Block(chain.CurrentBlockHash);
+            await _chainManager.AppendBlockToChainAsync(chain, block);
+            Assert.Equal(chain.CurrentBlockHeight, 2);
+            Assert.Equal(chain.CurrentBlockHash, block.GetHash());
         }
     }
 }
