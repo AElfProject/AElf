bc770cb22cee599a31587202fd750f0324304142
samfrb <samos2x@outlook.com>
2018-04-12 15:14:10 +0800

    Updated tests

--- AElf.Kernel.Tests/Crypto/ECDSA/EcdsaTest.cs
+++ AElf.Kernel.Tests/Crypto/ECDSA/EcdsaTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using AElf.Kernel.Crypto.ECDSA;
+using Org.BouncyCastle.Math;
+using Xunit;
 
 namespace AElf.Kernel.Tests.Crypto.ECDSA
 {
@@ -7,13 +9,34 @@ namespace AElf.Kernel.Tests.Crypto.ECDSA
         [Fact]
         public void VerifySignature_WrongData_ReturnsFalse()
         {
+            ECKeyPair keyPair = new KeyPairGenerator().Generate();
             
+            byte[] message = new BigInteger("968236873715988614170569073515315707566766479517").ToByteArray();
+            
+            ECSigner signer = new ECSigner();
+            ECSignature signature = signer.Sign(keyPair, message);
+            
+            // Change the message 
+            message = new BigInteger("9682368737159881417056907351531570756676647951").ToByteArray();
+            
+            ECVerifier verifier = new ECVerifier(keyPair);
+            
+            Assert.False(verifier.Verify(signature, message));
         }
 
         [Fact]
         public void VerifySignature_CorrectDataAndSig_ReturnsTrue()
         {
+            ECKeyPair keyPair = new KeyPairGenerator().Generate();
             
+            byte[] message = new BigInteger("968236873715988614170569073515315707566766479517").ToByteArray();
+            
+            ECSigner signer = new ECSigner();
+            ECSignature signature = signer.Sign(keyPair, message);
+            
+            ECVerifier verifier = new ECVerifier(keyPair);
+            
+            Assert.True(verifier.Verify(signature, message));
         }
     }
 }
--- AElf.Kernel/Crypto/ECDSA/ECVerifier.cs
+++ AElf.Kernel/Crypto/ECDSA/ECVerifier.cs
@@ -1,7 +1,25 @@
-﻿namespace AElf.Kernel.Crypto.ECDSA
+﻿using Org.BouncyCastle.Crypto.Signers;
+
+namespace AElf.Kernel.Crypto.ECDSA
 {
     public class ECVerifier
     {
+        private ECKeyPair _keyPair { get; set; }
         
+        public ECVerifier(ECKeyPair keyPair)
+        {
+            _keyPair = keyPair;
+        }
+        
+        public bool Verify(ECSignature signature, byte[] data)
+        {
+            if (signature == null || _keyPair == null || data == null)
+                return false;
+            
+            ECDsaSigner verifier = new ECDsaSigner();
+            verifier.Init(false, _keyPair.PublicKey);
+
+            return verifier.VerifySignature(data, signature.Signature[0], signature.Signature[1]);
+        }
     }
 }
