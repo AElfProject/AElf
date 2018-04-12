a16756200caaa1de9959b32a4cf2a8935f3d8694
samfrb <samos2x@outlook.com>
2018-04-12 14:14:32 +0800

    Added test for signing

---
+++ AElf.Kernel.Tests/Crypto/ECDSA/EcdsaTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace AElf.Kernel.Tests.Crypto.ECDSA
+{
+    public class EcdsaTest
+    {
+        [Fact]
+        public void VerifySignature_WrongData_ReturnsFalse()
+        {
+            
+        }
+
+        [Fact]
+        public void VerifySignature_CorrectDataAndSig_ReturnsTrue()
+        {
+            
+        }
+    }
+}
