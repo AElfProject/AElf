a0a4589a8c6d0dfc86983c8e1ddfa4f34c3e4e2d
swk <shiwk@tju.edu.cn>
2018-04-14 11:15:17 +0800

    fix bug

---
+++ AElf.Kernel/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AElf.Kernel
+{
+    /// <summary>
+    /// Account.
+    /// </summary>
+    public class Account : IAccount
+    {
+        public Account(Hash address)
+        {
+            Address = address;
+        }  
+
+        public Account():this(Hash.Zero)
+        {
+            
+        }
+
+        public Hash Address { get; set; }
+
+        public virtual byte[] Serialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hash GetAddress()
+        {
+            return Address;
+        }
+    }
+}
