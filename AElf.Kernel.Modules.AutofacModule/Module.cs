35903f6a460ae77335f871bc803c4848ab798dda
swk <shiwk@tju.edu.cn>
2018-04-14 11:15:06 +0800

    fix some bugs

--- AElf.Kernel.Modules.AutofacModule/Module.cs
+++ AElf.Kernel.Modules.AutofacModule/Module.cs
@@ -10,39 +10,12 @@ namespace AElf.Kernel.Modules.AutofacModule
     {
         protected override void Load(ContainerBuilder builder)
         {
-            var dataAccess = Assembly.GetExecutingAssembly();
+            var assembly = typeof(AElf.Kernel.IAccount).Assembly;
             
-            //builder.RegisterAssemblyTypes(dataAccess).Where(t=> t.Name.EndsWith("Service")|| t.Name.EndsWith("Manager")).AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
 
             builder.RegisterGeneric(typeof(Serializer<>)).As(typeof(ISerializer<>));
-            builder.RegisterType(typeof(SmartContractZero)).As(typeof(ISmartContractZero));
-            builder.RegisterType(typeof(SmartContractRunnerFactory)).As(typeof(ISmartContractRunnerFactory)).SingleInstance();
-            builder.RegisterType(typeof(ChainManager)).As(typeof(IChainManager)).SingleInstance();
-            builder.RegisterType(typeof(TransactionManager)).As(typeof(ITransactionManager)).SingleInstance();
-            builder.RegisterType(typeof(WorldStateManager)).As(typeof(IWorldStateManager)).SingleInstance();
-            builder.RegisterType(typeof(SmartContractManager)).As(typeof(ISmartContractManager)).SingleInstance();
-            builder.RegisterType(typeof(BlockManager)).As(typeof(IBlockManager)).SingleInstance();
-
-            builder.RegisterType(typeof(ChainStore)).As(typeof(IChainStore)).SingleInstance();            
-            builder.RegisterType(typeof(ChainBlockRelationStore)).As(typeof(IChainBlockRelationStore)).SingleInstance();
-            builder.RegisterType(typeof(TransactionStore)).As(typeof(ITransactionStore)).SingleInstance();            
-            builder.RegisterType(typeof(BlockBodyStore)).As(typeof(IBlockBodyStore)).SingleInstance();
-            builder.RegisterType(typeof(BlockHeaderStore)).As(typeof(IBlockHeaderStore)).SingleInstance();
-
-            builder.RegisterType(typeof(KeyValueDatabase)).As(typeof(IKeyValueDatabase)).SingleInstance();
-
-            builder.RegisterType(typeof(ChainCreationService)).As(typeof(IChainCreationService)).SingleInstance();
-            builder.RegisterType(typeof(AccountContextService)).As(typeof(IAccountContextService)).SingleInstance();
-            builder.RegisterType(typeof(ChainContextService)).As(typeof(IChainContextService)).SingleInstance();
             
-            builder.RegisterType(typeof(AccountDataProvider)).As(typeof(IAccountDataProvider))
-                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
-            builder.RegisterType(typeof(AccountDataContext)).As(typeof(IAccountDataContext))
-                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
-            
-            builder.RegisterType(typeof(Hash)).As(typeof(IHash));
-            builder.RegisterType(typeof(CSharpSmartContractRunner)).SingleInstance();
-
             base.Load(builder);
         }
     }
