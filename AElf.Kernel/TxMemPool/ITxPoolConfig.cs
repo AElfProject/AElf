46a5911228e2fc5e0d920fa8678bc60c43bef9c9
swk <shiwk@tju.edu.cn>
2018-04-14 11:32:10 +0800

    few modifications

--- AElf.Kernel/TxMemPool/ITxPoolConfig.cs
+++ AElf.Kernel/TxMemPool/ITxPoolConfig.cs
@@ -2,8 +2,19 @@
 {
     public interface ITxPoolConfig
     {
+        /// <summary>
+        /// pool size limit
+        /// </summary>
         ulong PoolLimitSize { get; }
+        
+        /// <summary>
+        /// tx size limit
+        /// </summary>
         ulong TxLimitSize { get; }
+        
+        /// <summary>
+        /// minimal tx fee 
+        /// </summary>
         ulong FeeThreshold { get; }
     }
 }
