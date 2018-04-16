ae542274d5bfdfb712b9ce5047a7919c2ff9cf28
swk <shiwk@tju.edu.cn>
2018-04-16 17:52:33 +0800

    change manager to service

---
+++ AElf.Kernel/TxMemPool/ITxPoolService.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AElf.Kernel.TxMemPool
+{
+    public interface ITxPoolService
+    {
+        
+        ///<summary>
+        /// add tx
+        /// </summary>
+        /// <param name="tx"></param>
+        /// <returns></returns>
+        Task<bool> AddTransaction(ITransaction tx);
+        
+        /// <summary>
+        /// add multi txs
+        /// </summary>
+        /// <param name="txs"></param>
+        /// <returns></returns>
+        Task AddTransactions(List<ITransaction> txs);
+        
+        /// <summary>
+        /// remove a tx
+        /// </summary>
+        /// <param name="txHash"></param>
+        Task Remove(Hash txHash);
+
+        /// <summary>
+        /// remove tx with worst price
+        /// </summary>
+        Task RemoveTxWithWorstFee();
+
+        /// <summary>
+        /// Remove transactions from mempool already in block
+        /// </summary>
+        /// <param name="blockHeight"></param>
+        /// <returns></returns>
+        Task RemoveTxsExecuted(ulong blockHeight);
+
+        /// <summary>
+        /// Remove invalid transactions from mempool
+        /// not in executable and waiting list 
+        /// </summary>
+        /// <returns></returns>
+        Task RemoveTxsInValid();
+
+        /// <summary>
+        /// return ready txs can be executed 
+        /// </summary>
+        /// <returns></returns>
+        Task<List<ITransaction>> GetReadyTxs();
+        
+        /// <summary>
+        /// return pool size
+        /// </summary>
+        /// <returns></returns>
+        Task<ulong> GetPoolSize();
+
+        /// <summary>
+        /// return a tx alread in pool
+        /// </summary>
+        /// <param name="txHash"></param>
+        /// <param name="tx"></param>
+        /// <returns></returns>
+        Task<bool> GetTransaction(Hash txHash, out ITransaction tx);
+
+        /// <summary>
+        /// clear tx pool
+        /// </summary>
+        /// <returns></returns>
+        Task Clear();
+        
+        /// <summary>
+        /// add txs to storage
+        /// </summary>
+        /// <param name="txHashes"></param>
+        /// <returns></returns>
+        Task PersistTxs(IEnumerable<Hash> txHashes)
+
+        /// <summary>
+        /// persistent Tx pool to storage
+        /// </summary>
+        /// <returns></returns>
+        Task SavePool();
+    }
+}
---
+++ AElf.Kernel/TxMemPool/TxPoolService.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AElf.Kernel.Lock;
+using AElf.Kernel.Storages;
+
+namespace AElf.Kernel.TxMemPool
+{
+    public class TxPoolService : ITxPoolService
+    {
+        private readonly ITxPool _txPool;
+        private ITransactionManager _transactionManager;
+
+        public TxPoolService(ITxPool txPool, TxPoolSchedulerLock @lock, ITransactionManager transactionManager)
+        {
+            _txPool = txPool;
+            Lock = @lock;
+            _transactionManager = transactionManager;
+        }
+        
+        public TxPoolSchedulerLock Lock { get; }
+
+        public Task<bool> AddTransaction(ITransaction tx)
+        {
+            return Lock.WriteAsync(() => _txPool.AddTx(tx));
+        }
+        
+        public Task AddTransactions(List<ITransaction> txs)
+        {
+            return Lock.WriteAsync(() =>
+            {
+                foreach (var tx in txs)
+                {
+                    _txPool.AddTx(tx);
+                }
+                return Task.CompletedTask;
+            });
+        }
+
+        public Task Remove(Hash txHash)
+        {
+            Lock.WriteAsync(() => _txPool.DisgardTx(txHash));
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveTxWithWorstFee()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemoveTxsExecuted(ulong blockHeight)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task PersistTxs(IEnumerable<Hash> txHashes)
+        {
+            foreach (var h in txHashes)
+            {
+                await Persist(h);
+            }
+        }
+
+        private async Task Persist(Hash txHash)
+        {
+            if (!GetTransaction(txHash, out var tx).Result)
+            {
+                // TODO: log error
+            }
+            else
+            {
+                await _transactionManager.AddTransactionAsync((Transaction)tx);
+            }
+        }
+        
+        public Task RemoveTxsInValid()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<ITransaction>> GetReadyTxs()
+        {
+            return Lock.ReadAsync(() => _txPool.Ready);
+        }
+
+        public Task<ulong> GetPoolSize()
+        {
+            return Lock.ReadAsync(() => _txPool.Size);
+        }
+
+        public Task<bool> GetTransaction(Hash txHash, out ITransaction tx)
+        {
+            tx = Lock.ReadAsync(() => _txPool.GetTransaction(txHash)).Result;
+            return Task.FromResult(tx != null);
+        }
+
+        public Task Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SavePool()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    
+    /// <summary>
+    /// A lock for managing asynchronous access to memory pool.
+    /// </summary>
+    public class TxPoolSchedulerLock : ReaderWriterLock
+    {
+    }
+}
