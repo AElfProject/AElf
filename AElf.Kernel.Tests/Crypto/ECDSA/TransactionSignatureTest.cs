65cfd0017351306163bc56c75ff5545221fcb258
samfrb <samos2x@outlook.com>
2018-04-16 11:56:33 +0800

    Transaction signature and crypto helpers

---
+++ AElf.Kernel.Tests/Crypto/ECDSA/TransactionSignatureTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AElf.Kernel.Crypto;
+using AElf.Kernel.Crypto.ECDSA;
+using AElf.Kernel.Extensions;
+using Xunit;
+
+namespace AElf.Kernel.Tests.Crypto.ECDSA
+{
+    public class TransactionSignatureTest
+    {
+        // The length of an AElf address
+        // todo : modify this constant when we reach an agreement on the length
+        private const int ADR_LENGTH = 42;
+            
+        [Fact]
+        public void SignAndVerifyTransaction()
+        {
+            // Generate the key pair 
+            ECKeyPair keyPair = new KeyPairGenerator().Generate();
+            
+            Transaction tx = new Transaction();
+            tx.From = new Hash(ByteArrayHelpers.RandomFill(ADR_LENGTH));
+            tx.To = new Hash(ByteArrayHelpers.RandomFill(ADR_LENGTH));
+            
+            // Serialize and hash the transaction
+            Hash hash = tx.GetHash();
+            
+            // Sign the hash
+            ECSigner signer = new ECSigner();
+            ECSignature signature = signer.Sign(keyPair, hash.GetHashBytes());
+            
+            ECVerifier verifier = new ECVerifier(keyPair);
+            
+            Assert.True(verifier.Verify(signature, hash.GetHashBytes()));
+        }
+    }
+}
