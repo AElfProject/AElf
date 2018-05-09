9996956d255ff4ac56fe5fda0b249b2122f7e69c
swk <shiwk@tju.edu.cn>
2018-05-09 21:22:28 +0800

    tx vallidation

--- AElf.Kernel/TxMemPool/TxValidation.cs
+++ AElf.Kernel/TxMemPool/TxValidation.cs
@@ -1,29 +1,47 @@
 ﻿using System;
+using AElf.Kernel.Crypto.ECDSA;
 
 namespace AElf.Kernel.TxMemPool
 {
     public static class TxValidation
     {
         
-        public static bool ValidateTx(this ITxPoolConfig config, Transaction tx)
+        /// <summary>
+        /// validate a tx size, signature, account format
+        /// </summary>
+        /// <param name="pool"></param>
+        /// <param name="tx"></param>
+        /// <returns></returns>
+        public static bool ValidateTx(this ITxPool pool, Transaction tx)
         {
-            // size validation
-            if (GetTxSize(tx) > config.TxLimitSize)
+            if (tx.From == null || tx.MethodName == "" || tx.Params.Length == 0 || tx.IncrementId < 0)
             {
-                // TODO: log errors 
+                // TODO: log errors
+                return false;
+            }
+            
+            // size validation
+            if (GetTxSize(tx) > pool.TxLimitSize)
+            {
+                // TODO: log errors, wrong size
                 return false;
             }
             
             // TODO: signature validation
+            if (!tx.VerifySignature())
+            {
+                // TODO: log errors, invalid tx signature
+                return false;
+            }
             
-            // account address validation
-            if (!tx.CheckAddress(out var addr) || !addr.Equals(tx.From))
+            if(!tx.CheckAccountAddress())
             {
                 // TODO: log errors, address error 
                 return false;
             }
+            
             // fee validation
-            if (tx.Fee < config.FeeThreshold)
+            if (tx.Fee < pool.MinimalFee)
             {
                 // TODO: log errors, not enough Fee error 
                 return false;
@@ -33,17 +51,37 @@ namespace AElf.Kernel.TxMemPool
             return true;
         }
 
+
         /// <summary>
-        /// check validity of address
+        /// verify signature in tx
         /// </summary>
         /// <param name="tx"></param>
-        /// <param name="accAddress"></param>
-        /// <returns></returns>    
-        /// <exception cref="NotImplementedException"></exception>
-        private static bool CheckAddress(this Transaction tx, out Hash accAddress)
+        /// <returns></returns>
+        private static bool VerifySignature(this Transaction tx)
         {
-            throw new NotImplementedException();
+            if (tx.P == null)
+            {
+                return false;
+            }
+            byte[] uncompressedPrivKey = tx.P.ToByteArray();
+            ECKeyPair recipientKeyPair = ECKeyPair.FromPublicKey(uncompressedPrivKey);
+            ECVerifier verifier = new ECVerifier(recipientKeyPair);
+            return verifier.Verify(tx.GetSignature(), tx.GetHash().GetHashBytes());
         }
+
+        
+        /// <summary>
+        /// verify address
+        /// </summary>
+        /// <param name="tx"></param>
+        /// <returns></returns>
+        private static bool CheckAccountAddress(this Transaction tx)
+        {
+            // TODO: more verifications
+            return tx.From.CalculateSize() != 32 && (tx.To == null || tx.To.CalculateSize() != 32);
+        }
+        
+       
         
         /// <summary>
         /// return size of given tx
