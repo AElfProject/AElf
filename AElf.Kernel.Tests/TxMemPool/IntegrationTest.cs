c2e2aa514a37d8123aea15b0b923097aaa0bc49e
swk <shiwk@tju.edu.cn>
2018-05-09 10:27:31 +0800

    modify test cases

---
+++ AElf.Kernel.Tests/TxMemPool/IntegrationTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AElf.Kernel.KernelAccount;
+using AElf.Kernel.TxMemPool;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+
+namespace AElf.Kernel.Tests.TxMemPool
+{
+    [UseAutofacTestFramework]
+    public class IntegrationTest
+    {
+        private readonly IAccountContextService _accountContextService;
+        private readonly ISmartContractZero _smartContractZero;
+
+        public IntegrationTest(IAccountContextService accountContextService, ISmartContractZero smartContractZero)
+        {
+            _accountContextService = accountContextService;
+            _smartContractZero = smartContractZero;
+        }
+        
+        private TxPool GetPool()
+        {
+            return new TxPool(new ChainContext(_smartContractZero, Hash.Generate()), TxPoolConfig.Default,
+                _accountContextService);
+        }
+
+        [Fact]
+        public async Task Start()
+        {
+            var pool = GetPool();
+            
+            var poolService = new TxPoolService(pool);
+            poolService.Start();
+            ulong queued = 0;
+            ulong exec = 0;
+            var tasks = new List<Task>();
+            int k = 0;
+            var threadNum = 10;
+            for (var j = 0; j < 10; j++)
+            {
+                var task = Task.Run(async () =>
+                {
+                    var sortedSet = new SortedSet<ulong>();
+                    var addr = Hash.Generate();
+                    var i = 0;
+                    while (i++ < 500)
+                    {
+                        var id = (ulong) new Random().Next(100);
+                        sortedSet.Add(id);
+                        var tx = new Transaction
+                        {
+                            From = addr,
+                            To = Hash.Generate(),
+                            IncrementId = id
+                        };
+
+                        await poolService.AddTxAsync(tx);
+                    }
+
+                    ulong c = 0;
+                    foreach (var t in sortedSet)
+                    {
+                        if (t != c)
+                            break;
+                        c++;
+                    }
+                    
+                    lock (this)
+                    {
+                        queued += (ulong) sortedSet.Count;
+                        exec += c;
+                        k++;
+                    }
+                });
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            pool.QueueTxs();
+            await poolService.PromoteAsync();
+            Assert.Equal(k, threadNum);
+            Assert.Equal(exec, poolService.GetExecutableSizeAsync().Result);
+            Assert.Equal(queued - exec, poolService.GetWaitingSizeAsync().Result);
+            
+        }
+    }
+}
---
+++ AElf.Kernel.Tests/TxMemPool/TxPoolTest.cs
@@ -0,0 +1,146 @@
+﻿using AElf.Kernel.KernelAccount;
+using AElf.Kernel.TxMemPool;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+
+namespace AElf.Kernel.Tests.TxMemPool
+{
+    [UseAutofacTestFramework]
+    public class TxPoolTest
+    {
+        private readonly IAccountContextService _accountContextService;
+        private readonly ISmartContractZero _smartContractZero;
+        
+        public TxPoolTest(IAccountContextService accountContextService, ISmartContractZero smartContractZero)
+        {
+            _accountContextService = accountContextService;
+            _smartContractZero = smartContractZero;
+        }
+
+        private TxPool GetPool()
+        {
+            return new TxPool(new ChainContext(_smartContractZero, Hash.Generate()), TxPoolConfig.Default,
+                _accountContextService);
+        }
+
+        private Transaction BuildTransaction(Hash adrFrom = null, Hash adrTo = null, ulong nonce = 0)
+        {
+            
+            var tx = new Transaction();
+            tx.From = adrFrom == null ? Hash.Generate() : adrFrom;
+            tx.To = adrTo == null ? Hash.Generate() : adrTo;
+            tx.IncrementId = nonce;
+
+            return tx;
+        }
+        
+        [Fact]
+        public void EntryThreshold_Test()
+        {
+            // setup config
+            TxPoolConfig conf = TxPoolConfig.Default;
+            conf.EntryThreshold = 1;
+
+            var pool = new TxPool(new ChainContext(_smartContractZero, Hash.Generate()), conf, _accountContextService);
+            
+            // Add a valid transaction
+            var tx = BuildTransaction();
+            pool.AddTx(tx);
+            
+            pool.GetPoolStates(out var executable, out var waiting, out var tmp);
+            
+            Assert.Equal(1, (int)tmp);
+            Assert.Equal(0, (int)waiting);
+            Assert.Equal(0, (int)executable);
+        }
+
+        // Adding a valide transaction to the pool => AddTx returns true
+        [Fact]
+        public void AddTx_ValidTransaction_ReturnsTrue()
+        {
+            TxPool pool = GetPool();
+            var tx = BuildTransaction();
+            
+            bool addResult = pool.AddTx(tx);
+            
+            Assert.True(addResult);
+        }
+
+        [Fact]
+        public void ContainsTx_ReturnsTrue_AfterAdd()
+        {
+            TxPool pool = GetPool();
+            var tx = BuildTransaction();
+            
+            pool.AddTx(tx);
+
+            bool res = pool.Contains(tx.GetHash());
+            
+            Assert.True(res);
+        }
+
+        [Fact]
+        public void QueueTxTest()
+        {
+            TxPool pool = GetPool();
+            var tx = BuildTransaction();
+            
+            pool.AddTx(tx);
+            pool.QueueTxs();
+            
+            pool.GetPoolStates(out var executable, out var waiting, out var tmp);
+            
+            Assert.Equal(0, (int)tmp);
+            Assert.Equal(1, (int)waiting);
+            Assert.Equal(0, (int)executable);
+        }
+
+        [Fact]
+        public void PromoteTest()
+        {
+            TxPool pool = GetPool();
+            var tx = BuildTransaction();
+            
+            pool.AddTx(tx);
+            pool.QueueTxs();
+            
+            pool.Promote();
+            
+            pool.GetPoolStates(out var executable, out var waiting, out var tmp);
+            Assert.Equal(0, (int)tmp);
+            Assert.Equal(0, (int)waiting);
+            Assert.Equal(1, (int)executable);
+        }
+
+        [Fact]
+        public void ReadyTxsTest()
+        {
+            TxPool pool = GetPool();
+            var tx = BuildTransaction();
+            
+            pool.AddTx(tx);
+            pool.QueueTxs();
+            
+            pool.Promote();
+            
+            var ready = pool.ReadyTxs();
+            
+            Assert.Equal(1, ready.Count);
+            Assert.True(ready.Contains(tx));
+        }
+
+
+        [Fact]
+        public void GetTxTest()
+        {
+            TxPool pool = GetPool();
+            var tx = BuildTransaction();
+            
+            pool.AddTx(tx);
+            
+            var t = pool.GetTx(tx.GetHash());
+            
+            Assert.Equal(tx, t);
+        }
+    }
+}
