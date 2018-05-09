9715f90542d6a5948361d8170b047725ce9a8621
samfrb <samos2x@outlook.com>
2018-05-09 15:12:39 +0800

    Startup project, node class

---
+++ AElf.Launcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AElf.Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
