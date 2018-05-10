1e34efa69eec8cd0b4b81fcb4a91c3347d8dc724
swk <shiwk@tju.edu.cn>
2018-05-10 20:51:55 +0800

    add transaction result manager

---
+++ AElf.Kernel/Managers/TransactionResultManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace AElf.Kernel.Managers
+{
+    public class TransactionResultManager : ITransactionResultManager
+    {
+        //TODO: access to result storage
+        public Task AddTransactionResultAsync(TransactionResult tr)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TransactionResult> GetTransactionResultAsync(Hash txId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
