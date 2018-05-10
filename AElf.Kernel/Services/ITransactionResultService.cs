cbf42e58131cb6e4c473e27cfac699dd0ef8e567
swk <shiwk@tju.edu.cn>
2018-05-10 20:53:43 +0800

    add transaction result service

---
+++ AElf.Kernel/Services/ITransactionResultService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace AElf.Kernel.Services
+{
+    public interface ITransactionResultService
+    {
+        Task<TransactionResult> GetResultAsync(Hash txId);
+        Task AddResultAsync(TransactionResult res);
+    }
+}
---
+++ AElf.Kernel/Services/TransactionResultService.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using AElf.Kernel.Managers;
+using AElf.Kernel.TxMemPool;
+
+namespace AElf.Kernel.Services
+{
+    public class TransactionResultService : ITransactionResultService
+    {
+        private readonly ITransactionResultManager _transactionResultManager;
+        private readonly ITxPoolService _txPoolService;
+        private readonly Dictionary<Hash, TransactionResult> _cacheResults = new Dictionary<Hash, TransactionResult>();
+
+        public TransactionResultService(ITxPoolService txPoolService, ITransactionResultManager transactionResultManager)
+        {
+            _txPoolService = txPoolService;
+            _transactionResultManager = transactionResultManager;
+        }
+
+        public async Task<TransactionResult> GetResultAsync(Hash txId)
+        {
+            if (_cacheResults.TryGetValue(txId, out var res))
+            {
+                return res;
+            }
+
+            res = await _transactionResultManager.GetTransactionResultAsync(txId);
+            if (res != null)
+            {
+                _cacheResults[txId] = res;
+                return res;
+            }
+
+            if (await _txPoolService.GetTxAsync(txId) != null)
+            {
+                return new TransactionResult
+                {
+                    TransactionId = txId,
+                    Status = Status.Pending
+                };
+            }
+            
+            return new TransactionResult
+            {
+                TransactionId = txId,
+                Status = Status.NotExisted
+            };
+        }
+
+        public async Task AddResultAsync(TransactionResult res)
+        {
+            _cacheResults[res.TransactionId] = res;
+            await _transactionResultManager.AddTransactionResultAsync(res);
+        }
+    }
+}
