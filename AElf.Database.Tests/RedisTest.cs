9a7321efd9f9d12f1514593a500a3119e3f444d7
EanCuznaivy <1036682639@qq.com>
2018-05-11 11:38:31 +0800

    Add a basic test case of redis with protobuf serializer

---
+++ AElf.Database.Tests/RedisTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using AElf.Kernel;
+using ServiceStack;
+using ServiceStack.Redis;
+using Xunit;
+using ProtobufSerializer = AElf.Serialization.Protobuf.ProtobufSerializer;
+
+namespace AElf.Database.Tests
+{
+    public class RedisTest
+    {
+        private const string IpAddress = "127.0.0.1";
+
+        private const int Port = 6379;
+
+        private static RedisClient RedisClient => new RedisClient(IpAddress, Port);
+
+        [Fact]
+        public void BasicTest()
+        {
+            var serializer = new ProtobufSerializer();
+            
+            const string key = "OneChange";
+
+            var change = new Change
+            {
+                After = Hash.Generate(),
+                LatestChangedBlockHash = Hash.Generate(),
+                TransactionIds = Hash.Generate()
+            };
+            change.AddHashBefore(Hash.Generate());
+            change.AddHashBefore(Hash.Generate());
+            change.AddHashBefore(Hash.Generate());
+            change.AddHashBefore(Hash.Generate());
+
+            var serializedValue = serializer.Serialize(change);
+
+            var success = RedisClient.Set(key, serializedValue);
+            Assert.True(success);
+            
+            var getChange = RedisClient.Get(key);
+            var getDeserializedChange = serializer.Deserialize<Change>(getChange);
+            
+            Assert.True(change.After == getDeserializedChange.After);
+            Assert.True(change.GetLastHashBefore() == getDeserializedChange.GetLastHashBefore());
+        }
+    }
+}
