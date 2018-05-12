4693d92892ae61065e188032c9510d734bdbeddd
swk <shiwk@tju.edu.cn>
2018-05-12 11:13:19 +0800

    add block generation service

---
+++ AElf.Kernel/Services/BlockGenerationService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AElf.Kernel.Managers;
+using QuickGraph.Collections;
+
+namespace AElf.Kernel.Services
+{
+    public class BlockGenerationService : IBlockGenerationService
+    {
+        private readonly IWorldStateManager _worldStateManager;
+
+        public BlockGenerationService(IWorldStateManager worldStateManager)
+        {
+            _worldStateManager = worldStateManager;
+        }
+        
+        /// <inheritdoc/>
+        public async Task<Block> BlockGeneration(Hash chainId, Hash lastBlockHash, IEnumerable<Hash> txIds)
+        {
+            var block = new Block(lastBlockHash);
+            
+            foreach (var txId in txIds)
+            {
+                block.AddTransaction(txId);
+            }
+            
+            block.FillTxsMerkleTreeRootInHeader();
+            var ws = await _worldStateManager.GetWorldStateAsync(chainId, lastBlockHash);
+            block.Header.MerkleTreeRootOfWorldState = await ws.GetWorldStateMerkleTreeRootAsync();
+            block.Body.BlockHeader = block.Header.GetHash();
+            
+            
+            return block;
+        }
+        
+        /// <inheritdoc/>
+        public async Task<BlockHeader> BlockHeaderGeneration(Hash chainId, Hash lastBlockHash, Hash merkleTreeRootForTransaction)
+        {
+            var ws = await _worldStateManager.GetWorldStateAsync(chainId, lastBlockHash);
+            var state = await ws.GetWorldStateMerkleTreeRootAsync();
+            var header = new BlockHeader
+            {
+                Version = 0,
+                PerviousBlock = lastBlockHash,
+                MerkleTreeRootOfWorldState = state,
+                MerkleTreeRootOfTransactions = merkleTreeRootForTransaction
+            };
+
+            return header;
+        }
+    }
+}
---
+++ AElf.Kernel/Services/IBlockGenerationService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AElf.Kernel.Services
+{
+    public interface IBlockGenerationService
+    {
+        /// <summary>
+        /// generate block
+        /// </summary>
+        /// <param name="chainId"></param>
+        /// <param name="lastBlockHash"></param>
+        /// <param name="txIds"></param>
+        /// <returns></returns>
+        Task<Block> BlockGeneration(Hash chainId, Hash lastBlockHash, IEnumerable<Hash> txIds);
+
+        /// <summary>
+        /// generate block header
+        /// </summary>
+        /// <param name="chainId"></param>
+        /// <param name="lastBlockHash"></param>
+        /// <param name="merkleTreeRootForTransaction"></param>
+        /// <returns></returns>
+        Task<BlockHeader> BlockHeaderGeneration(Hash chainId, Hash lastBlockHash, Hash merkleTreeRootForTransaction);
+    }
+}
