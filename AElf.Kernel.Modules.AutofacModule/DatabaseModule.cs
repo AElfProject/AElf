56ef97bd1d925d3ecd23dbf6ee51b155bec7da17
EanCuznaivy <1036682639@qq.com>
2018-05-14 20:41:41 +0800

    Add a simple swtich of database.

--- AElf.Kernel.Modules.AutofacModule/DatabaseModule.cs
+++ AElf.Kernel.Modules.AutofacModule/DatabaseModule.cs
@@ -1,13 +1,29 @@
-﻿using AElf.Database;
+﻿using System;
+using AElf.Database;
 using Autofac;
+using ServiceStack.Redis;
 
 namespace AElf.Kernel.Modules.AutofacModule
 {
     public class DatabaseModule : Module
     {
+        private static readonly bool _useRedis = false;
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<KeyValueDatabase>().As<IKeyValueDatabase>();
+            if (_useRedis)
+            {
+                if (!new RedisDatabase().IsConnected()) 
+                    return;
+                
+                builder.RegisterInstance(
+                        new DatabaseConfig {IpAddress = "127.0.0.1", Port = 6379})
+                    .As<DatabaseConfig>();
+                builder.RegisterType<RedisDatabase>().As<IKeyValueDatabase>();
+            }
+            else
+            {
+                builder.RegisterType<KeyValueDatabase>().As<IKeyValueDatabase>();
+            }
         }
     }
 }
