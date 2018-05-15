1d38b92c62570c6600e70d40b5d45fc58f485adc
gldeng <realgldeng@gmail.com>
2018-05-15 20:39:51 +0800

    Add ExecuteTransaction logics in ParallelExecutionTransactionExecutor

--- AElf.Kernel/Concurrency/Execution/Messages/Messages.cs
+++ AElf.Kernel/Concurrency/Execution/Messages/Messages.cs
@@ -2,6 +2,7 @@
 
 namespace AElf.Kernel.Concurrency.Execution.Messages
 {
+	#region AccountDataContext
 	public sealed class RequestAccountDataContext
 	{
 		public RequestAccountDataContext(long requestId, Hash hash)
@@ -25,4 +26,31 @@ namespace AElf.Kernel.Concurrency.Execution.Messages
 		public long RequestId { get; }
 		public IAccountDataContext AccountDataContext { get; }
 	}
+	#endregion AccountDataContext
+
+	#region TransactionExecution
+	public sealed class RequestTransactionExecution
+	{
+		public RequestTransactionExecution(long requestId, ITransaction transaction)
+		{
+			RequestId = requestId;
+			Transaction = transaction;
+		}
+
+		public long RequestId { get; }
+		public ITransaction Transaction { get; }
+	}
+
+	public sealed class RespondTransactionExecution
+    {
+		public RespondTransactionExecution(long requestId, TransactionResult transactionResult)
+        {
+            RequestId = requestId;
+			TransactionResult = transactionResult;
+        }
+
+        public long RequestId { get; }
+		public TransactionResult TransactionResult { get; }
+    }
+	#endregion TransactionExecution
 }
--- AElf.Kernel/Concurrency/Execution/ParallelExecutionTransactionExecutor.cs
+++ AElf.Kernel/Concurrency/Execution/ParallelExecutionTransactionExecutor.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Akka.Actor;
+using AElf.Kernel.Concurrency.Execution.Messages;
+using AElf.Kernel.KernelAccount;
 
 namespace AElf.Kernel.Concurrency.Execution
 {
@@ -14,8 +17,37 @@ namespace AElf.Kernel.Concurrency.Execution
 
 		protected override void OnReceive(object message)
 		{
-			// TODO: Implement
-			throw new System.NotImplementedException();
+			switch (message)
+			{
+				case RequestTransactionExecution req:
+					var origSender = Sender;
+					ExecuteTransaction(req.Transaction).ContinueWith(
+						task => new RespondTransactionExecution(req.RequestId, task.Result),
+						TaskContinuationOptions.AttachedToParent & TaskContinuationOptions.ExecuteSynchronously
+					).PipeTo(origSender);
+					break;
+					// TODO: More messages
+			}
+		}
+
+		private async Task<TransactionResult> ExecuteTransaction(ITransaction tx)
+		{
+			ISmartContractZero smartContractZero = _chainContext.SmartContractZero;
+			TransactionResult result = new TransactionResult();
+			result.TransactionId = tx.GetHash();
+			// TODO: Reject tx if IncrementId != Nonce
+         
+			try
+			{
+				await smartContractZero.InvokeAsync(caller: tx.From, methodname: tx.MethodName, bytes: tx.Params);
+				result.Status = Status.Mined;
+			}
+			catch
+			{
+				result.Status = Status.ExecutedFailed;
+			}
+
+			return result;
 		}
 
 		public static Props Props(IChainContext chainContext)
