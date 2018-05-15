c08661070273be1e202941d1ee4df7d20d1c30ff
gldeng <realgldeng@gmail.com>
2018-05-15 11:22:32 +0800

    Add messages and changed constructor of ParallelExecutionChainExecutor

---
+++ AElf.Kernel/Concurrency/Execution/Messages/Messages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AElf.Kernel.Concurrency.Execution.Messages
+{
+	public sealed class RequestAccountDataContext
+	{
+		public RequestAccountDataContext(long requestId, Hash hash)
+		{
+			RequestId = requestId;
+			AccountHash = hash;
+		}
+
+		public long RequestId { get; }
+		public Hash AccountHash { get; }
+	}
+
+	public sealed class RespondAccountDataContext
+	{
+		public RespondAccountDataContext(long requestId, IAccountDataContext accountDataContext)
+		{
+			RequestId = requestId;
+			AccountDataContext = accountDataContext;
+		}
+
+		public long RequestId { get; }
+		public IAccountDataContext AccountDataContext { get; }
+	}
+}
--- AElf.Kernel/Concurrency/Execution/ParallelExecutionChainExecutor.cs
+++ AElf.Kernel/Concurrency/Execution/ParallelExecutionChainExecutor.cs
@@ -1,27 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using Akka.Actor;
+using AElf.Kernel.Services;
+using AElf.Kernel.Concurrency.Execution.Messages;
 
 namespace AElf.Kernel.Concurrency.Execution
 {
 	public class ParallelExecutionChainExecutor : UntypedActor
-    {
-        private IChainContext _chainContext;
-		public ParallelExecutionChainExecutor(IChainContext chainContext)
-        {
-            _chainContext = chainContext;
-        }
+	{
+		private IChainContext _chainContext;
+		private IAccountContextService _accountContextService;
 
-        protected override void OnReceive(object message)
-        {
-			// TODO: Implement
-			throw new System.NotImplementedException();
-        }
+		public ParallelExecutionChainExecutor(IChainContext chainContext, IAccountContextService accountContextService)
+		{
+			_chainContext = chainContext;
+			_accountContextService = accountContextService;
+		}
 
-        public static Props Props(IChainContext chainContext)
-        {
-			return Akka.Actor.Props.Create(() => new ParallelExecutionChainExecutor(chainContext));
-        }
+		protected override void OnReceive(object message)
+		{
+			switch (message)
+			{
+				case RequestAccountDataContext req:
+					var accountDataContext = _accountContextService.GetAccountDataContext(req.AccountHash, _chainContext.ChainId);
+					Sender.Tell(new RespondAccountDataContext(req.RequestId, accountDataContext));
+					break;
+					// TODO: More messages
+			}
+		}
 
-    }
+		public static Props Props(IChainContext chainContext, IAccountContextService accountContextService)
+		{
+			return Akka.Actor.Props.Create(() => new ParallelExecutionChainExecutor(chainContext, accountContextService));
+		}
+
+	}
 }
