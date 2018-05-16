ef605fa61d617a8d091de0b29404609b00efce8f
gldeng <realgldeng@gmail.com>
2018-05-16 18:18:48 +0800

    Add job executor

---
+++ AElf.Kernel.Tests/Concurrency/Execution/ParallelExecutionJobExecutorTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+using Akka.Actor;
+using Akka.TestKit;
+using Akka.TestKit.Xunit;
+using Google.Protobuf;
+using AElf.Kernel.Concurrency.Execution;
+using AElf.Kernel.Concurrency.Execution.Messages;
+
+namespace AElf.Kernel.Tests.Concurrency.Execution
+{
+
+	[UseAutofacTestFramework]
+	public class ParallelExecutionJobExecutorTest : TestKitBase
+	{
+		private ActorSystem sys = ActorSystem.Create("test");
+		private IChainContext _chainContext;
+		private ProtobufSerializer _serializer = new ProtobufSerializer();
+
+		public ParallelExecutionJobExecutorTest(ChainContextWithSmartContractZeroWithTransfer chainContext) : base(new XunitAssertions())
+		{
+			_chainContext = chainContext;
+		}
+
+		private Transaction GetTransaction(Hash from, Hash to, ulong qty)
+		{
+			// TODO: Test with IncrementId
+			TransferArgs args = new TransferArgs()
+			{
+				From = from,
+				To = to,
+				Quantity = qty
+			};
+
+			ByteString argsBS = ByteString.CopyFrom(_serializer.Serialize(args));
+
+			Transaction tx = new Transaction()
+			{
+				IncrementId = 0,
+				From = from,
+				To = to,
+				MethodName = "Transfer",
+				Params = argsBS
+			};
+
+			return tx;
+		}
+
+		[Fact]
+		public void JobExecutionTest()
+		{
+			ProtobufSerializer serializer = new ProtobufSerializer();
+			Hash address1 = Hash.Generate();
+			Hash address2 = Hash.Generate();
+			Hash address3 = Hash.Generate();
+			Hash address4 = Hash.Generate();
+
+
+			SmartContractZeroWithTransfer smartContractZero = (_chainContext.SmartContractZero as SmartContractZeroWithTransfer);
+			smartContractZero.SetBalance(address1, 100);
+			smartContractZero.SetBalance(address2, 0);
+			smartContractZero.SetBalance(address3, 200);
+			smartContractZero.SetBalance(address4, 0);
+
+			var tx1 = GetTransaction(address1, address2, 10);
+			var tx2 = GetTransaction(address3, address4, 10);
+
+			// Normal transfer
+			var job1 = new List<Transaction>{
+				tx1,
+				tx2
+			};
+
+			var executor1 = ActorOf(ParallelExecutionJobExecutor.Props(_chainContext, job1, TestActor));
+			Watch(executor1);
+			executor1.Tell(new StartExecutionMessage());
+			var results = ExpectMsg<JobResultMessage>().TransactionResults.ToDictionary(x => x.TransactionId);//new TimeSpan(0, 0, 10)
+			Assert.Contains(tx1.GetHash(), results.Keys);
+			Assert.Contains(tx2.GetHash(), results.Keys);
+			var result1 = results[tx1.GetHash()];
+			var result2 = results[tx2.GetHash()];
+			Assert.Equal(Status.Mined, result1.Status);
+			Assert.Equal((ulong)90, smartContractZero.GetBalance(address1));
+			Assert.Equal((ulong)10, smartContractZero.GetBalance(address2));
+			Assert.Equal(Status.Mined, result2.Status);
+			Assert.Equal((ulong)190, smartContractZero.GetBalance(address3));
+			Assert.Equal((ulong)10, smartContractZero.GetBalance(address4));
+			ExpectTerminated(executor1);
+
+		}
+
+	}
+}
---
+++ AElf.Kernel/Concurrency/Execution/ParallelExecutionJobExecutor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Akka.Actor;
+using AElf.Kernel.Concurrency.Execution.Messages;
+using AElf.Kernel.Concurrency;
+
+namespace AElf.Kernel.Concurrency.Execution
+{
+	// TODO: Use real job class
+	using Job = List<Transaction>;
+
+	public class ParallelExecutionJobExecutor : UntypedActor
+	{
+		private IChainContext _chainContext;
+		private Job _job;
+		private IActorRef _resultCollector;
+		private Dictionary<IActorRef, Transaction> _actorToTransaction = new Dictionary<IActorRef, Transaction>();
+		private Dictionary<Hash, TransactionResult> _transactionResults = new Dictionary<Hash, TransactionResult>();
+
+		public ParallelExecutionJobExecutor(IChainContext chainContext, Job job, IActorRef resultCollector)
+		{
+			_chainContext = chainContext;
+			_job = job;
+			_resultCollector = resultCollector;
+		}
+
+		protected override void PreStart()
+		{
+			foreach (var tx in _job)
+			{
+				var actor = Context.ActorOf(ParallelExecutionTransactionExecutor.Props(_chainContext, tx, Self));
+				_actorToTransaction.Add(actor, tx);
+			}
+
+		}
+
+		protected override void OnReceive(object message)
+		{
+			switch (message)
+			{
+				case StartExecutionMessage start:
+					foreach (var a in _actorToTransaction.Keys)
+					{
+						a.Tell(start);
+					}
+					break;
+				case TransactionResultMessage res:
+					_transactionResults[res.TransactionResult.TransactionId] = res.TransactionResult;
+					CheckReceived();
+					break;
+				case Terminated t:
+					var txId = _actorToTransaction[Sender].GetHash();
+					if (!_transactionResults.ContainsKey(txId))
+					{
+						_transactionResults.Add(txId, new TransactionResult { TransactionId = txId, Status = Status.ExecutedFailed });
+					}
+					CheckReceived();
+					break;
+			}
+		}
+
+		private void CheckReceived()
+		{
+			if (_transactionResults.Count == _actorToTransaction.Count)
+			{
+				if (_resultCollector != null)
+				{
+					_resultCollector.Tell(new JobResultMessage(_transactionResults.Values.ToList()));
+				}
+				Context.Stop(Self);
+			}
+		}
+
+		public static Props Props(IChainContext chainContext, Job job, IActorRef resultCollector)
+		{
+			return Akka.Actor.Props.Create(() => new ParallelExecutionJobExecutor(chainContext, job, resultCollector));
+		}
+	}
+}
