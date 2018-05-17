d60ec2d2093550caa1046d5c5cbe25da90fb4300
gldeng <realgldeng@gmail.com>
2018-05-17 15:48:55 +0800

    Cleanup old files, code has been reorganized

--- AElf.Kernel/Concurrency/IParallelGroupService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace AElf.Kernel.Concurrency
-{
-    public interface IParallelGroupService
-    {
-        List<IParallelGroup> ProduceGroup(Dictionary<Hash, List<ITransaction>> txList);
-    }
-}
