2fae27e6e99e797b12b379cfe38f4b0b70fb2083
gldeng <realgldeng@gmail.com>
2018-05-17 13:46:41 +0800

    Simplify and standardize Batcher and Grouper

---
+++ AElf.Kernel/Concurrency/IBatcher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AElf.Kernel.Concurrency
+{
+    public interface IBatcher
+    {
+        List<List<Transaction>> Process(List<Transaction> transactions);
+    }
+}
