2ca4edddaa7261eb60dd33695e0c347f9d888af6
swk <shiwk@tju.edu.cn>
2018-05-17 22:19:11 +0800

    smart contract runner

--- AElf.Kernel/KernelAccount/ISmartContractRunner.cs
+++ AElf.Kernel/KernelAccount/ISmartContractRunner.cs
@@ -1,17 +1,31 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AElf.Kernel.KernelAccount
 {
     public interface ISmartContractRunner
     {
-        Task<ISmartContract> RunAsync(SmartContractRegistration reg);
+        Task<ISmartContract> RunAsync(SmartContractRegistration reg, SmartContractDeployment deployment);
     }
     
     public class KernelZeroSmartContractRunner: ISmartContractRunner
     {
-        public async Task<ISmartContract> RunAsync(SmartContractRegistration reg)
+        public async Task<ISmartContract> RunAsync(SmartContractRegistration reg, SmartContractDeployment deployment)
         {
-            throw new System.NotImplementedException();
+            var type = Type.GetType(reg.ContractBytes.ToString());
+            var paramTypes = deployment.ConstructParams.Select(p => p.Value().GetType()).ToArray();
+            var constructor = type.GetConstructor(paramTypes);
+            var paramArray = deployment.ConstructParams.Select(p => p.Value()).ToArray();
+
+            var smartContract = new CSharpSmartContract
+            {
+                Type = type,
+                Constructor = constructor,
+                Params = paramArray
+            };
+
+            return smartContract;
         }
     }
 }
