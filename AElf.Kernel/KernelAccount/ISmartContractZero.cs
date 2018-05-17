5f54bb69e10492c1252832f20d4b9e787b40aae3
swk <shiwk@tju.edu.cn>
2018-05-17 22:22:24 +0800

    change ISmartContractZero

--- AElf.Kernel/KernelAccount/ISmartContractZero.cs
+++ AElf.Kernel/KernelAccount/ISmartContractZero.cs
@@ -5,7 +5,7 @@ namespace AElf.Kernel.KernelAccount
     public interface ISmartContractZero : ISmartContract
     {
         Task RegisterSmartContract(SmartContractRegistration reg);
-        Task DeploySmartContract(SmartContractDeployment smartContractRegister);
+        Task DeploySmartContract(Hash account, SmartContractDeployment smartContractRegister);
         Task<ISmartContract> GetSmartContractAsync(Hash hash);
     }
 }
