1eab050907bb291e39d2b7c0e8f0a6d397fc7a5f
gldeng <realgldeng@gmail.com>
2018-05-17 22:21:32 +0800

    More detailed batch executor

--- AElf.Kernel/Concurrency/Execution/Messages/Messages.cs
+++ AElf.Kernel/Concurrency/Execution/Messages/Messages.cs
@@ -5,30 +5,30 @@ using System.Collections.Generic;
 namespace AElf.Kernel.Concurrency.Execution.Messages
 {
 	#region AccountDataContext
-    public sealed class RequestAccountDataContext
-    {
-        public RequestAccountDataContext(long requestId, Hash hash)
-        {
-            RequestId = requestId;
-            AccountHash = hash;
-        }
+	public sealed class RequestAccountDataContext
+	{
+		public RequestAccountDataContext(long requestId, Hash hash)
+		{
+			RequestId = requestId;
+			AccountHash = hash;
+		}
 
-        public long RequestId { get; }
-        public Hash AccountHash { get; }
-    }
+		public long RequestId { get; }
+		public Hash AccountHash { get; }
+	}
 
-    public sealed class RespondAccountDataContext
-    {
-        public RespondAccountDataContext(long requestId, IAccountDataContext accountDataContext)
-        {
-            RequestId = requestId;
-            AccountDataContext = accountDataContext;
-        }
+	public sealed class RespondAccountDataContext
+	{
+		public RespondAccountDataContext(long requestId, IAccountDataContext accountDataContext)
+		{
+			RequestId = requestId;
+			AccountDataContext = accountDataContext;
+		}
 
-        public long RequestId { get; }
-        public IAccountDataContext AccountDataContext { get; }
-    }
-    #endregion AccountDataContext
+		public long RequestId { get; }
+		public IAccountDataContext AccountDataContext { get; }
+	}
+	#endregion AccountDataContext
 
 	public sealed class TransactionResultMessage
 	{
@@ -51,4 +51,5 @@ namespace AElf.Kernel.Concurrency.Execution.Messages
 	}
 
 	public sealed class StartExecutionMessage { }
+	public sealed class StartGroupingMessage { }
 }
--- AElf.Kernel/Concurrency/Execution/ParallelExecutionBatchExecutor.cs
+++ AElf.Kernel/Concurrency/Execution/ParallelExecutionBatchExecutor.cs
@@ -1,27 +1,122 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using Akka.Actor;
+using AElf.Kernel.Concurrency.Execution.Messages;
+using AElf.Kernel.Concurrency;
 
 namespace AElf.Kernel.Concurrency.Execution
 {
+	// Inside Batch, there are Jobs that will be run in parallel
+	using Transactions = List<Transaction>;
+
+	interface IGrouper
+	{
+		List<List<Transaction>> Process(List<Transaction> transactions);
+	}
+
 	public class ParallelExecutionBatchExecutor : UntypedActor
 	{
+		enum State
+		{
+			PendingGrouping,
+			GroupingDone,
+			CreatingChildren,
+			ReadyToRun,
+			Running
+		}
+		private State _state = State.PendingGrouping;
+		private bool _startExecutionMessageReceived = false;
+		private IGrouper _grouper;
 		private IChainContext _chainContext;
-		public ParallelExecutionBatchExecutor(IChainContext chainContext)
+		private Transactions _transactions;
+		private List<Transactions> _grouped;
+		private IActorRef _resultCollector;
+		private Dictionary<IActorRef, Transaction> _actorToTransaction = new Dictionary<IActorRef, Transaction>();
+		private Dictionary<Hash, TransactionResult> _transactionResults = new Dictionary<Hash, TransactionResult>();
+
+		public ParallelExecutionBatchExecutor(IChainContext chainContext, Transactions transactions, IActorRef resultCollector)
 		{
 			_chainContext = chainContext;
+			_transactions = transactions;
+			_resultCollector = resultCollector;
+		}
+
+		protected override void PreStart()
+		{
+			Context.System.Scheduler.ScheduleTellOnce(new TimeSpan(0, 0, 0), Self, new StartGroupingMessage(), Self);
 		}
 
 		protected override void OnReceive(object message)
 		{
-			// TODO: Implement
-            throw new System.NotImplementedException();
+			switch (message)
+			{
+				case StartGroupingMessage startGrouping:
+					if (_state == State.PendingGrouping)
+					{
+						_grouped = _grouper.Process(_transactions);
+						_state = State.CreatingChildren;
+						CreateChildren();
+						_state = State.ReadyToRun;
+						MaybeStartChildren();
+					}
+					break;
+				case StartExecutionMessage start:
+					_startExecutionMessageReceived = true;
+					MaybeStartChildren();
+					break;
+				case TransactionResultMessage res:
+					_transactionResults[res.TransactionResult.TransactionId] = res.TransactionResult;
+					CheckReceived();
+					break;
+				case Terminated t:
+					var txId = _actorToTransaction[Sender].GetHash();
+					if (!_transactionResults.ContainsKey(txId))
+					{
+						_transactionResults.Add(txId, new TransactionResult { TransactionId = txId, Status = Status.ExecutedFailed });
+					}
+					CheckReceived();
+					break;
+			}
 		}
 
-		public static Props Props(IChainContext chainContext)
+		private void CreateChildren()
 		{
-			return Akka.Actor.Props.Create(() => new ParallelExecutionBatchExecutor(chainContext));
+			foreach (var tx in _transactions)
+			{
+				var actor = Context.ActorOf(ParallelExecutionTransactionExecutor.Props(_chainContext, tx, Self));
+				_actorToTransaction.Add(actor, tx);
+			}
 		}
 
+		private void MaybeStartChildren()
+		{
+			if (_state == State.Running)
+				return;
+			if (_state == State.ReadyToRun && _startExecutionMessageReceived)
+			{
+				foreach (var a in _actorToTransaction.Keys)
+				{
+					a.Tell(new StartExecutionMessage());
+				}
+			}
+		}
+
+		private void CheckReceived()
+		{
+			if (_transactionResults.Count == _actorToTransaction.Count)
+			{
+				if (_resultCollector != null)
+				{
+					_resultCollector.Tell(new JobResultMessage(_transactionResults.Values.ToList()));
+				}
+				Context.Stop(Self);
+			}
+		}
+
+		public static Props Props(IChainContext chainContext, Transactions job, IActorRef resultCollector)
+		{
+			return Akka.Actor.Props.Create(() => new ParallelExecutionBatchExecutor(chainContext, job, resultCollector));
+		}
 	}
 }
