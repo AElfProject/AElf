35a057e859b64f34b25ecc80e1f5a683f1454ff2
samfrb <samos2x@outlook.com>
2018-05-21 14:32:37 +0800

    Added bootnode file

---
+++ AElf.Kernel/Node/Network/Peers/Bootnodes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AElf.Kernel.Node.Network.Peers
+{
+    public static class Bootnodes
+    {
+        public static readonly List<string> BootNodes = new List<string>()
+        {
+            // Boot nodes
+        };
+    }
+}
