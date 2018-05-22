4cf670d8e0d9a06bbd767c27ecc56fce948183fa
swk <shiwk@tju.edu.cn>
2018-05-22 12:49:04 +0800

    contract test

---
+++ AElf.Kernel.Tests/SmartContractExecuting/ContractTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using AElf.Kernel.KernelAccount;
+using AElf.Kernel.SmartContracts.CSharpSmartContract;
+using Google.Protobuf;
+using Xunit;
+
+namespace AElf.Kernel.Tests.SmartContractExecuting
+{
+    public class ContractTest
+    {
+
+        [Fact]
+        public void RegisterContract()
+        {
+            Type smartContractZero = typeof(Class1);
+            
+            var registerTx = new Transaction
+            {
+                IncrementId = 0,
+                MethodName = nameof(ISmartContractZero.RegisterSmartContract),
+                To = Hash.Zero,
+                From = Hash.Zero,
+                Params = ByteString.CopyFrom(
+                    new Parameters
+                    {
+                        Params = 
+                        {
+                            new Param
+                            {
+                                RegisterVal = new SmartContractRegistration
+                                {
+                                    Category = 0,
+                                    ContractBytes = ByteString.CopyFromUtf8(smartContractZero.FullName),
+                                    ContractHash = Hash.Zero
+                                }
+                            }
+                        }
+                    }.ToByteArray()
+                )
+            };
+        }
+        
+        
+        public void DeployContract()
+        {
+            var deployTx = new Transaction
+            {
+                IncrementId = 1,
+                MethodName = nameof(ISmartContractZero.DeploySmartContract),
+                From = Hash.Zero,
+                To = Hash.Zero,
+                Params = ByteString.CopyFrom(
+                    new Parameters
+                        {
+                            Params = { 
+                                new Param
+                                {
+                                    HashVal = Hash.Zero
+                                },
+                                new Param
+                                {
+                                    DeploymentVal = new SmartContractDeployment
+                                    {
+                                        ContractHash = Hash.Zero
+                                    }
+                                }}
+                        }
+                        .ToByteArray()
+                )
+            };
+        }
+    }
+}
