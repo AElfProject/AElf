bd6b56d7d4d0ff809785511054ac2c9ca9ae466a
PadawanZH <zhangan19940520@gmail.com>
2018-05-23 20:48:06 +0800

    add graph in functuionMetadataService to check if calling graph is topological

--- AElf.Kernel/Concurrency/FunctionMetadataService.cs
+++ AElf.Kernel/Concurrency/FunctionMetadataService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Org.BouncyCastle.Security;
+using QuickGraph;
+using QuickGraph.Algorithms;
 using ServiceStack;
 
 namespace AElf.Kernel.Concurrency
@@ -9,8 +11,14 @@ namespace AElf.Kernel.Concurrency
     public class FunctionMetadataService : IFunctionMetaDataService
     {
         public Dictionary<string, FunctionMetadata> FunctionMetadataMap { get; } = new Dictionary<string, FunctionMetadata>();
-        
-        
+        private AdjacencyGraph<string, Edge<string>> _callingGraph;
+
+        public FunctionMetadataService()
+        {
+            _callingGraph = new AdjacencyGraph<string, Edge<string>>();
+        }
+
+
         public bool SetNewFunctionMetadata(string functionFullName, HashSet<string> otherFunctionsCallByThis, HashSet<Hash> nonRecursivePathSet)
         {
             if (FunctionMetadataMap.ContainsKey(functionFullName))
@@ -38,6 +46,12 @@ namespace AElf.Kernel.Concurrency
             var metadata = new FunctionMetadata(otherFunctionsCallByThis, pathSet);
             
             FunctionMetadataMap.Add(functionFullName, metadata);
+
+            _callingGraph.AddVertex(functionFullName);
+            foreach (var callingFunc in metadata.CallingSet)
+            {
+                _callingGraph.AddEdge(new Edge<string>(functionFullName, callingFunc));
+            }
             return true;
         }
 
@@ -60,11 +74,17 @@ namespace AElf.Kernel.Concurrency
             {
                 throw new InvalidOperationException("FunctionMetadataService: FunctionMetadataMap don't contain a function named " + functionFullName + " when trying to update this function's metadata");
             }
-
+            
             var oldMetadata = FunctionMetadataMap[functionFullName];
+
+            if (!TryUpdateCallingGraph(functionFullName, oldMetadata.CallingSet, newOtherFunctionsCallByThis))
+            {
+                return false;
+            }
 
             FunctionMetadataMap.Remove(functionFullName);
             
+            //
             if(!SetNewFunctionMetadata(functionFullName, newOtherFunctionsCallByThis, newNonRecursivePathSet)){
                 FunctionMetadataMap.Add(functionFullName, oldMetadata);
                 return false;
@@ -100,6 +120,30 @@ namespace AElf.Kernel.Concurrency
                 .Select(a => a.Key).ToList();
             
             return !callerFunctions.IsEmpty();
+        }
+
+        private bool TryUpdateCallingGraph(string updatingFunc, HashSet<string> oldCallingSet, HashSet<string> newOtherFunctionsCallByThis)
+        {
+            var newGraph = _callingGraph.CreateCopy();
+            newGraph.RemoveOutEdgeIf(updatingFunc, outEdge => oldCallingSet.Contains(outEdge.Target));
+            //TODO: some vertex should be deleted?
+            foreach (var newCallingFunc in newOtherFunctionsCallByThis)
+            {
+                newGraph.AddEdge(new Edge<string>(updatingFunc, newCallingFunc));
+            }
+            try
+            {
+                newGraph.TopologicalSort();
+            }
+            catch (NonAcyclicGraphException e)
+            {
+                Console.WriteLine("FunctionMetadataService: When update function " + updatingFunc + ", its new calling set form a acyclic graph, thus update don't take effect");
+                return false;
+            }
+
+            _callingGraph = newGraph;
+            
+            return true;
         }
     }
 }
