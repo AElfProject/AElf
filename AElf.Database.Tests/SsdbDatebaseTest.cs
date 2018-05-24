18e262e214fef8d79c92510fcb2f692a887ba6ef
sean.zhang <sean.zhang@hoopox.com>
2018-05-24 18:21:27 +0800

    add ssdb datebase and unittest

---
+++ AElf.Database.Tests/SsdbDatebaseTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AElf.Database.Client;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+
+namespace AElf.Database.Tests
+{
+    [UseAutofacTestFramework]
+    public class SsdbDatebaseTest
+    {
+        private readonly IKeyValueDatabase _database;
+
+        public SsdbDatebaseTest()
+        {
+            _database = new SsdbDatebase();
+        }
+
+        [Fact]
+        public void IsConnectedTest()
+        {
+            var result = _database.IsConnected();
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void SetAndGetTest()
+        {
+            var key = "UintTest";
+            var value = Guid.NewGuid().ToString();
+
+            _database.SetAsync(key, ConvertToBytes((value)));
+            var getResult = _database.GetAsync(key, null);
+            var getResultStr = ConvertToString(getResult.Result);
+            
+            Assert.Equal(value,getResultStr);
+        }
+        
+        private byte[] ConvertToBytes(string s)	{
+            return Encoding.Default.GetBytes(s);
+        }
+
+        private string ConvertToString(byte[] bytes)
+        {
+            return Encoding.Default.GetString(bytes);
+        }
+    }
+}
---
+++ AElf.Database/SsdbDatebase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using AElf.Database.Client;
+
+namespace AElf.Database
+{
+    public class SsdbDatebase:IKeyValueDatabase
+    {
+        private readonly SsdbClient _client;
+        
+        public SsdbDatebase()
+        {
+            _client = new SsdbClient("127.0.0.1", 8888);;
+        }
+
+        public SsdbDatebase(DatabaseConfig config)
+        {
+            _client = new SsdbClient(config.IpAddress, config.Port);
+        }
+        
+        public async Task<byte[]> GetAsync(string key, Type type)
+        {
+            return await Task.FromResult(_client.request("get", key)[1]);
+        }
+
+        public async Task SetAsync(string key, byte[] bytes)
+        {
+            var keyBytes = ConvertToBytes(key);
+            await Task.FromResult(_client.request("set",keyBytes,bytes));
+        }
+
+        public bool IsConnected()
+        {
+            try
+            {
+                _client.set("test", "test");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        private byte[] ConvertToBytes(string s)	{
+            return Encoding.Default.GetBytes(s);
+        }
+    }
+}
