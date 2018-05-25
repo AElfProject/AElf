1938f2e4a8cc47fe30f3466c13cb9938675a7b3f
PadawanZH <zhangan19940520@gmail.com>
2018-05-25 20:14:17 +0800

    Add attribute for smart contract's function and fields in order to mark the metadata for user-defined contracts' code

---
+++ AElf.Kernel/Concurrency/Metadata/SmartContractFieldDataAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AElf.Kernel.Concurrency
+{
+    public enum DataAccessMode{
+        ReadOnlyAccountSharing,
+        ReadWriteAccountSharing,
+        AccountSpecific
+    }
+    
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class SmartContractFieldDataAttribute : Attribute
+    {
+        public SmartContractFieldDataAttribute(string dataFullName, DataAccessMode dataAccessMode)
+        {
+            DataAccessMode = dataAccessMode;
+            DataFullName = dataFullName;
+        }
+
+        public DataAccessMode DataAccessMode { get; }
+        public string DataFullName { get; }
+    }
+}
---
+++ AElf.Kernel/Concurrency/Metadata/SmartContractFunctionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AElf.Kernel.Concurrency
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SmartContractFunctionAttribute : Attribute
+    {
+        public SmartContractFunctionAttribute(string functionFullName)
+        {
+            FunctionFullName = functionFullName;
+        }
+
+        public string FunctionFullName { get; }
+    }
+}
