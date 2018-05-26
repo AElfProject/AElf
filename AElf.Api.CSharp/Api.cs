137626e8e33d84a016ec8c48d84eedc1be9579c9
gldeng <realgldeng@gmail.com>
2018-05-26 10:56:57 +0800

    Add api for user CSharp contracts

---
+++ AElf.Api.CSharp/Api.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using AElf.Kernel;
+
+namespace AElf.Api.CSharp
+{
+    /// <summary>
+    /// Singleton that holds the smart contract API for interacting with the chain via the injected context.
+    /// </summary>
+    public class Api
+    {
+        private static Dictionary<string, IDataProvider> _dataProviders;
+        private static IDataProvider _dataProvider;
+        private static SmartContractRuntimeContext _context;
+
+        #region Setters used by runner and executor
+
+        public static void SetDataProvider(IDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+            _dataProviders = new Dictionary<string, IDataProvider>()
+            {
+                {"", _dataProvider}
+            };
+        }
+        
+        public static void SetContext(SmartContractRuntimeContext context)
+        {
+            _context = context;
+        }        
+
+        #endregion Setters used by runner and executor
+
+        #region Getters used by contract
+        
+        public static Hash GetChainId()
+        {
+            return _context.ChainId;
+        }
+
+        public static Hash GetContractAddress()
+        {
+            return _context.ContractAddress;
+        }
+
+        public static IDataProvider GetDataProvider(string name)
+        {
+            if (!_dataProviders.TryGetValue(name, out var dp))
+            {
+                dp = _dataProvider.GetDataProvider(name);
+                _dataProviders.Add(name, dp);
+            }
+
+            return dp;
+        }        
+
+        #endregion Getters used by contract
+
+    }
+}
---
+++ AElf.Api.CSharp/Map.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AElf.Kernel;
+
+namespace AElf.Api.CSharp
+{
+    public class AttributeException : Exception
+    {
+        public AttributeException(string message):base(message)
+        {
+            
+        }
+    }
+    public class Map
+    {
+        private string _name;
+
+        public Map(string name)
+        {
+            _name = name;
+        }
+        
+        public async Task SetValueAsync(Hash keyHash, byte[] value)
+        {
+            await Api.GetDataProvider(_name).SetAsync(keyHash, value);
+        }
+
+        public async Task<byte[]> GetValue(Hash keyHash)
+        {
+            return await Api.GetDataProvider(_name).GetAsync(keyHash);
+        }
+    }
+}
