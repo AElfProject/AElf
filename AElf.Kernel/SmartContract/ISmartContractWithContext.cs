77771a26bc054d38613f29e5fc966f5d643809fa
gldeng <realgldeng@gmail.com>
2018-05-29 12:10:36 +0800

    Change ISmartContract and remove ISmartContractWithContext

--- AElf.Kernel/SmartContract/ISmartContractWithContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AElf.Kernel
-{
-    public interface ISmartContractWithContext: ISmartContract
-    {
-        void SetDataProvider(IDataProvider dataProvider);
-        void SetContext(TransactionContext context);
-    }
-}
