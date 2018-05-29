45d3e56a3c2a2ab05fef1bcdca995be42e4c59a4
gldeng <realgldeng@gmail.com>
2018-05-29 11:41:17 +0800

    Add IExecutive

---
+++ AElf.Kernel/SmartContract/IExecutive.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AElf.Kernel
+{
+    public interface IExecutive
+    {
+        IExecutive SetContext(ITransactionContext context);
+        Task Apply();
+    }
+}
