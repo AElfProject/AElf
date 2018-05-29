ad5d81510c4a88322fbcf3d0c5929998e2f06836
gldeng <realgldeng@gmail.com>
2018-05-29 11:46:01 +0800

    Change runner and runner factory

--- AElf.Kernel/KernelAccount/ISmartContractRunner.cs
+++ AElf.Kernel/KernelAccount/ISmartContractRunner.cs
@@ -9,45 +9,6 @@ namespace AElf.Kernel.KernelAccount
 {
     public interface ISmartContractRunner
     {
-        Task<ISmartContract> RunAsync(SmartContractRegistration reg, SmartContractDeployment deployment, 
-            IAccountDataProvider adp);
+        Task<IExecutive> RunAsync(SmartContractRegistration reg, IDataProvider dataProvider);
     }
-    
-    /*public class KernelZeroSmartContractRunner: ISmartContractRunner
-    {
-        public Task<ISmartContract> RunAsync(SmartContractRegistration reg, SmartContractDeployment deployment)
-        {
-            throw new NotImplementedException();
-        }
-    }*/
-
-    public class CSharpSmartContractRunner : ISmartContractRunner
-    {
-
-        public async Task<ISmartContract> RunAsync(SmartContractRegistration reg, SmartContractDeployment deployment, 
-            IAccountDataProvider adp)
-        {
-            
-            var contractName = StringValue.Parser.ParseFrom(reg.ContractBytes).Value;
-            var type = System.Type.GetType(contractName);
-            
-            // construct instance
-            var constructorParams = Parameters.Parser.ParseFrom(deployment.ConstructParams).Params;
-            var parameterObjs = constructorParams.Select(p => p.Value()).ToArray();
-            var paramTypes = parameterObjs.Select(p => p.GetType()).ToArray();
-            var constructorInfo = type.GetConstructor(paramTypes);
-
-            var instance = constructorInfo.Invoke(parameterObjs);
-            
-            // inject instance
-            var smartContract = new CSharpSmartContract
-            {
-                Instance = instance
-            };
-            
-            // initialize account info 
-            await smartContract.InitializeAsync(adp);
-            return smartContract;
-        }
-    } 
 }
