c2c19d6a805a36f5f3578328ab934bdc28e660cd
gldeng <realgldeng@gmail.com>
2018-05-30 12:20:06 +0800

    Move TransactionContext into Kernel

--- AElf.Kernel/SmartContract/TransactionContext.cs
+++ AElf.Kernel/SmartContract/TransactionContext.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using AElf.Kernel;
-
-namespace AElf.Runtime.CSharp
+namespace AElf.Kernel
 {
     public class TransactionContext : ITransactionContext
     {
-        public Hash ChainId { get; set; }
-        public Hash ContractAddress { get; set; }
         public Hash PreviousBlockHash { get; set; }
         public ITransaction Transaction { get; set; }
         public TransactionResult TransactionResult { get; set; }
