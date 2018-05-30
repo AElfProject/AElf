704ad022852d2e2bccd5867f5ccaf5969aa7c42e
swk <shiwk@tju.edu.cn>
2018-05-30 10:11:29 +0800

    sign transaction

--- AElf.Kernel/Node/RPC/DTO/DtoHelpers.cs
+++ AElf.Kernel/Node/RPC/DTO/DtoHelpers.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using AElf.Kernel;
+using AElf.Kernel.Crypto.ECDSA;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
+using Parameters = AElf.Kernel.Parameters;
 
 namespace AElf.Node.RPC.DTO
 {
@@ -19,12 +24,34 @@ namespace AElf.Node.RPC.DTO
 
         public static Transaction ToTransaction(this TransactionDto dto)
         {
-            Transaction tx = new Transaction
+            var parameters = new Parameters();
+            foreach (var param in dto.Params)
+            {
+                parameters.Params.Add(param.ToParam());
+            }
+            
+            ECKeyPair keyPair = new KeyPairGenerator().Generate();
+
+            var tx = new Transaction
             {
                 From = dto.From,
-                To = dto.To
+                To = dto.To,
+                IncrementId = dto.IncrementId,
+                MethodName = dto.Method,
+                P = ByteString.CopyFrom(keyPair.PublicKey.Q.GetEncoded()),
+                Params = parameters.ToByteString()
             };
 
+            // Serialize and hash the transaction
+            Hash hash = tx.GetHash();
+            
+            // Sign the hash
+            ECSigner signer = new ECSigner();
+            ECSignature signature = signer.Sign(keyPair, hash.GetHashBytes());
+            
+            // Update the signature
+            tx.R = ByteString.CopyFrom(signature.R);
+            tx.S = ByteString.CopyFrom(signature.S);
             return tx;
         }
         
