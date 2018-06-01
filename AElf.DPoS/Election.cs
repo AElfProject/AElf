05b8090bbfa0c2db6ff2c2378542c7e91f32a8e5
EanCuznaivy <1036682639@qq.com>
2018-06-01 15:34:25 +0800

    Initial DPoS part.

---
+++ AElf.DPoS/Election.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using AElf.Kernel;
+using AElf.Kernel.Extensions;
+using AElf.Sdk.CSharp.Types;
+
+namespace AElf.DPoS
+{
+    public class Election
+    {
+        public Map GoLivePoolMembers = new Map("GoLivePoolMembers");
+        
+        public Map Votes = new Map("Votes");
+
+        public async Task<object> RegisterToCampaign(Hash accountHash, string alias)
+        {
+            await GoLivePoolMembers.SetValueAsync(accountHash, Encoding.UTF8.GetBytes(alias));
+            await Votes.SetValueAsync(alias.CalculateHash(), ((ulong) 0).ToBytes());
+            return null;
+        }
+        
+        
+    }
+}
