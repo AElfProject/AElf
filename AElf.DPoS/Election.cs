5ada5ca80bd7b4ece5b10f04707b7fc0c40a30b2
EanCuznaivy <1036682639@qq.com>
2018-06-01 16:29:21 +0800

    Basic logic of voting.

--- AElf.DPoS/Election.cs
+++ AElf.DPoS/Election.cs
@@ -11,7 +11,19 @@ namespace AElf.DPoS
     {
         public Map GoLivePoolMembers = new Map("GoLivePoolMembers");
         
+        //Potential mining nodes' votes.
         public Map Votes = new Map("Votes");
+        
+        //Remain votes of voters
+        public Map RemainVotes = new Map("RemainVotes");
+        
+        // TODO: Should use another smart contract to get ELF token balace.
+        public Map PreviousTermBalance = new Map("PreviousTermBalance");
+
+        public async Task<object> InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
 
         public async Task<object> RegisterToCampaign(Hash accountHash, string alias)
         {
@@ -20,6 +32,29 @@ namespace AElf.DPoS
             return null;
         }
         
-        
+        public async Task<object> Vote(Hash voterAddress, string alias, ulong votes)
+        {
+            if ((ulong)await GetRemainVotes(voterAddress) < votes)
+            {
+                return false;
+            }
+
+            var currentVotes = (await Votes.GetValue(alias.CalculateHash())).ToUInt64();
+            await Votes.SetValueAsync(alias.CalculateHash(), (currentVotes + votes).ToBytes());
+            return true;
+        }
+
+        public async Task<object> GetRemainVotes(Hash voterAddress)
+        {
+            return await RemainVotes.GetValue(voterAddress);
+        }
+
+        public async Task<object> InitialVoting(Hash voterAddress)
+        {
+            var balance = (await PreviousTermBalance.GetValue(voterAddress)).ToUInt64();
+            await RemainVotes.SetValueAsync(voterAddress, (balance * 30).ToBytes());
+            return true;
+        }
+
     }
 }
