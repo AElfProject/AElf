a0a5521b1e124c7dfa03c55cf6212e0fba49e192
Dennis-Furrer <dennis.furrer@ibm.com>
2018-06-02 13:09:38 +0100

    Bootnodes added to Bootnodes.cs (these are temporary values)

--- AElf.Network/Peers/Bootnodes.cs
+++ AElf.Network/Peers/Bootnodes.cs
@@ -7,7 +7,27 @@ namespace AElf.Network.Peers
     {
         public static readonly List<NodeData> BootNodes = new List<NodeData>()
         {
-            // Boot nodes
+            BootNode1,
+            BootNode2,
+            BootNode3
+        };
+
+        private static readonly NodeData BootNode1 = new NodeData()
+        {
+            IpAddress = "127.0.0.1",
+            Port = 6800
+        };
+        
+        private static readonly NodeData BootNode2 = new NodeData()
+        {
+            IpAddress = "127.0.0.1",
+            Port = 6801
+        };
+        
+        private static readonly NodeData BootNode3 = new NodeData()
+        {
+            IpAddress = "127.0.0.1",
+            Port = 6802
         };
     }
 }
