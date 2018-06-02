b9da4d2936e28d86e862cbcfe30d758d44da34a5
Dennis-Furrer <dennis.furrer@ibm.com>
2018-06-02 12:01:41 +0100

    Unit testing for PeerManager started

---
+++ AElf.Kernel.Tests/Network/Server/PeerManagerUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using AElf.Network;
+using AElf.Network.Config;
+using AElf.Network.Exceptions;
+using Xunit;
+
+namespace AElf.Kernel.Tests.Network.Server
+{
+    public class PeerManagerUnitTests
+    {
+        [Fact]
+        public async Task Start_ShouldThrow_NullServer()
+        {
+            IAElfServer _server = null;
+
+            Exception ex = 
+                await Assert.ThrowsAsync<NullReferenceException>(async () => await _server.StartAsync());
+            Assert.Equal("Could not start the server, server object is null.", ex.Message);
+        }
+        
+    }
+}
