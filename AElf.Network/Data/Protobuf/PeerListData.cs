836c9e2c2c600f97928f839e55ced88bfb13b206
Dennis-Furrer <dennis.furrer@ibm.com>
2018-06-04 14:30:16 +0100

    Improved logging upon receiving peers

--- AElf.Network/Data/Protobuf/PeerListData.cs
+++ AElf.Network/Data/Protobuf/PeerListData.cs
@@ -13,9 +13,10 @@ namespace AElf.Network.Data
                 return "{ " + NodeData[0].IpAddress + ":" + NodeData[0].Port + " }";
             
             StringBuilder strb = new StringBuilder();
+            strb.Append("{ ");
             foreach (var peer in NodeData)
             {
-                strb.Append("{ " + peer.IpAddress + ":" + peer.Port + ", ");
+                strb.Append(peer.IpAddress + ":" + peer.Port + ", ");
             }
             strb.Append(" }");
 
