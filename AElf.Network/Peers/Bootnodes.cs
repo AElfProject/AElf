9d6095a16f67c765726417be28e49086dde97345
samfrb <samos2x@outlook.com>
2018-06-04 11:44:44 +0800

    Bootnodes file reorg proposal

--- AElf.Network/Peers/Bootnodes.cs
+++ AElf.Network/Peers/Bootnodes.cs
@@ -5,32 +5,11 @@ namespace AElf.Network.Peers
 {
     public static class Bootnodes
     {
-        private static readonly NodeData BootNode1 = new NodeData()
-        {
-            IpAddress = "127.0.0.1",
-            Port = 6800,
-            IsBootnode = true
-        };
-        
-        private static readonly NodeData BootNode2 = new NodeData()
-        {
-            IpAddress = "127.0.0.1",
-            Port = 6801,
-            IsBootnode = true
-        };
-        
-        private static readonly NodeData BootNode3 = new NodeData()
-        {
-            IpAddress = "127.0.0.1",
-            Port = 6802,
-            IsBootnode = true
-        };
-        
         public static readonly List<NodeData> BootNodes = new List<NodeData>()
         {
-            BootNode1,
-            BootNode2,
-            BootNode3
+            new NodeData { IpAddress = "127.0.0.1", Port = 6800, IsBootnode = true},
+            new NodeData { IpAddress = "127.0.0.1", Port = 6801, IsBootnode = true},
+            new NodeData { IpAddress = "127.0.0.1", Port = 6802, IsBootnode = true}
         };
     }
 }
