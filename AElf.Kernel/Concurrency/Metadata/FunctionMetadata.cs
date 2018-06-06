4ebf62287a56f0914679fffff93e8a30b686163b
PadawanZH <zhangan19940520@gmail.com>
2018-06-06 11:09:51 +0800

    create metadatatemplate entity, create Resource entity to include the data access mode of resource

--- AElf.Kernel/Concurrency/Metadata/FunctionMetadata.cs
+++ AElf.Kernel/Concurrency/Metadata/FunctionMetadata.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AElf.Kernel.Concurrency.Metadata
 {
@@ -9,21 +10,13 @@ namespace AElf.Kernel.Concurrency.Metadata
     /// </summary>
     public class FunctionMetadata
     {
-        public FunctionMetadata(HashSet<string> callingSet, HashSet<string> fullResourceSet, HashSet<string> localResourceSet)
+        public FunctionMetadata(HashSet<string> callingSet, HashSet<Resource> fullResourceSet, HashSet<Resource> localResourceSet)
         {
-            IsTemplate = false;
             CallingSet = callingSet ?? new HashSet<string>();
-            FullResourceSet = fullResourceSet ?? new HashSet<string>();
-            LocalResourceSet = localResourceSet ?? new HashSet<string>();
+            FullResourceSet = fullResourceSet ?? new HashSet<Resource>();
+            LocalResourceSet = localResourceSet ?? new HashSet<Resource>();
         }
-
-        public FunctionMetadata(HashSet<string> callingSet, HashSet<string> localResourceSet)
-        {
-            IsTemplate = true;
-            CallingSet = callingSet ?? new HashSet<string>();
-            LocalResourceSet = localResourceSet ?? new HashSet<string>();
-        }
-
+        
         /// <summary>
         /// used to find influenced contract when a contract is updated
         /// </summary>
@@ -32,13 +25,57 @@ namespace AElf.Kernel.Concurrency.Metadata
         /// <summary>
         /// used to find what resource this function will access (recursive)
         /// </summary>
-        public HashSet<string> FullResourceSet { get; }
+        public HashSet<Resource> FullResourceSet { get; }
         
         /// <summary>
         /// used when updating a function, the caller functions of this updating function should use this NonRecursivePathSet to regenerate the new metadata
         /// </summary>
-        public HashSet<string> LocalResourceSet { get; }
+        public HashSet<Resource> LocalResourceSet { get; }
+    }
 
-        public bool IsTemplate;
+    public class FunctionMetadataTemplate
+    {
+        public FunctionMetadataTemplate(HashSet<string> callingSet, HashSet<Resource> localResourceSet)
+        {
+            CallingSet = callingSet ?? new HashSet<string>();
+            LocalResourceSet = localResourceSet ?? new HashSet<Resource>();
+        }
+        
+        /// <summary>
+        /// used to find influenced contract when a contract is updated
+        /// </summary>
+        public HashSet<string> CallingSet { get; }
+        
+        /// <summary>
+        /// used when updating a function, the caller functions of this updating function should use this NonRecursivePathSet to regenerate the new metadata
+        /// </summary>
+        public HashSet<Resource> LocalResourceSet { get; }
+    }
+
+    public class Resource
+    {
+        public Resource(string name, DataAccessMode dataAccessMode)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            DataAccessMode = dataAccessMode;
+        }
+
+        public string Name { get; }
+        public DataAccessMode DataAccessMode { get; }
+        
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + DataAccessMode.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Resource)
+            {
+                return this.Name == ((Resource) obj).Name && this.DataAccessMode == ((Resource) obj).DataAccessMode;
+            }
+
+            return false;
+        }
     }
 }
