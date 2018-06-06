c81971c2b7bb117c68c785d4d88c54237553481f
gldeng <realgldeng@gmail.com>
2018-06-06 19:33:35 +0800

    Fix JobExecutor

---
+++ AElf.Kernel/SmartContract/TransactionTrace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Protobuf.Collections;
+
+namespace AElf.Kernel
+{
+    public partial class TransactionTrace
+    {
+        public RepeatedField<LogEvent> FlattenedLogs
+        {
+            get
+            {
+                var o = Logs.Clone();
+                foreach (var t in InlineTraces)
+                {
+                    o.AddRange(t.FlattenedLogs);
+                }
+                return o;
+            }
+        }
+    }
+}
