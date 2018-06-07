ebd1b1634b318b4c7a98d08cdb110ed7fab826ad
EanCuznaivy <1036682639@qq.com>
2018-06-08 02:47:52 +0800

    Basically finished the initialization of contracts of dpos

--- AElf.Contracts.DPoS/Process.cs
+++ AElf.Contracts.DPoS/Process.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AElf.Kernel;
+using AElf.Kernel.Extensions;
 using AElf.Sdk.CSharp;
 using AElf.Sdk.CSharp.Types;
+using Akka.Util.Internal;
 using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using ServiceStack;
@@ -25,30 +26,82 @@ namespace AElf.Contracts.DPoS
         public Map Signatures = new Map("Signatures");
         
         public Map RoundsCount = new Map("RoundsCount");
+        
+        public Map Ins = new Map("Ins");
+        
+        public Map Outs = new Map("Outs");
 
         public async Task<object> RandomizeOrderForFirstTwoRounds()
         {
-            var foo = GetTime();
-            var length = foo.Length;
-            var bar = new int[foo.Length / 2];
-            for (var i = 0; i < length; i++)
+            var miningNodes = (MiningNodes) await GetMiningNodes();
+            var dict = new Dictionary<Hash, int>();
+            
+            // First round
+            foreach (var node in miningNodes.Nodes)
             {
-                bar[i] = foo[i] + foo[length - i - 1] + new Random(17).Next(5, 99);
+                dict.Add(node, new Random(GetTime().LastOrDefault()).Next(0, 1000));
+            }
+
+            var sortedMiningNodes =
+                from obj in dict
+                orderby obj.Value
+                select obj.Key;
+
+            var enumerable = sortedMiningNodes.ToList();
+            for (var i = 0; i < enumerable.Count; i++)
+            {
+                Hash key = enumerable[i].CalculateHashWith(new Hash((new UInt64Value {Value = 1}).CalculateHash()));
+                await TimeSlots.SetValueAsync(key, GetTime(i * 4 + 4));
             }
             
-            throw new NotImplementedException();
+            // Second roud
+            foreach (var node in miningNodes.Nodes)
+            {
+                dict[node] = new Random(GetTime().LastOrDefault()).Next(0, 1000);
+            }
+            
+            sortedMiningNodes =
+                from obj in dict
+                orderby obj.Value
+                select obj.Key;
+            
+            enumerable = sortedMiningNodes.ToList();
+            for (var i = 0; i < enumerable.Count; i++)
+            {
+                Hash key = enumerable[i].CalculateHashWith(new Hash((new UInt64Value {Value = 1}).CalculateHash()));
+                await TimeSlots.SetValueAsync(key, GetTime(i * 4 + miningNodes.Nodes.Count * 4 + 8));
+            }
+
+            return null;
+        }
+
+        public async Task<object> RandomizeSignaturesForFirstRound()
+        {
+            Hash roundsCountHash = ((UInt64Value) await GetRoundsCount()).CalculateHash();
+            var miningNodes = ((MiningNodes) await GetMiningNodes()).Nodes.ToList();
+            var miningNodesCount = miningNodes.Count;
+
+            for (var i = 0; i < miningNodesCount; i++)
+            {
+                Hash key = miningNodes[i].CalculateHashWith(roundsCountHash);
+                await Signatures.SetValueAsync(key, Hash.Generate().ToByteArray());
+            }
+
+            return null;
         }
 
         public async Task<object> GetTimeSlot(Hash accountHash)
         {
-            var timeSlot = await TimeSlots.GetValue(accountHash);
+            var roundsCount = (UInt64Value) await GetRoundsCount();
+            var key = accountHash.CalculateHashWith((Hash) roundsCount.CalculateHash());
+            var timeSlot = await TimeSlots.GetValue(key);
 
             Api.Return(new BytesValue {Value = ByteString.CopyFrom(timeSlot)});
             
             return timeSlot;
         }
 
-        public async Task<object> CanMining(Hash accountHash)
+        public async Task<object> AbleToMine(Hash accountHash)
         {
             var assignedTimeSlot = (byte[]) await GetTimeSlot(accountHash);
             var timeSlotEnd = DateTime
@@ -66,11 +119,31 @@ namespace AElf.Contracts.DPoS
         
         public async Task<object> CalculateSignature(Hash accountHash)
         {
-            throw new NotImplementedException();
+            // Get signatures of last round.
+            var currentRoundCount = (UInt64Value) await GetRoundsCount();
+            var lastRoundCount = new UInt64Value {Value = currentRoundCount.Value - 1};
+            Hash roundCountHash = lastRoundCount.CalculateHash();
+
+            var add = Hash.Zero;
+            var miningNodes = (MiningNodes) await GetMiningNodes();
+            foreach (var node in miningNodes.Nodes)
+            {
+                Hash key = node.CalculateHashWith(roundCountHash);
+                Hash lastSignature = await Signatures.GetValue(key);
+                add = add.CalculateHashWith(lastSignature);
+            }
+
+            var inValue = (Hash) await Ins.GetValue(accountHash);
+            Hash signature = inValue.CalculateHashWith(add);
+            
+            Api.Return(signature);
+
+            return signature;
         }
         
         public async Task<object> GetMiningNodes()
         {
+            // Should be set before
             var miningNodes = AElf.Kernel.MiningNodes.Parser.ParseFrom(
                 await MiningNodes.GetValue(Hash.Zero));
 
@@ -82,6 +155,51 @@ namespace AElf.Contracts.DPoS
             Api.Return(miningNodes);
 
             return miningNodes;
+        }
+
+        public async Task<object> SetRoundsCount(ulong count)
+        {
+            await RoundsCount.SetValueAsync(Hash.Zero, new UInt64Value {Value = count}.ToByteArray());
+
+            return null;
+        }
+
+        public async Task<object> GetRoundsCount()
+        {
+            var count = UInt64Value.Parser.ParseFrom(await RoundsCount.GetValue(Hash.Zero));
+            
+            Api.Return(count);
+            
+            return count;
+        }
+
+        public async Task<object> GenerateExtraBlockTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<object> SetOutValue(Hash accountHash, Hash outValue)
+        {
+            var roundsCount = (UInt64Value) await GetRoundsCount();
+            Hash key = accountHash.CalculateHashWith(roundsCount);
+            await Outs.SetValueAsync(key, outValue.ToByteArray());
+
+            return null;
+        }
+
+        public async Task<object> PublishInValue(Hash accountHash, Hash inValue)
+        {
+            var roundsCount = (UInt64Value) await GetRoundsCount();
+            var key = accountHash.CalculateHashWith((Hash) roundsCount.CalculateHash());
+            var timeSlot = await TimeSlots.GetValue(key);
+
+            if (CompareBytes(GetTime(-4), timeSlot))
+            {
+                await Ins.SetValueAsync(key, inValue.ToByteArray());
+                return true;
+            }
+
+            return false;
         }
         
         public override async Task InvokeAsync()
@@ -99,6 +217,11 @@ namespace AElf.Contracts.DPoS
         private byte[] GetTime()
         {
             return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffffff").ToUtf8Bytes();
+        }
+
+        private byte[] GetTime(int offset)
+        {
+            return DateTime.Now.AddMinutes(offset).ToString("yyyy-MM-dd HH:mm:ss.ffffff").ToUtf8Bytes();
         }
 
         private bool CompareBytes(byte[] bytes1, byte[] bytes2)
