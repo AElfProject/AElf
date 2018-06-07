43941f9b067b6f819ea77dcb41955bff94a13129
Dennis-Furrer <dennis.furrer@ibm.com>
2018-06-07 20:53:31 +0100

    CLI: Removed unused imports

--- AElf.CLI/Program.cs
+++ AElf.CLI/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 
 namespace AElf.CLI
 {
