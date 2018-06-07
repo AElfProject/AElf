13a2eb83c26e835bbb2e63ea809f06cef09faf63
swk <shiwk@tju.edu.cn>
2018-06-07 15:28:16 +0800

    add Miner module with autofac

---
+++ AElf.Kernel.Modules.AutofacModule/MinerModule.cs
@@ -0,0 +1,31 @@
+﻿using AElf.Kernel.Miner;
+using Autofac;
+
+namespace AElf.Kernel.Modules.AutofacModule
+{
+    public class MinerModule : Module
+    {
+        public IMinerConfig MinerConf { get; }
+
+        public MinerModule(IMinerConfig minerConfig)
+        {
+            MinerConf = minerConfig;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            if (MinerConf != null)
+            {
+                builder.RegisterInstance(MinerConf).As<IMinerConfig>();
+            }
+            else
+            {
+                builder.RegisterInstance(MinerConfig.Default).As<IMinerConfig>();
+            }
+
+            builder.RegisterType(typeof(Miner.Miner)).As<IMiner>();
+        }
+        
+
+    }
+}    
