0a484598c297c32922d514e1c98b06650917efbf
samfrb <samos2x@outlook.com>
2018-06-08 11:54:41 +0800

    Regions min the AElf options

--- AElf.Launcher/AElfOptions.cs
+++ AElf.Launcher/AElfOptions.cs
@@ -5,6 +5,8 @@ namespace AElf.Launcher
 {
     public class AElfOptions
     {
+        #region Network and RPC
+
         [Option('b', HelpText = "Replaces the bootnode list.")]
         public IEnumerable<string> Bootnodes { get; set; }
         
@@ -19,17 +21,23 @@ namespace AElf.Launcher
         
         [Option(Default = false, HelpText = "Starts the node without exposing the RPC interface")]
         public bool NoRpc { get; set; }
-
-        [Option('t',HelpText = "The type of database.")]
-        public string DBType { get; set; }
-
-        [Option('h',HelpText = "The IP address of database.")]
-        public string DBHost { get; set; }
-
-        [Option('p',HelpText = "The port of database.")]
-        public int? DBPort { get; set; }
-
+        
         [Option(HelpText = "The absolute path where to store the peer database.")]
         public string PeersDbPath { get; set; }
+
+        #endregion
+        
+        #region Database
+        
+        [Option('t', HelpText = "The type of database.")]
+        public string DBType { get; set; }
+
+        [Option('h', HelpText = "The IP address of database.")]
+        public string DBHost { get; set; }
+
+        [Option('p', HelpText = "The port of database.")]
+        public int? DBPort { get; set; }
+        
+        #endregion
     }
 }
