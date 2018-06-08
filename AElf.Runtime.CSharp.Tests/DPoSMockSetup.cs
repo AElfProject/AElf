ab68bdceb4628788e74f272cef3df8a55d0e3779
EanCuznaivy <1036682639@qq.com>
2018-06-08 23:01:58 +0800

    initial a test case about randomization of orders of first two rounds

--- AElf.Runtime.CSharp.Tests/DPoSMockSetup.cs
+++ AElf.Runtime.CSharp.Tests/DPoSMockSetup.cs
@@ -1,20 +1,64 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 using AElf.Kernel;
+using AElf.Kernel.KernelAccount;
+using AElf.Kernel.Managers;
 using AElf.Kernel.Services;
+using AElf.Kernel.Storages;
 using Google.Protobuf;
 using ServiceStack;
-using Path = System.IO.Path;
+using Path = AElf.Kernel.Path;
 
 namespace AElf.Runtime.CSharp.Tests
 {
     // ReSharper disable once InconsistentNaming
     public class DPoSMockSetup
     {
+        public Hash ChainId { get; } = Hash.Generate();
+
         public ISmartContractService SmartContractService;
 
         // ReSharper disable once InconsistentNaming
         public Hash DPoSContractAddress { get; } = Hash.Generate();
+        
+        private readonly ISmartContractManager _smartContractManager;
+        private readonly IWorldStateManager _worldStateManager;
+        
+        private readonly ISmartContractRunnerFactory _smartContractRunnerFactory 
+            = new SmartContractRunnerFactory();
+
+        public IAccountDataProvider AccountDataProviderOfAccountZero;
+
+        public DPoSMockSetup(ISmartContractStore smartContractStore, IWorldStateManager worldStateManager, 
+            IChainCreationService chainCreationService, IBlockManager blockManager)
+        {
+            _smartContractManager = new SmartContractManager(smartContractStore);
+            _worldStateManager = worldStateManager;
+            _chainCreationService = chainCreationService;
+            _blockManager = blockManager;
+            
+            var runner = new SmartContractRunner("../../../../AElf.Contracts.DPoS/bin/Debug/netstandard2.0/");
+            _smartContractRunnerFactory.AddRunner(0, runner);
+            
+            Task.Factory.StartNew(async () =>
+            {
+                await Init();
+            }).Unwrap().Wait();
+            
+            SmartContractService = new SmartContractService(_smartContractManager,
+                _smartContractRunnerFactory, _worldStateManager);
+
+            Task.Factory.StartNew(async () =>
+            {
+                await DeployDPoSContracts();
+            }).Unwrap().Wait();
+        }
+
+        private async Task Init()
+        {
+            AccountDataProviderOfAccountZero = (await _worldStateManager.OfChain(ChainId))
+                .GetAccountDataProvider(Path.CalculatePointerForAccountZero(ChainId));
+        }
 
         // ReSharper disable once InconsistentNaming
         public byte[] DPoSContractCode
@@ -23,7 +67,7 @@ namespace AElf.Runtime.CSharp.Tests
             {
                 byte[] code;
                 using (var file = File.OpenRead(
-                    Path.GetFullPath("../../../../AElf.Contracts.DPoS/bin/Debug/netstandard2.0/AElf.Contracts.DPoS.dll")))
+                    System.IO.Path.GetFullPath("../../../../AElf.Contracts.DPoS/bin/Debug/netstandard2.0/AElf.Contracts.DPoS.dll")))
                 {
                     code = file.ReadFully();
                 }
--- AElf.Runtime.CSharp.Tests/DPoSTest.cs
+++ AElf.Runtime.CSharp.Tests/DPoSTest.cs
@@ -1,10 +1,13 @@
 ﻿using System.Threading.Tasks;
+using AElf.Kernel;
 using AElf.Kernel.Services;
+using Google.Protobuf;
 using Xunit.Frameworks.Autofac;
 
 namespace AElf.Runtime.CSharp.Tests
 {
     [UseAutofacTestFramework]
+    // ReSharper disable once InconsistentNaming
     public class DPoSTest
     {
         private MockSetup _mock;
@@ -17,7 +20,29 @@ namespace AElf.Runtime.CSharp.Tests
 
         public async Task FirstTwoRoundsOrderTest()
         {
+            var executive = await _service.GetExecutiveAsync(Hash.Zero, _mock.ChainId1);
+            executive.SetSmartContractContext(new SmartContractContext()
+            {
+                ChainId = Hash.Zero,
+                ContractAddress = Hash.Zero,
+                DataProvider = _mock.DataProvider1.GetDataProvider(),
+                SmartContractService = _service
+            });
             
+            var randomizeOrder = new Transaction
+            {
+                From = Hash.Zero,
+                To = Hash.Zero,
+                MethodName = "RandomizeOrderForFirstTwoRounds",
+                Params = ByteString.CopyFrom(
+                    new Parameters().ToByteArray()
+                )
+            };
+            
+            await executive.SetTransactionContext(new TransactionContext
+            {
+                Transaction = randomizeOrder
+            }).Apply();
         }
     }
 }
