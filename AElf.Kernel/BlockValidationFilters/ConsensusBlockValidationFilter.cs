1c4439a1ffddfa03402797d6d7546487b524a46e
swk <shiwk@tju.edu.cn>
2018-06-09 20:51:38 +0800

    consensus block validation Filter

--- AElf.Kernel/BlockValidationFilters/ConsensusBlockValidationFilter.cs
+++ AElf.Kernel/BlockValidationFilters/ConsensusBlockValidationFilter.cs
@@ -1,13 +1,49 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AElf.Cryptography.ECDSA;
+using AElf.Kernel.Extensions;
+using AElf.Kernel.Node.Config;
 using AElf.Kernel.Services;
+using Google.Protobuf;
 
 namespace AElf.Kernel.BlockValidationFilters
 {
     public class ConsensusBlockValidationFilter: IBlockValidationFilter
     {
-        public Task<bool> ValidateBlockAsync(Block block, IChainContext context)
+        public async Task<bool> ValidateBlockAsync(IBlock block, IChainContext context)
         {
-            return Task.FromResult(true);
+            // block height
+            if (block.Header.Index != context.BlockHeight)
+                return false;
+
+            // previous block hash
+            if (!block.Header.PreviousBlockHash.Equals(context.BlockHash))
+                return false;
+            
+            // block signature
+            var pubkey = block.Header.P.ToBase64();
+            if (!MinersInfo.Instance.Producers.TryGetValue(pubkey, out var dict))
+            {
+                return false;
+            }
+            
+            byte[] uncompressedPrivKey = block.Header.P.ToByteArray();
+            Hash addr = uncompressedPrivKey.CalculateHash().Take(ECKeyPair.AddressLength).ToArray();
+
+            if (!addr.Equals(new Hash(ByteString.FromBase64(dict["coinbase"]))))
+                return false;
+            ECKeyPair recipientKeyPair = ECKeyPair.FromPublicKey(uncompressedPrivKey);
+            ECVerifier verifier = new ECVerifier(recipientKeyPair);
+            if (!verifier.Verify(block.Header.GetSignature(), block.Header.GetHash().GetHashBytes()))
+                return false;
+            
+            // todo: verify the identity of producer
+            var timestamp = block.Header.Time;
+            
+            var h = context.BlockHeight;
+
+            return true;
+
         }
     }
 }
