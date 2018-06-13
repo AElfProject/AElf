fa079bd0dc67961fa8fca2aa68073fcaaf25cb1f
PadawanZH <zhangan19940520@gmail.com>
2018-06-13 18:39:29 +0800

    Implement resource detection service

---
+++ AElf.Kernel/Concurrency/ResourceUsageDetectionService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AElf.Kernel.Concurrency.Metadata;
+
+namespace AElf.Kernel.Concurrency.Scheduling
+{
+    public class ResourceUsageDetectionService : IResourceUsageDetectionService
+    {
+        private readonly IChainFunctionMetadataService _chainFunctionMetadataService;
+
+        public ResourceUsageDetectionService(IChainFunctionMetadataService chainFunctionMetadataService)
+        {
+            _chainFunctionMetadataService = chainFunctionMetadataService;
+        }
+
+        public IEnumerable<string> GetResources(ITransaction transaction)
+        {
+            var addrs = Parameters.Parser.ParseFrom(transaction.Params).Params.Select(p => p.HashVal).Where(y => y != null).Select(a => a.Value.ToBase64()).ToList();
+
+            var results = new List<string>();
+            var resourceList = _chainFunctionMetadataService.GetFunctionMetadata(GetFunctionName(transaction)).FullResourceSet;
+            foreach (var resource in resourceList)
+            {
+                switch (resource.DataAccessMode)
+                {
+                    case DataAccessMode.AccountSpecific:
+                        foreach (var addr in addrs)
+                        {
+                            results.Add(resource.Name + "." + addr);
+                        }
+                        break;
+                    
+                    case DataAccessMode.ReadWriteAccountSharing:
+                        results.Add(resource.Name);
+                        break;
+                }
+            }
+
+            return results;
+        }
+
+        private string GetFunctionName(ITransaction tx)
+        {
+            return tx.To.Value.ToBase64() + "." + tx.MethodName;
+        }
+    }
+}
