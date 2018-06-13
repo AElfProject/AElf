851bee68ef8a5bbb7a35f9b7be4494dfa319c19e
gldeng <realgldeng@gmail.com>
2018-06-13 17:03:48 +0800

    Add a project for type specific operations (e.g. conversions and handlers)

---
+++ AElf.Types.CSharp/ConversionExtension.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Protobuf;
+using Google.Protobuf.Reflection;
+using Google.Protobuf.WellKnownTypes;
+using AElf.Kernel;
+
+namespace AElf.Types.CSharp
+{
+    public static class ConversionExtension
+    {
+        #region bool
+        public static IMessage ToPbMessage(this bool value)
+        {
+            return new BoolValue() { Value = value };
+        }
+
+        public static Any ToAny(this bool value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static bool AnyToBool(this Any any)
+        {
+            return any.Unpack<BoolValue>().Value;
+        }
+        #endregion bool
+        #region int
+        public static IMessage ToPbMessage(this int value)
+        {
+            return new Int32Value() { Value = value };
+        }
+
+        public static Any ToAny(this int value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static int AnyToInt32(this Any any)
+        {
+            return any.Unpack<Int32Value>().Value;
+        }
+        #endregion int
+        #region uint
+        public static IMessage ToPbMessage(this uint value)
+        {
+            return new UInt32Value() { Value = value };
+        }
+
+        public static Any ToAny(this uint value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static uint AnyToUInt32(this Any any)
+        {
+            return any.Unpack<UInt32Value>().Value;
+        }
+        #endregion uint
+        #region long
+        public static IMessage ToPbMessage(this long value)
+        {
+            return new Int64Value() { Value = value };
+        }
+
+        public static Any ToAny(this long value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static long AnyToInt64(this Any any)
+        {
+            return any.Unpack<Int64Value>().Value;
+        }
+        #endregion long
+        #region ulong
+        public static IMessage ToPbMessage(this ulong value)
+        {
+            return new UInt64Value() { Value = value };
+        }
+
+        public static Any ToAny(this ulong value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static ulong AnyToUInt64(this Any any)
+        {
+            return any.Unpack<UInt64Value>().Value;
+        }
+        #endregion ulong
+        #region string
+        public static IMessage ToPbMessage(this string value)
+        {
+            return new StringValue() { Value = value };
+        }
+
+        public static Any ToAny(this string value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static string AnyToString(this Any any)
+        {
+            return any.Unpack<StringValue>().Value;
+        }
+        #endregion string
+        #region byte[]
+        public static IMessage ToPbMessage(this byte[] value)
+        {
+            return new BytesValue() { Value = ByteString.CopyFrom(value) };
+        }
+
+        public static Any ToAny(this byte[] value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static byte[] AnyToBytes(this Any any)
+        {
+            return any.Unpack<BytesValue>().Value.ToByteArray();
+        }
+        #endregion byte[]
+        #region IMessage
+        public static IMessage ToPbMessage(this IMessage value)
+        {
+            return value;
+        }
+
+        public static Any ToAny(this IMessage value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static IMessage AnyToPbMessage(this Any any, System.Type type)
+        {
+            if (!type.IsPbMessageType())
+            {
+                throw new Exception("Type given is not an IMessage.");
+            }
+            var target = (IMessage)Activator.CreateInstance(type);
+            if (Any.GetTypeName(any.TypeUrl) != target.Descriptor.FullName)
+            {
+                throw new Exception(
+                    $"Full type name for {target.Descriptor.Name} is {target.Descriptor.FullName}; Any message's type url is {any.TypeUrl}");
+            }
+            target.MergeFrom(any.Value);
+            return target;
+        }
+        #endregion IMessage
+        #region UserType
+        public static IMessage ToPbMessage(this UserType value)
+        {
+            return value.Pack();
+        }
+
+        public static Any ToAny(this UserType value)
+        {
+            return Any.Pack(value.ToPbMessage());
+        }
+
+        public static UserType AnyToUserType(this Any any, System.Type type)
+        {
+            if (!type.IsUserType())
+                throw new Exception("Type given is not a UserType.");
+            var obj = (UserType)Activator.CreateInstance(type);
+            obj.Unpack(any.Unpack<UserTypeHolder>());
+            return obj;
+        }
+        #endregion UserType
+    }
+}
---
+++ AElf.Types.CSharp/ParamsHolder.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using Google.Protobuf;
+
+namespace AElf.Types.CSharp
+{
+    public partial class ParamsHolder
+    {
+        /// <summary>
+        /// Pack plain CLR type data into Protobuf messages.
+        /// </summary>
+        /// <returns>Packed <see cref="ParamsHolder"/> message.</returns>
+        /// <param name="objs">Objects of plain CLR types.</param>
+        public static ParamsHolder Pack(params object[] objs)
+        {
+            if (objs.Length == 0)
+                return new ParamsHolder();
+            // Put plain clr data in Pb types.
+            if (!objs.All(o => o.GetType().IsAllowedType()))
+            {
+                throw new Exception("Contains invalid type.");
+            }
+            var ph = new ParamsHolder();
+            foreach (var o in objs)
+            {
+                if (o is bool)
+                {
+                    ph.Params.Add(((bool)o).ToAny());
+                }
+                else if (o is int)
+                {
+                    ph.Params.Add(((int)o).ToAny());
+                }
+                else if (o is uint)
+                {
+                    ph.Params.Add(((uint)o).ToAny());
+                }
+                else if (o is long)
+                {
+                    ph.Params.Add(((long)o).ToAny());
+                }
+                else if (o is ulong)
+                {
+                    ph.Params.Add(((ulong)o).ToAny());
+                }
+                else if (o is string)
+                {
+                    ph.Params.Add(((string)o).ToAny());
+                }
+                else if (o is byte[])
+                {
+                    ph.Params.Add(((byte[])o).ToAny());
+                }
+                else if (o is IMessage)
+                {
+                    ph.Params.Add(((IMessage)o).ToAny());
+                }
+                else if (o is UserType)
+                {
+                    ph.Params.Add(((UserType)o).ToAny());
+                }
+            }
+            return ph;
+        }
+
+        public object[] Unpack(params Type[] types)
+        {
+            if (types.Length != Params.Count)
+            {
+                throw new Exception("Length doesn't match.");
+            }
+            if (types.Length == 0)
+                return new object[0];
+            if (!types.All(t => t.IsAllowedType()))
+            {
+                throw new Exception("Contains invalid type.");
+            }
+            object[] objs = new object[types.Length];
+            for (int i = 0; i < types.Length; i++)
+            {
+                if (types[i] == typeof(bool))
+                {
+                    objs[i] = Params[i].AnyToBool();
+                }
+                else if (types[i] == typeof(int))
+                {
+                    objs[i] = Params[i].AnyToInt32();
+                }
+                else if (types[i] == typeof(uint))
+                {
+                    objs[i] = Params[i].AnyToUInt32();
+                }
+                else if (types[i] == typeof(long))
+                {
+                    objs[i] = Params[i].AnyToInt64();
+                }
+                else if (types[i] == typeof(ulong))
+                {
+                    objs[i] = Params[i].AnyToUInt64();
+                }
+                else if (types[i] == typeof(string))
+                {
+                    objs[i] = Params[i].AnyToString();
+                }
+                else if (types[i] == typeof(byte[]))
+                {
+                    objs[i] = Params[i].AnyToBytes();
+                }
+                else if (types[i].IsPbMessageType())
+                {
+                    objs[i] = Params[i].AnyToPbMessage(types[i]);
+                }
+                else if (types[i].IsUserType())
+                {
+                    objs[i] = Params[i].AnyToUserType(types[i]);
+                }
+            }
+            return objs;
+        }
+    }
+}
