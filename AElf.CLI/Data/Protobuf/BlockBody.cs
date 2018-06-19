f449afc7967d0f8cddcbb14c4bf1dc8c71b9c968
Dennis-Furrer <dennis.furrer@ibm.com>
2018-06-19 12:18:17 +0100

    CLI: Protobuf type added for block

---
+++ AElf.CLI/Data/Protobuf/BlockBody.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Google.Protobuf.WellKnownTypes;
+using ProtoBuf;
+
+namespace AElf.CLI.Data.Protobuf
+{
+    [ProtoContract]
+    public class BlockBody
+    {
+        [ProtoMember(1)]
+        public Hash BlockHeader { get; set; }
+        
+        [ProtoMember(2)]
+        public List<Hash> Transactions { get; set; }
+    }
+}
---
+++ AElf.CLI/Data/Protobuf/BlockHeader.cs
@@ -0,0 +1,39 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using ProtoBuf;
+
+namespace AElf.CLI.Data.Protobuf
+{
+    [ProtoContract]
+    public class BlockHeader
+    {
+        [ProtoMember(1)]
+        public int Version { get; set; }
+        
+        [ProtoMember(2)]
+        public Hash PreviousBlockHash { get; set; }
+        
+        [ProtoMember(3)]
+        public Hash MerkleTreeRootOfTransactions { get; set; }
+        
+        [ProtoMember(4)]
+        public Hash MerkleTreeRootOfWorldState { get; set; }
+        
+        [ProtoMember(5)]
+        public ulong Index { get; set; }
+        
+        [ProtoMember(6)]
+        public byte[] R { get; set; }
+        
+        [ProtoMember(7)]
+        public byte[] S { get; set; }
+        
+        [ProtoMember(8)]
+        public byte[] P { get; set; }
+        
+        [ProtoMember(9)]
+        public Timestamp Time { get; set; }
+        
+        [ProtoMember(10)]
+        public Hash ChainId { get; set; }
+    }
+}
