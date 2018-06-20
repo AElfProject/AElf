483f314dbd0a092c0ca7d61e6eefcc468009bbfd
swk <shiwk@tju.edu.cn>
2018-06-20 22:31:03 +0800

    add block generation in miner

--- AElf.Kernel/Miner/Miner.cs
+++ AElf.Kernel/Miner/Miner.cs
@@ -1,23 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using AElf.Common.Application;
-using AElf.Cryptography;
+using AElf.Common.Attributes;
 using AElf.Cryptography.ECDSA;
 using AElf.Kernel.Managers;
-using AElf.Kernel.Services;
 using AElf.Kernel.TxMemPool;
 using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
 using ReaderWriterLock = AElf.Common.Synchronisation.ReaderWriterLock;
 
 namespace AElf.Kernel.Miner
 {
+    [LoggerName("Node")]
     public class Miner : IMiner
     {
-        private readonly IBlockGenerationService _blockGenerationService;
         private readonly ITxPoolService _txPoolService;
         private readonly IParallelTransactionExecutingService _parallelTransactionExecutingService;
         private ECKeyPair _keyPair;
+        private readonly IChainManager _chainManager;
+        private readonly IBlockManager _blockManager;
+        private readonly IWorldStateManager _worldStateManager;
+
         private readonly Dictionary<ulong, IBlock> waiting = new Dictionary<ulong, IBlock>();
 
         private MinerLock Lock { get; } = new MinerLock();
@@ -37,45 +41,154 @@ namespace AElf.Kernel.Miner
 
         public Hash Coinbase => Config.CoinBase;
 
-        public Miner(IBlockGenerationService blockGenerationService, IMinerConfig config, 
-            ITxPoolService txPoolService, IParallelTransactionExecutingService parallelTransactionExecutingService)
+        public Miner(IMinerConfig config, ITxPoolService txPoolService, 
+            IParallelTransactionExecutingService parallelTransactionExecutingService, 
+                IChainManager chainManager, IBlockManager blockManager, IWorldStateManager worldStateManager)
         {
-            _blockGenerationService = blockGenerationService;
             Config = config;
             _txPoolService = txPoolService;
             _parallelTransactionExecutingService = parallelTransactionExecutingService;
+            _chainManager = chainManager;
+            _blockManager = blockManager;
+            _worldStateManager = worldStateManager;
         }
 
         
         public async Task<IBlock> Mine()
         {
-            if (Cts == null || Cts.IsCancellationRequested)
-                return null;
+            try
+            {
+                if (Cts == null || Cts.IsCancellationRequested)
+                    return null;
             
-            var ready = await _txPoolService.GetReadyTxsAsync(Config.TxCount);
-            // TODO：dispatch txs with ISParallel, return list of tx results
-            
-            var results =  await _parallelTransactionExecutingService.ExecuteAsync(ready, Config.ChainId);
-            
-            // reset Promotable and update account context
-            
-            
-            // TODO: commit tx results
-            
-            // generate block
-            var block = await _blockGenerationService.GenerateBlockAsync(Config.ChainId, results);
-            
-            await _txPoolService.ResetAndUpdate(results);
-            // sign block
-            ECSigner signer = new ECSigner();
-            ECSignature signature = signer.Sign(_keyPair, block.GetHash().GetHashBytes());
+                var ready = await _txPoolService.GetReadyTxsAsync(Config.TxCount);
+                // TODO：dispatch txs with ISParallel, return list of tx results
 
-            block.Header.P = ByteString.CopyFrom(_keyPair.PublicKey.Q.GetEncoded());
-            block.Header.R = ByteString.CopyFrom(signature.R);
-            block.Header.S = ByteString.CopyFrom(signature.S);
+                List<TransactionResult> results;
+                if (Config.IsParallel)
+                {
+                    results = ready.Count == 0
+                        ? new List<TransactionResult>()
+                        : await _parallelTransactionExecutingService.ExecuteAsync(ready, Config.ChainId);
+                }
+                else
+                {
+                    foreach (var transaction in ready)
+                    {
+                        
+                    }
+                }
+            
+                // reset Promotable and update account context
+            
+            
+                // TODO: commit tx results
+            
+                // generate block
+                var block = await GenerateBlockAsync(Config.ChainId, results);
+            
+                await _txPoolService.ResetAndUpdate(results);
+                // sign block
+                ECSigner signer = new ECSigner();
+                var hash = block.GetHash();
+                var bytes = hash.GetHashBytes();
+                ECSignature signature = signer.Sign(_keyPair, bytes);
+
+                block.Header.P = ByteString.CopyFrom(_keyPair.PublicKey.Q.GetEncoded());
+                block.Header.R = ByteString.CopyFrom(signature.R);
+                block.Header.S = ByteString.CopyFrom(signature.S);
+            
+                // append block
+                await _blockManager.AddBlockAsync(block);
+                await _chainManager.AppendBlockToChainAsync(block);
+            
+            
+                return block;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        
+        
+        
+        /// <summary>
+        /// generate block
+        /// </summary>
+        /// <param name="chainId"></param>
+        /// <param name="results"></param>
+        /// <returns></returns>
+        public async Task<IBlock> GenerateBlockAsync(Hash chainId, IEnumerable<TransactionResult> results)
+        {
+            
+            var lastBlockHash = await _chainManager.GetChainLastBlockHash(chainId);
+            var index = await _chainManager.GetChainCurrentHeight(chainId);
+            var block = new Block(lastBlockHash);
+            block.Header.Index = index;
+            block.Header.ChainId = chainId;
+
+            // add tx hash
+            foreach (var r in results)
+            {
+                block.AddTransaction(r.TransactionId);
+            }
+        
+            // calculate and set tx merkle tree root
+            block.FillTxsMerkleTreeRootInHeader();
+            
+            
+            // set ws merkle tree root
+            await _worldStateManager.OfChain(chainId);
+            
+            await _worldStateManager.SetWorldStateAsync(lastBlockHash);
+            var ws = await _worldStateManager.GetWorldStateAsync(lastBlockHash);
+            block.Header.Time = Timestamp.FromDateTime(DateTime.UtcNow);
+            
+            
+
+            if(ws != null)
+                block.Header.MerkleTreeRootOfWorldState = await ws.GetWorldStateMerkleTreeRootAsync();
+            block.Body.BlockHeader = block.Header.GetHash();
+
             
             return block;
         }
+        
+        
+        /// <summary>
+        /// generate block header
+        /// </summary>
+        /// <param name="chainId"></param>
+        /// <param name="merkleTreeRootForTransaction"></param>
+        /// <returns></returns>
+        public async Task<IBlockHeader> GenerateBlockHeaderAsync(Hash chainId, Hash merkleTreeRootForTransaction)
+        {
+            // get ws merkle tree root
+            var lastBlockHash = await _chainManager.GetChainLastBlockHash(chainId);
+            var index = await _chainManager.GetChainCurrentHeight(chainId);
+            var block = new Block(lastBlockHash);
+            block.Header.Index = index + 1;
+            block.Header.ChainId = chainId;
+            
+            
+            await _worldStateManager.OfChain(chainId);
+            var ws = await _worldStateManager.GetWorldStateAsync(lastBlockHash);
+            var state = await ws.GetWorldStateMerkleTreeRootAsync();
+            
+            var header = new BlockHeader
+            {
+                Version = 0,
+                PreviousBlockHash = lastBlockHash,
+                MerkleTreeRootOfWorldState = state,
+                MerkleTreeRootOfTransactions = merkleTreeRootForTransaction
+            };
+
+            return header;
+
+        }
+        
 
         
         /// <summary>
