0e106f671d48daa3b6a08320ff8725e1539b24b1
PadawanZH <zhangan19940520@gmail.com>
2018-06-21 19:53:00 +0800

    Delete unsafe code in contract

--- AElf.Kernel.Tests.TestContractZero/TestContractZero.cs
+++ AElf.Kernel.Tests.TestContractZero/TestContractZero.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Loader;
 using System.Threading.Tasks;
 using AElf.Kernel;
 using AElf.Kernel.Concurrency.Metadata;
@@ -24,24 +22,6 @@ namespace AElf.Kernel.Tests
         public async Task<Hash> DeploySmartContract(SmartContractRegistration registration)
         {
             var tx = Api.GetTransaction();
-
-            var code = registration.ContractBytes.ToByteArray();
-            Assembly assembly = null;
-            using (Stream stream = new MemoryStream(code))
-            {
-                assembly = AssemblyLoadContext.Default.LoadFromStream(stream);
-            }
-
-            if (assembly == null)
-            {
-                Console.WriteLine("Invalid code");
-                throw new InvalidDataException("Invalid binary code.");
-            }
-            else
-            {
-                Console.WriteLine(assembly.GetType().Name);
-            }
-            
             
             // calculate new account address
             var account = Path.CalculateAccountAddress(tx.From, tx.IncrementId);
