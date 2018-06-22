ca4054fdd25408888fb12f31fa4a91f0a346a3e4
swk <shiwk@tju.edu.cn>
2018-06-22 20:43:00 +0800

    modify Deployment interface

--- AElf.Kernel.Tests.TestContractZero/TestContractZero.cs
+++ AElf.Kernel.Tests.TestContractZero/TestContractZero.cs
@@ -5,6 +5,7 @@ using AElf.Kernel;
 using AElf.Kernel.Extensions;
 using AElf.Kernel.KernelAccount;
 using AElf.Sdk.CSharp;
+using Google.Protobuf;
 using CSharpSmartContract = AElf.Sdk.CSharp.CSharpSmartContract;
 
 namespace AElf.Kernel.Tests
@@ -16,14 +17,22 @@ namespace AElf.Kernel.Tests
             await Task.CompletedTask;
         }
 
-        public async Task<Hash> DeploySmartContract(SmartContractRegistration registration)
+        public async Task<Hash> DeploySmartContract(int category, byte[] contract)
         {
+            SmartContractRegistration registration = new SmartContractRegistration
+            {
+                Category = category,
+                ContractBytes = ByteString.CopyFrom(contract),
+                ContractHash = contract.CalculateHash() // maybe no usage  
+            };
+            
             var tx = Api.GetTransaction();
 
             // calculate new account address
             var account = Path.CalculateAccountAddress(tx.From, tx.IncrementId);
 
             await Api.DeployContractAsync(account, registration);
+            Console.WriteLine("Deployment success");
             return account;
         }
 
