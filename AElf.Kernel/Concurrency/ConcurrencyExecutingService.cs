6e50a0e13f042f06bc6e21d94f20322b26d528da
zhxymh <13552710076@126.com>
2018-06-23 17:00:53 +0800

    add service

---
+++ AElf.Kernel/Concurrency/ConcurrencyExecutingService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AElf.Kernel.Concurrency.Execution.Messages;
+using AElf.Kernel.Concurrency.Scheduling;
+using Akka.Actor;
+
+namespace AElf.Kernel.Concurrency
+{
+    public class ConcurrencyExecutingService
+    {
+//        private readonly IGrouper _grouper;
+//        private readonly IActorRef _requestor;
+//
+//        public ParallelTransactionExecutingService(IActorRef requestor, IGrouper grouper)
+//        {
+//            _requestor = requestor;
+//            _grouper = grouper;
+//        }
+//
+//        public async Task<List<TransactionTrace>> ExecuteAsync(List<ITransaction> transactions, Hash chainId)
+//        {
+//            
+//        }
+//
+//
+//        private void CancelExecutions(object stateInfo)
+//        {
+//            _requestor.Tell(JobExecutionCancelMessage.Instance);
+//        }
+    }
+}
---
+++ AElf.Kernel/Concurrency/IConcurrencyExecutingService.cs
@@ -0,0 +1,7 @@
+﻿namespace AElf.Kernel.Concurrency
+{
+    public interface IConcurrencyExecutingService
+    {
+        
+    }
+}
