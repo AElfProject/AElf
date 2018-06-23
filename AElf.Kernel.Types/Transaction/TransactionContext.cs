47a737cbd6fddac82a3dc47bf79b7a88083e8122
swk <shiwk@tju.edu.cn>
2018-06-23 22:46:20 +0800

    refactor

---
+++ AElf.Kernel.Types/Transaction/TransactionContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AElf.Kernel.Types;
+
+namespace AElf.Kernel
+{
+    public class TransactionContext : ITransactionContext
+    {
+        public TransactionContext()
+        {
+            Origin = new Hash();
+            Miner = new Hash();
+            PreviousBlockHash = new Hash();
+            Transaction = new Transaction();
+            Trace = new TransactionTrace();
+        }
+        public Hash Origin { get; set; }
+        public Hash Miner { get; set; }
+        public Hash PreviousBlockHash { get; set; }
+        public ITransaction Transaction { get; set; }
+        public TransactionTrace Trace { get; set; }
+    }
+}
