b171a6ef903daab6e0424a8faf3109514b5f4812
swk <shiwk@tju.edu.cn>
2018-06-25 22:43:05 +0800

    add WorldStateConsole

---
+++ AElf.Kernel.Modules.AutofacModule/WorldStateConsoleModule.cs
@@ -0,0 +1,14 @@
+﻿using AElf.Kernel.Managers;
+using Autofac;
+using Org.BouncyCastle.Crypto.Tls;
+
+namespace AElf.Kernel.Modules.AutofacModule
+{
+    public class WorldStateManagerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<WorldStateConsole>().As<IWorldStateConsole>().SingleInstance();
+        }
+    }
+}
