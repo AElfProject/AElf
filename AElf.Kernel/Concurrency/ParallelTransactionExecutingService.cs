0a1ac49cd77036dac99381b958ac6a4cd68fbc3f
PadawanZH <zhangan19940520@gmail.com>
2018-06-27 12:58:53 +0800

    enable merging in grouper, for now all the group will be merged into 8 groups, this "max groups count" need to go into config file in future

--- AElf.Kernel/Concurrency/ParallelTransactionExecutingService.cs
+++ AElf.Kernel/Concurrency/ParallelTransactionExecutingService.cs
@@ -34,7 +34,8 @@ namespace AElf.Kernel.Concurrency
                 TimeSpan.FromMilliseconds(-1)
             ))
             {
-                var tasks = _grouper.Process(chainId, transactions).Select(
+                //TODO: the core count should in the configure file
+                var tasks = _grouper.ProcessWithCoreCount(8, chainId, transactions).Select(
                     txs => Task.Run(() => AttemptToSendExecutionRequest(chainId, txs, cts.Token), cts.Token)
                 ).ToArray();
 
