da783ea6b1e8319c415ccf128c54057f35c371e2
gldeng <realgldeng@gmail.com>
2018-07-01 18:57:57 +0800

    Add test for token contract

---
+++ AElf.Contracts.Token.Tests/MockSetup.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+using AElf.Kernel;
+using AElf.Kernel.Storages;
+using AElf.Kernel.KernelAccount;
+using AElf.Kernel.Managers;
+using AElf.Kernel.Services;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using ServiceStack;
+using Xunit;
+using AElf.Runtime.CSharp;
+using AElf.Kernel.Concurrency.Execution;
+using AElf.Kernel.Concurrency.Metadata;
+using Xunit.Frameworks.Autofac;
+using Path = AElf.Kernel.Path;
+
+namespace AElf.Contracts.Token.Tests
+{
+    public class MockSetup
+    {
+        // IncrementId is used to differentiate txn
+        // which is identified by From/To/IncrementId
+        private static int _incrementId = 0;
+        public ulong NewIncrementId()
+        {
+            var n = Interlocked.Increment(ref _incrementId);
+            return (ulong)n;
+        }
+
+        public Hash ChainId1 { get; } = Hash.Generate();
+        public ISmartContractManager SmartContractManager;
+        public ISmartContractService SmartContractService;
+        private IFunctionMetadataService _functionMetadataService;
+
+        public IChainContextService ChainContextService;
+
+        public IAccountDataProvider DataProvider1;
+
+        public ServicePack ServicePack;
+
+        private IWorldStateDictator _worldStateDictator;
+        private IChainCreationService _chainCreationService;
+        private IBlockManager _blockManager;
+
+        private ISmartContractRunnerFactory _smartContractRunnerFactory;
+
+        public MockSetup(IWorldStateDictator worldStateDictator, IChainCreationService chainCreationService, IBlockManager blockManager, ISmartContractStore smartContractStore, IChainContextService chainContextService, IFunctionMetadataService functionMetadataService, ISmartContractRunnerFactory smartContractRunnerFactory)
+        {
+            _worldStateDictator = worldStateDictator;
+            _chainCreationService = chainCreationService;
+            _blockManager = blockManager;
+            ChainContextService = chainContextService;
+            _functionMetadataService = functionMetadataService;
+            _smartContractRunnerFactory = smartContractRunnerFactory;
+            SmartContractManager = new SmartContractManager(smartContractStore);
+            Task.Factory.StartNew(async () =>
+            {
+                await Init();
+            }).Unwrap().Wait();
+            SmartContractService = new SmartContractService(SmartContractManager, _smartContractRunnerFactory, _worldStateDictator, _functionMetadataService);
+
+            ServicePack = new ServicePack()
+            {
+                ChainContextService = chainContextService,
+                SmartContractService = SmartContractService,
+                ResourceDetectionService = null,
+                WorldStateDictator = _worldStateDictator
+            };
+        }
+
+        public byte[] SmartContractZeroCode
+        {
+            get
+            {
+                byte[] code = null;
+                using (FileStream file = File.OpenRead(System.IO.Path.GetFullPath("../../../../AElf.Contracts.Genesis/bin/Debug/netstandard2.0/AElf.Contracts.Genesis.dll")))
+                {
+                    code = file.ReadFully();
+                }
+                return code;
+            }
+        }
+        
+        private async Task Init()
+        {
+            var reg = new SmartContractRegistration
+            {
+                Category = 0,
+                ContractBytes = ByteString.CopyFrom(SmartContractZeroCode),
+                ContractHash = Hash.Zero
+            };
+            var chain1 = await _chainCreationService.CreateNewChainAsync(ChainId1, reg);
+            var genesis1 = await _blockManager.GetBlockAsync(chain1.GenesisBlockHash);
+            DataProvider1 = await (_worldStateDictator.SetChainId(ChainId1)).GetAccountDataProvider(Path.CalculatePointerForAccountZero(ChainId1));
+        }
+        
+        public async Task<IExecutive> GetExecutiveAsync(Hash address)
+        {
+            var executive = await SmartContractService.GetExecutiveAsync(address, ChainId1);
+            return executive;
+        }
+    }
+}
---
+++ AElf.Contracts.Token.Tests/TokenContractTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using AElf.Kernel;
+using AElf.Types.CSharp;
+using Akka.Event;
+using Xunit.Frameworks.Autofac;
+using Xunit;
+using ServiceStack;
+
+namespace AElf.Contracts.Token.Tests
+{
+    [UseAutofacTestFramework]
+    public class TokenContractTest
+    {
+        private ContractZeroShim _contractZero;
+        private TokenContractShim _contract;
+        private MockSetup _mock;
+
+        private IExecutive Executive { get; set; }
+
+        public byte[] Code
+        {
+            get
+            {
+                byte[] code = null;
+                using (FileStream file = File.OpenRead(System.IO.Path.GetFullPath("../../../../AElf.Contracts.Token/bin/Debug/netstandard2.0/AElf.Contracts.Token.dll")))
+                {
+                    code = file.ReadFully();
+                }
+                return code;
+            }
+        }
+        
+        public TokenContractTest(MockSetup mock, ContractZeroShim contractZero)
+        {
+            _mock = mock;
+            _contractZero = contractZero;
+            Init();
+        }
+
+        private void Init()
+        {
+            _contractZero.DeploySmartContract(0, Code);
+            var address = new Hash(_contractZero.TransactionContext.Trace.RetVal.DeserializeToBytes());
+            _contract = new TokenContractShim(_mock, address);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            _contractZero.GetContractOwner(_contract.Address);
+            
+            // Initialize
+            _contract.Initialize("ELF", "AElf Token", 1000000000, 1);
+            
+            Assert.Null(_contract.TransactionContext.Trace.StdErr);
+            Assert.Equal((ulong)0, _contract.TotalSupply());
+        }
+    }
+}
