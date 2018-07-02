413a12bae279b3b5ec4b2a3c9fe77cf744b4cb1d
Peng Rong <rongpeng@live.com>
2018-07-02 19:08:11 +0800

    Improve logging to file

--- AElf.Kernel.Modules.AutofacModule/LoggerModule.cs
+++ AElf.Kernel.Modules.AutofacModule/LoggerModule.cs
@@ -4,9 +4,7 @@ using AElf.Common.Attributes;
 using Autofac;
 using Autofac.Core;
 using NLog;
-using NLog.Conditions;
 using NLog.Config;
-using NLog.Targets;
 
 namespace AElf.Kernel.Modules.AutofacModule
 {
@@ -14,34 +12,12 @@ namespace AElf.Kernel.Modules.AutofacModule
     {
         protected override void Load(ContainerBuilder builder)
         {
-            // Step 1. Create configuration object 
-            var config = new LoggingConfiguration();
-
-            // Step 2. Create targets and add them to the configuration 
-            var consoleTarget = new ColoredConsoleTarget();
-            config.AddTarget("console", consoleTarget);
-
-            consoleTarget.UseDefaultRowHighlightingRules = false;
-            
-            var highlightRule = new ConsoleRowHighlightingRule();
-            
-            /*highlightRule.Condition = ConditionParser.ParseExpression("level == LogLevel.Trace");
-            highlightRule.BackgroundColor = ConsoleOutputColor.White;
-            highlightRule.ForegroundColor = ConsoleOutputColor.Black;
-            consoleTarget.RowHighlightingRules.Add(highlightRule);*/
-
-            // Step 3. Set target properties 
-            consoleTarget.Layout = @"[ ${date:format=HH\:mm\:ss} - ${logger} ] : ${message} ${exception:format=toString}";
-
-            // Step 4. Define rules
-            var rule1 = new LoggingRule("*", LogLevel.Trace, consoleTarget);
-            config.LoggingRules.Add(rule1);
-            
-            // Step 5. Activate the configuration
-            LogManager.Configuration = config;
+            var logConfigFile = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NLog.config");
+            LogManager.Configuration = new XmlLoggingConfiguration(logConfigFile);
         }
-        
-        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry, IComponentRegistration registration)
+
+        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry,
+            IComponentRegistration registration)
         {
             // Handle constructor parameters.
             registration.Preparing += OnComponentPreparing;
@@ -59,12 +35,14 @@ namespace AElf.Kernel.Modules.AutofacModule
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex);
             }
-            
+
             e.Parameters = e.Parameters.Union(
                 new[]
                 {
-                    new ResolvedParameter((p, i) => p.ParameterType == typeof (ILogger), (p, i) => LogManager.GetLogger(loggerName?.Name ?? t.Name))
+                    new ResolvedParameter((p, i) => p.ParameterType == typeof(ILogger),
+                        (p, i) => LogManager.GetLogger(loggerName?.Name ?? t.Name))
                 });
         }
     }
