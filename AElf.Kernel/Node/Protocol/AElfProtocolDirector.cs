447d4c8715a214ce19c097f3745769757159411f
samfrb <samos2x@outlook.com>
2018-07-05 21:44:30 +0800

    Extra logging.

--- AElf.Kernel/Node/Protocol/AElfProtocolDirector.cs
+++ AElf.Kernel/Node/Protocol/AElfProtocolDirector.cs
@@ -200,7 +200,7 @@ namespace AElf.Kernel.Node.Protocol
             }
             catch (Exception e)
             {
-                ; // todo
+                _logger?.Trace(e, "Error while during HandleBlockRequest.");
             }
         }
 
@@ -215,7 +215,7 @@ namespace AElf.Kernel.Node.Protocol
             }
             catch (Exception e)
             {
-                ; // todo
+                _logger?.Trace(e, "Error while during HandleHeightRequest.");
             }
         }
 
@@ -228,7 +228,7 @@ namespace AElf.Kernel.Node.Protocol
             }
             catch (Exception e)
             {
-                ; // todo
+                _logger?.Trace(e, "Error while during HandlePeerHeightReception.");
             }
         }
 
@@ -237,11 +237,12 @@ namespace AElf.Kernel.Node.Protocol
             try
             {
                 var fromSend = message.MsgType == (int) MessageTypes.Tx;
+                
                 await _node.ReceiveTransaction(message.Payload, fromSend);
             }
             catch (Exception e)
             {
-                ; // todo
+                _logger?.Trace(e, "Error while receiving transaction.");
             }
         }
 
@@ -265,9 +266,9 @@ namespace AElf.Kernel.Node.Protocol
                     await _blockSynchronizer.AddRequestedBlock(b);
                 }*/
             }
-            catch (Exception exception)
+            catch (Exception e)
             {
-                ; // todo
+                _logger?.Trace(e, "Error while receiving HandleBlockReception.");
             }
         }
 
