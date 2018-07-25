7f2d75e4a66e11e20a524bd9b969b436de55af20
zhxymh <13552710076@126.com>
2018-07-25 19:04:18 +0800

    manager add command option

--- AElf.Concurrency.Manager/ManagementService.cs
+++ AElf.Concurrency.Manager/ManagementService.cs
@@ -17,8 +17,10 @@ namespace AElf.Concurrency.Manager
         {
             var clusterConfig = ConfigurationFactory.ParseString(ActorConfig.Instance.ManagerHoconConfig);
             var systemName = clusterConfig.GetConfig("manager").GetString("system-name");
-            var ipAddress = clusterConfig.GetConfig("akka.remote").GetString("dot-netty.tcp.hostname");
-            var port = clusterConfig.GetConfig("akka.remote").GetString("dot-netty.tcp.port");
+//            var ipAddress = clusterConfig.GetConfig("akka.remote").GetString("dot-netty.tcp.hostname");
+//            var port = clusterConfig.GetConfig("akka.remote").GetString("dot-netty.tcp.port");
+            var ipAddress = ActorConfig.Instance.HostName;
+            var port = ActorConfig.Instance.Port;
             var selfAddress = $"akka.tcp://{systemName}@{ipAddress}:{port}";
 
             var seeds = clusterConfig.GetStringList("akka.cluster.seed-nodes");
@@ -30,6 +32,8 @@ namespace AElf.Concurrency.Manager
             seedConfigString += "]";
 
             var finalConfig = ConfigurationFactory.ParseString(seedConfigString)
+                .WithFallback(ConfigurationFactory.ParseString("akka.remote.dot-netty.tcp.hostname=" + ActorConfig.Instance.HostName))
+                .WithFallback(ConfigurationFactory.ParseString("akka.remote.dot-netty.tcp.port=" + ActorConfig.Instance.Port))
                 .WithFallback(clusterConfig);
             return ActorSystem.Create(systemName, finalConfig);
         }
