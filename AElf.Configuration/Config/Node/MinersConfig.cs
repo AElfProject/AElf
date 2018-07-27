d40e0b722b43940fd7fb610c94eeacb721a5359c
zhxymh <13552710076@126.com>
2018-07-27 11:04:28 +0800

    add miners.json and remove minerconfig constructor

--- AElf.Configuration/Config/Node/MinersConfig.cs
+++ AElf.Configuration/Config/Node/MinersConfig.cs
@@ -8,31 +8,6 @@ namespace AElf.Configuration
     {
         [JsonProperty("producers")]
         public Dictionary<string, Dictionary<string, string>> Producers { get; set; }
-
-        public MinersConfig()
-        {
-            Producers = new Dictionary<string, Dictionary<string, string>>
-            {
-                {
-                    "1", new Dictionary<string, string>
-                    {
-                        {"address", "04e4bb019b3d8eb89c12a32ede9f82c77c24"}
-                    }
-                },
-                {
-                    "2", new Dictionary<string, string>
-                    {
-                        {"address", "04d977d9e16e6c12055da9fc5d9eca287829"}
-                    }
-                },
-                {
-                    "3", new Dictionary<string, string>
-                    {
-                        {"address", "04b478c4e2e7ff66003f095acdaae2fcfef5"}
-                    }
-                }
-            };
-        }
     }
 }
 
