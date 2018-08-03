5aef7936b9085fc0a22315e5d2c19bb9372955f4
mengrui.pan <mengrui.pan@126.com>
2018-08-03 18:08:33 +0800

    code format

--- AElf.Node/Node/RPC/RpcServer.cs
+++ AElf.Node/Node/RPC/RpcServer.cs
@@ -296,7 +296,7 @@ namespace AElf.Kernel.Node.RPC
                 return error;
 
             var transactionPoolSize = await _node.GetTransactionPoolSize();
-               
+
             var response = new JObject
             {
                 ["result"] = new JObject
@@ -314,8 +314,6 @@ namespace AElf.Kernel.Node.RPC
                     ["Body"] = new JObject
                     {
                         ["TransactionsCount"] = blockinfo.Body.TransactionsCount
-                        
-                    
                     },
                     ["CurrentTransactionPoolSize"] = transactionPoolSize
                 }
@@ -324,14 +322,15 @@ namespace AElf.Kernel.Node.RPC
             {
                 var transactions = blockinfo.Body.Transactions;
                 var txs = new List<string>();
-           
+
                 foreach (var txHash in transactions)
                 {
                     txs.Add(txHash.ToHex());
                 }
+
                 response["result"]["Body"]["Transactions"] = JArray.FromObject(txs);
             }
-                     
+
             return JObject.FromObject(response);
         }
 
@@ -384,11 +383,10 @@ namespace AElf.Kernel.Node.RPC
 
             var txInfo = transaction == null ? new JObject {["tx"] = "Not Found"} : transaction.GetTransactionInfo();
 
-            
+
             var txResult = await _node.GetTransactionResult(txHash);
             var response = new JObject
             {
-                //["tx_id"] = txResult.TransactionId.ToHex(),
                 ["tx_status"] = txResult.Status.ToString(),
                 ["tx_info"] = txInfo["tx"]
             };
@@ -495,7 +493,7 @@ namespace AElf.Kernel.Node.RPC
             var response = new List<object>();
             foreach (var rawtx in reqParams["rawtxs"].ToString().Split(','))
             {
-                var result = await ProcessBroadcastTx(new JObject{["rawtx"] = rawtx});
+                var result = await ProcessBroadcastTx(new JObject {["rawtx"] = rawtx});
                 if (result.ContainsKey("error"))
                     break;
                 response.Add(result["hash"].ToString());
