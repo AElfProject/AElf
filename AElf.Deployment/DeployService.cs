82566e7b60b9406589442fddfe454d5310aaa851
zhxymh <13552710076@126.com>
2018-08-03 20:18:54 +0800

    add statefullset

--- AElf.Deployment/DeployService.cs
+++ AElf.Deployment/DeployService.cs
@@ -77,91 +77,6 @@ namespace AElf.Deployment
             return K8SRequestHelper.ListNamespacedPod("default");
         }
 
-//        public void Scale()
-//        {
-//            var body = new Extensionsv1beta1Scale();
-//            body.Kind = "Scale";
-//            body.ApiVersion = "extensions/v1beta1";
-//            body.Metadata = new V1ObjectMeta();
-//            body.Metadata.Name = "worker-test-scale";
-//            body.Spec = new Extensionsv1beta1ScaleSpec(3);
-//            
-//            body.Metadata.OwnerReferences=new List<V1OwnerReference>();
-//            var reference = new V1OwnerReference();
-//            reference.ApiVersion = "extensions/v1beta1";
-//            reference.Kind = "Deployment";
-//            reference.Name = "worker-test";
-//            reference.Uid = "worker-test";
-//            
-//            body.Metadata.OwnerReferences.Add(reference);
-//            
-//            KubernetesHelper.ReplaceNamespacedDeploymentScale3(body, "worker-test-scale", "default");
-//        }
-
-        public void ReplaceDeployment()
-        {
-            var body = new Extensionsv1beta1Deployment();
-            body.ApiVersion = "extensions/v1beta1";
-            body.Kind = "Deployment";
-            
-            body.Metadata= new V1ObjectMeta();
-            body.Metadata.Name = "worker-test";
-            body.Metadata.Labels=new Dictionary<string, string>();
-            body.Metadata.Labels.Add("name","worker-test");
-            
-            body.Spec = new Extensionsv1beta1DeploymentSpec();
-            body.Spec.Selector=new V1LabelSelector();
-            body.Spec.Selector.MatchLabels = body.Metadata.Labels;
-
-            body.Spec.Replicas = 3;
-            
-            body.Spec.Template = new V1PodTemplateSpec();
-            body.Spec.Template.Metadata = new V1ObjectMeta();
-            body.Spec.Template.Metadata.Labels = body.Metadata.Labels;
-            
-            body.Spec.Template.Spec=new V1PodSpec();
-            
-            body.Spec.Template.Spec.Containers=new List<V1Container>();
-            var container1 = new V1Container();
-            container1.Name = "worker-test";
-            container1.Image = "aelf/node:worker";
-            container1.Ports=new List<V1ContainerPort>();
-            container1.Ports.Add(new V1ContainerPort(32551));
-            
-            container1.Env=new List<V1EnvVar>();
-            var env1 = new V1EnvVar();
-            env1.Name = "POD_IP";
-            env1.ValueFrom=new V1EnvVarSource();
-            env1.ValueFrom.FieldRef=new V1ObjectFieldSelector();
-            env1.ValueFrom.FieldRef.FieldPath = "status.podIP";
-            container1.Env.Add(env1);
-            
-            container1.Args=new List<string>();
-            container1.Args.Add("--actor.host");
-            container1.Args.Add("$(POD_IP)");
-            container1.Args.Add("--actor.port");
-            container1.Args.Add("32551");
-            
-            container1.VolumeMounts=new List<V1VolumeMount>();
-            var volumeMount1 = new V1VolumeMount();
-            volumeMount1.MountPath = "/app/aelf/config";
-            volumeMount1.Name = "config";
-            container1.VolumeMounts.Add(volumeMount1);
-            
-            body.Spec.Template.Spec.Containers.Add(container1);
-            
-            body.Spec.Template.Spec.Volumes=new List<V1Volume>();
-            var volume1 = new V1Volume();
-            volume1.Name = "config";
-            volume1.ConfigMap=new V1ConfigMapVolumeSource();
-            volume1.ConfigMap.Name = "aelf-config";
-            body.Spec.Template.Spec.Volumes.Add(volume1);
-            
-            var namespaceParameter = "default";
-
-            K8SRequestHelper.ReplaceNamespacedDeployment3(body, "worker-test", "default");
-        }
-
         public void PatchDepoyment()
         {
             var patch = new JsonPatchDocument<Extensionsv1beta1Deployment>();
@@ -201,7 +116,5 @@ namespace AElf.Deployment
         {
             var status = K8SRequestHelper.DeleteNamespace(new V1DeleteOptions(), "test");
         }
-        
-        
     }
 }
