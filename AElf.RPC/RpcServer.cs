1885bcf1dc74cc9ffb06c159b041b2cb136bba82
gldeng <realgldeng@gmail.com>
2018-08-12 19:08:43 +0800

    Cleaner RpcServer

--- AElf.RPC/RpcServer.cs
+++ AElf.RPC/RpcServer.cs
@@ -2,19 +2,12 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Autofac;
-using NLog;
 
 namespace AElf.RPC
 {
     public class RpcServer
     {
-        private readonly ILogger _logger;
         private IWebHost _host;
-
-        public RpcServer(ILogger logger)
-        {
-            _logger = logger;
-        }
 
         public bool Initialize(ILifetimeScope scope, string rpcHost, int rpcPort)
         {
@@ -30,7 +23,6 @@ namespace AElf.RPC
             }
             catch (Exception e)
             {
-                _logger.Log(LogLevel.Error, e, "Error while starting the RPC server.");
                 return false;
             }
 
