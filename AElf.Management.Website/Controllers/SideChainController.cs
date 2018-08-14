d9816c0995be73ae1ac61a33ee156e0e12b488e9
zhxymh <13552710076@126.com>
2018-08-14 14:15:58 +0800

    add management website

---
+++ AElf.Management.Website/Controllers/SideChainController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AElf.Management.Website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SideChainController : ControllerBase
+    {
+        // POST api/sidechain
+        [HttpPost("{chainId}")]
+        public void Post(string chainId,[FromBody] TestModel value)
+        {
+            Console.WriteLine(chainId);
+            Console.WriteLine(value.Value);
+        }
+
+        // DELETE api/sidechain/abcde
+        [HttpDelete("{chainId}")]
+        public void Delete(string chainId)
+        {
+            Console.WriteLine(chainId);
+        }
+    }
+
+    public class TestModel
+    {
+        public string Value { get; set; }
+    }
+}
