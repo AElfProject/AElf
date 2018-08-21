64308f1470470d9d7a7f77a6c04352a461ccaef9
swk <shiwk@tju.edu.cn>
2018-08-21 15:22:08 +0800

    add miner grpc

---
+++ AElf.Miner.Rpc/Client/HeaderInfoClientImpl.cs
@@ -0,0 +1,12 @@
+﻿namespace AElf.Miner.Rpc.Client
+{
+    public class HeaderInfoClientImpl
+    {
+        readonly HeaderInfoRpc.HeaderInfoRpcClient client;
+
+        public HeaderInfoClientImpl(HeaderInfoRpc.HeaderInfoRpcClient client)
+        {
+            this.client = client;
+        }
+    }
+}
