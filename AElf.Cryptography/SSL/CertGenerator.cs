d108b9379110f01c4dbf94520e1df3d1564491ac
swk <shiwk@tju.edu.cn>
2018-08-23 21:10:24 +0800

    add  certification generator

---
+++ AElf.Cryptography/SSL/CertGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Operators;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.X509;
+namespace AElf.Cryptography.SSL
+{
+    public class CertGenerator
+    {
+        private readonly X509V3CertificateGenerator _certificateGenerator;
+        private const string SignatureAlgorithm = "SHA256WithRSA";
+        private const string DefaultSubjectName = "AElf";
+        public static double DefautIntervalDays { get; } = 365;
+        private SecureRandom random = new SecureRandom();
+
+        public CertGenerator(string name, double days = 0)
+        {
+            _certificateGenerator = new X509V3CertificateGenerator();
+            var subjectDn = new X509Name(DefaultSubjectName);
+            var issuerDn = new X509Name(name);
+            
+            _certificateGenerator.SetIssuerDN(issuerDn);
+            _certificateGenerator.SetSubjectDN(subjectDn);
+            
+            var notBefore = DateTime.UtcNow.Date;
+            var notAfter = notBefore.AddDays(Math.Abs(days) >= 1 ?days : DefautIntervalDays);
+            
+            _certificateGenerator.SetNotBefore(notBefore);
+            _certificateGenerator.SetNotAfter(notAfter);
+        }
+
+        public CertGenerator SetSignatureAlgorithm()
+        {
+            
+        }
+        
+        public X509Certificate Generate(AsymmetricKeyParameter privateKey)
+        {
+            Asn1SignatureFactory asn1SignatureFactory = new Asn1SignatureFactory(SignatureAlgorithm, privateKey);
+            return _certificateGenerator.Generate(asn1SignatureFactory);
+        }
+    }
+}
