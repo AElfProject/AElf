d255ae5e5901b0219721d849091ce08d2928d86a
swk <shiwk@tju.edu.cn>
2018-08-25 14:27:12 +0800

    add san in certificate generator

--- AElf.Cryptography/SSL/CertGenerator.cs
+++ AElf.Cryptography/SSL/CertGenerator.cs
@@ -1,46 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Org.BouncyCastle.Asn1.X509;
 using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Operators;
 using Org.BouncyCastle.Math;
 using Org.BouncyCastle.Security;
 using Org.BouncyCastle.Utilities;
 using Org.BouncyCastle.X509;
+using X509Certificate = Org.BouncyCastle.X509.X509Certificate;
+
 namespace AElf.Cryptography.SSL
 {
     public class CertGenerator
     {
         private readonly X509V3CertificateGenerator _certificateGenerator;
-        private const string SignatureAlgorithm = "SHA256WithRSA";
-        private const string DefaultSubjectName = "AElf";
+        private string _signatureAlgorithm = "SHA256WITHECDSA";
+        private const string DefaultSubjectName = "aelf";
         public static double DefautIntervalDays { get; } = 365;
         private SecureRandom random = new SecureRandom();
 
         public CertGenerator(string name, double days = 0)
         {
             _certificateGenerator = new X509V3CertificateGenerator();
-            var subjectDn = new X509Name(DefaultSubjectName);
-            var issuerDn = new X509Name(name);
-            
+            _certificateGenerator.SetSignatureAlgorithm(_signatureAlgorithm);
+            var subjectDn = new X509Name("CN=" + DefaultSubjectName);
+            var issuerDn = new X509Name("CN=" + name);
             _certificateGenerator.SetIssuerDN(issuerDn);
             _certificateGenerator.SetSubjectDN(subjectDn);
-            
             var notBefore = DateTime.UtcNow.Date;
             var notAfter = notBefore.AddDays(Math.Abs(days) >= 1 ?days : DefautIntervalDays);
-            
             _certificateGenerator.SetNotBefore(notBefore);
             _certificateGenerator.SetNotAfter(notAfter);
+            _certificateGenerator.SetSerialNumber(BigIntegers.CreateRandomInRange(BigInteger.One,
+                BigInteger.ValueOf(Int64.MaxValue), random));
         }
 
-        public CertGenerator SetSignatureAlgorithm()
+        public CertGenerator SetSignatureAlgorithm(string algorithm)
         {
-            
+            _signatureAlgorithm = algorithm;
+            return this;
         }
-        
+
+        public CertGenerator SetPublicKey(AsymmetricKeyParameter publcKey)
+        {
+            _certificateGenerator.SetPublicKey(publcKey);
+            return this;
+        }
         public X509Certificate Generate(AsymmetricKeyParameter privateKey)
         {
-            Asn1SignatureFactory asn1SignatureFactory = new Asn1SignatureFactory(SignatureAlgorithm, privateKey);
-            return _certificateGenerator.Generate(asn1SignatureFactory);
+            return _certificateGenerator.Generate(privateKey);
+        }
+
+        public CertGenerator AddALternativeName(params string[] addresses)
+        {
+            var generalNames = addresses.Select(addr => new GeneralName(GeneralName.IPAddress, addr)).ToArray();
+            generalNames = generalNames.Append(new GeneralName(GeneralName.DnsName, "localhost")).ToArray();
+            GeneralNames subjectAltName = new GeneralNames(generalNames);
+            _certificateGenerator.AddExtension(X509Extensions.SubjectAlternativeName, false, subjectAltName);
+            return this;
         }
     }
 }
