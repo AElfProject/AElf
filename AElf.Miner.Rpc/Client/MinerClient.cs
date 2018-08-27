6142b94dfe5b51dececfe8d874f99795f6d97217
swk <shiwk@tju.edu.cn>
2018-08-27 21:04:20 +0800

    improve grpc server and miner

--- AElf.Miner.Rpc/Client/MinerClient.cs
+++ AElf.Miner.Rpc/Client/MinerClient.cs
@@ -1,46 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using AElf.Common.Application;
-using AElf.Common.ByteArrayHelpers;
 using AElf.Configuration.Config.GRPC;
+using AElf.Cryptography.Certificate;
 using AElf.Kernel;
-using Akka.Util.Internal;
+using AElf.Miner.Rpc.Exceptions;
 using Grpc.Core;
  
  namespace AElf.Miner.Rpc.Client
  {
      public class MinerClient
      {
-         private readonly Dictionary<string, HeaderInfoRpc.HeaderInfoRpcClient> _channels = new Dictionary<string, HeaderInfoRpc.HeaderInfoRpcClient>();
-         public void Init()
-         {
-             var childChains = GrpcConfig.Instance.ChildChains;
-             foreach (var chainIdUri in childChains)
-             {
-                 var uri = chainIdUri.Value.Address + ":" + chainIdUri.Value.Port;
-                 var channOptions = new List<ChannelOption>
-                 {
-                     new ChannelOption(ChannelOptions.SslTargetNameOverride,"aelf")
-                 };
-                // var channelCredentials = new SslCredentials(File.ReadAllText(Path.Combine(ApplicationHelpers.GetDefaultDataDir() + "/certs/" + chainIdUri.Key + "_cert.pem")));
-                 string certificate = File.ReadAllText(ApplicationHelpers.GetDefaultDataDir() + "/certs/" + "sidechain_cert.pem");
-                 string privateKey = File.ReadAllText(ApplicationHelpers.GetDefaultDataDir() + "/certs/" + "sidechain_key.pem");
-                 string crt = File.ReadAllText(ApplicationHelpers.GetDefaultDataDir() + "/certs/" + "main_cert.pem");
-                 var channelCredentials = new SslCredentials(crt);
-                 var channel = new Channel(uri, channelCredentials);
-                 _channels.Add(chainIdUri.Key, new HeaderInfoRpc.HeaderInfoRpcClient(channel));
-             }
-         }
+         private readonly Dictionary<Hash, HeaderInfoRpc.HeaderInfoRpcClient> _clients =
+             new Dictionary<Hash, HeaderInfoRpc.HeaderInfoRpcClient>();
 
-         public ReponseIndexedInfo GetHeaderInfo(RequestIndexedInfo request)
+         private CertificateStore _certificateStore;
+
+         public ReponseIndexedInfo GetHeaderInfo(Hash chainId, RequestIndexedInfo request)
          {
-             if (!_channels.TryGetValue(request.ChainId.ToHex(), out var client))
+             if (!_clients.TryGetValue(chainId, out var client))
              {
-                 throw new Exception("Not existed chain");
+                 throw new ClientNotFoundException("Not existed client");
              }
              var headerInfo = client.GetHeaderInfo(request);
              return headerInfo;
+         }
+
+         public void Init(string dir)
+         {
+             _certificateStore =
+                 new CertificateStore(dir);
+         }
+         
+         public HeaderInfoRpc.HeaderInfoRpcClient StartNewClient(Hash targetChainId)
+         {
+             if (_clients.TryGetValue(targetChainId, out var client)) return client;
+             string ch = targetChainId.ToHex();
+             if(!GrpcConfig.Instance.ChildChains.ContainsKey(ch))
+                 throw new ChainInfoNotFoundException("Unable to get chain Info.");
+             var chainIdUri = GrpcConfig.Instance.ChildChains[ch];
+             var uri = chainIdUri.Address + ":" + chainIdUri.Port;
+             string crt = _certificateStore.GetCertificate(ch);
+             if(crt == null)
+                 throw new CertificateException("Unable to load Certificate.");
+             var channelCredentials = new SslCredentials(crt);
+             var channel = new Channel(uri, channelCredentials);
+             client = new HeaderInfoRpc.HeaderInfoRpcClient(channel);
+             _clients.Add(targetChainId, client);
+             return client;
          }
      }
  }
--- AElf.Miner.Rpc/Server/MinerServer.cs
+++ AElf.Miner.Rpc/Server/MinerServer.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using AElf.Common.Application;
 using AElf.Common.Attributes;
 using AElf.Configuration.Config.GRPC;
+using AElf.Cryptography.Certificate;
 using AElf.Kernel;
+using AElf.Miner.Rpc.Exceptions;
 using Grpc.Core;
 using NLog;
 
@@ -18,26 +19,37 @@ namespace AElf.Miner.Rpc.Server
         private static readonly int Port = GrpcConfig.Instance.LocalMinerServerPort;
         private static readonly string Address = GrpcConfig.Instance.LocalMinerServerIP;
         private Grpc.Core.Server _server;
+
+        private CertificateStore _certificateStore;
+        private SslServerCredentials _sslServerCredentials;
         public MinerServer(ILogger logger, HeaderInfoServerImpl headerInfoServerImpl)
         {
             _logger = logger;
             _headerInfoServerImpl = headerInfoServerImpl;
-            
         }
 
-        public void StartUp(Hash chainId)
+        public void Init(Hash chainId, string dir)
         {
-            string certificate = File.ReadAllText(ApplicationHelpers.GetDefaultDataDir() + "/certs/" + "main_cert.pem");
-            string privateKey = File.ReadAllText(ApplicationHelpers.GetDefaultDataDir() + "/certs/" + "main_key.pem");
-            string crt = File.ReadAllText(ApplicationHelpers.GetDefaultDataDir() + "/certs/" + "sidechain_cert.pem");
-
+            _certificateStore =
+                new CertificateStore(dir);
+            string ch = chainId.ToHex();
+            string certificate = _certificateStore.GetCertificate(ch);
+            if(certificate == null)
+                throw new CertificateException("Unable to load Certificate.");
+            string privateKey = _certificateStore.GetPrivateKey(ch);
+            if(privateKey == null)
+                throw new PrivateKeyException("Unable to load private key.");
             var keyCertificatePair = new KeyCertificatePair(certificate, privateKey);
-            var credentials = new SslServerCredentials(new List<KeyCertificatePair> {keyCertificatePair});
-            
+            _sslServerCredentials = new SslServerCredentials(new List<KeyCertificatePair> {keyCertificatePair});
+        }
+        
+        
+        public void StartUp()
+        {
             _server = new Grpc.Core.Server
             {
                 Services = {HeaderInfoRpc.BindService(_headerInfoServerImpl)},
-                Ports = {new ServerPort(Address, Port, credentials)}
+                Ports = {new ServerPort(Address, Port, _sslServerCredentials)}
             };
             _server.Start();
             _logger.Log(LogLevel.Debug, "Miner server listening on port " + Port);          
