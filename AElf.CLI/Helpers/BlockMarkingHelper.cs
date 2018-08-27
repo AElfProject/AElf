d5dc558e5084f268ea418b1a2656d3dbd0e2618d
gldeng <realgldeng@gmail.com>
2018-08-27 17:34:20 +0800

    Add cache for reference block info

--- AElf.CLI/Helpers/BlockMarkingHelper.cs
+++ AElf.CLI/Helpers/BlockMarkingHelper.cs
@@ -13,10 +13,23 @@ namespace AElf.CLI.Helpers
 {
     public static class BlockMarkingHelper
     {
+        private static DateTime _refBlockTime = DateTime.Now;
+        private static ulong _cachedHeight;
+        private static string _cachedHash;
+
         public static Transaction AddBlockReference(this Transaction transaction, string rpcAddress)
         {
-            var height = ulong.Parse(GetBlkHeight(rpcAddress));
-            var hash = GetBlkHash(rpcAddress, height.ToString());
+            var height = _cachedHeight;
+            var hash = _cachedHash;
+            if (height == default(ulong) || (DateTime.Now - _refBlockTime).Seconds > 60)
+            {
+                height = ulong.Parse(GetBlkHeight(rpcAddress));
+                hash = GetBlkHash(rpcAddress, height.ToString());
+                _cachedHeight = height;
+                _cachedHash = hash;
+                _refBlockTime = DateTime.Now;
+            }
+
             transaction.RefBlockNumber = height;
             transaction.RefBlockPrefix = ByteArrayHelpers.FromHexString(hash).Where((b, i) => i < 4).ToArray();
             return transaction;
