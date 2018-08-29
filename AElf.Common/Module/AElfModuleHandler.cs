1015f56be5a87b263ace6a790ff65066b2e8cfbb
zhxymh <13552710076@126.com>
2018-08-29 19:59:43 +0800

    add common  module

---
+++ AElf.Common/Module/AElfModuleHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Autofac;
+
+namespace AElf.Common.Module
+{
+    public class AElfModuleHandler
+    {
+        private readonly ContainerBuilder _builder;
+        private IContainer _container;
+
+        private List<IAElfModlule> _modlules;
+
+        public AElfModuleHandler()
+        {
+            _builder = new ContainerBuilder();
+            _modlules = new List<IAElfModlule>();
+        }
+
+        public void Build()
+        {
+            _modlules.ForEach(m => m.Init(_builder));
+
+            _container = _builder.Build();
+
+            _modlules.ForEach(m => m.Run(_container));
+        }
+    }
+}
---
+++ AElf.Common/Module/IAElfModlule.cs
@@ -0,0 +1,10 @@
+﻿using Autofac;
+
+namespace AElf.Common.Module
+{
+    public interface IAElfModlule
+    {
+        void Init(ContainerBuilder builder);
+        void Run(IContainer container);
+    }
+}
