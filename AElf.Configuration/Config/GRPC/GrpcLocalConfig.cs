99ac14df7b8504a40fe2e9a6b2ed44b98d2e0013
swk <shiwk@tju.edu.cn>
2018-09-04 21:27:55 +0800

    miner grpc config

--- AElf.Configuration/Config/GRPC/GrpcLocalConfig.cs
+++ AElf.Configuration/Config/GRPC/GrpcLocalConfig.cs
@@ -6,6 +6,8 @@ namespace AElf.Configuration.Config.GRPC
     [ConfigFile(FileName = "grpclocal.json")]
     public class GrpcLocalConfig : ConfigBase<GrpcLocalConfig>
     {
+        public bool IsCluster { get; set; }
+        public int WaitingIntervalInMillisecond { get; set; }
         public string LocalServerIP { get; set; }
         public int LocalServerPort { get; set; }
     }
