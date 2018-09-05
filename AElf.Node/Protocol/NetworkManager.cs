2019d4b4d148a4ee1820762d86c0ea3e42de64f2
samfrb <samos2x@outlook.com>
2018-09-05 10:48:07 +0800

    Network: better logging semantics.

--- AElf.Node/Protocol/NetworkManager.cs
+++ AElf.Node/Protocol/NetworkManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 using AElf.ChainController.EventMessages;
 using AElf.ChainController.TxMemPool;
@@ -147,7 +146,7 @@ namespace AElf.Node.Protocol
                 }
                 catch (Exception e)
                 {
-                    _logger?.Trace(e, "Error while processing incoming messages");
+                    _logger?.Error(e, "Error while processing incoming messages");
                 }
             }
         }
@@ -156,7 +155,7 @@ namespace AElf.Node.Protocol
         {
             if (args?.Peer == null || args.Message == null)
             {
-                _logger.Error("Invalid message from peer.");
+                _logger.Warn("Invalid message from peer.");
                 return;
             }
             
@@ -225,14 +224,14 @@ namespace AElf.Node.Protocol
 
                 if (addResult == TxValidation.TxInsertionAndBroadcastingError.Success)
                 {
-                    _logger?.Trace($"Transaction (new) with hash {txHash.ToHex()} added to the pool.");
+                    _logger?.Debug($"Transaction (new) with hash {txHash.ToHex()} added to the pool.");
                         
                     foreach (var p in _peers.Where(p => !p.Equals(peer)))
                         p.EnqueueOutgoing(msg);
                 }
                 else
                 {
-                    _logger?.Trace($"New transaction from {peer} not added to the pool: {addResult}");
+                    _logger?.Debug($"New transaction from {peer} not added to the pool: {addResult}");
                 }
             }
             catch (Exception e)
@@ -300,7 +299,7 @@ namespace AElf.Node.Protocol
             }
             catch (Exception e)
             {
-                _logger?.Trace(e, "Error while requesting transactions.");
+                _logger?.Error(e, "Error while requesting transactions.");
             }
         }
         
@@ -330,7 +329,7 @@ namespace AElf.Node.Protocol
             }
             catch (Exception e)
             {
-                _logger?.Trace(e, $"Error while requesting block for index {index}.");
+                _logger?.Error(e, $"Error while requesting block for index {index}.");
             }
         }
 
@@ -343,7 +342,7 @@ namespace AElf.Node.Protocol
         {
             if (sender == null)
             {
-                _logger?.Trace("Request timeout - sender null.");
+                _logger?.Warn("Request timeout - sender null.");
                 return;
             }
 
@@ -390,7 +389,7 @@ namespace AElf.Node.Protocol
                 TriedPeers = req.TriedPeers.ToList()
             };
 
-            _logger?.Trace("Request failed : " + req.RequestMessage.RequestLogString + $" after {req.TriedPeers.Count} tries. Max tries : {req.MaxRetryCount}.");
+            _logger?.Warn("Request failed : " + req.RequestMessage.RequestLogString + $" after {req.TriedPeers.Count} tries. Max tries : {req.MaxRetryCount}.");
                     
             RequestFailed?.Invoke(this, reqFailedEventArgs);
         }
@@ -430,7 +429,7 @@ namespace AElf.Node.Protocol
         {
             if (msg == null)
             {
-                _logger?.Trace("Handle message : peer or message null.");
+                _logger?.Warn("Handle message : peer or message null.");
                 return null;
             }
             
@@ -455,19 +454,19 @@ namespace AElf.Node.Protocol
                     
                     if (request.IsTxRequest && request.TransactionHashes != null && request.TransactionHashes.Any())
                     {
-                        _logger?.Trace("Matched : [" + string.Join(", ", request.TransactionHashes.Select(kvp => kvp.ToHex()).ToList()) + "]");
+                        _logger?.Debug("Matched : [" + string.Join(", ", request.TransactionHashes.Select(kvp => kvp.ToHex()).ToList()) + "]");
                     }
                 }
                 else
                 {
-                    _logger?.Trace($"Request not found. Index : {msg.Id.ToHex()}.");
+                    _logger?.Warn($"Request not found. Index : {msg.Id.ToHex()}.");
                 }
 
                 return request;
             }
             catch (Exception e)
             {
-                _logger?.Trace(e, "Exception while handling request message.");
+                _logger?.Error(e, "Exception while handling request message.");
                 return null;
             }
         }
