55ae1f39363ca85974d378d78748fd0b85a446ce
zhxymh <13552710076@126.com>
2018-09-14 12:17:08 +0800

    add AuthenticationFilter

---
+++ AElf.Management.Website/AuthenticationFilter.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using AElf.Management.Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AElf.Management.Website
+{
+    public class AuthenticationFilter: IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var signType = context.HttpContext.Request.Headers["auth-type"];
+            
+            if (signType == "apikey")
+            {
+                var sign = context.HttpContext.Request.Headers["sign"];
+                var chainId = context.ActionArguments["chainId"];
+                var method = context.HttpContext.Request.Method;
+                var timestamp = context.HttpContext.Request.Headers["timestamp"];
+                
+                context.Result = new JsonResult(new ApiEmptyResult(401, "Unauthorized"));
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+    }
+}
