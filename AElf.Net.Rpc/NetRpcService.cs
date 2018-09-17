280240d41883da75879d776f07edec4dd344aa48
samfrb <samos2x@outlook.com>
2018-09-17 19:53:25 +0800

    Network: add and remove peer rpc interface.

--- AElf.Net.Rpc/NetRpcService.cs
+++ AElf.Net.Rpc/NetRpcService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using AElf.Network.Data;
 using AElf.Network.Peers;
 using AElf.RPC;
 using Community.AspNetCore.JsonRpc;
@@ -15,6 +17,48 @@ namespace AElf.Net.Rpc
         public async Task<JObject> GetPeers()
         {
             return await Manager.GetPeers();
+        }
+
+        [JsonRpcMethod("add_peer", "address")]
+        public async Task<JObject> AddPeer(string address)
+        {
+            NodeData nodeData = null;
+            
+            try
+            {
+                nodeData = NodeData.FromString(address);
+            }
+            catch { }
+
+            if (nodeData == null)
+            {
+                throw new JsonRpcServiceException(-32602, "Invalid address");
+            }
+
+            await Task.Run(() => Manager.AddPeer(nodeData));
+            
+            return new JObject { ["result"] = true };
+        }
+        
+        [JsonRpcMethod("remove_peer", "address")]
+        public async Task<JObject> RemovePeer(string address)
+        {
+            NodeData nodeData = null;
+            
+            try
+            {
+                nodeData = NodeData.FromString(address);
+            }
+            catch { }
+
+            if (nodeData == null)
+            {
+                throw new JsonRpcServiceException(-32602, "Invalid address");
+            }
+
+            await Task.Run(() => Manager.RemovePeer(nodeData));
+            
+            return new JObject { ["result"] = true };
         }
     }
 }
