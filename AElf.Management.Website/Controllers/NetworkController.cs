f00577cdfa268c5f0853a2699f0a515870863ea7
zhxymh <13552710076@126.com>
2018-10-20 13:48:27 +0800

    remove network monitor

--- AElf.Management.Website/Controllers/NetworkController.cs
+++ AElf.Management.Website/Controllers/NetworkController.cs
@@ -26,22 +26,22 @@ namespace AElf.Management.Website.Controllers
             return new ApiResult<PeerResult>(result);
         }
         
-        [HttpGet]
-        [Route("pool/{chainId}")]
-        public ApiResult<PoolStateResult> PoolState(string chainId)
-        {
-            var result = _networkService.GetPoolState(chainId);
-            
-            return new ApiResult<PoolStateResult>(result);
-        }
-        
-        [HttpGet]
-        [Route("poolstatehistory/{chainId}")]
-        public ApiResult<List<PoolStateHistory>> StateHistory(string chainId)
-        {
-            var result = _networkService.GetPoolStateHistory(chainId);
-            
-            return new ApiResult<List<PoolStateHistory>>(result);
-        }
+//        [HttpGet]
+//        [Route("pool/{chainId}")]
+//        public ApiResult<PoolStateResult> PoolState(string chainId)
+//        {
+//            var result = _networkService.GetPoolState(chainId);
+//            
+//            return new ApiResult<PoolStateResult>(result);
+//        }
+//        
+//        [HttpGet]
+//        [Route("poolstatehistory/{chainId}")]
+//        public ApiResult<List<PoolStateHistory>> StateHistory(string chainId)
+//        {
+//            var result = _networkService.GetPoolStateHistory(chainId);
+//            
+//            return new ApiResult<List<PoolStateHistory>>(result);
+//        }
     }
 }
