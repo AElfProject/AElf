5f11d6a268ae861aac428ce31ed282c2db23d05c
zhxymh <13552710076@126.com>
2018-11-26 17:23:03 +0800

    add messge: BlockMinedAndStored

---
+++ AElf.Miner/EventMessages/BlockMinedAndStored.cs
@@ -0,0 +1,14 @@
+﻿using AElf.Kernel;
+
+namespace AElf.Miner.EventMessages
+{
+    public sealed class BlockMinedAndStored
+    {
+        public BlockMinedAndStored(IBlock block)
+        {
+            Block = block;
+        }
+
+        public IBlock Block { get; }
+    }
+}
