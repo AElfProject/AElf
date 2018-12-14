add8c3f5e9ab288754e8513296399d4650f32a9d
EanCuznaivy <eancuznaivy@gmail.com>
2018-12-14 20:44:44 +0800

    Interface of RPC.

--- AElf.SmartContract/Consensus/IElectionInfo.cs
+++ AElf.SmartContract/Consensus/IElectionInfo.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using AElf.Kernel;
+
 namespace AElf.SmartContract.Consensus
 {
     public interface IElectionInfo
     {
-        
+        bool IsCandidate(string publicKey);
+        Tickets GetTicketsInfo(string publicKey);
+        Round GetRoundNumber(ulong roundNumber);
+        List<string> GetCurrentMines();
     }
 }
