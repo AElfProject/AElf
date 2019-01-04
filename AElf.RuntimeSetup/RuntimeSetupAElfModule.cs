7bf6e3148a1ba3034fca1cb5a701edebe799e4a6
Loning <aloning@gmail.com>
2019-01-04 18:30:26 +0800

    change log4net adapter

--- AElf.RuntimeSetup/RuntimeSetupAElfModule.cs
+++ AElf.RuntimeSetup/RuntimeSetupAElfModule.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using apache.log4net.Extensions.Logging;
 using AElf.Modularity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -17,13 +16,7 @@ namespace AElf.RuntimeSetup
         {
             context.Services.AddLogging(builder =>
             {
-                builder.AddLog4Net(new Log4NetSettings()
-                {
-                    ConfigFile = Path.GetFullPath(Path.Combine(
-                        Path.GetDirectoryName(new Uri(typeof(RuntimeSetupAElfModule).GetTypeInfo().Assembly.CodeBase)
-                            .LocalPath) ?? string.Empty, "log4net.config"))
-                    //ConfigFile = Path.Combine(Directory.GetCurrentDirectory(),"log4net.config")
-                });
+                builder.AddLog4Net();
                 builder.SetMinimumLevel(LogLevel.Trace);
             });
         }
