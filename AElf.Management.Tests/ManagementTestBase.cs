d3c8994f65fd08a259c5f2153f271907db296a1f
zhxymh <13552710076@126.com>
2019-01-18 13:16:27 +0800

    remove unused method

--- AElf.Management.Tests/ManagementTestBase.cs
+++ AElf.Management.Tests/ManagementTestBase.cs
@@ -4,9 +4,5 @@ namespace AElf.Management.Tests
 {
     public class ManagementTestBase: AElfIntegratedTest<ManagementTestAElfModule>
     {
-        public new T GetRequiredService<T>()
-        {
-            return base.GetRequiredService<T>();
-        }
     }
 }
