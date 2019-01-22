9343e9ac97f2949c8f7001e05b7f209eed8cbdaa
samfrb <samos2x@outlook.com>
2019-01-22 15:19:03 +0800

    Two sided handshake calls.

--- AElf.Node/Protocol/NetworkService.cs
+++ AElf.Node/Protocol/NetworkService.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 using AElf.Common;
 using AElf.Configuration;
 using AElf.Cryptography.ECDSA;
+using AElf.Kernel;
+using AElf.Miner.EventMessages;
 using AElf.Network;
 using AElf.Network.Data;
+using AElf.Network.Peers;
 using AElf.Node.Protocol.Protobuf.Generated;
+using Easy.MessageHub;
 using Google.Protobuf;
 using Grpc.Core;
 using Microsoft.Extensions.Options;
@@ -25,7 +29,7 @@ namespace AElf.Node.Protocol
     {
         public ILogger<NetworkService> Logger { get; set; }
         
-        private List<Channel> _channels;
+        private List<Protobuf.Generated.PeerService.PeerServiceClient> _channels;
         
         private readonly NetworkOptions _networkOptions;
         private ECKeyPair _nodeKey;
@@ -35,13 +39,15 @@ namespace AElf.Node.Protocol
             Logger = NullLogger<NetworkService>.Instance;
             
             _networkOptions = options.Value;
-            _channels = new List<Channel>();
+            _channels = new List<Protobuf.Generated.PeerService.PeerServiceClient>();
             _nodeKey = NodeConfig.Instance.ECKeyPair;
         }
         
         public async Task Start()
         {
-            var p = new PeerService(_networkOptions.ListeningPort);
+            var p = new PeerService(Logger);
+            p.PeerAdded += POnPeerAdded;
+            p.SetHandshakeProvider(BuildHandshake);
             
             Server server = new Server
             {
@@ -64,7 +70,9 @@ namespace AElf.Node.Protocol
                         var client = new Protobuf.Generated.PeerService.PeerServiceClient(channel);
                         var hsk = BuildHandshake();
                         
-                        var resp = await client.AuthentifyAsync(hsk);
+                        var resp = await client.ConnectAsync(hsk);
+
+                        _channels.Add(client);
                     }
                     catch (Exception e)
                     {
@@ -75,6 +83,27 @@ namespace AElf.Node.Protocol
             else
             {
                 Logger.LogWarning("Boot nodes list is empty.");
+            }
+            
+            MessageHub.Instance.Subscribe<BlockMined>(async inBlock =>
+            {
+                if (inBlock?.Block == null)
+                {
+                    Logger.LogWarning("[event] Block null.");
+                    return;
+                }
+
+                byte[] blockHash = inBlock.Block.GetHash().DumpByteArray();
+
+                await BroadcastAnnounce(new Announcement {Id = ByteString.CopyFrom(blockHash)});
+            });
+        }
+
+        private void POnPeerAdded(object sender, EventArgs e)
+        {
+            if (e is PeerAddedEventArgs pe)
+            {
+                _channels.Add(pe.Client);
             }
         }
 
@@ -99,47 +128,105 @@ namespace AElf.Node.Protocol
             return hsk;
         }
 
+        public async Task BroadcastAnnounce(Announcement an)
+        {
+            foreach (var client in _channels)
+            {
+                await client.AnnounceAsync(an);
+            }
+        }
+
         public Task Stop()
         {
             return Task.FromResult(true);
         }
     }
+
+    public class PeerAddedEventArgs : EventArgs
+    {
+        public Protobuf.Generated.PeerService.PeerServiceClient Client;
+    }
     
     public class PeerService : Protobuf.Generated.PeerService.PeerServiceBase
     {
+        public event EventHandler PeerAdded;
+        
         private readonly int _localPort;
-        public ILogger<PeerService> Logger { get; set; }
+        public ILogger<NetworkService> Logger { get; set; }
         private Protobuf.Generated.PeerService.PeerServiceClient client;
+
+        private Func<Handshake> _handshakeProvider;
         
-        public PeerService(int localPort)
+        public PeerService(ILogger<NetworkService> logger)
         {
-            _localPort = localPort;
-            Logger = NullLogger<PeerService>.Instance;
+            Logger = logger;
         }
 
-        public override Task<Handshake> Authentify(Handshake request, ServerCallContext context)
-        {            
-            var nodeKey = NodeConfig.Instance.ECKeyPair;
+        public void SetHandshakeProvider(Func<Handshake> handshakeProvider)
+        {
+            _handshakeProvider = handshakeProvider;
+        }
+
+        /// <summary>
+        /// Used to initiate a connection. The provided payload should be the clients authentication
+        /// information. When receiving this call, protocol dictates you send the client your auth
+        /// information. The response says whether or not you can connect.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<AuthResponse> Connect(Handshake request, ServerCallContext context)
+        {
+            Logger?.LogTrace($"[{context.Peer}] has initiated a request");
             
-            var nd = new HandshakeData
+            try
             {
-                ListeningPort = _localPort,
-                PublicKey = ByteString.CopyFrom(nodeKey.PublicKey),
-                Version = GlobalConfig.ProtocolVersion,
-            };
+                Channel channel = new Channel(context.Peer.Split(":")[1] + ":" + request.HskData.ListeningPort, ChannelCredentials.Insecure);
+                client = new Protobuf.Generated.PeerService.PeerServiceClient(channel);
             
-            ECSigner signer = new ECSigner();
-            ECSignature sig = signer.Sign(nodeKey, SHA256.Create().ComputeHash(nd.ToByteArray()));
+                // todo verify auth
+                var hsk = _handshakeProvider();
+                var resp = client.Authentify(hsk);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
             
-            Channel channel = new Channel(context.Peer.Split(":")[1] + ":" + request.HskData.ListeningPort, ChannelCredentials.Insecure);
-            client = new Protobuf.Generated.PeerService.PeerServiceClient(channel);
+            // todo if resp ok
+            PeerAdded?.Invoke(this, new PeerAddedEventArgs { Client = client });
+            
+            return Task.FromResult(new AuthResponse());
         }
 
-        private async void RequestBlock(object o)
-        {
-            await client.RequestBlockAsync(new BlockRequest { BlockNumber = 5 });
+        /// <summary>
+        /// Second step of the connect/auth process.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<AuthResponse> Authentify(Handshake request, ServerCallContext context)
+        {   
+            // todo verify auth
+            Logger.LogTrace("Send connecting guy our auth...");
+            return Task.FromResult(new AuthResponse());
         }
-        
+
+        public override Task<Announcement> Announce(Announcement an, ServerCallContext context)
+        {
+            try
+            {
+                Logger.LogTrace($"Received announce {an.Id.ToByteArray().ToHex()} from {context.Peer}.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return Task.FromResult(an);
+        }
+
         public override Task<BlockReply> RequestBlock(BlockRequest request, ServerCallContext context)
         {
             return Task.Run(async () =>  {
