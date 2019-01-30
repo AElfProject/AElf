7eb9d63b0351c09114715f7044f6fe30cd840b75
Loning <aloning@gmail.com>
2019-01-31 06:26:05 +0800

    add light blockchain service

--- AElf.Kernel/Services/ILightBlockchainService.cs
+++ AElf.Kernel/Services/ILightBlockchainService.cs
@@ -7,12 +7,9 @@ namespace AElf.Kernel.Services
     public interface ILightBlockchainService
     {
         Task<bool> HasHeaderAsync(int chainId, Hash blockHash);
-        Task<bool> IsOnCanonical(int chainId, Hash blockId);
         Task AddHeadersAsync(int chainId, IEnumerable<IBlockHeader> headers);
-        Task<ulong> GetCurrentBlockHeightAsync(int chainId);
-        Task<Hash> GetCurrentBlockHashAsync(int chainId);
-        Task<IBlockHeader> GetHeaderByHashAsync(int chainId,Hash blockHash);
-        Task<IBlockHeader> GetHeaderByHeightAsync(int chainId, ulong height);
-        Task<Hash> GetCanonicalHashAsync(ulong height);
+        Task<Chain> GetChainAsync(int chainId);
+        Task<IBlockHeader> GetHeaderByHashAsync(int chainId, Hash blockHash);
+        Task<IBlockHeader> GetIrreversibleHeaderByHeightAsync(int chainId, long height);
     }
 }
