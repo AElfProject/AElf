d8a09dc58b606f771073ff6e23b4a7aab42218a3
gldeng <realgldeng@gmail.com>
2019-02-15 21:23:07 +0800

    Convert Authorization contract using sdk2

---
+++ AElf.Contracts.Authorization2/AuthorizationContract_Helper.cs
@@ -0,0 +1,14 @@
+using AElf.Kernel;
+
+namespace AElf.Contracts.Authorization2
+{
+    public partial class AuthorizationContract
+    {
+        private Miners GetMiners()
+        {
+            var roundNumber = State.ConsensusContract.GetCurrentRoundNumber();
+            var round = State.ConsensusContract.GetRoundInfo(roundNumber);
+            return round.RealTimeMinersInfo.Keys.ToMiners();
+        }
+    }
+}
