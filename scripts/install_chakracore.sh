549335d3bdf1b52f323eb41fbb19c413207b6d0b
Peng Rong <rongpeng@live.com>
2019-02-15 18:57:57 +0800

    Fix path error

--- scripts/install_chakracore.sh
+++ scripts/install_chakracore.sh
@@ -4,7 +4,6 @@ FILE_NAME="ChakraCore.dll"
 TO_FILE=$1${FILE_NAME}
 FROM_FILE=
 
-echo ${TO_FILE}
 if [[ -f "$TO_FILE" ]]; then
     echo "$TO_FILE exists, no need to download again"
     exit
@@ -31,6 +30,7 @@ GET_OS()
 
 GET_OS
 
+WORK_PATH=`pwd`
 SCRIPTS_PATH=$(cd "$(dirname "$0")";pwd)
 DOWNLOAD_PATH=${SCRIPTS_PATH}/.tmp/chakracore
 
@@ -42,4 +42,5 @@ if [[ ! -f "$CHAKRACORE_FILE" ]]; then
     bash ${SCRIPTS_PATH}/download_chakracore.sh 1_11_1
 fi
 
+cd ${WORK_PATH}
 cp ${CHAKRACORE_FILE} ${TO_FILE}
