7d7a16acfcae3a0b93899e90a146eeaef8653b1a
zhxymh <13552710076@126.com>
2019-02-15 14:45:49 +0800

    format name in wallet rpc

--- AElf.Wallet.Rpc/WalletRpcService.cs
+++ AElf.Wallet.Rpc/WalletRpcService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AElf.Common;
 using AElf.Common.Application;
 using AElf.Cryptography;
+using AElf.Cryptography.ECDSA;
 using AElf.Kernel;
 using AElf.RPC;
 using Anemonis.AspNetCore.JsonRpc;
@@ -42,52 +43,47 @@ namespace AElf.Wallet.Rpc
 
         #region Methods
 
-        [JsonRpcMethod("create_account", "password")]
+        [JsonRpcMethod("CreateAccount", "password")]
         public async Task<JObject> CreateAccount(string password)
         {
+            ECKeyPair keypair;
             try
             {
-                var keypair = await KeyStore.CreateAsync(password, _chainOptions.ChainId);
-                if (keypair != null)
-                {
-                    // todo warning return null for now
-                    return new JObject();
-                }
-
-                return new JObject
-                {
-                    ["address"] = "0x",
-                    ["error"] = "Failed"
-                };
+                keypair = await KeyStore.CreateAsync(password, _chainOptions.ChainId);
             }
-            catch (Exception e)
+            catch(Exception e)
             {
-                return new JObject
-                {
-                    ["error"] = e.ToString()
-                };
+                throw new JsonRpcServiceException(WalletRpcErrorConsts.CreateAccountFailed,
+                    WalletRpcErrorConsts.RpcErrorMessage[WalletRpcErrorConsts.CreateAccountFailed], e);
             }
+
+            var createResult = keypair != null;
+            return new JObject(createResult);
         }
 
-        [JsonRpcMethod("list_accounts")]
-        public async Task<List<string>> ListAccounts()
+        [JsonRpcMethod("ListAccounts")]
+        public async Task<JObject> ListAccounts()
         {
-            return await KeyStore.ListAccountsAsync();
+            var accounts = await KeyStore.ListAccountsAsync();
+            return JObject.FromObject(accounts);
         }
 
-        [JsonRpcMethod("sign_hash", "address", "password", "hash")]
+        [JsonRpcMethod("SignHash", "address", "password", "hash")]
         public async Task<JObject> SignHash(string address, string password, string hash)
         {
             var tryOpen = await KeyStore.OpenAsync(address, password);
             if (tryOpen == AElfKeyStore.Errors.WrongPassword)
             {
-                return new JObject
-                {
-                    ["error"] = "Wrong password."
-                };
+                throw new JsonRpcServiceException(WalletRpcErrorConsts.WrongPassword,
+                    WalletRpcErrorConsts.RpcErrorMessage[WalletRpcErrorConsts.WrongPassword]);
             }
 
             var kp = KeyStore.GetAccountKeyPair(address);
+            if (kp == null)
+            {
+                throw new JsonRpcServiceException(WalletRpcErrorConsts.AccountNotExist,
+                    WalletRpcErrorConsts.RpcErrorMessage[WalletRpcErrorConsts.AccountNotExist]);
+            }
 
             var toSig = ByteArrayHelpers.FromHexString(hash);
             // Sign the hash
@@ -97,7 +93,7 @@ namespace AElf.Wallet.Rpc
             // todo test
             return new JObject
             {
-                ["sig"] = signature
+                ["Signature"] = signature
             };
         }
 
