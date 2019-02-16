cb4586006d3f9bf50649a6414c6c77bf1e606a64
zhxymh <13552710076@126.com>
2019-02-16 11:51:05 +0800

    use new sdk context

--- AElf.Sdk.CSharp2/Context.cs
+++ AElf.Sdk.CSharp2/Context.cs
@@ -3,6 +3,7 @@ using AElf.Common;
 using AElf.Kernel;
 using System.Linq;
 using System.Reflection;
+using AElf.Cryptography;
 using AElf.Sdk.CSharp.ReadOnly;
 using AElf.Sdk.CSharp.State;
 using AElf.SmartContract;
@@ -37,8 +38,30 @@ namespace AElf.Sdk.CSharp
             TransactionContext.Trace.Logs.Add(logEvent.GetLogEvent(Self));
         }
 
+        public Transaction Transaction => TransactionContext.Transaction.ToReadOnly();
+        public Hash TransactionId => TransactionContext.Transaction.GetHash();
         public Address Sender => TransactionContext.Transaction.From.ToReadOnly();
         public Address Self => SmartContractContext.ContractAddress.ToReadOnly();
+        public Address Genesis => Address.Genesis.ToReadOnly();
+        public ulong CurrentHeight => TransactionContext.BlockHeight;
+        public DateTime CurrentBlockTime => TransactionContext.CurrentBlockTime;
+        public Hash PreviousBlockHash => TransactionContext.PreviousBlockHash.ToReadOnly();
+
+        public byte[] RecoverPublicKey(byte[] signature, byte[] hash)
+        {
+            var cabBeRecovered = CryptoHelpers.RecoverPublicKey(signature, hash, out var publicKey);
+            return !cabBeRecovered ? null : publicKey;
+        }
+
+        /// <summary>
+        /// Recovers the first public key signing this transaction.
+        /// </summary>
+        /// <returns>Public key byte array</returns>
+        public byte[] RecoverPublicKey()
+        {
+            return RecoverPublicKey(TransactionContext.Transaction.Sigs.First().ToByteArray(),
+                TransactionContext.Transaction.GetHash().DumpByteArray());
+        }
 
         public void SendInline(Address address, string methodName, params object[] args)
         {
@@ -54,6 +77,28 @@ namespace AElf.Sdk.CSharp
         public Block GetBlockByHeight(ulong height)
         {
             return (Block) BlockChain.GetBlockByHeightAsync(height, true).Result;
+        }
+
+        public bool VerifySignature(Transaction tx)
+        {
+            if (tx.Sigs == null || tx.Sigs.Count == 0)
+            {
+                return false;
+            }
+
+            if (tx.Sigs.Count == 1 && tx.Type != TransactionType.MsigTransaction)
+            {
+                var canBeRecovered = CryptoHelpers.RecoverPublicKey(tx.Sigs.First().ToByteArray(),
+                    tx.GetHash().DumpByteArray(), out var pubKey);
+                return canBeRecovered && Address.FromPublicKey(pubKey).Equals(tx.From);
+            }
+
+            return true;
+        }
+
+        public void SendDeferredTransaction(Transaction deferredTxn)
+        {
+            TransactionContext.Trace.DeferredTransaction = deferredTxn.ToByteString();
         }
     }
 }
