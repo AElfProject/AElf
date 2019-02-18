64e25f43ce706f23842a64a0c87a5dd555474251
samfrb <samos2x@outlook.com>
2019-02-18 17:50:43 +0800

    Fork download checks and logging improvement.

--- AElf.OS/Network/Jobs/ForkDownloadJob.cs
+++ AElf.OS/Network/Jobs/ForkDownloadJob.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Threading.Tasks;
 using AElf.Common;
 using AElf.Kernel;
 using AElf.Kernel.Services;
 using AElf.OS.Jobs;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 
@@ -27,19 +29,39 @@ namespace AElf.OS.Network.Jobs
 
         protected override async Task ExecuteAsync(ForkDownloadJobArgs args)
         {
-            var chain = await BlockchainService.GetChainAsync(ChainId);
-            
-            foreach (var hash in args.BlockHashes)
+            try
             {
-                var hasBlock = await BlockchainService.HasBlockAsync(ChainId, hash);
+                Logger?.LogDebug($"Starting download of {args.BlockHashes.Count} blocks from {args.Peer}.");
                 
-                if (hasBlock)
-                    continue; // todo review myabe no need to go further.
+                var chain = await BlockchainService.GetChainAsync(ChainId);
 
-                Block block = (Block)await NetworkService.GetBlockByHash(hash, args.Peer);
+                if (chain == null)
+                {
+                    Logger.LogError($"Failed to finish download of {args.BlockHashes.Count} blocks from {args.Peer}: chain not found.");
+                }
+            
+                foreach (var hash in args.BlockHashes)
+                {
+                    // Check that some other job didn't get this before.
+                    var hasBlock = await BlockchainService.HasBlockAsync(ChainId, hash);
+                
+                    if (hasBlock)
+                        continue; // todo review maybe no need to go further.
 
-                await BlockchainService.AddBlockAsync(ChainId, block);
-                await BlockchainService.AttachBlockToChainAsync(chain, block);
+                    // Query the peer
+                    Block block = (Block)await NetworkService.GetBlockByHash(hash, args.Peer);
+
+                    // Add to our chain
+                    await BlockchainService.AddBlockAsync(ChainId, block);
+                    await BlockchainService.AttachBlockToChainAsync(chain, block);
+                    
+                    Logger.LogDebug($"Added {block}.");
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, $"Failed to finish download job from {args.Peer}");
+                throw;
             }
         }
     }
