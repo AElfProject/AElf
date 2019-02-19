747defca1cef9910054a3cca965dca0ba0acba16
Loning <aloning@gmail.com>
2019-02-19 17:10:05 +0800

    remove projects

--- AElf.Sdk.CSharp.Tests/CSharpSdkTestBase.cs
+++
@@ -1,9 +0,0 @@
-using AElf.Database;
-using Volo.Abp;
-
-namespace AElf.Sdk.CSharp.Tests
-{
-    public class CSharpSdkTestBase : AElf.TestBase.AElfIntegratedTest<TestCSharpSdkAElfModule>
-    {
-    }
-}
