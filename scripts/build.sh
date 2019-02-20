51d7fe5004dba8fe6ab5382e9bc8d2dfcab9d9e8
loning <aloning@gmail.com>
2019-02-21 02:12:19 +0800

    commit code coverage

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-dotnet tool install --global coverlet.console
 dotnet restore -s "https://nuget.cdn.azure.cn/v3/index.json" -s "https://api.nuget.org/v3/index.json" "AElf.sln"
 dotnet build
 
@@ -9,7 +8,7 @@ if [ $? -ne 0 ] ; then
   exit 1
 fi
 
-dotnet test --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput='./result.xml'
+dotnet test --no-build /p:CollectCoverage=true /p:CoverletOutputFormat='json%2copencover' /p:CoverletOutput="../results/coverage" /p:MergeWith="../results/coverage.json"
 
 if [ $? -ne 0 ] ; then
   echo "Test failed."
