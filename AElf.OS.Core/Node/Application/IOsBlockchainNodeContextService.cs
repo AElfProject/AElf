c5a58e84daed400a5019b58c2ac8acdecaea88d7
Loning <aloning@gmail.com>
2019-02-20 20:34:56 +0800

    add IOsBlockchainNodeContextService

---
+++ AElf.OS.Core/Node/Application/IOsBlockchainNodeContextService.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using AElf.Kernel.Node.Application;
+using AElf.OS.Node.Domain;
+
+namespace AElf.OS.Node.Application
+{
+    public class OsBlockchainNodeContextStartDto
+    {
+        public BlockchainNodeContextStartDto BlockchainNodeContextStartDto { get; set; }
+    }
+    
+    public interface IOsBlockchainNodeContextService
+    {
+        Task<OsBlockchainNodeContext> StartAsync(OsBlockchainNodeContextStartDto dto);
+
+        Task StopAsync(OsBlockchainNodeContext blockchainNodeContext);
+    }
+    public class OsBlockchainNodeContextService: IOsBlockchainNodeContextService
+    {
+
+        private BlockchainNodeContextService _blockchainNodeContextService;
+
+        public OsBlockchainNodeContextService(BlockchainNodeContextService blockchainNodeContextService)
+        {
+            _blockchainNodeContextService = blockchainNodeContextService;
+        }
+
+        public async Task<OsBlockchainNodeContext> StartAsync(OsBlockchainNodeContextStartDto dto)
+        {
+            await _blockchainNodeContextService.StartAsync(dto.BlockchainNodeContextStartDto);
+        }
+
+        public async Task StopAsync(OsBlockchainNodeContext blockchainNodeContext)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
