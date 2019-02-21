bbfb46c1a56dad024edf902d23fcb7f79414abd5
Peng Rong <rongpeng@live.com>
2019-02-21 20:44:16 +0800

    Remove contains check for random run.

--- AElf.Kernel.Types.Tests/ChainHelpersTests.cs
+++ AElf.Kernel.Types.Tests/ChainHelpersTests.cs
@@ -22,9 +22,9 @@ namespace AElf.Kernel.Types.Tests
         [Fact]
         public void GetChainId_By_SerialNumber()
         {
-            var base58HashSet = new HashSet<string>();
-            var intHashSet = new HashSet<int>();
             // Have tested all the conditions (195112UL ~ 11316496UL), To save time, just do some random test
+            //var base58HashSet = new HashSet<string>();
+            //var intHashSet = new HashSet<int>();
             // for (var i = ; i < 11316496UL; i++)
             for (var i = 0; i < 1000; i++)
             {
@@ -33,8 +33,9 @@ namespace AElf.Kernel.Types.Tests
                 base58String.Length.ShouldBe(4);
                 var newChainId = ChainHelpers.ConvertBase58ToChainId(base58String);
                 newChainId.ShouldBe(chainId);
-                base58HashSet.Add(base58String).ShouldBe(true);
-                intHashSet.Add(newChainId).ShouldBe(true);
+                // Uncomment this for go through all conditions
+                // base58HashSet.Add(base58String).ShouldBe(true);
+                // intHashSet.Add(newChainId).ShouldBe(true);
             }
         }
     }
