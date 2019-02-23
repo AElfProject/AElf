1f235b717f874b10bdc3a43d4d37c6a9e494233c
swk <shiwk@tju.edu.cn>
2019-02-23 17:29:01 +0800

    Remove redundant classes.

--- AElf.Crosschain/ICrossChainDataProducerConsumerService.cs
+++
@@ -1,8 +0,0 @@
-namespace AElf.Crosschain
-{
-    public interface ICrossChainDataProducerConsumerService
-    {
-        (ICrossChainDataConsumer, ICrossChainDataProducer) CreateConsumerProducer(CommunicationContextDto communicationContextDto);
-        void UpdateRequestInterval(int interval);
-    }
-}
