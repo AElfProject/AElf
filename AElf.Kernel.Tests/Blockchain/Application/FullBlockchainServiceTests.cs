cadc97cb05519c004ee8322b57c9082029e3f44a
zhxymh <13552710076@126.com>
2019-02-25 10:52:48 +0800

    add full block chain service tests

---
+++ AElf.Kernel.Tests/Blockchain/Application/FullBlockchainServiceTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading.Tasks;
+using AElf.Common;
+using AElf.Kernel.Blockchain.Application;
+using AElf.Kernel.Blockchain.Events;
+using Google.Protobuf.WellKnownTypes;
+using Shouldly;
+using Volo.Abp.EventBus.Local;
+using Xunit;
+
+namespace AElf.Kernel
+{
+    public class FullBlockchainServiceTests: AElfKernelTestBase
+    {
+        private readonly IFullBlockchainService _fullBlockchainService;
+        private readonly ILocalEventBus _localEventBus;
+        
+        public FullBlockchainServiceTests()
+        {
+            _fullBlockchainService = GetRequiredService<IFullBlockchainService>();
+            _localEventBus = GetRequiredService<ILocalEventBus>();
+        }
+
+        [Fact]
+        public async Task Should_Create_Chain_Success()
+        {
+            var chainId = 1;
+
+            var args = new BestChainFoundEvent();
+            _localEventBus.Subscribe<BestChainFoundEvent>(message =>
+            {
+                args = message;
+                return Task.CompletedTask;
+            });
+            
+            var block = new Block();
+            block.Header = new BlockHeader
+            {
+                Height = GlobalConfig.GenesisBlockHeight,
+                PreviousBlockHash = Hash.Genesis,
+                ChainId = chainId,
+                Time = Timestamp.FromDateTime(DateTime.UtcNow)
+            };
+            block.Body = new BlockBody();
+            await _fullBlockchainService.CreateChainAsync(chainId, block);
+
+            var chain = await _fullBlockchainService.GetChainAsync(chainId);
+            chain.Id.ShouldBe(chainId);
+
+            args.ChainId.ShouldBe(chainId);
+        }
+    }
+}
--- AElf.Kernel.Tests/KernelTestAElfModule.cs
+++ AElf.Kernel.Tests/KernelTestAElfModule.cs
@@ -1,13 +1,21 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using AElf.Database;
+using AElf.Kernel.Blockchain.Application;
 using AElf.Kernel.Infrastructure;
 using AElf.Modularity;
 using AElf.TestBase;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Volo.Abp;
+using Volo.Abp.EventBus;
 using Volo.Abp.Modularity;
 
 namespace AElf.Kernel
 {
-    [DependsOn(typeof(CoreKernelAElfModule),
+    [DependsOn(
+        typeof(AbpEventBusModule),
+        typeof(CoreKernelAElfModule),
         typeof(TestBaseAElfModule))]
     public class KernelTestAElfModule : AElfModule
     {
@@ -17,6 +25,14 @@ namespace AElf.Kernel
 
             services.AddKeyValueDbContext<BlockchainKeyValueDbContext>(o => o.UseInMemoryDatabase());
             services.AddKeyValueDbContext<StateKeyValueDbContext>(o => o.UseInMemoryDatabase());
+
+            var mockBlockExecutingService = new Mock<IBlockExecutingService>();
+            mockBlockExecutingService.Setup(m => m.ExecuteBlockAsync(It.IsAny<int>(), It.IsAny<BlockHeader>(),
+                    It.IsAny<IEnumerable<Transaction>>()))
+                .Returns<int, BlockHeader, IEnumerable<Transaction>>((chainId, blockHeader, nonCancellableTransactions)
+                    => Task.FromResult(new Block {Header = blockHeader}));
+
+            services.AddTransient<IBlockExecutingService>(p => mockBlockExecutingService.Object);
         }
 
         public override void OnPreApplicationInitialization(ApplicationInitializationContext context)
