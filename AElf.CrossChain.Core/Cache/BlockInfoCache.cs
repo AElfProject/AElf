0f92ec241b2497bc23ebb305dadf7619ffa56193
swk <shiwk@tju.edu.cn>
2019-02-26 20:18:27 +0800

    Improve cross chain BlockInfoCache.

--- AElf.CrossChain.Core/Cache/BlockInfoCache.cs
+++ AElf.CrossChain.Core/Cache/BlockInfoCache.cs
@@ -11,26 +11,35 @@ namespace AElf.CrossChain
             new BlockingCollection<IBlockInfo>(new ConcurrentQueue<IBlockInfo>());
 
         private Queue<IBlockInfo> CachedIndexedBlockInfoQueue { get; } = new Queue<IBlockInfo>();
-        private readonly int _irreversible;
-        private readonly int _cachedBoundedCapacity;
-        public ulong TargetChainHeight { get; set; }
+        private static readonly int CacheSizeLimit = CrossChainConsts.MinimalBlockInfoCacheThreshold;
+        private static readonly int CachedBoundedCapacity = Math.Max(CrossChainConsts.MaximalCountForIndexingSideChainBlock,
+                                                          CrossChainConsts.MaximalCountForIndexingParentChainBlock) * CacheSizeLimit;
+        private readonly ulong _initTargetHeight;
+        public ulong TargetChainHeight
+        {
+            get
+            {
+                var lastQueuedHeight = LastOneHeightInQueue();
+                if (lastQueuedHeight != 0)
+                    return lastQueuedHeight + 1;
+                var lastCachedHeight = LastOneInCache()?.Height ?? 0;
+                if (lastCachedHeight != 0)
+                    return lastCachedHeight + 1;
+                return _initTargetHeight;
+            }
+        }
 
         public BlockInfoCache(ulong chainHeight)
         {
-            _irreversible = CrossChainConsts.MinimalBlockInfoCacheThreshold;
-            _cachedBoundedCapacity = Math.Max(CrossChainConsts.MaximalCountForIndexingSideChainBlock,
-                                         CrossChainConsts.MaximalCountForIndexingParentChainBlock) * _irreversible;
-            TargetChainHeight = chainHeight;
+            _initTargetHeight = chainHeight;
         }
 
         public bool TryAdd(IBlockInfo blockInfo)
         {
-            var lastOne = ToBeIndexedBlockInfoQueue.LastOrDefault();
-            if (lastOne != null && blockInfo.Height != TargetChainHeight)
+            // thread unsafe in some extreme cases, but it can be covered with caching mechanism.
+            if (blockInfo.Height != TargetChainHeight)
                 return false;
             var res = ToBeIndexedBlockInfoQueue.TryAdd(blockInfo);
-            if (res)
-                TargetChainHeight = blockInfo.Height + 1;
             return res;
         }
         
@@ -39,52 +48,76 @@ namespace AElf.CrossChain
         /// </summary>
         /// <param name="height">Height of block info needed</param>
         /// <param name="blockInfo"></param>
-        /// <param name="isCacheSizeLimited">Use <see cref="_cachedBoundedCapacity"/> as cache count threshold if true.</param>
+        /// <param name="isCacheSizeLimited">Use <see cref="CachedBoundedCapacity"/> as cache count threshold if true.</param>
         /// <returns></returns>
         public bool TryTake(ulong height, out IBlockInfo blockInfo, bool isCacheSizeLimited)
         {
-            var first = First();
+            // clear outdated data
+            var cachedInQueue = CacheBlockInfoBeforeHeight(height);
             // isCacheSizeLimited means minimal caching size , for most nodes have this block.
-            if (first != null 
-                && first.Height == height && 
-                !(isCacheSizeLimited && ToBeIndexedBlockInfoQueue.LastOrDefault()?.Height < height + (ulong) _irreversible))
+            
+            if (cachedInQueue && !(isCacheSizeLimited && LastOneHeightInQueue() < height + (ulong) CacheSizeLimit))
             {
-                var res = ToBeIndexedBlockInfoQueue.TryTake(out blockInfo, CrossChainConsts.WaitingIntervalInMillisecond);
+                var res = ToBeIndexedBlockInfoQueue.TryTake(out blockInfo, 
+                    CrossChainConsts.WaitingIntervalInMillisecond);
                 if(res)
                     CacheBlockInfo(blockInfo);
-                
                 return res;
             }
             
             // this is because of rollback 
-            blockInfo = null;
-            blockInfo = CachedIndexedBlockInfoQueue.FirstOrDefault(c => c.Height == height);
+            blockInfo = LastOneInCache(height);
             if (blockInfo != null)
                 return !isCacheSizeLimited ||
                        ToBeIndexedBlockInfoQueue.Count + CachedIndexedBlockInfoQueue.Count(ci => ci.Height >= height) 
-                       >= _irreversible;
+                       >= CacheSizeLimit;
             
             return false;
         }
-        
+
         /// <summary>
         /// Return first element in cached queue.
         /// </summary>
         /// <returns></returns>
-        private IBlockInfo First()
+        private IBlockInfo LastOneInCache(ulong height = 0)
         {
-            return ToBeIndexedBlockInfoQueue.FirstOrDefault();
+            return height == 0 ? CachedIndexedBlockInfoQueue.FirstOrDefault() 
+                : CachedIndexedBlockInfoQueue.FirstOrDefault(c => c.Height == height);
+        }
+
+        private ulong LastOneHeightInQueue()
+        {
+            return ToBeIndexedBlockInfoQueue.LastOrDefault()?.Height ?? 0;
+        }
+        
+        /// <summary>
+        /// Cache outdated data. The block with height lower than <paramref name="height"/> is outdated.
+        /// </summary>
+        /// <param name="height"></param>
+        private bool CacheBlockInfoBeforeHeight(ulong height)
+        {
+            while (true)
+            {
+                var blockInfo = ToBeIndexedBlockInfoQueue.FirstOrDefault();
+                if (blockInfo == null || blockInfo.Height > height)
+                    return false;
+                if (blockInfo.Height == height)
+                    return true;
+                var res = ToBeIndexedBlockInfoQueue.TryTake(out blockInfo, CrossChainConsts.WaitingIntervalInMillisecond);
+                if (res)
+                    CacheBlockInfo(blockInfo);
+            }
         }
         
         /// <summary>
         /// Cache block info lately removed.
-        /// Dequeue one element if the cached count reaches <see cref="_cachedBoundedCapacity"/>
+        /// Dequeue one element if the cached count reaches <see cref="CachedBoundedCapacity"/>
         /// </summary>                                                   
         /// <param name="blockInfo"></param>
         private void CacheBlockInfo(IBlockInfo blockInfo)
         {
             CachedIndexedBlockInfoQueue.Enqueue(blockInfo);
-            if (CachedIndexedBlockInfoQueue.Count <= _cachedBoundedCapacity)
+            if (CachedIndexedBlockInfoQueue.Count <= CachedBoundedCapacity)
                 return;
             CachedIndexedBlockInfoQueue.Dequeue();
         }
