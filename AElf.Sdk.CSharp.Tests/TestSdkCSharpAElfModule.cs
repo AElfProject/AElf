8f1d6ad9025b33967e95cfbdf74a6507a7694ce0
zhxymh <13552710076@126.com>
2019-02-28 12:30:28 +0800

    add context tests

--- AElf.Sdk.CSharp.Tests/TestSdkCSharpAElfModule.cs
+++ AElf.Sdk.CSharp.Tests/TestSdkCSharpAElfModule.cs
@@ -1,10 +1,15 @@
-﻿using AElf.Database;
+﻿using System.Threading.Tasks;
+using AElf.Common;
+using AElf.Database;
+using AElf.Kernel;
 using AElf.Kernel.Infrastructure;
 using AElf.Kernel.SmartContract;
+using AElf.Kernel.SmartContract.Application;
 using AElf.Kernel.SmartContract.Contexts;
 using AElf.Modularity;
 using AElf.TestBase;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Volo.Abp.Modularity;
 
 namespace AElf.Sdk.CSharp.Tests
@@ -13,7 +18,7 @@ namespace AElf.Sdk.CSharp.Tests
         typeof(DatabaseAElfModule),
         typeof(TestBaseAElfModule),
         typeof(SmartContractAElfModule))]
-    public class TestSdkCSharpAElfModule: AElfModule
+    public class TestSdkCSharpAElfModule : AElfModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
@@ -21,6 +26,22 @@ namespace AElf.Sdk.CSharp.Tests
             context.Services.AddKeyValueDbContext<BlockchainKeyValueDbContext>(o => o.UseInMemoryDatabase());
             context.Services.AddKeyValueDbContext<StateKeyValueDbContext>(o => o.UseInMemoryDatabase());
             services.AddTransient<IStateProviderFactory, StateProviderFactory>();
+
+            services.AddTransient<ISmartContractService>(p =>
+            {
+                var mockService = new Mock<ISmartContractService>();
+                mockService
+                    .Setup(m => m.DeployContractAsync(It.IsAny<int>(), It.IsAny<Address>(),
+                        It.IsAny<SmartContractRegistration>(),
+                        It.IsAny<bool>()))
+                    .Returns(Task.CompletedTask);
+                mockService
+                    .Setup(m => m.UpdateContractAsync(It.IsAny<int>(), It.IsAny<Address>(),
+                        It.IsAny<SmartContractRegistration>(),
+                        It.IsAny<bool>()))
+                    .Returns(Task.CompletedTask);
+                return mockService.Object;
+            });
         }
     }
 }
