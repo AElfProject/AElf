128a498acc7548c40da79cac9d2f82a856e2fc35
Eric Shu <blackeye286@126.com>
2019-02-28 10:12:38 +0800

    basic type convert test

---
+++ AElf.Types.CSharp.Tests/ConversionExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf;
+using Xunit;
+
+namespace AElf.Types.CSharp
+{
+    public class ConversionExtensionTests
+    {
+        [Fact]
+        public void ByteString_Convertion()
+        {
+            var bytes = new byte[]
+            {
+                125, 33, 27, 37, 202, 102, 171, 207, 118, 196, 214, 99, 224, 148, 157, 25, 230, 96, 125, 28, 227, 78, 1, 228, 24, 161, 56, 125, 186, 214
+            };
+            var byteString = ByteString.CopyFrom(bytes);
+            var newBytes = byteString.DeserializeToBytes();
+            Assert.Equal(newBytes, bytes);
+        }
+    }
+}
