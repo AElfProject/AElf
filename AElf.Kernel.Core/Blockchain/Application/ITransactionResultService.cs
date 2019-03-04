53d4db3c5f5633ff56b4481a2d09794478042884
gldeng <realgldeng@gmail.com>
2019-03-04 12:35:50 +0800

    Implement ITransactionResultService

---
+++ AElf.Kernel.Core/Blockchain/Application/ITransactionResultService.cs
@@ -0,0 +1,87 @@
+using System.Threading.Tasks;
+using AElf.Common;
+using AElf.Kernel.Account.Application;
+using AElf.Kernel.Blockchain.Domain;
+
+namespace AElf.Kernel.Blockchain.Application
+{
+    public interface ITransactionResultSettingService
+    {
+        Task AddTransactionResultAsync(TransactionResult transactionResult, BlockHeader blockHeader);
+    }
+
+    public interface ITransactionResultGettingService
+    {
+        Task<TransactionResult> GetTransactionResultAsync(Hash transactionId);
+    }
+
+    public interface ITransactionBlockIndexSettingService
+    {
+        Task SetTransactionBlockIndexAsync(Hash transactionId, TransactionBlockIndex transactionBlockIndex);
+    }
+
+    public interface ITransactionResultService : ITransactionResultSettingService, ITransactionResultGettingService,
+        ITransactionBlockIndexSettingService
+    {
+    }
+
+    public class TransactionResultService : ITransactionResultService
+    {
+        private readonly ITransactionResultManager _transactionResultManager;
+        private readonly ITransactionBlockIndexManager _transactionBlockIndexManager;
+        private readonly IBlockchainService _blockchainService;
+
+        public TransactionResultService(ITransactionResultManager transactionResultManager,
+            ITransactionBlockIndexManager transactionBlockIndexManager, IBlockchainService blockchainService)
+        {
+            _transactionResultManager = transactionResultManager;
+            _transactionBlockIndexManager = transactionBlockIndexManager;
+            _blockchainService = blockchainService;
+        }
+
+        public async Task AddTransactionResultAsync(TransactionResult transactionResult, BlockHeader blockHeader)
+        {
+            var disambiguatingHash = blockHeader.IsMined() ? blockHeader.GetHash() : blockHeader.GetPreMiningHash();
+            await _transactionResultManager.AddTransactionResultAsync(transactionResult, disambiguatingHash);
+        }
+
+        public async Task<TransactionResult> GetTransactionResultAsync(Hash transactionId)
+        {
+            var transactionBlockIndex =
+                await _transactionBlockIndexManager.GetTransactionBlockIndexAsync(transactionId);
+            if (transactionBlockIndex != null)
+            {
+                return await _transactionResultManager.GetTransactionResultAsync(transactionId,
+                    transactionBlockIndex.BlockHash);
+            }
+
+            var chain = await _blockchainService.GetChainAsync();
+            var hash = chain.BestChainHash;
+            while (hash != chain.LastIrreversibleBlockHash)
+            {
+                var result = await _transactionResultManager.GetTransactionResultAsync(transactionId, hash);
+                if (result != null)
+                {
+                    return result;
+                }
+
+                var header = await _blockchainService.GetBlockHeaderByHashAsync(hash);
+                result = await _transactionResultManager.GetTransactionResultAsync(transactionId,
+                    header.GetPreMiningHash());
+                if (result != null)
+                {
+                    return result;
+                }
+
+                hash = header.PreviousBlockHash;
+            }
+
+            return null;
+        }
+
+        public async Task SetTransactionBlockIndexAsync(Hash transactionId, TransactionBlockIndex transactionBlockIndex)
+        {
+            await _transactionBlockIndexManager.SetTransactionBlockIndexAsync(transactionId, transactionBlockIndex);
+        }
+    }
+}
--- AElf.Kernel.Core/Blockchain/Domain/ITransactionResultManager.cs
+++ AElf.Kernel.Core/Blockchain/Domain/ITransactionResultManager.cs
@@ -1,11 +1,33 @@
 ﻿using System.Threading.Tasks;
 using AElf.Common;
+using AElf.Kernel.Blockchain.Infrastructure;
 
 namespace AElf.Kernel.Blockchain.Domain
 {
     public interface ITransactionResultManager
     {
-        Task AddTransactionResultAsync(TransactionResult tr);
-        Task<TransactionResult> GetTransactionResultAsync(Hash txId);
+        Task AddTransactionResultAsync(TransactionResult transactionResult, Hash disambiguationHash);
+        Task<TransactionResult> GetTransactionResultAsync(Hash txId, Hash disambiguationHash);
+    }
+
+    public class TransactionResultManager : ITransactionResultManager
+    {
+        private readonly IBlockchainStore<TransactionResult> _transactionResultStore;
+
+        public TransactionResultManager(IBlockchainStore<TransactionResult> transactionResultStore)
+        {
+            _transactionResultStore = transactionResultStore;
+        }
+
+        public async Task AddTransactionResultAsync(TransactionResult transactionResult, Hash disambiguationHash)
+        {
+            await _transactionResultStore.SetAsync(transactionResult.TransactionId.Xor(disambiguationHash).ToHex(),
+                transactionResult);
+        }
+
+        public async Task<TransactionResult> GetTransactionResultAsync(Hash txId, Hash disambiguationHash)
+        {
+            return await _transactionResultStore.GetAsync(txId.Xor(disambiguationHash).ToHex());
+        }
     }
 }
