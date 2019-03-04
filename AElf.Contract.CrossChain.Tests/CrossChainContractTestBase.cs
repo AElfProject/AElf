6af4211bc55b8d1747831038aba7a0325f9fc1a7
swk <shiwk@tju.edu.cn>
2019-03-04 20:44:21 +0800

    Remove cross chain types from Types.CSharp.

--- AElf.Contract.CrossChain.Tests/CrossChainContractTestBase.cs
+++ AElf.Contract.CrossChain.Tests/CrossChainContractTestBase.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AElf.Common;
 using AElf.Contracts.TestBase;
+using AElf.CrossChain;
 using AElf.Cryptography.ECDSA;
 using AElf.Kernel;
 using AElf.TestBase;
 using AElf.Types.CSharp;
+using Google.Protobuf;
 using Shouldly;
 using Volo.Abp.Threading;
 
@@ -41,13 +43,37 @@ namespace AElf.Contract.CrossChain.Tests
                 callOwner, CrossChainContractAddress);
         }
 
-        protected async Task Initialize(ulong tokenAmount)
+        protected async Task Initialize(ulong tokenAmount, int parentChainId = 0)
         {
             var tx1 = ContractTester.GenerateTransaction(TokenContractAddress, "Initialize",
                 "ELF", "elf token", tokenAmount, 2U);
             var tx2 = ContractTester.GenerateTransaction(CrossChainContractAddress, "Initialize",
-                ConsensusContractAddress, TokenContractAddress, AuthorizationContractAddress);
+                ConsensusContractAddress, TokenContractAddress, AuthorizationContractAddress, parentChainId == 0 ? ChainHelpers.GetRandomChainId() : parentChainId);
             await ContractTester.MineABlockAsync(new List<Transaction> {tx1, tx2});
+        }
+
+        protected async Task<int> InitAndCreateSideChain(int parentChainId = 0)
+        {
+            await Initialize(1000, parentChainId);
+            ulong lockedTokenAmount = 10;
+            await ApproveBalance(lockedTokenAmount);
+            var sideChainInfo = new SideChainInfo
+            {
+                SideChainStatus = SideChainStatus.Apply,
+                ContractCode = ByteString.Empty,
+                IndexingPrice = 1,
+                Proposer = CrossChainContractTestHelper.GetAddress(),
+                LockedTokenAmount = lockedTokenAmount
+            };
+            
+            var tx1 = ContractTester.GenerateTransaction(CrossChainContractAddress, "RequestChainCreation",
+                sideChainInfo);
+            await ContractTester.MineABlockAsync(new List<Transaction> {tx1});
+            var chainId = ChainHelpers.GetChainId(1);
+            var tx2 = ContractTester.GenerateTransaction(CrossChainContractAddress, "CreateSideChain",
+                    ChainHelpers.ConvertChainIdToBase58(chainId));
+            await ContractTester.MineABlockAsync(new List<Transaction> {tx2});
+            return chainId;
         }
     }
 }
---
+++ AElf.Contract.CrossChain.Tests/CrossChainIndexingActionTest.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using AElf.Common;
+using AElf.CrossChain;
+using Xunit;
+
+namespace AElf.Contract.CrossChain.Tests
+{
+    public class CrossChainIndexingActionTest : CrossChainContractTestBase
+    {
+        [Fact]
+        public async Task RecordCrossChainData()
+        {
+            int parentChainId = 123;
+            var sideChainId = await InitAndCreateSideChain(parentChainId);
+            var fakeSideChainBlockHash = Hash.FromString("sideChainBlockHash");
+            var fakeTxMerkleTreeRoot = Hash.FromString("txMerkleTreeRoot");
+            var sideChainBlockData = new SideChainBlockData
+            {
+                BlockHeaderHash = fakeSideChainBlockHash,
+                SideChainHeight = 1,
+                SideChainId = sideChainId,
+                TransactionMKRoot = fakeTxMerkleTreeRoot
+            };
+            var parentChainBlockData = new ParentChainBlockData
+            {
+                Root = new ParentChainBlockRootInfo
+                {
+                    ParentChainId = parentChainId,
+                    ParentChainHeight = 1
+                }
+            };
+            var crossChainBlockData = new CrossChainBlockData
+            {
+                ParentChainBlockData = {parentChainBlockData},
+                SideChainBlockData = { sideChainBlockData}
+            };
+            
+        }
+    }
+}
