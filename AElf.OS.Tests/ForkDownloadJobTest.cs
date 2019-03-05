76f1d466c03281b6d18c81399e155b3e9dc315f0
samfrb <samos2x@outlook.com>
2019-03-05 18:36:35 +0800

    Test for job.

---
+++ AElf.OS.Tests/ForkDownloadJobTest.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using AElf.Common;
+using AElf.Kernel.Blockchain.Application;
+using AElf.Kernel.SmartContractExecution.Application;
+using AElf.OS.Jobs;
+using AElf.OS.Network;
+using AElf.OS.Network.Application;
+using AElf.OS.Network.Infrastructure;
+using AElf.OS.Tests.Network;
+using Microsoft.Extensions.Options;
+using Shouldly;
+using Xunit;
+
+namespace AElf.OS.Tests
+{
+    public class ForkDownloadJobTest : NetTestBase
+    {
+        private IBlockchainService _blockChainService;
+        private IPeerPool _peerPool;
+        private IBlockchainExecutingService _blockExecService;
+        private INetworkService _netService;
+
+        private ForkDownloadJob _job;
+
+        public ForkDownloadJobTest()
+        {
+            _blockChainService = GetRequiredService<IBlockchainService>();
+            _peerPool = GetRequiredService<IPeerPool>();
+            _blockExecService = GetRequiredService<IBlockchainExecutingService>();
+            _netService = GetRequiredService<INetworkService>();
+            
+            // Build download job because it's difficult to injection for this. 
+            _job = new ForkDownloadJob();
+            _job.BlockchainService = _blockChainService;
+            _job.BlockchainExecutingService = _blockExecService;
+            _job.NetworkService = _netService;
+            _job.NetworkOptions = GetRequiredService<IOptionsSnapshot<NetworkOptions>>();
+        }
+
+        [Fact]
+        public async Task ExecSyncJob_ShouldSyncChain()
+        {
+            var initialState = await _blockChainService.GetChainAsync();
+            var genHash = initialState.LongestChainHash;
+            
+            _job.Execute(new ForkDownloadJobArgs { BlockHash = genHash.DumpByteArray(), BlockHeight = 3 });
+            
+            var chain = await _blockChainService.GetChainAsync();
+            chain.LongestChainHeight.ShouldBe<ulong>(6);
+        }
+    }
+}
--- AElf.OS.Tests/Network/NetTestAElfModule.cs
+++ AElf.OS.Tests/Network/NetTestAElfModule.cs
@@ -1,13 +1,97 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AElf.Common;
+using AElf.CrossChain;
+using AElf.Kernel;
+using AElf.Kernel.Blockchain.Application;
+using AElf.Kernel.Blockchain.Domain;
+using AElf.Kernel.SmartContractExecution.Application;
 using AElf.Modularity;
+using AElf.OS.Network;
+using AElf.OS.Network.Infrastructure;
 using AElf.TestBase;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NSubstitute;
+using Volo.Abp;
 using Volo.Abp.EventBus;
 using Volo.Abp.Modularity;
 
 namespace AElf.OS.Tests.Network
 {
-    [DependsOn(typeof(TestBaseAElfModule), typeof(AbpEventBusModule))]
+    [DependsOn(typeof(TestBaseAElfModule), typeof(AbpEventBusModule), typeof(TestsOSAElfModule), typeof(TestBaseKernelAElfModule))]
     public class NetTestAElfModule : AElfModule
     {
+        private static int height = 0;
+        
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddTransient<IBlockchainExecutingService>(p =>
+            {
+                var mockExec = new Mock<IBlockchainExecutingService>();
+                mockExec.Setup(exec =>
+                        exec.ExecuteBlocksAttachedToLongestChain(It.IsAny<Chain>(),
+                            It.IsAny<BlockAttachOperationStatus>()))
+                    .Returns<Chain, BlockAttachOperationStatus>((c, a) => Task.FromResult(new List<ChainBlockLink>()));
+                return mockExec.Object;
+            });
 
+            Configure<NetworkOptions>(m => new NetworkOptions());
+            
+            // mock a peer
+            Mock<IPeer> peerMock = new Mock<IPeer>();
+            
+            peerMock.Setup(p => p.GetBlocksAsync(It.IsAny<Hash>(), It.IsAny<int>()))
+                .Returns<Hash, int>((hash, cnt) =>
+                {
+                    var blockList = new List<Block>();
+                    
+                    var genesis = new Block 
+                    {
+                        Header = new BlockHeader { Height = ChainConsts.GenesisBlockHeight, PreviousBlockHash = Hash.Genesis },
+                        Body = new BlockBody()
+                    };
+
+                    var h = genesis.GetHash();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        var blk = new Block 
+                        {
+                            Header = new BlockHeader { Height = (ulong)i+2, PreviousBlockHash = h }, 
+                            Body = new BlockBody()
+                        };
+                        
+                        blockList.Add(blk);
+                        h = blk.GetHash();
+                    }
+                                        
+                    return Task.FromResult(blockList);
+                });
+            
+            Mock<IPeerPool> peerPoolMock = new Mock<IPeerPool>();
+            peerPoolMock.Setup(p => p.FindPeer(It.IsAny<string>(), It.IsAny<byte[]>()))
+                .Returns<string, byte[]>((adr, pubkey) => peerMock.Object);
+            peerPoolMock.Setup(p => p.GetPeers())
+                .Returns(new List<IPeer> { peerMock.Object });
+
+            context.Services.AddSingleton<IPeerPool>(peerPoolMock.Object);
+            // mock the pool
+        }
+        
+        public override void OnPreApplicationInitialization(ApplicationInitializationContext context)
+        {
+            //init test data here
+            var blockChainService = context.ServiceProvider.GetService<IBlockchainService>();
+            
+            var genesis = new Block {
+                Header = new BlockHeader {
+                    Height = ChainConsts.GenesisBlockHeight,
+                    PreviousBlockHash = Hash.Genesis,
+                },
+                Body = new BlockBody()
+            };
+            
+            blockChainService.CreateChainAsync(genesis);
+        }
     }
 }
