e23a75a33e34ce2ef5c1ea9e98c5515839034b19
zhxymh <13552710076@126.com>
2019-03-06 11:17:50 +0800

    remove unused using

--- AElf.Kernel.SmartContractExecution.Tests/SmartContractExecutionExecutingTestAElfModule.cs
+++ AElf.Kernel.SmartContractExecution.Tests/SmartContractExecutionExecutingTestAElfModule.cs
@@ -1,19 +1,11 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using AElf.Database;
-using AElf.Kernel.Infrastructure;
 using AElf.Kernel.SmartContractExecution.Application;
-using AElf.Kernel.SmartContractExecution.Scheduling;
 using AElf.Modularity;
-using AElf.TestBase;
-using Castle.DynamicProxy.Generators;
 using Google.Protobuf;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
-using Volo.Abp.EventBus;
 using Volo.Abp.Modularity;
 
 namespace AElf.Kernel.SmartContractExecution
