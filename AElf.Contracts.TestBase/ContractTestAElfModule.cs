2a04bd840ae776b404fb90d5737d530d122a806b
Loning <aloning@gmail.com>
2019-03-07 19:27:46 +0800

    fix unit tests

--- AElf.Contracts.TestBase/ContractTestAElfModule.cs
+++ AElf.Contracts.TestBase/ContractTestAElfModule.cs
@@ -9,9 +9,12 @@ using AElf.Kernel.Infrastructure;
 using AElf.Kernel.SmartContract.Application;
 using AElf.Kernel.Tests;
 using AElf.Modularity;
+using AElf.OS;
+using AElf.OS.Network.Infrastructure;
 using AElf.Runtime.CSharp;
 using Google.Protobuf;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Volo.Abp;
 using Volo.Abp.Modularity;
 
@@ -19,13 +22,15 @@ namespace AElf.Contracts.TestBase
 {
     [DependsOn(
         typeof(CSharpRuntimeAElfModule),
-        typeof(KernelTestAElfModule)
+        typeof(KernelTestAElfModule),
+        typeof(CoreOSAElfModule)
     )]
     public class ContractTestAElfModule : AElfModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-
+            var services = context.Services;
+            services.AddSingleton<IAElfNetworkServer>(o => Mock.Of<IAElfNetworkServer>());
         }
     }
 }
