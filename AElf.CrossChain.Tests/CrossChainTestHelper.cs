999dff325c17b53f4d8690bb36387039300544d4
swk <shiwk@tju.edu.cn>
2019-03-07 17:15:03 +0800

    Refactor cross chain test module.

--- AElf.CrossChain.Tests/CrossChainTestHelper.cs
+++ AElf.CrossChain.Tests/CrossChainTestHelper.cs
@@ -1,51 +1,101 @@
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using AElf.Common;
-using AElf.Contracts.TestBase;
-using AElf.Cryptography;
-using AElf.Cryptography.ECDSA;
+using System.Linq;
 using AElf.Kernel;
+using AElf.Types.CSharp;
 using Google.Protobuf;
-using Shouldly;
-using Volo.Abp.Threading;
 
 namespace AElf.CrossChain
 {
     public class CrossChainTestHelper
     {
-        public static readonly ECKeyPair EcKeyPair = CryptoHelpers.GenerateKeyPair();
-        
-        public readonly Dictionary<int, long> SideChainIdHeights = new Dictionary<int, long>();
-        public readonly Dictionary<int, long> ParentChainIdHeight = new Dictionary<int, long>();
-        public readonly Dictionary<long, CrossChainBlockData> IndexedCrossChainBlockData = new Dictionary<long, CrossChainBlockData>();
-        public static byte[] Sign(byte[] data)
-        {
-            return CryptoHelpers.SignWithPrivateKey(EcKeyPair.PrivateKey, data);
-        }
-
-        public static byte[] GetPubicKey()
-        {
-            return EcKeyPair.PublicKey;
-        }
-        
-        public static Address GetAddress()
-        {
-            return Address.FromPublicKey(GetPubicKey());
-        }
-
+        private readonly Dictionary<int, long> _sideChainIdHeights = new Dictionary<int, long>();
+        private readonly Dictionary<int, long> _parentChainIdHeight = new Dictionary<int, long>();
+        private readonly Dictionary<long, CrossChainBlockData> _indexedCrossChainBlockData = new Dictionary<long, CrossChainBlockData>();
         public void AddFakeSideChainIdHeight(int sideChainId, long height)
         {
-            SideChainIdHeights.Add(sideChainId, height);
+            _sideChainIdHeights.Add(sideChainId, height);
         }
 
         public void AddFakeParentChainIdHeight(int parentChainId, long height)
         {
-            ParentChainIdHeight.Add(parentChainId, height);
+            _parentChainIdHeight.Add(parentChainId, height);
         }
 
         public void AddFakeIndexedCrossChainBlockData(long height, CrossChainBlockData crossChainBlockData)
         {
-            IndexedCrossChainBlockData.Add(height, crossChainBlockData);
+            _indexedCrossChainBlockData.Add(height, crossChainBlockData);
+        }
+
+        public TransactionTrace CreateFakeTransactionTrace(Transaction transaction)
+        {
+            string methodName = transaction.MethodName;
+
+            var trace = new TransactionTrace
+            {
+                TransactionId = transaction.GetHash(),
+                ExecutionStatus = ExecutionStatus.ExecutedButNotCommitted,
+                RetVal = new RetVal()
+            };
+            trace.RetVal.Data = CreateFakeReturnValue(trace, transaction, methodName);
+            
+            return trace;
+        }
+
+        private ByteString CreateFakeReturnValue(TransactionTrace trace, Transaction transaction, string methodName)
+        {
+            if (methodName == CrossChainConsts.GetParentChainIdMethodName)
+            {
+                var parentChainId = _parentChainIdHeight.Keys.FirstOrDefault();
+                if (parentChainId != 0) 
+                    return parentChainId.ToPbMessage().ToByteString();
+                trace.ExecutionStatus = ExecutionStatus.ContractError;
+                return ByteString.Empty;
+            }
+            
+            if (methodName == CrossChainConsts.GetParentChainHeightMethodName)
+            {
+                return _parentChainIdHeight.Values.First().ToPbMessage()
+                    .ToByteString();
+            }
+
+            if (methodName == CrossChainConsts.GetSideChainHeightMethodName)
+            {
+                int sideChainId =
+                    (int) ParamsPacker.Unpack(transaction.Params.ToByteArray(), new[] {typeof(int)})[0];
+                var exist = _sideChainIdHeights.TryGetValue(sideChainId, out var sideChainHeight);
+                if (exist)
+                    return sideChainHeight.ToPbMessage().ToByteString();
+                trace.ExecutionStatus = ExecutionStatus.ContractError;
+                return ByteString.Empty;
+            }
+
+            if (methodName == CrossChainConsts.GetAllChainsIdAndHeightMethodName)
+            {
+                var dict = new SideChainIdAndHeightDict();
+                dict.IdHeighDict.Add(_sideChainIdHeights);
+                dict.IdHeighDict.Add(_parentChainIdHeight);
+                return dict.ToByteString();
+            }
+
+            if (methodName == CrossChainConsts.GetSideChainIdAndHeightMethodName)
+            {
+                var dict = new SideChainIdAndHeightDict();
+                dict = new SideChainIdAndHeightDict();
+                dict.IdHeighDict.Add(_sideChainIdHeights);
+                return dict.ToByteString();
+            }
+            
+            if (methodName == CrossChainConsts.GetIndexedCrossChainBlockDataByHeight)
+            {
+                long height =
+                    (long) ParamsPacker.Unpack(transaction.Params.ToByteArray(), new[] {typeof(long)})[0];
+                if (_indexedCrossChainBlockData.TryGetValue(height, out var crossChainBlockData))
+                    return trace.RetVal.Data = crossChainBlockData.ToByteString();
+                trace.ExecutionStatus = ExecutionStatus.ContractError;
+                return ByteString.Empty;
+            }
+
+            return ByteString.Empty;
         }
     }
 }
--- AElf.CrossChain.Tests/CrossChainTestModule.cs
+++ AElf.CrossChain.Tests/CrossChainTestModule.cs
@@ -1,15 +1,13 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using AElf.Common;
 using AElf.Contracts.TestBase;
+using AElf.Cryptography;
 using AElf.Kernel;
 using AElf.Kernel.Account.Application;
 using AElf.Kernel.SmartContractExecution.Application;
 using AElf.Kernel.Tests;
 using AElf.Modularity;
-using AElf.Types.CSharp;
-using Google.Protobuf;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Volo.Abp.Modularity;
@@ -24,13 +22,12 @@ namespace AElf.CrossChain
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            var keyPair = CrossChainTestHelper.EcKeyPair;
+            var keyPair = CryptoHelpers.GenerateKeyPair();
             var mockAccountService = new Mock<IAccountService>();
             mockAccountService.Setup(m => m.GetPublicKeyAsync()).Returns(Task.FromResult(keyPair.PublicKey));
             mockAccountService.Setup(m => m.GetAccountAsync())
                 .Returns(Task.FromResult(Address.FromPublicKey(keyPair.PublicKey)));
             context.Services.AddTransient(provider =>  mockAccountService.Object);
-            context.Services.AddSingleton<CrossChainTestHelper>();
             
 //            var extraDataOrderInformation = context.Services.GetRequiredServiceLazy<IBlockExtraDataOrderService>().Value;
 //            var blockExtraDataProviders = context.Services.GetRequiredServiceLazy<IEnumerable<IBlockExtraDataProvider>>().Value;
@@ -38,7 +35,8 @@ namespace AElf.CrossChain
 //            {
 //                extraDataOrderInformation.AddExtraDataProvider(blockExtraDataProvider.GetType());
 //            }
-            
+
+            context.Services.AddSingleton<CrossChainTestHelper>();
             context.Services.AddTransient(provider =>
             {
                 var mockTransactionReadOnlyExecutionService = new Mock<ITransactionReadOnlyExecutionService>();
@@ -46,65 +44,8 @@ namespace AElf.CrossChain
                 .Setup(m => m.ExecuteAsync(It.IsAny<IChainContext>(), It.IsAny<Transaction>(), It.IsAny<DateTime>()))
                 .Returns<IChainContext, Transaction, DateTime>((chainContext, transaction, dateTime) =>
                 {
-                    var crossChainTestHelper = context.Services.GetRequiredServiceLazy<CrossChainTestHelper>().Value;
-                    string methodName = transaction.MethodName;
-                    var trace = new TransactionTrace
-                    {
-                        TransactionId = transaction.GetHash(),
-                        ExecutionStatus = ExecutionStatus.ExecutedButNotCommitted,
-                        RetVal = new RetVal()
-                    };
-                    if (methodName == CrossChainConsts.GetParentChainIdMethodName)
-                    {
-                        var parentChainId = crossChainTestHelper.ParentChainIdHeight.Keys.FirstOrDefault();
-                        if(parentChainId == 0)
-                            trace.ExecutionStatus = ExecutionStatus.ContractError;
-                        else
-                            trace.RetVal.Data = parentChainId.ToPbMessage().ToByteString();
-                    }
-                    else if (methodName == CrossChainConsts.GetParentChainHeightMethodName)
-                    {
-                        trace.RetVal.Data = crossChainTestHelper.ParentChainIdHeight.Values.First().ToPbMessage()
-                            .ToByteString();
-                    }
-                    else if (methodName == CrossChainConsts.GetSideChainHeightMethodName)
-                    {
-                        int sideChainId =
-                            (int) ParamsPacker.Unpack(transaction.Params.ToByteArray(), new[] {typeof(int)})[0];
-                        var exist = crossChainTestHelper.SideChainIdHeights.TryGetValue(sideChainId, out var sideChainHeight);
-                        if (!exist)
-                            trace.ExecutionStatus = ExecutionStatus.ContractError;
-                        else
-                            trace.RetVal.Data = sideChainHeight.ToPbMessage().ToByteString();
-                    }
-                    else if (methodName == CrossChainConsts.GetAllChainsIdAndHeightMethodName)
-                    {
-                        var dict = new SideChainIdAndHeightDict();
-                        dict.IdHeighDict.Add(crossChainTestHelper.SideChainIdHeights);
-                        dict.IdHeighDict.Add(crossChainTestHelper.ParentChainIdHeight);
-                        trace.RetVal.Data = dict.ToByteString();
-                    }
-                    else if (methodName == CrossChainConsts.GetSideChainIdAndHeightMethodName)
-                    {
-                        var dict = new SideChainIdAndHeightDict();
-                        dict = new SideChainIdAndHeightDict();
-                        dict.IdHeighDict.Add(crossChainTestHelper.SideChainIdHeights);
-                        trace.RetVal.Data = dict.ToByteString();
-                    }
-                    else if(methodName == CrossChainConsts.GetIndexedCrossChainBlockDataByHeight)
-                    {
-                        long height =
-                            (long) ParamsPacker.Unpack(transaction.Params.ToByteArray(), new[] {typeof(long)})[0];
-                        if (!crossChainTestHelper.IndexedCrossChainBlockData.TryGetValue(height,
-                            out var crossChainBlockData))
-                        {
-                            trace.ExecutionStatus = ExecutionStatus.ContractError;
-                        }
-                        else
-                            trace.RetVal.Data = crossChainBlockData.ToByteString();
-                    }
-
-                    return Task.FromResult(trace);
+                    var crossChainTestHelper = context.Services.GetRequiredServiceLazy<CrossChainTestHelper>().Value;                   
+                    return Task.FromResult(crossChainTestHelper.CreateFakeTransactionTrace(transaction));
                 });
                 return mockTransactionReadOnlyExecutionService.Object;
             });
