002b3b2b35890d4c7b3884a183aa8bcf45b635b7
zhxymh <13552710076@126.com>
2019-03-07 15:15:45 +0800

    remove repeated configure

--- AElf.Contracts.TestBase/ContractTestBase.cs
+++ AElf.Contracts.TestBase/ContractTestBase.cs
@@ -9,12 +9,6 @@ namespace AElf.Contracts.TestBase
     public class ContractTestBase<TModule> : AElfIntegratedTest<TModule>
         where TModule : AElfModule
     {
-        protected override void SetAbpApplicationCreationOptions(AbpApplicationCreationOptions options)
-        {
-            base.SetAbpApplicationCreationOptions(options);
-
-            options.Services.AddKeyValueDbContext<StateKeyValueDbContext>(o => o.UseInMemoryDatabase());
-            options.Services.AddKeyValueDbContext<BlockchainKeyValueDbContext>(o => o.UseInMemoryDatabase());
-        }
+        
     }
 }
--- AElf.CrossChain.Tests/CrossChainTestModule.cs
+++ AElf.CrossChain.Tests/CrossChainTestModule.cs
@@ -1,25 +1,17 @@
-using System.Collections.Generic;
-using AElf.Database;
-using AElf.Kernel.Blockchain.Application;
-using AElf.Kernel.Infrastructure;
+using AElf.Kernel.Tests;
 using AElf.Modularity;
-using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.EventBus;
 using Volo.Abp.Modularity;
 
 namespace AElf.CrossChain
 {
     [DependsOn(typeof(AbpEventBusModule),
-        typeof(CrossChainAElfModule))]
+        typeof(CrossChainAElfModule),
+        typeof(KernelTestAElfModule))]
     public class CrossChainTestModule : AElfModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            var services = context.Services;
-
-            services.AddKeyValueDbContext<BlockchainKeyValueDbContext>(o => o.UseInMemoryDatabase());
-            services.AddKeyValueDbContext<StateKeyValueDbContext>(o => o.UseInMemoryDatabase());
-
             //TODO: please mock data here, do not directly new object, if you have multiple dependency, you should have 
             //different modules, like  AElfIntegratedTest<AAACrossChainTestModule>,  AElfIntegratedTest<BBBCrossChainTestModule>
         }
