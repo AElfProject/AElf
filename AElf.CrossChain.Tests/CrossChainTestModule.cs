934444c79f24655c8e92e66c1d2d598a9a070442
EanCuznaivy <EanCuznaivy@users.noreply.github.com>
2019-03-09 20:07:45 +0800

    Recover CrossChainTestModule.
    Somehow local git abnormal.

--- AElf.CrossChain.Tests/CrossChainTestModule.cs
+++ AElf.CrossChain.Tests/CrossChainTestModule.cs
@@ -1,19 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using AElf.Common;
 using AElf.Kernel;
+using AElf.Kernel.SmartContract.Application;
+using AElf.Kernel.SmartContractExecution.Application;
 using AElf.Modularity;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Volo.Abp.Modularity;
 
 namespace AElf.CrossChain
 {
     [DependsOn(
         typeof(CrossChainAElfModule),
-        typeof(KernelTestAElfModule)
+        typeof(KernelCoreTestAElfModule)
         )]
     public class CrossChainTestModule : AElfModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            //TODO: please mock data here, do not directly new object, if you have multiple dependency, you should have 
-            //different modules, like  AElfIntegratedTest<AAACrossChainTestModule>,  AElfIntegratedTest<BBBCrossChainTestModule>
+            //context.Services.AddTransient<IBlockValidationProvider, CrossChainValidationProvider>();
+            context.Services.AddSingleton<CrossChainTestHelper>();
+            context.Services.AddTransient(provider =>
+            {
+                var mockTransactionReadOnlyExecutionService = new Mock<ITransactionReadOnlyExecutionService>();
+                mockTransactionReadOnlyExecutionService
+                    .Setup(m => m.ExecuteAsync(It.IsAny<IChainContext>(), It.IsAny<Transaction>(), It.IsAny<DateTime>()))
+                    .Returns<IChainContext, Transaction, DateTime>((chainContext, transaction, dateTime) =>
+                    {
+                        var crossChainTestHelper = context.Services.GetRequiredServiceLazy<CrossChainTestHelper>().Value;                   
+                        return Task.FromResult(crossChainTestHelper.CreateFakeTransactionTrace(transaction));
+                    });
+                return mockTransactionReadOnlyExecutionService.Object;
+            });
+            context.Services.AddTransient(provider =>
+            {
+                var mockSmartContractAddressService = new Mock<ISmartContractAddressService>();
+                mockSmartContractAddressService.Setup(m => m.GetAddressByContractName(It.IsAny<Hash>()))
+                    .Returns(Address.Generate);
+                return mockSmartContractAddressService.Object;
+            });
         }
     }
 }
