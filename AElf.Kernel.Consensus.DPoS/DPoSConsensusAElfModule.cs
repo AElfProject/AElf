5ae3119cf69e38a4c26ea89bfc793353bc8f0318
Loning <aloning@gmail.com>
2019-03-11 21:03:44 +0800

    move namespace

--- AElf.Kernel.Consensus.DPoS/DPoSConsensusAElfModule.cs
+++ AElf.Kernel.Consensus.DPoS/DPoSConsensusAElfModule.cs
@@ -5,6 +5,7 @@ using AElf.Kernel.Consensus.Infrastructure;
 using AElf.Kernel.Consensus.Scheduler.FluentScheduler;
 using AElf.Kernel.Consensus.Scheduler.RxNet;
 using AElf.Kernel.Miner.Application;
+using AElf.Kernel.SmartContract;
 using AElf.Kernel.SmartContract.Application;
 using AElf.Modularity;
 using Microsoft.Extensions.DependencyInjection;
