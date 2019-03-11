3090ae7bf43cdbdf57e4080d1cfdf202182d5f6e
EanCuznaivy <1036682639@qq.com>
2019-03-11 15:08:52 +0800

    [Tune] Remove one useless test project.

--- AElf.Contracts.Consensus.Tests/ConsensusContractTestBase.cs
+++
@@ -1,8 +0,0 @@
-using AElf.Contracts.TestBase;
-
-namespace AElf.Contracts.Consensus.Tests
-{
-    public class ConsensusContractTestBase : ContractTestBase<ConsensusContractTestAElfModule>
-    {
-    }
-}
