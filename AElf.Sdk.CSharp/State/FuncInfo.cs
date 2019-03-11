74051d106d047eaa10429187c57db8f477c7b906
gldeng <realgldeng@gmail.com>
2019-03-11 17:49:45 +0800

    Implement readonly Call in SmartContractBridgeContext #1025

--- AElf.Sdk.CSharp/State/FuncInfo.cs
+++ AElf.Sdk.CSharp/State/FuncInfo.cs
@@ -22,49 +22,7 @@ namespace AElf.Sdk.CSharp.State
 
         internal T Call(params object[] args)
         {
-            throw new NotImplementedException();
-            //TODO: create a call method in smart contract context 
-            /*
-            var svc = _owner.Context.SmartContractContext.SmartContractExecutiveService;
-            var transactionContext = new TransactionContext()
-            {
-                Transaction = new Transaction()
-                {
-                    From = _owner.Context.Self,
-                    To = _owner.Value,
-                    MethodName = _name,
-                    Params = ByteString.CopyFrom(ParamsPacker.Pack(args))
-                }
-            };
-            int chainId = _owner.Context.SmartContractContext.ChainId;
-            var chainContext = new ChainContext()
-            {
-                ChainId = chainId,
-                BlockHash = _owner.Context.TransactionContext.PreviousBlockHash,
-                BlockHeight = _owner.Context.TransactionContext.BlockHeight - 1
-            };
-            AsyncHelper.RunSync(async () =>
-            {
-                var executive = await svc.GetExecutiveAsync(chainId, chainContext, _owner.Value);
-                executive.SetDataCache(_owner.Provider.Cache);
-                try
-                {
-                    // view only, write actions need to be sent via SendInline
-                    await executive.SetTransactionContext(transactionContext).Apply();
-                }
-                finally
-                {
-                    await svc.PutExecutiveAsync(chainId, _owner.Value, executive);
-                }
-            });
-            // TODO: Maybe put readonly call trace into inlinetraces to record data access
-
-            if (!transactionContext.Trace.IsSuccessful())
-            {
-                throw new ContractCallError($"Failed to call view method {_name} in contract {_owner.Value}.");
-            }
-
-            return (T) transactionContext.Trace.RetVal.Data.DeserializeToType(typeof(T));*/
+            return _owner.Context.Call<T>(_owner.Provider.Cache, _owner.Value, _name, args);
         }
     }
 }
