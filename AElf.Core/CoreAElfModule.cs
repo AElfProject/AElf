d769ec14235fa7342020a598070d779e4175fef6
Loning <aloning@gmail.com>
2019-03-12 15:13:01 +0800

    add service container

--- AElf.Core/CoreAElfModule.cs
+++ AElf.Core/CoreAElfModule.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using AElf.Modularity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Volo.Abp.Modularity;
 
 namespace AElf
@@ -13,7 +18,53 @@ namespace AElf
 
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            
+            context.Services.AddTransient(typeof(IServiceContainer<>),
+                typeof(ServiceContainerFactory<>));
+        }
+    }
+
+    public static class ServiceProviderExtensions
+    {
+        public static IEnumerable<T> GetServices<T>(this IServiceProvider provider, IEnumerable<Type> types)
+        {
+            return types.Select(type => (T) provider.GetService(type));
+        }
+
+        /*public static IEnumerable<T> GetServices<T>(this IServiceProvider provider, params Type[] types)
+        {
+            return provider.GetServices<T>((IEnumerable<Type>) types);
+        }*/
+    }
+
+    public interface IServiceContainer<T> : IEnumerable<T>
+    {
+    }
+
+
+    public class ServiceContainerFactoryOptions<T>
+    {
+        public List<Type> Types { get; set; } = new List<Type>();
+    }
+
+    public class ServiceContainerFactory<T> : IServiceContainer<T>
+    {
+        private readonly IEnumerable<T> _services;
+
+        public ServiceContainerFactory(IOptionsSnapshot<ServiceContainerFactoryOptions<T>> options,
+            IServiceProvider serviceProvider)
+        {
+            _services = serviceProvider.GetServices<T>(options.Value.Types);
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _services.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
