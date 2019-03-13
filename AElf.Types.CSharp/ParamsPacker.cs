2e81b0b0f8f15326a71283a6ec4d8f1a0f0c4e13
EricShu <blackeye286@126.com>
2019-03-13 16:08:07 +0800

    Add case to cover IsUserType.

--- AElf.Types.CSharp/ParamsPacker.cs
+++ AElf.Types.CSharp/ParamsPacker.cs
@@ -9,7 +9,7 @@ namespace AElf.Types.CSharp
 {
     public class ParamsPacker
     {
-        //Todo Add Pack to array type data
+        //Todo Add Pack to array type data case
         public static byte[] Pack(params object[] objs)
         {
             if (objs.Length == 0)
