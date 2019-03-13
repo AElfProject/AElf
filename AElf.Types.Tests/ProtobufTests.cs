c380dd5039d2acaeeadb5648a69a23f2fca50637
Loning <aloning@gmail.com>
2019-03-13 16:44:43 +0800

    fix

--- AElf.Types.Tests/ProtobufTests.cs
+++ AElf.Types.Tests/ProtobufTests.cs
@@ -17,9 +17,20 @@ namespace AElf.Types.Tests
 
             var reg = TypeRegistry.FromFiles(file);
 
-            var messageDescriptor = reg.Find("AElf.Common.Hash");
+            var messageDescriptor = reg.Find("Hash");
             
-            //messageDescriptor.Name.ShouldBe("Hash");
+            messageDescriptor.Name.ShouldBe("Hash");
+            
+            var hash=Hash.FromString("hello");
+
+            var json = JsonFormatter.Default.Format(hash);
+
+            //JsonParser.Default.Parse(json, messageDescriptor);
+            //it will not work, messageDescriptor clr type is null, the factory of parser is null
+
+            var deserializedHash = (Hash) JsonParser.Default.Parse(json, Hash.Descriptor);
+            JsonParser.Default.Parse<Hash>(json);
+
         }
     }
 }
