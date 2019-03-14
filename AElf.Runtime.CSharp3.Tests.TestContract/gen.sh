7fba9adf5b45f9109434805e57706ac7e63b3426
gldeng <realgldeng@gmail.com>
2019-03-14 20:15:48 +0800

    Modify gen.sh

--- AElf.Runtime.CSharp3.Tests.TestContract/gen.sh
+++ AElf.Runtime.CSharp3.Tests.TestContract/gen.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-chmod a+x grpc_contract_csharp_plugin
+dir=`dirname "$0"`
+
+chmod a+x ${dir}/grpc_contract_csharp_plugin
 
 protoc --proto_path=./proto \
 --csharp_out=./Generated \
 --csharp_opt=file_extension=.g.cs \
 --contract_out=./Generated test_contract.proto \
---plugin=protoc-gen-contract=./grpc_contract_csharp_plugin
+--plugin=protoc-gen-contract=${dir}/grpc_contract_csharp_plugin
