d8caaaafe90054b91407e7d696681e5a8b970317
EanCuznaivy <1036682639@qq.com>
2019-03-14 14:29:35 +0800

    [Fix] Tickets can only withdraw the sender's.

--- AElf.Contracts.Consensus.DPoS/ConsensusContract_Election.cs
+++ AElf.Contracts.Consensus.DPoS/ConsensusContract_Election.cs
@@ -262,6 +262,8 @@ namespace AElf.Contracts.Consensus.DPoS
                 ContractErrorCode.GetErrorMessage(ContractErrorCode.InvalidOperation,
                     "This voting record can't withdraw for now."));
 
+            Assert(votingRecord.From == Context.RecoverPublicKey().ToHex(), "Can only withdraw tickets of the sender.");
+
             // Update voting record map.
             var blockchainStartTimestamp = State.BlockchainStartTimestamp.Value;
             votingRecord.WithdrawTimestamp =
