85febab025e87b050002b8786d31a428a27f7c1b
loning <aloning@gmail.com>
2019-03-20 01:03:47 +0800

    ci

--- AElf.Sdk.CSharp/SmartContractBridgeContextExtensions.cs
+++ AElf.Sdk.CSharp/SmartContractBridgeContextExtensions.cs
@@ -2,6 +2,7 @@ using AElf.Common;
 using AElf.Kernel;
 using AElf.Kernel.SmartContract;
 using AElf.Kernel.SmartContract.Sdk;
+using AElf.Types.CSharp;
 using Google.Protobuf;
 
 namespace AElf.Sdk.CSharp
@@ -18,20 +19,25 @@ namespace AElf.Sdk.CSharp
         public static T Call<T>(this ISmartContractBridgeContext context, IStateCache stateCache, Address address,
             string methodName, IMessage message)
         {
-            return context.Call<T>(stateCache, address, methodName, message.ToByteString());
+            return context.Call<T>(stateCache, address, methodName, ConvertToByteString(message));
         }
 
         public static void SendInline(this ISmartContractBridgeContext context, Address toAddress, string methodName,
             IMessage message)
         {
-            context.SendInline(toAddress, methodName, message.ToByteString());
+            context.SendInline(toAddress, methodName,ConvertToByteString(message));
         }
 
         public static void SendVirtualInline(this ISmartContractBridgeContext context, Hash fromVirtualAddress,
             Address toAddress, string methodName, IMessage message)
         {
             context.SendVirtualInline(fromVirtualAddress, toAddress, methodName,
-                message.ToByteString());
+                ConvertToByteString(message));
+        }
+
+        public static ByteString ConvertToByteString(IMessage message)
+        {
+            return ByteString.CopyFrom(ParamsPacker.Pack(message));
         }
     }
 }
