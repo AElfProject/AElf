fa2f31af4ba920dbc926325f6fe8e665de168fbb
jinpeng wang <jason.wang@hoopox.com>
2019-03-19 16:19:01 +0800

    resolve #1256

---
+++ AElf.Kernel.SmartContract.Tests/SerializedCallGraphTest.cs
@@ -0,0 +1,91 @@
+using System;
+using Shouldly;
+using Xunit;
+
+namespace AElf.Kernel.SmartContract
+{
+    public class SerializedCallGraphTest
+    {
+        [Fact]
+        public void IEquatable_SerializedCallGraph_Equals_True()
+        {
+            IEquatable<SerializedCallGraph> serializedCallGraph = new SerializedCallGraph
+            {
+                Edges =
+                {
+                    new GraphEdge {Source = "Source1", Target = "Target1"},
+                    new GraphEdge {Source = "Source2", Target = "Target2"},
+                    new GraphEdge {Source = "Source3", Target = "Target3"}
+                },
+                Vertices = {"Vertices"}
+            };
+            
+            var otherSerializedCallGraph = new SerializedCallGraph
+            {
+                Edges =
+                {
+                    new GraphEdge {Source = "Source1", Target = "Target1"},
+                    new GraphEdge {Source = "Source2", Target = "Target2"},
+                    new GraphEdge {Source = "Source3", Target = "Target3"}
+                },
+                Vertices = {"Vertices"}
+            };
+
+            serializedCallGraph.Equals(otherSerializedCallGraph).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void IEquatable_SerializedCallGraph_Equals_False()
+        {
+            IEquatable<SerializedCallGraph> serializedCallGraph = new SerializedCallGraph
+            {
+                Edges =
+                {
+                    new GraphEdge {Source = "Source1", Target = "Target1"},
+                    new GraphEdge {Source = "Source2", Target = "Target2"},
+                    new GraphEdge {Source = "Source3", Target = "Target3"}
+                },
+                Vertices = {"Vertices"}
+            };
+            
+            var otherSerializedCallGraph1 = new SerializedCallGraph
+            {
+                Edges =
+                {
+                    new GraphEdge {Source = "Source1", Target = "Target1"},
+                    new GraphEdge {Source = "Source2", Target = "Target2"},
+                    new GraphEdge {Source = "Source3", Target = "Target4"}
+                },
+                Vertices = {"Vertices"}
+            };
+
+            serializedCallGraph.Equals(otherSerializedCallGraph1).ShouldBeFalse();
+            
+            var otherSerializedCallGraph2 = new SerializedCallGraph
+            {
+                Edges =
+                {
+                    new GraphEdge {Source = "Source1", Target = "Target1"},
+                    new GraphEdge {Source = "Source2", Target = "Target2"},
+                    new GraphEdge {Source = "Source3", Target = "Target3"}
+                },
+                Vertices = {"OtherVertices"}
+            };
+
+            serializedCallGraph.Equals(otherSerializedCallGraph2).ShouldBeFalse();
+            
+            var otherSerializedCallGraph3 = new SerializedCallGraph
+            {
+                Edges =
+                {
+                    new GraphEdge {Source = "Source1", Target = "Target1"},
+                    new GraphEdge {Source = "Source2", Target = "Target2"},
+                    new GraphEdge {Source = "Source3", Target = "Target4"}
+                },
+                Vertices = {"OtherVertices"}
+            };
+
+            serializedCallGraph.Equals(otherSerializedCallGraph3).ShouldBeFalse();
+        }
+    }
+}
