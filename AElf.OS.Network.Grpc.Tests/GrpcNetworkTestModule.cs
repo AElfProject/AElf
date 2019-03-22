4f3bfe812f5aed8027621e1b3cca0ae68b6f1294
samfrb <samos2x@outlook.com>
2019-03-22 18:34:56 +0800

    Test data to consts.

--- AElf.OS.Network.Grpc.Tests/GrpcNetworkTestModule.cs
+++ AElf.OS.Network.Grpc.Tests/GrpcNetworkTestModule.cs
@@ -20,7 +20,7 @@ namespace AElf.OS.Network
             base.OnApplicationInitialization(context);
             
             var pool = context.ServiceProvider.GetRequiredService<IPeerPool>();
-            pool.AddPeer(new GrpcPeer(new Channel("127.0.0.1:", ChannelCredentials.Insecure), null, "0454dcd0afc20d015e328666d8d25f3f28b13ccd9744eb6b153e4a69709aab399", "127.0.0.1:666"));
+            pool.AddPeer(new GrpcPeer(new Channel(GrpcTestConstants.FakeListeningPort, ChannelCredentials.Insecure), null, GrpcTestConstants.FakePubKey, GrpcTestConstants.FakeListeningPort));
         }
     }
 }
