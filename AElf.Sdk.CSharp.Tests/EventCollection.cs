1d2cb99ac9a7d08c8f045f9846ad0c12603869ac
EricShu <blackeye286@126.com>
2019-03-22 22:33:07 +0800

    make different events and testing

---
+++ AElf.Sdk.CSharp.Tests/EventCollection.cs
@@ -0,0 +1,62 @@
+﻿using AElf.Common;
+using AElf.Kernel;
+using AElf.Kernel.Types;
+using AElf.Types.CSharp;
+using Google.Protobuf;
+
+namespace AElf.Sdk.CSharp
+{
+    // Lot of types data should not be defined including: byte, sbyte, char, short, ushort, decimal, float and double. 
+    // Becasue ParamsPacker.Pack() those types data wil be throw exception.
+     
+    public class TransactionEvent : Event
+    {
+        [Indexed] public Transaction IndexTransaction { get; set; }
+        public Transaction NonIndexTransaction { get; set; }
+    }
+
+    public class AddressEvent : Event
+    {
+        [Indexed] public Address IndexAddress { get; set; }
+        public Address NonIndexAddress { get; set; }
+    }
+
+    public class StringEvent : Event
+    {
+        [Indexed] public string IndexString { get; set; }
+        public string NonIndexString { get; set; }
+    }
+
+    public class DataEvent : Event
+    {
+        [Indexed] public int IndexInt { get; set; }
+        [Indexed] public uint IndexUInt {get;set;}
+        [Indexed] public long IndexLong {get;set;}
+        [Indexed] public ulong IndexULong {get;set;}
+
+        public int NonIndexInt { get; set; }
+        public long NonIndexLong {get;set;}
+    }
+
+    public class MultiEvent : Event
+    {
+        [Indexed] public Transaction IndexTransaction {get;set;}
+        [Indexed] public Address IndexAddress {get;set;}
+        [Indexed] public string IndexString {get;set;}
+        [Indexed] public int IndexInt {get;set;}
+        [Indexed] public long IndexLong {get;set;}
+
+        public Address NonIndexAddress {get;set;}
+        public string NonIndexString {get;set;}
+        public int NonIndexInt {get;set;}
+    }
+
+    public class NotSupportEvent : Event
+    {
+        [Indexed] public double IndexDouble {get;set;}
+        [Indexed] public float IndexFloat {get;set;}
+
+        public double NonIndexDouble {get;set;}
+        public float NonIndexFloat {get;set;}
+    }
+}
--- AElf.Sdk.CSharp.Tests/EventTests.cs
+++ AElf.Sdk.CSharp.Tests/EventTests.cs
@@ -6,109 +6,94 @@ using Google.Protobuf;
 using Shouldly;
 using Xunit;
 
-namespace AElf.Sdk.CSharp.Tests
+namespace AElf.Sdk.CSharp.Tests 
 {
     public class EventTests
     {
-        // Lot of types data should not be defined including: byte, sbyte, char, short, ushort, decimal, float and double.
         [Fact]
-        public void Parse_EventTest()
+        public void Parse_ReferenceData() 
         {
-            var eventDemo = new EventDemo()
+            var transactionEvent = new TransactionEvent 
             {
-               //init index value
-               IndexAddress = Address.Generate(),
-               IndexSymbol = "ELF",
-               //IndexByte = 125,
-               //IndexSbyte = 100,
-               //IndexChar = 'a',
-               //IndexShort = 3,
-               //IndexUShort = 4,
-               IndexInt = 5,
-               IndexUInt = 6,
-               IndexLong = 10,
-               IndexULong = 20,
-               //IndexDecimal = new decimal(2.45),
-               //IndexFloat = 128,
-               //IndexDouble = 3.6,
-               
-               //init non index value
-               Address = Address.Generate(),
-               Symbol = "ELF",
-               //Byte = 99,
-               //Sbyte = 60,
-               //Char = 'a',
-               //Short = 3,
-               //UShort = 4,
-               Int = 5,
-               UInt = 6,
-               Long = 10,
-               ULong = 20,
-               //Decimal = new decimal(2.45),
-               //Float = 128,
-               //Double = 3.6,
+                IndexTransaction = new Transaction { From = Address.Generate(), To = Address.Generate(), MethodName = "Test1" },
+                NonIndexTransaction = new Transaction { From = Address.Generate(), To = Address.Generate(), MethodName = "Test2" }
             };
-            
-            var data = EventExtension.FireEvent(eventDemo);
-            data.Address.ShouldBeNull();
-            data.Topics.Count.ShouldBeGreaterThanOrEqualTo(6);
+            var transactionLog = EventExtension.FireEvent(transactionEvent);
+
+            var addressEvent = new AddressEvent 
+            {
+                IndexAddress = Address.Generate(),
+                NonIndexAddress = Address.Generate()
+            };
+            var addressLog = EventExtension.FireEvent(addressEvent);
         }
-        
+
         [Fact]
-        public void Pack_Test()
+        public void Parse_StringEvent() 
         {
-            //var value = (byte)32;
-            //var value = (sbyte)32;
-            //var value = 'c';
-            //var value = (short) 5;
-            //var value = (ushort) 5;
-            //var value = (decimal) 3.45;
-            //var value = (float)124;
-            var value = (double)36.4;
-            Should.Throw<System.Exception>(()=>ParamsPacker.Pack(value));
+            var stringEvent = new StringEvent 
+            {
+                IndexString = "test1",
+                NonIndexString = "test2"
+            };
+            var stringLog = EventExtension.FireEvent(stringEvent);
+        }
+
+        [Fact]
+        public void Parse_DataEvent() 
+        {
+            var dataEvent = new DataEvent 
+            {
+                IndexInt = (int) 8,
+                IndexUInt = (uint) 12,
+                IndexLong = (long) 24,
+                IndexULong = (ulong) 32,
+
+                NonIndexInt = (int) 36,
+                NonIndexLong = (long) 48
+            };
+            var dataLog = EventExtension.FireEvent(dataEvent);
+        }
+
+        [Fact]
+        public void Parse_MultiEvent() 
+        {
+            var multiEvent = new MultiEvent 
+            {
+                IndexTransaction = new Transaction { From = Address.Generate(), To = Address.Generate(), MethodName = "test_method" },
+                IndexAddress = Address.Generate(),
+                IndexString = "index test info",
+                IndexInt = 24,
+                IndexLong = 36,
+
+                NonIndexAddress = Address.Generate(),
+                NonIndexString = "non index test info",
+                NonIndexInt = 48
+            };
+            var multiLog = EventExtension.FireEvent(multiEvent);
+        }
+
+        [Fact]
+        public void Parse_NotSupport_EventType() 
+        {
+            var notsupportEvent = new NotSupportEvent 
+            {
+                IndexDouble = (double) 36,
+                IndexFloat = (float) 2.45,
+
+                NonIndexDouble = (double) 27,
+                NonIndexFloat = (float) 3.67
+            };
+            Should.Throw<System.Exception>(() => EventExtension.FireEvent(notsupportEvent));
         }
     }
-    
-    public static class EventExtension
+
+    public static class EventExtension 
     {
-        public static LogEvent FireEvent<TEvent>(TEvent e) where TEvent : Event
+        public static LogEvent FireEvent<TEvent>(TEvent e) where TEvent : Event 
         {
             var data = EventParser<TEvent>.ToLogEvent(e);
             return data;
         }
-    }
-    
-    public class EventDemo : Event {
-        //Index
-        [Indexed] public Address IndexAddress { get; set; }
-        [Indexed] public string IndexSymbol { get; set; }
-        //[Indexed] public byte IndexByte { get; set; }
-        //[Indexed] public sbyte IndexSbyte { get; set; }
-        //[Indexed] public char IndexChar { get; set; }
-        //[Indexed] public short IndexShort { get; set; }
-        //[Indexed] public ushort IndexUShort { get; set; }
-        [Indexed] public int IndexInt { get; set; }
-        [Indexed] public uint IndexUInt { get; set; }
-        [Indexed] public long IndexLong { get; set; }        
-        [Indexed] public ulong IndexULong { get; set; }
-        //[Indexed] public decimal IndexDecimal { get; set; }
-        //[Indexed] public float IndexFloat { get; set; }
-        //[Indexed] public double IndexDouble { get; set; }
-        
-        //NonIndex
-        public Address Address { get; set; }
-        public string Symbol { get; set; }
-        //public byte Byte { get; set; }
-        //public sbyte Sbyte { get; set; }
-        //public char Char { get; set; }
-        //public short Short { get; set; }
-        //public ushort UShort { get; set; }
-        public int Int { get; set; }
-        public uint UInt { get; set; }
-        public long Long { get; set; }        
-        public ulong ULong { get; set; }
-        //public decimal Decimal { get; set; }
-        //public float Float { get; set; }
-        //public double Double { get; set; }
     }
 }
