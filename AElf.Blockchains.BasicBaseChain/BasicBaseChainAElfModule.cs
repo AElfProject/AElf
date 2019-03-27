7c317db13d6a38fa534a32a98e68abdaf421292e
Loning <aloning@gmail.com>
2019-03-27 14:11:54 +0800

    reorg launcher

--- AElf.Blockchains.BasicBaseChain/BasicBaseChainAElfModule.cs
+++ AElf.Blockchains.BasicBaseChain/BasicBaseChainAElfModule.cs
@@ -1,7 +1,10 @@
-﻿using AElf.Kernel;
+﻿using System;
+using AElf.Common;
+using AElf.Kernel;
 using AElf.Kernel.Consensus.DPoS;
 using AElf.Modularity;
 using AElf.OS;
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Modularity;
 
 namespace AElf.Blockchains.BasicBaseChain
@@ -13,6 +16,13 @@ namespace AElf.Blockchains.BasicBaseChain
     )]
     public class BasicBaseChainAElfModule : AElfModule<BasicBaseChainAElfModule>
     {
-        
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var config = context.Services.GetConfiguration();
+            Configure<ChainOptions>(option =>
+            {
+                option.ChainId = ChainHelpers.ConvertBase58ToChainId(config["ChainId"]);
+            });
+        }
     }
 }
