e115fb88825db80518351a6a37ba36230eb10add
Peng Rong <rongpeng@live.com>
2019-03-31 17:16:09 +0800

    Improve ci scripts

--- scripts/deploy_myget.sh
+++ scripts/deploy_myget.sh
@@ -5,7 +5,7 @@ TAG=$1
 MYGET_API_KEY=$2
 VERSION=`echo ${TAG} | cut -b 2-`
 
-for name in `ls -lh | grep ^d | grep AElf | grep -v Tests| awk '{print $NF}'`;
+for name in `ls -lh | grep ^d | grep AElf | grep -v Tests | awk '{print $NF}'`;
 do
     if [[ -f ${name}/${name}.csproj ]] && [[ 1 -eq $(grep -c "GeneratePackageOnBuild"  ${name}/${name}.csproj) ]];then
         dotnet build ${name}/${name}.csproj --configuration Release -P:Version=${VERSION} -P:Authors=AElf -o ../
@@ -13,8 +13,8 @@ do
 done
 
 # push
-for name  in `ls *.nupkg`;
+for name in `ls *.nupkg`;
 do
-  echo ${name}
-  dotnet nuget push ${name}  -k ${MYGET_API_KEY}  -s https://www.myget.org/F/aelf-project/api/v3/index.json
+    echo ${name}
+    dotnet nuget push ${name} -k ${MYGET_API_KEY} -s https://www.myget.org/F/aelf-project/api/v3/index.json
 done
--- scripts/deploy_myget_daily.sh
+++ scripts/deploy_myget_daily.sh
@@ -3,21 +3,20 @@
 VERSION_PREFIX=$1
 MYGET_API_KEY=$2
 
-# Days Since 1/1/1970 as build version
+# days since 1970-1-1 as build version
 BUILD_VERSION=`expr $(date +%s) / 86400`
-
-VERSION=VERSION_PREFIX.${VERSION_PREFIX}
+VERSION=${VERSION_PREFIX}.${BUILD_VERSION}
 
 for name in `ls -lh | grep ^d | grep AElf | grep -v Tests| awk '{print $NF}'`;
 do
-    if [[ -f ${name}/${name}.csproj ]] && [[ 1 -eq $(grep -c "GeneratePackageOnBuild"  ${name}/${name}.csproj) ]];then
+    if [[ -f ${name}/${name}.csproj ]] && [[ 1 -eq $(grep -c "GeneratePackageOnBuild" ${name}/${name}.csproj) ]];then
         dotnet build ${name}/${name}.csproj --configuration Release -P:Version=${VERSION} -P:Authors=AElf -o ../
     fi
 done
 
 # push
-for name  in `ls *.nupkg`;
+for name in `ls *.nupkg`;
 do
-  echo ${name}
-  dotnet nuget push ${name}  -k ${MYGET_API_KEY}  -s https://www.myget.org/F/aelf-project-dev/api/v3/index.json
+    echo ${name}
+    dotnet nuget push ${name} -k ${MYGET_API_KEY} -s https://www.myget.org/F/aelf-project-dev/api/v3/index.json
 done
