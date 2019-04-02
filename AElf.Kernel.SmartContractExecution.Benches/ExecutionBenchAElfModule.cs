f88446ded13bdf827e0e2abd4df3853b5eeb3a22
zhxymh <13552710076@126.com>
2019-04-02 15:15:21 +0800

    add executing performance test

---
+++ AElf.Kernel.SmartContractExecution.Benches/ExecutionBenchAElfModule.cs
@@ -0,0 +1,14 @@
+using AElf.BenchBase;
+using AElf.OS;
+using Volo.Abp.Modularity;
+
+namespace AElf.Kernel.SmartContractExecution.Benches
+{
+    [DependsOn(
+        typeof(OSCoreWithChainTestAElfModule)
+    )]
+    public class ExecutionBenchAElfModule: BenchBaseAElfModule
+    {
+        
+    }
+}
