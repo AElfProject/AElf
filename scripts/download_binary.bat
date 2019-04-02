8f92a706061ca0e4f39699062419f5cb8255fa4a
gldeng <realgldeng@gmail.com>
2019-04-02 13:33:33 +0800

    Download binary during install

--- scripts/download_binary.bat
+++ scripts/download_binary.bat
@@ -3,4 +3,5 @@ SET scriptdir=%~dp0
 if not exist "%scriptdir%contract_csharp_plugin.exe" (
     powershell -Command "(new-object net.webclient).DownloadFile('https://github.com/AElfProject/contract-plugin/releases/download/v1.0.0/contract_csharp_plugin-v1.0.0-win32.zip', '%scriptdir%contract_csharp_plugin-v1.0.0-win32.zip')"
     unzip "%scriptdir%contract_csharp_plugin-v1.0.0-win32.zip"  -d "%scriptdir%"
+    del "%scriptdir%contract_csharp_plugin-v1.0.0-win32.zip"
 )
