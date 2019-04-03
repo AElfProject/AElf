cd43f0bf8015aca2534df4317bb68445809770d8
gldeng <realgldeng@gmail.com>
2019-04-03 14:16:11 +0800

    Use contract plugin v1.0.1

--- scripts/download_binary.bat
+++ scripts/download_binary.bat
@@ -1,7 +1,10 @@
 SET scriptdir=%~dp0
 
+SET version="v1.0.1"
+SET filename="contract_csharp_plugin-%version%-win32.zip"
+
 if not exist "%scriptdir%contract_csharp_plugin.exe" (
-    powershell -Command "(new-object net.webclient).DownloadFile('https://github.com/AElfProject/contract-plugin/releases/download/v1.0.0/contract_csharp_plugin-v1.0.0-win32.zip', '%scriptdir%contract_csharp_plugin-v1.0.0-win32.zip')"
-    unzip "%scriptdir%contract_csharp_plugin-v1.0.0-win32.zip"  -d "%scriptdir%"
-    del "%scriptdir%contract_csharp_plugin-v1.0.0-win32.zip"
+    powershell -Command "(new-object net.webclient).DownloadFile('https://github.com/AElfProject/contract-plugin/releases/download/%version%/%filename%', '%scriptdir%%filename%')"
+    unzip "%scriptdir%%filename%"  -d "%scriptdir%"
+    del "%scriptdir%%filename%"
 )
--- scripts/download_binary.sh
+++ scripts/download_binary.sh
@@ -13,7 +13,7 @@ fi
 
 plugin="${scriptdir}/contract_csharp_plugin"
 
-version="v1.0.0"
+version="v1.0.1"
 
 if [[ ! -f ${plugin} ]]; then
     if [ $osn == "macosx" ]; then
