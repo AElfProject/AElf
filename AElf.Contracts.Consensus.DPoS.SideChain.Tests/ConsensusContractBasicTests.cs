2d59a300e20c0ee3de8f8023732e987c75d7cd40
EricShu <blackeye286@126.com>
2019-04-04 13:39:18 +0800

    update test cases

--- AElf.Contracts.Consensus.DPoS.SideChain.Tests/ConsensusContractBasicTests.cs
+++ AElf.Contracts.Consensus.DPoS.SideChain.Tests/ConsensusContractBasicTests.cs
@@ -5,6 +5,7 @@ using AElf.Common;
 using AElf.Consensus.DPoS;
 using AElf.Kernel;
 using AElf.Kernel.Consensus.Application;
+using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using Shouldly;
 using Xunit;
@@ -30,6 +31,29 @@ namespace AElf.Contracts.DPoS.SideChain
 
             var validationResult = await TesterManager.Testers[0].ValidateConsensusAfterExecutionAsync(dposInformation);
             validationResult.Success.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task Get_ConsensusCommand_Success()
+        {
+            TesterManager.InitialTesters();
+            
+            var input = new CommandInput
+            {
+                PublicKey = ByteString.CopyFrom(TesterManager.Testers[0].KeyPair.PublicKey)
+            };
+
+            var consensusCommand = await TesterManager.Testers[0].GetConsensusCommandAsync();
+            consensusCommand.ShouldNotBeNull();
+
+            var behavior = DPoSHint.Parser.ParseFrom(consensusCommand.Hint.ToByteArray()).Behaviour;
+            behavior.ShouldBe(DPoSBehaviour.UpdateValue);
+        }
+
+        [Fact]
+        public async Task InitialConsensus_Success()
+        {
+            
         }
     }
 }
