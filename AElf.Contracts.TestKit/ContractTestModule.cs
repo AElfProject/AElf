2e2768ccf68f9e384546b65f189684738357064f
gldeng <realgldeng@gmail.com>
2019-04-13 12:53:12 +0800

    Simplify modules

--- AElf.Contracts.TestKit/ContractTestModule.cs
+++ AElf.Contracts.TestKit/ContractTestModule.cs
@@ -7,9 +7,11 @@ using AElf.Kernel.ChainController.Application;
 using AElf.Kernel.Consensus;
 using AElf.Kernel.Consensus.Application;
 using AElf.Kernel.Infrastructure;
+using AElf.Kernel.Node;
 using AElf.Kernel.SmartContract;
 using AElf.Kernel.SmartContract.Infrastructure;
 using AElf.Kernel.SmartContractExecution;
+using AElf.Kernel.Token;
 using AElf.Kernel.TransactionPool;
 using AElf.Kernel.TransactionPool.Infrastructure;
 using AElf.Modularity;
@@ -19,7 +21,6 @@ using AElf.OS.Network.Infrastructure;
 using AElf.OS.Node.Application;
 using AElf.OS.Node.Domain;
 using AElf.Runtime.CSharp;
-using AElf.Runtime.CSharp.ExecutiveTokenPlugin;
 using MartinCostello.Logging.XUnit;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -32,121 +33,61 @@ using Xunit.Abstractions;
 
 namespace AElf.Contracts.TestKit
 {
-    #region Dummy
-    [DependsOn(
-        typeof(AbpTestBaseModule))]
-    public class TestBaseAElfModule : AElfModule
-    {
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            ITestOutputHelperAccessor testOutputHelperAccessor = new TestOutputHelperAccessor();
-
-            context.Services.AddSingleton(testOutputHelperAccessor);
-
-            context.Services.AddLogging(o =>
-            {
-                o.AddXUnit(testOutputHelperAccessor);
-                 
-            });
-        }
-    }
-
-    public class TestOutputHelperAccessor : ITestOutputHelperAccessor
-    {
-        public ITestOutputHelper OutputHelper { get; set; }
-    }
-
-    [DependsOn(
-        typeof(TestBaseAElfModule), typeof(CoreKernelAElfModule))]
-    public class TestBaseKernelAElfModule : AElfModule
-    {
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            var services = context.Services;
-
-            services.AddKeyValueDbContext<BlockchainKeyValueDbContext>(o => o.UseInMemoryDatabase());
-            services.AddKeyValueDbContext<StateKeyValueDbContext>(o => o.UseInMemoryDatabase());
-
-//            services.AddSingleton<KernelTestHelper>();
-        }
-    }
     [DependsOn(
         typeof(AbpEventBusModule),
-        typeof(TestBaseKernelAElfModule))]
-    public class KernelCoreTestAElfModule : AElfModule
-    {
-    }
-    [DependsOn(
-        typeof(SmartContractAElfModule),
-        typeof(KernelCoreTestAElfModule))]
-    public class SmartContractTestAElfModule: AElfModule
-    {
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            var services = context.Services;
-            services.AddSingleton<SmartContractRunnerContainer>();
-        }
-    }
-    [DependsOn(
-        typeof(SmartContractExecutionAElfModule),
-        typeof(KernelCoreTestAElfModule)
-    )]
-    public class SmartContractExecutionTestAElfModule : AElfModule
-    {
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-        }
-    }
-    [DependsOn(
-        typeof(TransactionPoolAElfModule),
-        typeof(KernelCoreTestAElfModule)
-    )]
-    public class TransactionPoolTestAElfModule: AElfModule
-    {
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            context.Services.AddSingleton<TxHub>();
-        }
-    }
-    [DependsOn(
-        typeof(ChainControllerAElfModule),
-        typeof(KernelCoreTestAElfModule))]
-    public class ChainControllerTestAElfModule : AElfModule
-    {
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            var services = context.Services;
-            
-            services.AddTransient<ChainCreationService>();
-        }
-    }
-    #endregion
-    [DependsOn(
-        typeof(CSharpRuntimeAElfModule),
-        typeof(CoreOSAElfModule),
+        typeof(AbpTestBaseModule),
+        typeof(CoreKernelAElfModule),
         typeof(KernelAElfModule),
+        typeof(NodeAElfModule),
+        typeof(CoreOSAElfModule),
         typeof(ConsensusAElfModule),
-        typeof(KernelCoreTestAElfModule),
-        typeof(SmartContractTestAElfModule),
-        typeof(SmartContractExecutionTestAElfModule),
-        typeof(TransactionPoolTestAElfModule),
-        typeof(ChainControllerTestAElfModule)
+        typeof(SmartContractAElfModule),
+        typeof(SmartContractExecutionAElfModule),
+        typeof(TransactionPoolAElfModule),
+        typeof(ChainControllerAElfModule),
+        typeof(CSharpRuntimeAElfModule),
+        typeof(TokenKernelAElfModule)
     )]
     public class ContractTestModule : AElfModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             var services = context.Services;
+
+            #region Infra
+
+            services.AddKeyValueDbContext<BlockchainKeyValueDbContext>(o => o.UseInMemoryDatabase());
+            services.AddKeyValueDbContext<StateKeyValueDbContext>(o => o.UseInMemoryDatabase());
+            services.AddTransient<ChainCreationService>();
+            services.AddSingleton<TxHub>();
+            services.AddSingleton<SmartContractRunnerContainer>();
+
+            #endregion
+
+
+            #region Logger
+
+            ITestOutputHelperAccessor testOutputHelperAccessor = new TestOutputHelperAccessor();
+            services.AddSingleton(testOutputHelperAccessor);
+            services.AddLogging(o => { o.AddXUnit(testOutputHelperAccessor); });
+
+            #endregion
+
+            #region Mocks
+
             services.AddSingleton(o => Mock.Of<IAElfNetworkServer>());
             services.AddSingleton(o => Mock.Of<IPeerPool>());
 
             services.AddSingleton(o => Mock.Of<INetworkService>());
-            
+
             // When testing contract and packaging transactions, no need to generate and schedule real consensus stuff.
             context.Services.AddSingleton(o => Mock.Of<IConsensusInformationGenerationService>());
             context.Services.AddSingleton(o => Mock.Of<IConsensusScheduler>());
             context.Services.AddTransient(o => Mock.Of<IConsensusService>());
             context.Services.AddTransient(o => Mock.Of<IAccountService>());
+
+            #endregion
+
             context.Services.AddTransient<IContractTesterFactory, ContractTesterFactory>();
             context.Services.AddTransient<ITransactionExecutor, TransactionExecutor>();
             context.Services.AddSingleton<IBlockTimeProvider, BlockTimeProvider>();
@@ -174,6 +115,10 @@ namespace AElf.Contracts.TestKit
             var that = this;
             AsyncHelper.RunSync(() => osService.StopAsync(that.OsBlockchainNodeContext));
         }
+    }
 
+    public class TestOutputHelperAccessor : ITestOutputHelperAccessor
+    {
+        public ITestOutputHelper OutputHelper { get; set; }
     }
 }
