e0cd2d84f94858ad141d735990a1ecc28644e3f3
Peng Rong <rongpeng@live.com>
2019-04-13 15:43:50 +0800

    Fix windows ci issue

--- scripts/download_binary.bat
+++ scripts/download_binary.bat
@@ -1,10 +1,12 @@
 SET scriptdir=%~dp0
-
-SET version="v1.0.2"
-SET filename="contract_csharp_plugin-%version%-win32.zip"
+SET version=v1.0.2
+SET filename=contract_csharp_plugin-%version%-win32.zip
+SET url=https://github.com/AElfProject/contract-plugin/releases/download/%version%/%filename%
+SET file=%scriptdir%%filename%
 
 if not exist "%scriptdir%contract_csharp_plugin.exe" (
-    powershell -Command "(new-object net.webclient).DownloadFile('https://github.com/AElfProject/contract-plugin/releases/download/%version%/%filename%', '%scriptdir%%filename%')"
-    unzip -o "%scriptdir%%filename%"  -d "%scriptdir%"
-    del "%scriptdir%%filename%"
+    powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (new-object 
+net.webclient).DownloadFile('%url%', '%file%')"
+    unzip %scriptdir%%filename% -d %scriptdir%
+    del %scriptdir%%filename%
 )
