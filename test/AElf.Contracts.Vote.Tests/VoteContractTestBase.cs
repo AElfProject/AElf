519c42730c0b1da729ed160036b9546378a6dbd5
EricShu <blackeye286@126.com>
2019-04-13 12:01:42 +0800

    init two contract test projects.

---
+++ test/AElf.Contracts.Vote.Tests/VoteContractTestBase.cs
@@ -0,0 +1,9 @@
+using AElf.Contracts.TestKit;
+
+namespace AElf.Contracts.Vote
+{
+    public class VoteContractTestBase : ContractTestBase<VoteContractTestAElfModule>
+    {
+        
+    }
+}
---
+++ test/AElf.Contracts.Vote.Tests/VoteTests.cs
@@ -0,0 +1,9 @@
+namespace AElf.Contracts.Vote
+{
+    public class VoteTests : VoteContractTestBase
+    {
+        public VoteTests()
+        {
+        }
+    }
+}
