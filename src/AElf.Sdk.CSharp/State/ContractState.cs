c0452ab60aaaf6848902d984fbe8c858eced1dce
swk <shiwk@tju.edu.cn>
2019-04-15 12:16:28 +0800

    Fix build errors.

--- src/AElf.Sdk.CSharp/State/ContractState.cs
+++ src/AElf.Sdk.CSharp/State/ContractState.cs
@@ -1,11 +1,9 @@
 using System.IO;
-using AElf.Common;
 using AElf.Kernel;
 
 namespace AElf.Sdk.CSharp.State
 {
     public class ContractState : StructuredState
     {
-        public SingletonState<Address> Owner { get; set; }
     }
 }
