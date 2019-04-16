979dd13f4357e60832fe4df7e59171469701f78f
EricShu <blackeye286@126.com>
2019-04-16 19:01:25 +0800

    add vote contract test cases

--- src/AElf.Contracts.Vote/VoteContract.cs
+++ src/AElf.Contracts.Vote/VoteContract.cs
@@ -82,12 +82,14 @@ namespace AElf.Contracts.Vote
             State.VotingResults[votingResultHash] = new VotingResult
             {
                 Topic = input.Topic,
-                Sponsor = Context.Sender
+                Sponsor = Context.Sender,
+                EpochNumber = 1
             };
 
             return new Empty();
         }
 
+        //TODO: User cannot vote when Event CurrentEpoch >= EpochNumber + 1
         public override Empty Vote(VoteInput input)
         {
             var votingEvent = AssertVotingEvent(input.Topic, input.Sponsor);
@@ -136,10 +138,10 @@ namespace AElf.Contracts.Vote
             {
                 Voter = votingRecord.Voter
             };
-            var voteHash = votingResult.GetHash().ToHex();
-            if (!votingHistories.Votes.ContainsKey(voteHash))
+            var votingEventHash = votingEvent.GetHash().ToHex();
+            if (!votingHistories.Votes.ContainsKey(votingEventHash))
             {
-                votingHistories.Votes[voteHash] = new VotingHistory
+                votingHistories.Votes[votingEventHash] = new VotingHistory
                 {
                     ActiveVotes = {input.VoteId}
                 };
@@ -147,7 +149,7 @@ namespace AElf.Contracts.Vote
             }
             else
             {
-                votingHistories.Votes[votingResult.GetHash().ToHex()].ActiveVotes.Add(input.VoteId);
+                votingHistories.Votes[votingEventHash].ActiveVotes.Add(input.VoteId);
             }
 
             State.VotingRecords[input.VoteId] = votingRecord;
@@ -176,7 +178,7 @@ namespace AElf.Contracts.Vote
         public override Empty Withdraw(WithdrawInput input)
         {
             var votingRecord = State.VotingRecords[input.VoteId];
-            Assert(votingRecord.EpochNumber == 0, "Voting record not found.");
+            Assert(votingRecord != null, "Voting record not found.");
 
             var votingEventHash = new VotingEvent
             {
@@ -200,12 +202,12 @@ namespace AElf.Contracts.Vote
                 Topic = votingRecord.Topic,
                 EpochNumber = votingRecord.EpochNumber
             }.GetHash();
-
-            var votingHistories = UpdateHistoryAfterWithdrawing(votingRecord.Voter, votingGoingHash, input.VoteId);
+            
+            var votingHistories = UpdateHistoryAfterWithdrawing(votingRecord.Voter, votingEventHash, input.VoteId);
 
             var votingResult = State.VotingResults[votingGoingHash];
-            votingResult.Results[votingRecord.Option] -= votingRecord.Amount;
-            if (!votingHistories.Votes[votingGoingHash.ToHex()].ActiveVotes.Any())
+           votingResult.Results[votingRecord.Option] -= votingRecord.Amount;
+            if (!votingHistories.Votes[votingEventHash.ToHex()].ActiveVotes.Any())
             {
                 votingResult.VotersCount -= 1;
             }
@@ -220,7 +222,7 @@ namespace AElf.Contracts.Vote
                     Symbol = votingRecord.Currency,
                     Amount = votingRecord.Amount,
                     LockId = input.VoteId,
-                    To = votingRecord.Sponsor,
+                    To = Context.Self,
                     Usage = $"Withdraw votes for {votingRecord.Topic}"
                 });
             }
@@ -228,6 +230,7 @@ namespace AElf.Contracts.Vote
             return new Empty();
         }
 
+        //TODO: EpochNumber cannot update when CurrentEpoch >= EpochNumber + 1 
         public override Empty UpdateEpochNumber(UpdateEpochNumberInput input)
         {
             var votingEvent = AssertVotingEvent(input.Topic, Context.Sender);
@@ -298,7 +301,10 @@ namespace AElf.Contracts.Vote
         public override VotingHistory GetVotingHistory(GetVotingHistoryInput input)
         {
             var votingEvent = AssertVotingEvent(input.Topic, input.Sponsor);
-            var votes = State.VotingHistoriesMap[input.Voter].Votes[votingEvent.GetHash().ToHex()];
+            var allVotes = State.VotingHistoriesMap[input.Voter];
+            Assert(allVotes != null, "Voting record not found.");
+            var votes = allVotes.Votes[votingEvent.GetHash().ToHex()];
+            Assert(votes != null, "Voting record not found.");
             var activeVotes = votes.ActiveVotes;
             var withdrawnVotes = votes.WithdrawnVotes;
             return new VotingHistory
@@ -319,11 +325,11 @@ namespace AElf.Contracts.Vote
             return State.VotingEvents[votingEventHash];
         }
 
-        private VotingHistories UpdateHistoryAfterWithdrawing(Address voter, Hash votingGoingHash, Hash voteId)
+        private VotingHistories UpdateHistoryAfterWithdrawing(Address voter, Hash votingEventHash, Hash voteId)
         {
             var votingHistories = State.VotingHistoriesMap[voter];
-            votingHistories.Votes[votingGoingHash.ToHex()].ActiveVotes.Remove(voteId);
-            votingHistories.Votes[votingGoingHash.ToHex()].WithdrawnVotes.Add(voteId);
+            votingHistories.Votes[votingEventHash.ToHex()].ActiveVotes.Remove(voteId);
+            votingHistories.Votes[votingEventHash.ToHex()].WithdrawnVotes.Add(voteId);
             State.VotingHistoriesMap[voter] = votingHistories;
             return votingHistories;
         }
--- test/AElf.Contracts.Vote.Tests/VoteTests.cs
+++ test/AElf.Contracts.Vote.Tests/VoteTests.cs
@@ -310,6 +310,53 @@ namespace AElf.Contracts.Vote
         }
 
         [Fact]
+        public async Task VoteContract_GetVotingHistory()
+        {
+            var topic = "vote test";
+            var voteUsrer = SampleECKeyPairs.KeyPairs[2];
+            await GenerateNewVoteEvent(topic, 1, 10, 3, false);
+            
+            //voting event not exist
+            {
+                var votingHistory = await VoteContractStub.GetVotingHistory.CallAsync(
+                    new GetVotingHistoryInput
+                    {
+                        Topic = "test1",
+                        Sponsor = DefaultSender,
+                        Voter = Address.Generate()
+                    });
+                votingHistory.ActiveVotes.Count.ShouldBe(0);
+                
+            }
+            //voting without result
+            {
+                var votingHistory = await VoteContractStub.GetVotingHistory.CallAsync(
+                    new GetVotingHistoryInput
+                    {
+                        Topic = topic,
+                        Sponsor = DefaultSender,
+                        Voter = Address.FromPublicKey(voteUsrer.PublicKey)
+                    });
+                votingHistory.ActiveVotes.Count.ShouldBe(0);
+
+            }
+            //success
+            {
+                await UserVote(voteUsrer, topic, DefaultSender, Options[0], 200);
+                await UserVote(voteUsrer, topic, DefaultSender, Options[1], 800);
+                
+                var votingHistory = await VoteContractStub.GetVotingHistory.CallAsync(
+                    new GetVotingHistoryInput
+                    {
+                        Topic = topic,
+                        Sponsor = DefaultSender,
+                        Voter = Address.FromPublicKey(voteUsrer.PublicKey)
+                    });
+                votingHistory.ActiveVotes.Count.ShouldBe(2);
+            }
+        }
+        
+        [Fact]
         public async Task VoteContract_GetVotingHistories()
         {
             var topic = "vote test";
@@ -342,6 +389,248 @@ namespace AElf.Contracts.Vote
                 
                 votes.Values.First().ActiveVotes.Count.ShouldBe(2);
             }
+        }
+
+        [Fact]
+        public async Task VoteContract_UpdateEpochNumber()
+        {
+            var topic = "vote test";
+            var voteUser = SampleECKeyPairs.KeyPairs[2];
+            //totalEpoch is 1
+            {
+                await GenerateNewVoteEvent(topic, 1, 10, 3, false);
+                await UserVote(voteUser, topic, DefaultSender, Options[1], 1000L);
+
+                var transactionResult = (await VoteContractStub.UpdateEpochNumber.SendAsync(
+                    new UpdateEpochNumberInput
+                    {
+                        Topic = topic,
+                        EpochNumber = 2
+                    })).TransactionResult;
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+                var votingResult = await VoteContractStub.GetVotingResult.CallAsync(
+                    new GetVotingResultInput
+                    {
+                        Topic = topic,
+                        Sponsor = DefaultSender,
+                        EpochNumber = 2
+                    });
+                votingResult.EpochNumber.ShouldBe(2);
+                votingResult.EndTimestamp.ShouldBeLessThanOrEqualTo(DateTime.UtcNow.ToTimestamp());
+            }
+        }
+
+        [Fact]
+        public async Task VoteContract_Withdraw()
+        {
+            //not exist voting record
+            {
+                var topic = "test1";
+                var voteUser = SampleECKeyPairs.KeyPairs[1];
+                var voteUserStub = GetVoteContractTester(voteUser);
+                
+                var transactionResult = (await voteUserStub.Withdraw.SendAsync(
+                    new WithdrawInput
+                    {
+                        VoteId = Hash.Generate()
+                    })).TransactionResult;
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                transactionResult.Error.Contains("Voting record not found").ShouldBeTrue();
+            }
+            //withdraw ongoing event
+            {
+                var topic = "test2";
+                var voteUser = SampleECKeyPairs.KeyPairs[2];
+                var voteUserStub = GetVoteContractTester(voteUser);
+                await GenerateNewVoteEvent(topic, 1, 100, 2, false);
+                await UserVote(voteUser, topic, DefaultSender, Options[0], 200);
+
+                var votingHistory = await VoteContractStub.GetVotingHistory.CallAsync(new GetVotingHistoryInput
+                {
+                    Topic = topic,
+                    Sponsor = DefaultSender,
+                    Voter = Address.FromPublicKey(voteUser.PublicKey)
+                });
+                
+                var transactionResult = (await voteUserStub.Withdraw.SendAsync(
+                    new WithdrawInput
+                    {
+                        VoteId = votingHistory.ActiveVotes.First()
+                    })).TransactionResult;
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                transactionResult.Error.Contains("Cannot withdraw votes of on-going voting event").ShouldBeTrue();
+            }
+            
+            //withdraw one completed event
+            {
+                var topic = "test3";
+                var voteUser = SampleECKeyPairs.KeyPairs[3];
+                var voteAmount = 500L;
+                var voteUserStub = GetVoteContractTester(voteUser);
+                
+                await GenerateNewVoteEvent(topic, 1, 100, 2, false);
+                await UserVote(voteUser, topic, DefaultSender, Options[0], voteAmount);
+                
+                var beforeBalance = await GetUserBalance(voteUser.PublicKey);
+                
+                await VoteContractStub.UpdateEpochNumber.SendAsync(new UpdateEpochNumberInput
+                {
+                    Topic = topic,
+                    EpochNumber = 2
+                });
+                
+                var votingHistory = await VoteContractStub.GetVotingHistory.CallAsync(new GetVotingHistoryInput
+                {
+                    Topic = topic,
+                    Sponsor = DefaultSender,
+                    Voter = Address.FromPublicKey(voteUser.PublicKey)
+                });
+                
+                var transactionResult = (await voteUserStub.Withdraw.SendAsync(
+                    new WithdrawInput
+                    {
+                        VoteId = votingHistory.ActiveVotes.First()
+                    })).TransactionResult;
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+                var afterBalance = await GetUserBalance(voteUser.PublicKey);
+                afterBalance.ShouldBe(beforeBalance + voteAmount);
+            }
+            
+            //withdraw multiple times of completed event
+            {
+                var topic = "test4";
+                var voteUser = SampleECKeyPairs.KeyPairs[4];
+                var voteAmount = 500L;
+                var voteUserStub = GetVoteContractTester(voteUser);
+                
+                await GenerateNewVoteEvent(topic, 2, 100, 2, false);
+                await UserVote(voteUser, topic, DefaultSender, Options[0], voteAmount);
+                await UserVote(voteUser, topic, DefaultSender, Options[1], voteAmount);
+                
+                var beforeBalance = await GetUserBalance(voteUser.PublicKey);
+                
+                await VoteContractStub.UpdateEpochNumber.SendAsync(new UpdateEpochNumberInput
+                {
+                    Topic = topic,
+                    EpochNumber = 2
+                });
+                
+                var votingHistory = await VoteContractStub.GetVotingHistory.CallAsync(new GetVotingHistoryInput
+                {
+                    Topic = topic,
+                    Sponsor = DefaultSender,
+                    Voter = Address.FromPublicKey(voteUser.PublicKey)
+                });
+                
+                var transactionResult = (await voteUserStub.Withdraw.SendAsync(
+                    new WithdrawInput
+                    {
+                        VoteId = votingHistory.ActiveVotes.First()
+                    })).TransactionResult;
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+                
+                transactionResult = (await voteUserStub.Withdraw.SendAsync(
+                    new WithdrawInput
+                    {
+                        VoteId = votingHistory.ActiveVotes.Last()
+                    })).TransactionResult;
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+                
+                var afterBalance = await GetUserBalance(voteUser.PublicKey);
+                afterBalance.ShouldBe(beforeBalance + voteAmount*2);
+            }
+            
+            //withdraw all tokens after event completed
+            {
+                var topic = "test5";
+                var voteUser = SampleECKeyPairs.KeyPairs[5];
+                var voteAmount = 500L;
+                var voteUserStub = GetVoteContractTester(voteUser);
+                
+                await GenerateNewVoteEvent(topic, 3, 100, 2, false);
+                await UserVote(voteUser, topic, DefaultSender, Options[0], voteAmount);
+                await UserVote(voteUser, topic, DefaultSender, Options[1], voteAmount);
+                
+                await VoteContractStub.UpdateEpochNumber.SendAsync(new UpdateEpochNumberInput
+                {
+                    Topic = topic,
+                    EpochNumber = 2
+                });
+                
+                await UserVote(voteUser, topic, DefaultSender, Options[1], voteAmount);
+                
+                var beforeBalance = await GetUserBalance(voteUser.PublicKey);
+                
+                var votingHistory = await VoteContractStub.GetVotingHistory.CallAsync(new GetVotingHistoryInput
+                {
+                    Topic = topic,
+                    Sponsor = DefaultSender,
+                    Voter = Address.FromPublicKey(voteUser.PublicKey)
+                });
+                
+                votingHistory.ActiveVotes.Count.ShouldBe(3);
+                
+                var transactionResult = (await voteUserStub.Withdraw.SendAsync(
+                    new WithdrawInput
+                    {
+                        VoteId = votingHistory.ActiveVotes.Last()
+                    })).TransactionResult;
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                
+                await VoteContractStub.UpdateEpochNumber.SendAsync(new UpdateEpochNumberInput
+                {
+                    Topic = topic,
+                    EpochNumber = 3
+                });
+
+                foreach (var hash in votingHistory.ActiveVotes)
+                {
+                    transactionResult = (await voteUserStub.Withdraw.SendAsync(
+                        new WithdrawInput
+                        {
+                            VoteId = hash
+                        })).TransactionResult;
+                    transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+                }
+                
+                var afterBalance = await GetUserBalance(voteUser.PublicKey);
+                afterBalance.ShouldBe(beforeBalance + voteAmount*3);
+            }
+        }
+
+        [Fact]
+        public void TestHash()
+        {
+            var hash = Hash.FromMessage(new VotingResult
+            {
+                Topic = "test3",
+                Sponsor = DefaultSender,
+                EpochNumber = 0
+            });
+        }
+        
+        [Fact]
+        public async Task VoteContract_GetVotingResult()
+        {
+            var topic = "vote test";
+            var voteUser = SampleECKeyPairs.KeyPairs[2];
+            await GenerateNewVoteEvent(topic, 1, 10, 3, false);
+            await UserVote(voteUser, topic, DefaultSender, Options[1], 1000L);
+
+            var votingResult = await VoteContractStub.GetVotingResult.CallAsync(new GetVotingResultInput
+            {
+                Topic = topic,
+                Sponsor = DefaultSender,
+                EpochNumber = 1
+            });
+            
+            votingResult.Topic.ShouldBe(topic);
+            votingResult.VotersCount.ShouldBe(1);
+            votingResult.Results.Values.First().ShouldBe(1000L);
         }
         
         private async Task<TransactionResult> GenerateNewVoteEvent(string topic, int totalEpoch, int activeDays, int optionCount, bool delegated)
