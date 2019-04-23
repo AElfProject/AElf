ed486f03bc6302d175b05102f67a45ec408e165b
EricShu <blackeye286@126.com>
2019-04-23 10:37:50 +0800

    add test case and remove ignore contract test coverage

--- test/AElf.Contracts.Election.Tests/ElectionTests.cs
+++ test/AElf.Contracts.Election.Tests/ElectionTests.cs
@@ -28,6 +28,162 @@ namespace AElf.Contracts.Election
             transactionResult.Error.Contains("Already initialized.").ShouldBeTrue();
         }
 
+        [Fact]
+        public async Task AnnounceElection_Without_EnoughToken()
+        {
+            var userKeyPair = SampleECKeyPairs.KeyPairs[11];
+
+            var transactionResult = await UserAnnounceElection(userKeyPair);
+            
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+            transactionResult.Error.Contains("Insufficient balance").ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task AnnounceElection_Success()
+        {
+            var userKeyPair = SampleECKeyPairs.KeyPairs[1];
+            var beforeBalance = await GetUserBalance(userKeyPair.PublicKey);
+            
+            var transactionResult = await UserAnnounceElection(userKeyPair);
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+
+            var afterBalance = await GetUserBalance(userKeyPair.PublicKey);
+            
+            beforeBalance.ShouldBe(afterBalance + ElectionContractConsts.LockTokenForElection);
+        }
+
+        [Fact]
+        public async Task AnnounceElection_Twice()
+        {
+            await AnnounceElection_Success();
+            
+            var userKeyPair = SampleECKeyPairs.KeyPairs[1];
+            var transactionResult = await UserAnnounceElection(userKeyPair);
+            
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+            transactionResult.Error.Contains("Voter can't announce election").ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task QuitElection_WithCandidate()
+        {
+            await AnnounceElection_Success();
+            
+            var userKeyPair = SampleECKeyPairs.KeyPairs[1];
+            var beforeBalance = await GetUserBalance(userKeyPair.PublicKey);
+            
+            var transactionResult = await UserQuiteElection(userKeyPair);
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+            
+            var afterBalance = await GetUserBalance(userKeyPair.PublicKey);
+            afterBalance.ShouldBe(beforeBalance + ElectionContractConsts.LockTokenForElection);
+        }
+
+        [Fact]
+        public async Task QuitElection_WithCommonUser()
+        {
+            var userKeyPair = SampleECKeyPairs.KeyPairs[2];
+            
+            var transactionResult = await UserQuiteElection(userKeyPair);
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+            transactionResult.Error.Contains("").ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task UserVote_Candidate_Success()
+        {
+            var candidateUser = SampleECKeyPairs.KeyPairs[1];
+            await UserAnnounceElection(candidateUser);
+            
+            var voteUser = SampleECKeyPairs.KeyPairs[11];
+            var beforeBalance = await GetUserBalance(voteUser.PublicKey);
+            
+            var transactionResult = await UserVoteForCandidate(voteUser, candidateUser.PublicKey.ToHex(), 100, 500);
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+
+            var afterBalance = await GetUserBalance(voteUser.PublicKey);
+            afterBalance.ShouldBe(beforeBalance - 500);
+        }
+
+        [Fact]
+        public async Task UserVote_Candidate_Failed()
+        {
+            var commonUser = SampleECKeyPairs.KeyPairs[1];            
+            var voteUser = SampleECKeyPairs.KeyPairs[11];
+            
+            //candidate is not in list
+            {
+                var transactionResult = await UserVoteForCandidate(voteUser, commonUser.PublicKey.ToHex(), 120, 100);
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                transactionResult.Error.Contains("").ShouldBeTrue();
+            }
+
+            await UserAnnounceElection(commonUser);
+            
+            //user token is not enough
+            {
+                var transactionResult = await UserVoteForCandidate(voteUser, commonUser.PublicKey.ToHex(), 120, 100_000);
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                transactionResult.Error.Contains("Insufficient balance").ShouldBeTrue();
+            }
+            
+            //lock time is not over 90 days
+            {
+                var transactionResult = await UserVoteForCandidate(voteUser, commonUser.PublicKey.ToHex(), 80, 1000);
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                transactionResult.Error.Contains("Should lock token for at least 90 days").ShouldBeTrue();
+            }
+        }
+
+        [Fact]
+        public async Task UserWithdraw_Success()
+        {
+            var candidateUser = SampleECKeyPairs.KeyPairs[1];     
+            await UserAnnounceElection(candidateUser);
+
+            var voteUser = SampleECKeyPairs.KeyPairs[11];
+            var beforeBalance = await GetUserBalance(voteUser.PublicKey);
+            
+            var transactionResult = await UserVoteForCandidate(voteUser, candidateUser.PublicKey.ToHex(), 120, 1000);
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+
+            var afterBalance = await GetUserBalance(voteUser.PublicKey);
+            afterBalance.ShouldBe(beforeBalance - 1000);
+        }
+        
+        [Fact]
+        public async Task UserWithdraw_Failed()
+        {
+        }
+
+        [Fact]
+        public async Task UpdateTermNumber_Success()
+        {
+            
+        } 
+        
+        [Fact]
+        public async Task UpdateTermNumber_Failed()
+        {
+            
+        }
+
+        [Fact]
+        public async Task GetElectionResult()
+        {
+            
+        }
+
+        [Fact]
+        public async Task GetTicketsInformation()
+        {
+            
+        }
+
         private async Task<TransactionResult> UserAnnounceElection(ECKeyPair userKeyPair)
         {
             var electionStub = GetElectionContractTester(userKeyPair);
