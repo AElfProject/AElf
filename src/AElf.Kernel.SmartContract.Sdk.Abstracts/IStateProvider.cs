990a974d4d0978720014a83d94c070413dd35fc2
gldeng <realgldeng@gmail.com>
2019-04-23 12:18:53 +0800

    Move ISmartContractBridgeContext to AElf.Types

--- src/AElf.Kernel.SmartContract.Sdk.Abstracts/IStateProvider.cs
+++ src/AElf.Kernel.SmartContract.Sdk.Abstracts/IStateProvider.cs
@@ -2,11 +2,6 @@ using System.Threading.Tasks;
 
 namespace AElf.Kernel.SmartContract.Sdk
 {
-    public interface IStateProvider
-    {
-        Task<byte[]> GetAsync(StatePath path);
-    }
-
     public interface ICachedStateProvider : IStateProvider
     {
         IStateCache Cache { get; set; }
