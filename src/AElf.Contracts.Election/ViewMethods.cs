e9870e162c7376f856ef2f9694f83e90342bb075
EanCuznaivy <dansuirenqu@126.com>
2019-04-27 17:07:57 +0800

    [Feature] View methods related to ElectionVotingReocrds

--- src/AElf.Contracts.Election/ViewMethods.cs
+++ src/AElf.Contracts.Election/ViewMethods.cs
@@ -33,12 +33,94 @@ namespace AElf.Contracts.Election
 
         public override CandidateHistory GetCandidateHistory(StringInput input)
         {
-            return State.Histories[input.Value];
+            return State.Histories[input.Value] ?? new CandidateHistory();
         }
 
         public override TermSnapshot GetTermSnapshot(GetTermSnapshotInput input)
         {
-            return State.Snapshots[input.TermNumber];
+            return State.Snapshots[input.TermNumber] ?? new TermSnapshot();
+        }
+
+        public override Votes GetVotesInformation(StringInput input)
+        {
+            return State.Votes[input.Value] ?? new Votes();
+        }
+
+        public override Votes GetVotesInformationWithRecords(StringInput input)
+        {
+            var votes = State.Votes[input.Value];
+            if (votes == null) return new Votes();
+            
+            var votedRecords = State.VoteContract.GetVotingRecords.Call(new GetVotingRecordsInput
+            {
+                Ids = {votes.ActiveVotesIds}
+            }).Records;
+            var index = 0;
+            foreach (var record in votedRecords)
+            {
+                var voteId = votes.ActiveVotesIds[index++];
+                votes.ActiveVotesRecords.Add(TransferVotingRecordToElectionVotingRecord(record, voteId));
+            }
+            
+            var obtainedRecords = State.VoteContract.GetVotingRecords.Call(new GetVotingRecordsInput
+            {
+                Ids = {votes.ObtainedActiveVotesIds}
+            }).Records;
+            index = 0;
+            foreach (var record in obtainedRecords)
+            {
+                var voteId = votes.ActiveVotesIds[index++];
+                votes.ObtainedActiveVotesRecords.Add(TransferVotingRecordToElectionVotingRecord(record, voteId));
+            }
+
+            return votes;
+        }
+
+        public override Votes GetVotesInformationWithAllRecords(StringInput input)
+        {
+            var votes = GetVotesInformationWithRecords(input);
+            
+            var votedWithdrawnRecords = State.VoteContract.GetVotingRecords.Call(new GetVotingRecordsInput
+            {
+                Ids = {votes.WithdrawnVotesIds}
+            }).Records;
+            var index = 0;
+            foreach (var record in votedWithdrawnRecords)
+            {
+                var voteId = votes.ActiveVotesIds[index++];
+                votes.WithdrawnVotesRecords.Add(TransferVotingRecordToElectionVotingRecord(record, voteId));
+            }
+            
+            var obtainedWithdrawnRecords = State.VoteContract.GetVotingRecords.Call(new GetVotingRecordsInput
+            {
+                Ids = {votes.ObtainedWithdrawnVotesIds}
+            }).Records;
+            index = 0;
+            foreach (var record in obtainedWithdrawnRecords)
+            {
+                var voteId = votes.ActiveVotesIds[index++];
+                votes.ObtainedWithdrawnVotesRecords.Add(TransferVotingRecordToElectionVotingRecord(record, voteId));
+            }
+
+            return votes;
+        }
+
+        private ElectionVotingRecord TransferVotingRecordToElectionVotingRecord(VotingRecord votingRecord, Hash voteId)
+        {
+            var lockDays = State.LockTimeMap[voteId];
+            return new ElectionVotingRecord
+            {
+                Voter = votingRecord.Voter,
+                Candidate = votingRecord.Option,
+                Amount = votingRecord.Amount,
+                TermNumber = votingRecord.EpochNumber,
+                VoteId = voteId,
+                LockTime = lockDays,
+                VoteTimestamp = votingRecord.VoteTimestamp,
+                WithdrawTimestamp = votingRecord.WithdrawTimestamp,
+                UnlockTimestamp = votingRecord.VoteTimestamp.ToDateTime().AddDays(lockDays).ToTimestamp(),
+                IsWithdrawn = votingRecord.IsWithdrawn
+            };
         }
     }
 }
