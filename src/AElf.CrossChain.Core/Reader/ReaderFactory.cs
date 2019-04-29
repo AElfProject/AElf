79e656e5ecd15d5e8dbd1033f224f215477ff6c1
gldeng <realgldeng@gmail.com>
2019-04-29 17:48:42 +0800

    Create factories for stub

---
+++ src/AElf.CrossChain.Core/Reader/ReaderFactory.cs
@@ -0,0 +1,33 @@
+using AElf.Contracts.CrossChain;
+using AElf.Kernel;
+using AElf.Kernel.SmartContract.Application;
+
+namespace AElf.CrossChain
+{
+    internal interface IReaderFactory
+    {
+        CrossChainContractContainer.CrossChainContractStub Create(IChainContext chainContext);
+    }
+
+    internal class ReaderFactory : IReaderFactory
+    {
+        private readonly ITransactionReadOnlyExecutionService _transactionReadOnlyExecutionService;
+        private readonly ISmartContractAddressService _smartContractAddressService;
+
+        public ReaderFactory(ITransactionReadOnlyExecutionService transactionReadOnlyExecutionService,
+            ISmartContractAddressService smartContractAddressService)
+        {
+            _transactionReadOnlyExecutionService = transactionReadOnlyExecutionService;
+            _smartContractAddressService = smartContractAddressService;
+        }
+
+        public CrossChainContractContainer.CrossChainContractStub Create(IChainContext chainContext)
+        {
+            return new CrossChainContractContainer.CrossChainContractStub()
+            {
+                __factory = new MethodStubFactory(_transactionReadOnlyExecutionService, _smartContractAddressService,
+                    chainContext)
+            };
+        }
+    }
+}
