440bda46ab7747eeb124348f979aaa42b60cec07
Anil Erkol <aerkol@aelf.io>
2019-04-29 17:09:08 +0800

    Add tests to check system contracts to ensure default policy is compatible with system contracts.

--- test/AElf.Runtime.CSharp.Tests/ContractAuditorTests.cs
+++ test/AElf.Runtime.CSharp.Tests/ContractAuditorTests.cs
@@ -1,4 +1,11 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using AElf.Contracts.AssociationAuth;
+using AElf.Contracts.Consensus.DPoS;
+using AElf.Contracts.CrossChain;
+using AElf.Contracts.Genesis;
+using AElf.Contracts.MultiToken;
 using Shouldly;
 using Xunit;
 
@@ -14,14 +21,31 @@ namespace AElf.Runtime.CSharp.Tests
         }
         
         [Fact]
-        public void CodeCheck_WithoutPrivilege()
+        public void CodeCheck_TestContract()
         {
-            var code = ReadCode(typeof(TestContract.TestContract).Assembly.Location);
-            
-            var result = _auditor.Audit(code, false);
-            
-            // Temporary disable until the rules are complete for the test contract at least
-            //result.ShouldBeEmpty();
+            var code = ReadCode(typeof(TokenContract).Assembly.Location);
+
+            Should.NotThrow(()=>_auditor.Audit(code, false));
+        }
+
+        [Fact]
+        public void CodeCheck_SystemContracts()
+        {
+            var contracts = new[]
+            {
+                typeof(BasicContractZero),
+                typeof(AssociationAuthContract),
+                // typeof(ConsensusContract),
+                // typeof(Contracts.Consensus.DPoS.SideChain.ConsensusContract),
+                // typeof(CrossChainContract),
+            };
+
+            foreach (var contract in contracts)
+            {
+                var code = ReadCode(contract.Assembly.Location);
+    
+                Should.NotThrow(()=>_auditor.Audit(code, false));
+            }
         }
 
         private byte[] ReadCode(string path)
@@ -29,5 +53,4 @@ namespace AElf.Runtime.CSharp.Tests
             return File.Exists(path) ? File.ReadAllBytes(path) : null;
         }
     }
-
 }
