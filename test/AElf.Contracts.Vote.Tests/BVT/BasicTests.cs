e4205f6e5e78fe75735817bec14a2106f2a8bb63
EricShu <blackeye286@126.com>
2019-05-05 20:34:46 +0800

    update basic vote cases

--- test/AElf.Contracts.Vote.Tests/BVT/BasicTests.cs
+++ test/AElf.Contracts.Vote.Tests/BVT/BasicTests.cs
@@ -45,13 +45,108 @@ namespace AElf.Contracts.Vote
         [Fact]
         public async Task VoteContract_Vote()
         {
+            //voting item not exist
+            {
+                var transactionResult = await Vote(DefaultSenderKeyPair, Hash.Generate(), string.Empty, 100);
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                transactionResult.Error.Contains("Voting item not found").ShouldBeTrue();
+            }
             
+            //voting item have been out of date
+            {
+                var registerItem = await RegisterVotingItemAsync(100, 3, true, DefaultSender, 1); 
+                await TakeSnapshot(registerItem.VotingItemId, 1);
+
+                var voter = SampleECKeyPairs.KeyPairs[11];
+                var voteResult = await Vote(voter, registerItem.VotingItemId, registerItem.Options[0], 100);
+                voteResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                voteResult.Error.Contains("Current voting item already ended").ShouldBeTrue();
+            }
+            
+            //vote without enough token
+            {
+                var registerItem = await RegisterVotingItemAsync(100, 3, true, DefaultSender, 1); 
+                var voter = SampleECKeyPairs.KeyPairs[31];
+                var voteResult = await Vote(voter, registerItem.VotingItemId, registerItem.Options[0], 100);
+                voteResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                voteResult.Error.Contains("Insufficient balance").ShouldBeTrue();
+            }
+            
+            //vote option not exist
+            {
+                var registerItem = await RegisterVotingItemAsync(100, 3, true, DefaultSender, 1); 
+                var voter = SampleECKeyPairs.KeyPairs[11];
+                var option = Address.Generate().GetFormatted();
+                var voteResult = await Vote(voter, registerItem.VotingItemId, option, 100);
+                voteResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                voteResult.Error.Contains($"Option {option} not found").ShouldBeTrue();
+            }
+            
+            //vote success
+            {
+                var registerItem = await RegisterVotingItemAsync(100, 3, true, DefaultSender, 1); 
+                var voter = SampleECKeyPairs.KeyPairs[11];
+                var voteResult = await Vote(voter, registerItem.VotingItemId, registerItem.Options[1], 100);
+                voteResult.Status.ShouldBe(TransactionResultStatus.Mined);
+            }
         }
         
         [Fact]
         public async Task VoteContract_Withdraw()
         {
+            //without vote
+            {
+                var withdrawResult = await Withdraw(SampleECKeyPairs.KeyPairs[1], Hash.Generate());
+                withdrawResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                withdrawResult.Error.Contains("Voting record not found").ShouldBeTrue();
+            }
             
+            //withdraw with other person
+            {
+                var registerItem = await RegisterVotingItemAsync(100, 3, true, DefaultSender, 1); 
+
+                var voteUser = SampleECKeyPairs.KeyPairs[1];
+                var withdrawUser = SampleECKeyPairs.KeyPairs[2];
+                
+                await Vote(voteUser, registerItem.VotingItemId, registerItem.Options[1], 100);
+                var snapshotResult = await TakeSnapshot(registerItem.VotingItemId, 1);
+
+                var voteIds = await GetVoteIds(voteUser, registerItem.VotingItemId);
+                var transactionResult = await Withdraw(withdrawUser, voteIds.WithdrawnVotes.First());
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                transactionResult.Error.Contains("").ShouldBeTrue();
+            }
+            
+            //vote but not expire
+            {
+                var registerItem = await RegisterVotingItemAsync(100, 3, true, DefaultSender, 1); 
+
+                var voteUser = SampleECKeyPairs.KeyPairs[1];
+                
+                await Vote(voteUser, registerItem.VotingItemId, registerItem.Options[1], 100);
+
+                var voteIds = await GetVoteIds(voteUser, registerItem.VotingItemId);
+                var transactionResult = await Withdraw(voteUser, voteIds.ActiveVotes.First());
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+                transactionResult.Error.Contains("").ShouldBeTrue();
+            }
+            
+            //success
+            {
+                var registerItem = await RegisterVotingItemAsync(100, 3, true, DefaultSender, 1); 
+
+                var voteUser = SampleECKeyPairs.KeyPairs[1];
+                
+                await Vote(voteUser, registerItem.VotingItemId, registerItem.Options[1], 100);
+                await TakeSnapshot(registerItem.VotingItemId, 1);
+
+                var voteIds = await GetVoteIds(voteUser, registerItem.VotingItemId);
+                var transactionResult = await Withdraw(voteUser, voteIds.WithdrawnVotes.First());
+                
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+            }
         }
         
         [Fact]
--- test/AElf.Contracts.Vote.Tests/VoteContractTestHelpers.cs
+++ test/AElf.Contracts.Vote.Tests/VoteContractTestHelpers.cs
@@ -48,6 +48,31 @@ namespace AElf.Contracts.Vote
                 {VotingItemId = input.GetHash(sender)});
         }
 
+        private async Task<TransactionResult> TakeSnapshot(Hash votingItemId, long snapshotNumber)
+        {
+            var input = new TakeSnapshotInput
+            {
+                VotingItemId = votingItemId,
+                SnapshotNumber = snapshotNumber
+            };
+            var transactionResult = (await VoteContractStub.TakeSnapshot.SendAsync(input)).TransactionResult;
+
+            return transactionResult;
+        }
+
+        private async Task<TransactionResult> Withdraw(ECKeyPair owner, Hash voteId)
+        {
+            var voterStub = GetVoteContractTester(owner);
+            var input = new WithdrawInput
+            {
+                VoteId = voteId
+            };
+
+            var withdrawResult = (await voterStub.Withdraw.SendAsync(input)).TransactionResult;
+
+            return withdrawResult;
+        } 
+        
         private async Task<TransactionResult> Vote(ECKeyPair voterKeyPair, Hash votingItemId, string option,
             long amount)
         {
@@ -59,6 +84,19 @@ namespace AElf.Contracts.Vote
             })).TransactionResult;
         }
 
+        private async Task<VotedIds> GetVoteIds(ECKeyPair voterKeyPair, Hash votingItemId)
+        {
+            var voterStub = GetVoteContractTester(voterKeyPair);
+
+            var votingIds = await voterStub.GetVotingIds.CallAsync(new GetVotingIdsInput
+            {
+                Voter = Address.FromPublicKey(voterKeyPair.PublicKey),
+                VotingItemId = votingItemId
+            });
+
+            return votingIds;
+        }
+        
         private List<string> GenerateOptions(int count = 1)
         {
             return Enumerable.Range(0, count).Select(_ => Address.Generate().GetFormatted()).ToList();
