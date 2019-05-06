9d676ffd799a3724a54bc36fbbbe2bc2cea54596
EricShu <blackeye286@126.com>
2019-05-06 14:26:33 +0800

    update snapshot test cases

--- test/AElf.Contracts.Vote.Tests/BVT/SnapshotTests.cs
+++ test/AElf.Contracts.Vote.Tests/BVT/SnapshotTests.cs
@@ -10,68 +10,68 @@ namespace AElf.Contracts.Vote
     public partial class VoteTests : VoteContractTestBase
     {
         [Fact]
-        public async Task VoteContract_TakeSnapshot()
+        public async Task VoteContract_TakeSnapshot_WithoutPermission()
         {
-            //without permission
-            {
-                var votingItem = await RegisterVotingItemAsync(10, 4, true, DefaultSender, 1);
+            var votingItem = await RegisterVotingItemAsync(10, 4, true, DefaultSender, 1);
 
-                var otherUser = SampleECKeyPairs.KeyPairs[2];
-                var transactionResult = (await GetVoteContractTester(otherUser).TakeSnapshot.SendAsync(
-                    new TakeSnapshotInput
-                    {
-                        VotingItemId = votingItem.VotingItemId,
-                        SnapshotNumber = 1
-                    })).TransactionResult;
-                
-                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
-                transactionResult.Error.Contains("").ShouldBeTrue();
-            }
-            
-            //voting event not exist
-            {
-                var transactionResult = (await VoteContractStub.TakeSnapshot.SendAsync(
-                    new TakeSnapshotInput
-                    {
-                        VotingItemId = Hash.Generate(),
-                        SnapshotNumber = 1
-                    })).TransactionResult;
-                
-                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
-                transactionResult.Error.Contains("").ShouldBeTrue();
-            }
-            
-            //snapshot number not correct
-            {
-                var votingItem = await RegisterVotingItemAsync(10, 4, true, DefaultSender, 2);
-                var transactionResult = (await VoteContractStub.TakeSnapshot.SendAsync(
-                    new TakeSnapshotInput
-                    {
-                        VotingItemId = votingItem.VotingItemId,
-                        SnapshotNumber = 2
-                    })).TransactionResult;
-                
-                transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
-                transactionResult.Error.Contains("").ShouldBeTrue();
-            }
-            
-            //success
-            {
-                var registerItem = await RegisterVotingItemAsync(10, 4, true, DefaultSender, 3);
-                for (int i = 0; i < 3; i++)
+            var otherUser = SampleECKeyPairs.KeyPairs[2];
+            var transactionResult = (await GetVoteContractTester(otherUser).TakeSnapshot.SendAsync(
+                new TakeSnapshotInput
                 {
-                    var transactionResult = (await VoteContractStub.TakeSnapshot.SendAsync(
-                        new TakeSnapshotInput
-                        {
-                            VotingItemId = registerItem.VotingItemId,
-                            SnapshotNumber = i+1
-                        })).TransactionResult;
-                
-                    transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+                    VotingItemId = votingItem.VotingItemId,
+                    SnapshotNumber = 1
+                })).TransactionResult;
 
-                    var votingItem = await GetVoteItem(registerItem.VotingItemId);
-                    votingItem.CurrentSnapshotNumber.ShouldBe(i+2);
-                }
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+            transactionResult.Error.Contains("").ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task VoteContract_TakeSnapshot_WithoutVotingItem()
+        {
+            var transactionResult = (await VoteContractStub.TakeSnapshot.SendAsync(
+                new TakeSnapshotInput
+                {
+                    VotingItemId = Hash.Generate(),
+                    SnapshotNumber = 1
+                })).TransactionResult;
+
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+            transactionResult.Error.Contains("Voting item not found").ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task VoteContract_TakeSnapshot_WithWrongSnapshotNumber()
+        {
+            var votingItem = await RegisterVotingItemAsync(10, 4, true, DefaultSender, 2);
+            var transactionResult = (await VoteContractStub.TakeSnapshot.SendAsync(
+                new TakeSnapshotInput
+                {
+                    VotingItemId = votingItem.VotingItemId,
+                    SnapshotNumber = 2
+                })).TransactionResult;
+
+            transactionResult.Status.ShouldBe(TransactionResultStatus.Failed);
+            transactionResult.Error.Contains("Can only take snapshot of current snapshot number").ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task VoteContract_TakeSnapshot_Success()
+        {
+            var registerItem = await RegisterVotingItemAsync(10, 4, true, DefaultSender, 3);
+            for (int i = 0; i < 3; i++)
+            {
+                var transactionResult = (await VoteContractStub.TakeSnapshot.SendAsync(
+                    new TakeSnapshotInput
+                    {
+                        VotingItemId = registerItem.VotingItemId,
+                        SnapshotNumber = i + 1
+                    })).TransactionResult;
+
+                transactionResult.Status.ShouldBe(TransactionResultStatus.Mined);
+
+                var votingItem = await GetVoteItem(registerItem.VotingItemId);
+                votingItem.CurrentSnapshotNumber.ShouldBe(i + 2);
             }
         }
     }
