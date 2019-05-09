a26161e21d8cb9c2df8f60c4d3520a9c15d6cee9
gldeng <realgldeng@gmail.com>
2019-05-09 16:47:59 +0800

    Fix AElf.Benchmark.PerformanceTestContract

--- test/AElf.Contracts.Genesis.Tests/GenesisContractTest.cs
+++ test/AElf.Contracts.Genesis.Tests/GenesisContractTest.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Acs0;
 using AElf.Contracts.MultiToken;
-using AElf.Contracts.Resource;
 using AElf.Contracts.TestKit;
 using AElf.Cryptography.ECDSA;
 using AElf.Kernel;
