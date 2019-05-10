9ac4fa200b47647d794d2897d7babf0a3356e8a4
gldeng <realgldeng@gmail.com>
2019-05-10 12:08:40 +0800

    Fix dividend identifier

--- test/AElf.Contracts.MultiToken.Tests/MultiTokenContractTestBase.cs
+++ test/AElf.Contracts.MultiToken.Tests/MultiTokenContractTestBase.cs
@@ -5,7 +5,7 @@ using AElf.Contracts.TestBase;
  {
      public class MultiTokenContractTestBase : ContractTestBase<MultiTokenContractTestAElfModule>
      {
-         public byte[] DividendContractCode => Codes.Single(kv => kv.Key.Contains("MultiToken")).Value;
+         public byte[] DividendContractCode => Codes.Single(kv => kv.Key.Contains("Dividend")).Value;
          public byte[] TokenContractCode => Codes.Single(kv => kv.Key.Contains("MultiToken")).Value;
      }
  }
