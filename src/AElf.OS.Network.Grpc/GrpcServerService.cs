b721c88145c7dce1ee66c1c7118e12525c9b27ab
samfrb <samos2x@outlook.com>
2019-05-14 17:12:05 +0800

    Limit transaction receiving when the ref block to heigh.

--- src/AElf.OS.Network.Grpc/GrpcServerService.cs
+++ src/AElf.OS.Network.Grpc/GrpcServerService.cs
@@ -122,11 +122,18 @@ namespace AElf.OS.Network.Grpc
         /// <summary>
         /// This method is called when another peer broadcasts a transaction.
         /// </summary>
-        public override Task<VoidReply> SendTransaction(Transaction tx, ServerCallContext context)
+        public override async Task<VoidReply> SendTransaction(Transaction tx, ServerCallContext context)
         {
+            var chain = await _blockChainService.GetChainAsync();
+            
+            // if this transaction's ref block is a lot higher than our chain 
+            // then don't participate in p2p network
+            if (tx.RefBlockNumber > chain.LongestChainHeight + NetworkConsts.DefaultMinBlockGapBeforeSync)
+                return new VoidReply();
+            
             _ = EventBus.PublishAsync(new TransactionsReceivedEvent { Transactions = new List<Transaction> {tx} });
 
-            return Task.FromResult(new VoidReply());
+            return new VoidReply();
         }
 
         /// <summary>
