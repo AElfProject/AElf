77f26437aee6c0a6c6a9fc34f84009863f6351bf
aerkol <anil@erkol.com>
2019-05-16 17:35:36 +0800

    Initial version of Timestamp extensions that can be used in contracts

---
+++ src/AElf.Sdk.CSharp/TimeStampExtensions.cs
@@ -0,0 +1,65 @@
+using System;
+using Google.Protobuf.WellKnownTypes;
+
+namespace AElf.Sdk.CSharp
+{
+    public static class TimeStampExtensions
+    {
+        public static Timestamp AddDays(this Timestamp timestamp, long days)
+        {
+            checked
+            {
+                return Timestamp.FromDateTimeOffset(
+                    DateTimeOffset.FromUnixTimeSeconds(timestamp.Seconds + days * 24 * 60 * 60));
+            }
+        }
+        
+        public static Timestamp AddHours(this Timestamp timestamp, long hours)
+        {
+            checked
+            {
+                return Timestamp.FromDateTimeOffset(
+                    DateTimeOffset.FromUnixTimeSeconds(timestamp.Seconds + hours * 60 * 60));
+            }
+        }
+        
+        public static Timestamp AddMinutes(this Timestamp timestamp, long minutes)
+        {
+            checked
+            {
+                return Timestamp.FromDateTimeOffset(
+                    DateTimeOffset.FromUnixTimeSeconds(timestamp.Seconds + minutes * 60));
+            }
+        }
+
+        public static long Days(this Duration duration)
+        {
+            return duration.Seconds / 60 / 60 / 24;
+        }
+
+        public static long Hours(this Duration duration)
+        {
+            return duration.Seconds / 60 / 60;
+        }
+        
+        public static long Minutes(this Duration duration)
+        {
+            return duration.Seconds / 60;
+        }
+        
+        public static double DecimalDays(this Duration duration)
+        {
+            return (double) duration.Seconds / 60 / 60 / 24;
+        }
+
+        public static double DecimalHours(this Duration duration)
+        {
+            return (double) duration.Seconds / 60 / 60;
+        }
+        
+        public static double DecimalMinutes(this Duration duration)
+        {
+            return (double) duration.Seconds / 60;
+        }
+    }
+}
