8f59f147bc78fe0263dfe229ce785c97c0308e15
EanCuznaivy <dansuirenqu@126.com>
2019-05-16 11:25:51 +0800

    [Tune] For testing.

--- src/AElf.Contracts.Consensus.AEDPoS/AEDPoSContractConstants.cs
+++ src/AElf.Contracts.Consensus.AEDPoS/AEDPoSContractConstants.cs
@@ -2,6 +2,6 @@ namespace AElf.Contracts.Consensus.AEDPoS
 {
     public static class AEDPoSContractConstants
     {
-        public const int TinyBlocksNumber = 8;
+        public const int TinyBlocksNumber = 4;
     }
 }
