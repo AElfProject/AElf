2bc2962b33f7ccafb3e3d29128160076fccdf76d
jinpeng wang <jason.wang@hoopox.com>
2019-05-20 21:11:35 +0800

    Add MerkleTreeRootOfTransactionStatus to BlockHeader

--- src/AElf.Kernel.Core/Blockchain/Application/BlockExtraDataService.cs
+++ src/AElf.Kernel.Core/Blockchain/Application/BlockExtraDataService.cs
@@ -47,22 +47,13 @@ namespace AElf.Kernel.Blockchain.Application
         public void FillMerkleTreeRootExtraDataForTransactionStatus(BlockHeader blockHeader,
             IEnumerable<(Hash, TransactionResultStatus)> blockExecutionReturnSet)
         {
-            var extraDataCount = blockHeader.BlockExtraDatas.Count;
-            if( blockHeader.Height != Constants.GenesisBlockHeight 
-                && extraDataCount != _blockExtraDataProviders.Count 
-                && extraDataCount != _blockExtraDataProviders.Count + 1)
-                throw new Exception("Incorrect filled extra data");
-            
             var nodes = new List<Hash>();
             foreach (var (transactionId, status) in blockExecutionReturnSet)
             {
                 nodes.Add(GetHashCombiningTransactionAndStatus(transactionId, status));
             }
-            var rootByteString = new BinaryMerkleTree().AddNodes(nodes).ComputeRootHash().ToByteString();
-            if (blockHeader.Height == Constants.GenesisBlockHeight || extraDataCount == _blockExtraDataProviders.Count)
-                blockHeader.BlockExtraDatas.Add(rootByteString); // not filled.
-            else
-                blockHeader.BlockExtraDatas[_blockExtraDataProviders.Count] = rootByteString; //reset it since already filled
+
+            blockHeader.MerkleTreeRootOfTransactionStatus = new BinaryMerkleTree().AddNodes(nodes).ComputeRootHash();
         }
 
         /// <summary>
