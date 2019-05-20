d4da0ff84293136a3e30df77bd20c439d300d93d
samfrb <samos2x@outlook.com>
2019-05-20 15:42:10 +0800

    Announcement handler.

--- src/AElf.OS/Handlers/SyncStateAnnouncementEventHandler.cs
+++ src/AElf.OS/Handlers/SyncStateAnnouncementEventHandler.cs
@@ -1,22 +1,70 @@
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
+using AElf.Kernel;
+using AElf.Kernel.Blockchain.Application;
+using AElf.Kernel.Node.Application;
 using AElf.OS.Network.Events;
 using AElf.OS.Network.Grpc;
+using AElf.OS.Network.Infrastructure;
+using Microsoft.Extensions.Logging;
 using Volo.Abp.EventBus;
 
 namespace AElf.OS.Handlers
 {
     public class SyncStateAnnouncementEventHandler : ILocalEventHandler<AnnouncementReceivedEventData>
     {
+        private readonly IPeerPool _peerPool;
+        private readonly IBlockchainNodeContextService _blockchainNodeContextService;
+        private readonly IBlockchainService _blockchainService;
+        
+        public ILogger<SyncStateAnnouncementEventHandler> Logger { get; set; }
+
+        public SyncStateAnnouncementEventHandler(IPeerPool peerPool, 
+            IBlockchainNodeContextService blockchainNodeContextService,
+            IBlockchainService blockchainService)
+        {
+            _peerPool = peerPool;
+            _blockchainNodeContextService = blockchainNodeContextService;
+            _blockchainService = blockchainService;
+        }
+        
         public async Task HandleEventAsync(AnnouncementReceivedEventData eventData)
         {
-            await ProcessAnnouncement(eventData, eventData.SenderPubKey);
+            await UpdateSyncState(eventData);
         }
 
-        private async Task ProcessAnnouncement(AnnouncementReceivedEventData eventData, string eventDataSenderPubKey)
+        private async Task UpdateSyncState(AnnouncementReceivedEventData eventData)
         {
-            // check how many peer know about
-            // set state accordingly
+            if (eventData?.Announce == null)
+            {
+                Logger?.LogWarning("Null announcement received.");
+                return;
+            }
+
+            var blockHash = eventData.Announce.BlockHash;
+            var blockHeight = eventData.Announce.BlockHeight;
+            
+            var peers = _peerPool.GetPeers().ToList();
+            
+            var peersWithAnnounce = peers.Where(p =>
+            {
+                if (p.RecentBlockHeightAndHashMappings.TryGetValue(blockHeight, out var hash))
+                    return blockHash == hash;
+                
+                return false;
+            }).ToList();
+            
+            var necessaryCount = (int) (peers.Count * 2d / 3);
+            if (peersWithAnnounce.Count >= necessaryCount)
+            {
+                var chain = await _blockchainService.GetChainAsync();
+
+                if (blockHeight > chain.BestChainHeight)
+                    _blockchainNodeContextService.SetSyncing(true);
+                else
+                    _blockchainNodeContextService.SetSyncing(false);
+            }
         }
     }
 }
