2279b43f0cc82367b2c609b2e445cc997bfbbb7c
EanCuznaivy <dansuirenqu@126.com>
2019-05-24 22:33:17 +0800

    [Fix] Remove RemainSlots const.

--- src/AElf.Contracts.Consensus.AEDPoS/AEDPoSContractConstants.cs
+++ src/AElf.Contracts.Consensus.AEDPoS/AEDPoSContractConstants.cs
@@ -3,7 +3,6 @@ namespace AElf.Contracts.Consensus.AEDPoS
     public static class AEDPoSContractConstants
     {
         public const int TinyBlocksNumber = 8;
-        private const int RemainSlots = 2;
-        public const int TotalTinySlots = TinyBlocksNumber + RemainSlots;
+        public const int TotalTinySlots = TinyBlocksNumber;
     }
 }
