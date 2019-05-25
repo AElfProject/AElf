f9ac2993729d6768ccc4984f6e0fd4b0065233af
gldeng <realgldeng@gmail.com>
2019-05-25 10:40:35 +0800

    Disable GenerateDependencyFile

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dotnet restore -s "https://nuget.cdn.azure.cn/v3/index.json" -s "https://api.nuget.org/v3/index.json" "AElf.sln"
-dotnet build /p:GeneratePackageOnBuild=false "AElf.sln"
+dotnet build /p:GeneratePackageOnBuild=false /p:GenerateDependencyFile=false "AElf.sln"
 
 if [[ $? -ne 0 ]] ; then
   echo "Build failed."
