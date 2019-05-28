18c812a281f8b55e409070d025cab60a428e4e49
jinpeng wang <jason.wang@hoopox.com>
2019-05-28 10:47:29 +0800

    Test better code

--- src/AElf.Kernel.Core/Blockchain/Application/IBlockGenerationService.cs
+++ src/AElf.Kernel.Core/Blockchain/Application/IBlockGenerationService.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AElf.Types;
 
 namespace AElf.Kernel.Blockchain.Application
 {
+    public class GenerateBlockDto
+    {
+        public Hash PreviousBlockHash { get; set; }
+        public long PreviousBlockHeight { get; set; }
+
+        public DateTime BlockTime { get; set; } = DateTime.UtcNow;
+    }
+    
     public interface IBlockGenerationService
     {
         Task<Block> GenerateBlockBeforeExecutionAsync(GenerateBlockDto generateBlockDto);
