90a96012c28ca4d9ff8eba2431a5b0c1581e53b7
jinpeng wang <jason.wang@hoopox.com>
2019-05-28 22:15:41 +0800

    Disable network lib

--- src/AElf.OS.Consensus.DPos/DPoSAnnouncementReceivedEventDataHandler.cs
+++ src/AElf.OS.Consensus.DPos/DPoSAnnouncementReceivedEventDataHandler.cs
@@ -30,6 +30,8 @@ namespace AElf.OS.Consensus.DPos
 
         public async Task HandleEventAsync(AnnouncementReceivedEventData eventData)
         {
+            //Disable network lib
+            return;
             var irreversibleBlockIndex =
                 await _idpoSLastLastIrreversibleBlockDiscoveryService.FindLastLastIrreversibleBlockAsync(
                     eventData.SenderPubKey);
