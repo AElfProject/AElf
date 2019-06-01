1d31ae8e0910d71687a039b13e2b1b4b44e3d0f4
zhxymh <13552710076@126.com>
2019-06-01 12:04:19 +0800

    Fixed contract reference problem

--- bench/AElf.Benchmark/Program.cs
+++ bench/AElf.Benchmark/Program.cs
@@ -1,4 +1,7 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using Volo.Abp;
 
@@ -8,6 +11,7 @@ namespace AElf.Benchmark
     {
         static void Main(string[] args)
         {
+            AddSameFolderLoader();
             using (var application = AbpApplicationFactory.Create<BenchmarkAElfModule>(options =>
             {
                 options.UseAutofac();
@@ -16,6 +20,22 @@ namespace AElf.Benchmark
                 application.Initialize();
                 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());          
             }
+        }
+        
+        static void AddSameFolderLoader()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+
+            currentDomain.AssemblyResolve += LoadFromSameFolder;
+        }
+
+        static Assembly LoadFromSameFolder(object sender, ResolveEventArgs args)
+        {
+            string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
+            if (!File.Exists(assemblyPath)) return null;
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            return assembly;
         }
     }
 }
