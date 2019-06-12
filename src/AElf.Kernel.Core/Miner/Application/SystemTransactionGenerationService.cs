591433d36b638cba78e6e9bbf0a68a74e6ee82d0
EanCuznaivy <dansuirenqu@126.com>
2019-06-12 14:54:14 +0800

    [Logs] Add logs for debug.

--- src/AElf.Kernel.Core/Miner/Application/SystemTransactionGenerationService.cs
+++ src/AElf.Kernel.Core/Miner/Application/SystemTransactionGenerationService.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using AElf.Types;
+using Microsoft.Extensions.Logging;
 
 namespace AElf.Kernel.Miner.Application
 {
     public class SystemTransactionGenerationService : ISystemTransactionGenerationService
     {
         private readonly IEnumerable<ISystemTransactionGenerator> _systemTransactionGenerators;
+
+        public ILogger<SystemTransactionGenerationService> Logger { get; set; }
 
         public SystemTransactionGenerationService(IEnumerable<ISystemTransactionGenerator> systemTransactionGenerators)
         {
@@ -18,6 +21,7 @@ namespace AElf.Kernel.Miner.Application
             foreach (var generator in _systemTransactionGenerators)
             {
                 generator.GenerateTransactions(@from, preBlockHeight, preBlockHash, ref generatedTxns);
+                Logger.LogDebug($"[Mine] Generated system transaction from {generator.GetType().Name}");
             }
 
             return generatedTxns;
