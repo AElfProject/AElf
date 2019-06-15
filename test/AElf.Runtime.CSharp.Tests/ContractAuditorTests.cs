a733b0f4a817258c7fbc0fe091dc764dd9d68f5c
aerkol <anil@erkol.com>
2019-06-15 13:25:04 +0800

    Refactor contract auditor tests

--- test/AElf.Runtime.CSharp.Tests/ContractAuditorTests.cs
+++ test/AElf.Runtime.CSharp.Tests/ContractAuditorTests.cs
@@ -17,34 +17,53 @@ using AElf.Runtime.CSharp.Validators;
 using AElf.Runtime.CSharp.Validators.Method;
 using Shouldly;
 using Xunit;
-using Xunit.Abstractions;
 using ValidationResult = AElf.Runtime.CSharp.Validators.ValidationResult;
 
 namespace AElf.Runtime.CSharp.Tests
 {
-    public class ContractAuditorTests : CSharpRuntimeTestBase
+    public class ContractAuditorFixture : IDisposable
     {
         private ContractAuditor _auditor;
-        private IEnumerable<ValidationResult> _findings;
-        private readonly string _contractDllDir = "../../../contracts/";
 
-        public ContractAuditorTests(ITestOutputHelper testOutputHelper)
+        public ContractAuditorFixture()
         {
             _auditor = new ContractAuditor(null, null);
-            
-            Should.Throw<InvalidCodeException>(() =>
-            {
-                try
-                {
-                    _auditor.Audit(ReadCode(_contractDllDir + typeof(BadContract.BadContract).Module),
-                        false);
-                }
-                catch (InvalidCodeException ex)
-                {
-                    _findings = ex.Findings;
-                    throw ex;
-                }
-            });
+        }
+
+        public void Audit(byte[] code)
+        {
+            _auditor.Audit(code, false);
+        }
+
+        public void Dispose()
+        {
+            _auditor = null;
+        }
+    }
+
+    public class ContractAuditorTests : CSharpRuntimeTestBase, IClassFixture<ContractAuditorFixture>
+    {
+        private readonly ContractAuditorFixture _auditorFixture;
+        private readonly string _contractDllDir = "../../../contracts/";
+        private readonly Type[] contracts = {
+            typeof(AssociationAuthContract),
+            typeof(AEDPoSContract),
+            typeof(CrossChainContract),
+            typeof(ElectionContract),
+            typeof(BasicContractZero),
+            typeof(TokenContract),
+            typeof(ParliamentAuthContract),
+            typeof(ProfitContract),
+            typeof(ReferendumAuthContract),
+            typeof(FeeReceiverContract),
+            typeof(TokenConverterContract),
+            typeof(TestContract.TestContract),
+        };
+
+        public ContractAuditorTests(ContractAuditorFixture auditorFixture)
+        {
+            // Use fixture to instantiate auditor only once
+            _auditorFixture = auditorFixture;
         }
         
         #region Positive Cases
@@ -52,54 +71,20 @@ namespace AElf.Runtime.CSharp.Tests
         [Fact]
         public void CheckSystemContracts_AllShouldPass()
         {
-            var contracts = new[]
-            {
-                typeof(AssociationAuthContract).Module.ToString(),
-                typeof(AEDPoSContract).Module.ToString(),
-                typeof(CrossChainContract).Module.ToString(),
-                typeof(ElectionContract).Module.ToString(),
-                typeof(BasicContractZero).Module.ToString(),
-                typeof(TokenContract).Module.ToString(),
-                typeof(ParliamentAuthContract).Module.ToString(),
-                typeof(ProfitContract).Module.ToString(),
-                typeof(ReferendumAuthContract).Module.ToString(),
-                typeof(FeeReceiverContract).Module.ToString(),
-                typeof(TokenConverterContract).Module.ToString(),
-                typeof(TestContract.TestContract).Module.ToString(),
-            };
-
             // Load the DLL's from contracts folder to prevent codecov injection
-            foreach (var contract in contracts)
+            foreach (var contractPath in contracts.Select(c => _contractDllDir + c.Module.ToString()))
             {
-                var contractDllPath = _contractDllDir + contract;
-                
-                Should.NotThrow(()=>_auditor.Audit(ReadCode(contractDllPath), true));
+                Should.NotThrow(()=>_auditorFixture.Audit(ReadCode(contractPath)));
             }
         }
         
         [Fact]
         public void CheckSystemContracts_Injected_AllShouldPass()
         {
-            var injectedContracts = new[]
+            // Load codecov injected DLL's
+            foreach (var contractPath in contracts.Select(c => c.Assembly.Location))
             {
-                typeof(AssociationAuthContract).Assembly.Location,
-                typeof(AEDPoSContract).Assembly.Location,
-                typeof(CrossChainContract).Assembly.Location,
-                typeof(ElectionContract).Assembly.Location,
-                typeof(BasicContractZero).Assembly.Location,
-                typeof(TokenContract).Assembly.Location,
-                typeof(ParliamentAuthContract).Assembly.Location,
-                typeof(ProfitContract).Assembly.Location,
-                typeof(ReferendumAuthContract).Assembly.Location,
-                typeof(FeeReceiverContract).Assembly.Location,
-                typeof(TokenConverterContract).Assembly.Location,
-                typeof(TestContract.TestContract).Assembly.Location,
-            };
-
-            // Load the DLL's from contracts folder to prevent codecov injection
-            foreach (var contract in injectedContracts)
-            {
-                Should.NotThrow(()=>_auditor.Audit(ReadCode(contract), true));
+                Should.NotThrow(()=>_auditorFixture.Audit(ReadCode(contractPath)));
             }
         }
         
@@ -108,122 +93,104 @@ namespace AElf.Runtime.CSharp.Tests
         #region Negative Cases
 
         [Fact]
-        public void CheckBadContract_ForRandomUsage()
+        public void CheckBadContract_ForFindings()
         {
-            LookFor(_findings, 
+            List<ValidationResult> findings = null;
+            
+            Should.Throw<InvalidCodeException>(() =>
+            {
+                try
+                {
+                    _auditorFixture.Audit(ReadCode(_contractDllDir + typeof(BadContract.BadContract).Module));
+                }
+                catch (InvalidCodeException ex)
+                {
+                    findings = ex.Findings;
+                    throw ex;
+                }
+            });
+            
+            // Random usage
+            LookFor(findings, 
                     "UpdateStateWithRandom", 
                     i => i.Namespace == "System" && i.Type == "Random")
                 .ShouldNotBeNull();
-        }
-        
-        [Fact]
-        public void CheckBadContract_ForDateTimeUtcNowUsage()
-        {
-            LookFor(_findings, 
+            
+            // DateTime UtcNow usage
+            LookFor(findings, 
                     "UpdateStateWithCurrentTime", 
                     i => i.Namespace == "System" && i.Type == "DateTime" && i.Member == "get_UtcNow")
                 .ShouldNotBeNull();
-        }
-        
-        [Fact]
-        public void CheckBadContract_ForDateTimeNowUsage()
-        {
-            LookFor(_findings, 
+            
+            // DateTime Now usage
+            LookFor(findings, 
                     "UpdateStateWithCurrentTime",
                     i => i.Namespace == "System" && i.Type == "DateTime" && i.Member == "get_Now")
                 .ShouldNotBeNull();
-        }
-        
-        [Fact]
-        public void CheckBadContract_ForDateTimeTodayUsage()
-        {
-            LookFor(_findings, 
+            
+            // DateTime Today usage
+            LookFor(findings, 
                     "UpdateStateWithCurrentTime",
                     i => i.Namespace == "System" && i.Type == "DateTime" && i.Member == "get_Today")
                 .ShouldNotBeNull();
-        }
-
-        [Fact]
-        public void CheckBadContract_ForDoubleTypeUsage()
-        {
-            LookFor(_findings, 
+            
+            // Double type usage
+            LookFor(findings, 
                     "UpdateDoubleState",
                     i => i.Namespace == "System" && i.Type == "Double")
                 .ShouldNotBeNull();
-        }
-        
-        [Fact]
-        public void CheckBadContract_ForFloatTypeUsage()
-        {
-            // http://docs.microsoft.com/en-us/dotnet/api/system.single
-            LookFor(_findings, 
+            
+            // Float type usage
+            LookFor(findings, 
                     "UpdateFloatState",
                     i => i.Namespace == "System" && i.Type == "Single") 
                 .ShouldNotBeNull();
-        }
-        
-        [Fact]
-        public void CheckBadContract_ForDiskOpsUsage()
-        {
-            LookFor(_findings, 
+            
+            // Disk Ops usage
+            LookFor(findings, 
                     "WriteFileToNode",
                     i => i.Namespace == "System.IO")
                 .ShouldNotBeNull();
-        }
-
-        [Fact]
-        public void CheckBadContract_ForStringConstructorUsage()
-        {
-            LookFor(_findings, 
-                "InitLargeStringDynamic",
-                i => i.Namespace == "System" && i.Type == "String" && i.Member == ".ctor")
+            
+            // String constructor usage
+            LookFor(findings, 
+                    "InitLargeStringDynamic",
+                    i => i.Namespace == "System" && i.Type == "String" && i.Member == ".ctor")
                 .ShouldNotBeNull();
-        }
-
-        [Fact]
-        public void CheckBadContract_ForDeniedMemberUseInNestedClass()
-        {
-            LookFor(_findings, 
+            
+            // Denied member use in nested class
+            LookFor(findings, 
                     "UseDeniedMemberInNestedClass",
                     i => i.Namespace == "System" && i.Type == "DateTime" && i.Member == "get_Now")
                 .ShouldNotBeNull();
-        }
-        
-        [Fact]
-        public void CheckBadContract_ForDeniedMemberUseInSeparateClass()
-        {
-            LookFor(_findings, 
+            
+            // Denied member use in separate class
+            LookFor(findings, 
                     "UseDeniedMemberInSeparateClass",
                     i => i.Namespace == "System" && i.Type == "DateTime" && i.Member == "get_Now")
                 .ShouldNotBeNull();
-        }
-        
-        [Fact]
-        public void CheckBadContract_ForLargeArrayInitialization()
-        {
-            _findings.FirstOrDefault(f => f is ArrayValidationResult && f.Info.ReferencingMethod == "InitLargeArray")
+            
+            // Large array initialization
+            findings.FirstOrDefault(f => f is ArrayValidationResult && f.Info.ReferencingMethod == "InitLargeArray")
+                .ShouldNotBeNull();
+            
+            // Float operations
+            findings.FirstOrDefault(f => f is FloatOpsValidationResult)
                 .ShouldNotBeNull();
         }
-        
-        [Fact]
-        public void CheckBadContract_ForFloatOperations()
-        {
-            _findings.FirstOrDefault(f => f is FloatOpsValidationResult)
-                .ShouldNotBeNull();
-        }
-        
+
         #endregion
-
+        
         #region Test Helpers
-
-        private Info LookFor(IEnumerable<ValidationResult>  findings, string referencingMethod, Func<Info, bool> criteria)
-        {
-            return findings.Select(f => f.Info).FirstOrDefault(i => i != null && i.ReferencingMethod == referencingMethod && criteria(i));
-        }
-
-        private byte[] ReadCode(string path)
+        
+        byte[] ReadCode(string path)
         {
             return File.Exists(path) ? File.ReadAllBytes(path) : throw new FileNotFoundException("Contract DLL cannot be found. " + path);
+        }
+        
+        Info LookFor(IEnumerable<ValidationResult> findings, string referencingMethod, Func<Info, bool> criteria)
+        {
+            return findings.Select(f => f.Info).FirstOrDefault(i => i != null && i.ReferencingMethod == referencingMethod && criteria(i));
         }
         
         #endregion
