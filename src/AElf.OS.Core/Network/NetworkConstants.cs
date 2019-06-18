97427f9e29607878af9d71bd7bf1d650cd7cb213
peng <rongpeng@live.com>
2019-06-18 14:47:25 +0800

    Change DefaultQueueWorkerCount to 20

--- src/AElf.OS.Core/Network/NetworkConstants.cs
+++ src/AElf.OS.Core/Network/NetworkConstants.cs
@@ -11,9 +11,7 @@ namespace AElf.OS.Network
         public const int DefaultMaxPeers = 25;
         
         public const int DefaultAnnouncementQueueWorkerCount = 20;
-        public const int DefaultTransactionQueueWorkerCount = 4;
-        
-        public const int DefaultQueueWorkerCount = 10;
+        public const int DefaultTransactionQueueWorkerCount = 20;
 
         public const string AnnouncementQueueName = "AnnouncementQueue";
         public const string TransactionQueueName = "TransactionQueue";
