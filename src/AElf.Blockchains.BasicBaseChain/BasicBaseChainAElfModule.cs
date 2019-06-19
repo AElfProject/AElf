e70d3fad6a435564f4cefbfb30abf6cff6986bd6
zhxymh <13552710076@126.com>
2019-06-19 10:08:44 +0800

    remve wallet rpc reference

--- src/AElf.Blockchains.BasicBaseChain/BasicBaseChainAElfModule.cs
+++ src/AElf.Blockchains.BasicBaseChain/BasicBaseChainAElfModule.cs
@@ -15,7 +15,6 @@ using AElf.OS.Node.Application;
 using AElf.OS.Node.Domain;
 using AElf.OS.Rpc.ChainController;
 using AElf.OS.Rpc.Net;
-using AElf.OS.Rpc.Wallet;
 using AElf.Runtime.CSharp;
 using AElf.RuntimeSetup;
 using AElf.WebApp.Web;
@@ -40,7 +39,6 @@ namespace AElf.Blockchains.BasicBaseChain
 
         //TODO: should move to OSAElfModule
         typeof(ChainControllerRpcModule),
-        typeof(WalletRpcModule),
         typeof(NetRpcAElfModule),
         typeof(RuntimeSetupAElfModule),
         typeof(GrpcCrossChainAElfModule),
