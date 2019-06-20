561ba7900fe4e2bf36c3d5f0f7d61a86d3c57b35
aerkol <anil@erkol.com>
2019-06-20 17:26:30 +0800

    Remove obsolete codes related to parallel processing

--- src/AElf.Kernel.SmartContractExecution/Scheduling/GroupStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AElf.Kernel.SmartContractExecution.Scheduling
-{
-    public enum GroupStrategy
-    {
-        NaiveGroup,
-        Limited_MaxAddMins,
-        Limited_MinsAddUp
-    }
-}
