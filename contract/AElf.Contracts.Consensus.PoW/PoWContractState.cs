5c9d07a170a8f1cada3ac0a8e896b74f62c79a4a
Loning <aloning@gmail.com>
2019-06-20 23:27:52 +0800

    pow contract

--- contract/AElf.Contracts.Consensus.PoW/PoWContractState.cs
+++ contract/AElf.Contracts.Consensus.PoW/PoWContractState.cs
@@ -1,9 +1,19 @@
+using Acs4;
 using AElf.Sdk.CSharp.State;
+using Google.Protobuf.WellKnownTypes;
 
 namespace AElf.Contracts.Consensus.PoW
 {
     public class PoWContractState : ContractState
     {
-        
+    }
+
+    public class PowContract : PoWContractImplContainer.PoWContractImplBase
+    {
+        public override ConsensusCommand GetConsensusCommand(BytesValue input)
+        {
+            var cmd = new ConsensusCommand();
+            return cmd;
+        }
     }
 }
