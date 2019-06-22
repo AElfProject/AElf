627ff986a7fb14def447c24b6fd7009d1cb78151
Loning <aloning@gmail.com>
2019-06-22 20:41:35 +0800

    add extra data to block header

--- contract/AElf.Contracts.Consensus.PoW/PoWContractState.cs
+++ contract/AElf.Contracts.Consensus.PoW/PoWContractState.cs
@@ -1,5 +1,6 @@
 using Acs4;
 using AElf.Sdk.CSharp.State;
+using AElf.Types;
 using Google.Protobuf.WellKnownTypes;
 
 namespace AElf.Contracts.Consensus.PoW
@@ -12,8 +13,51 @@ namespace AElf.Contracts.Consensus.PoW
     {
         public override ConsensusCommand GetConsensusCommand(BytesValue input)
         {
-            var cmd = new ConsensusCommand();
+            var cmd = new ConsensusCommand
+            {
+                ExpectedMiningTime = Context.CurrentBlockTime,
+                NextBlockMiningLeftMilliseconds = 0,
+                LimitMillisecondsOfMiningBlock = int.MaxValue
+            };
+
             return cmd;
+        }
+
+        public override BytesValue GetInformationToUpdateConsensus(BytesValue input)
+        {
+            return GetPowBlockHeaderExtraData(new Empty()).ToBytesValue();
+        }
+
+        public override PowBlockHeaderExtraData GetPowBlockHeaderExtraData(Empty input)
+        {
+            PowBlockHeaderExtraData extraData = new PowBlockHeaderExtraData()
+            {
+                Nonce = long.MaxValue - 1,
+                Bits = 1 //
+            };
+
+            return extraData;
+        }
+
+        public override TransactionList GenerateConsensusTransactions(BytesValue input)
+        {
+            return new TransactionList();
+        }
+
+        public override ValidationResult ValidateConsensusAfterExecution(BytesValue input)
+        {
+            return new ValidationResult() {Success = true};
+        }
+
+        public override ValidationResult ValidateConsensusBeforeExecution(BytesValue input)
+        {
+            //TODO: validate NBits and Hash 
+
+            var data = PowBlockHeaderExtraData.Parser.ParseFrom(input.Value);
+            
+            //first: validate NBits 
+            
+            return new ValidationResult() {Success = true};
         }
     }
 }
