3c9765a0bfae3f28ea328ee2cd7634a96f036d90
samfrb <samos2x@outlook.com>
2019-06-22 16:55:16 +0800

    Replace pubkey test consts with correct format.

--- test/AElf.OS.Network.Grpc.Tests/GrpcTestConstants.cs
+++ test/AElf.OS.Network.Grpc.Tests/GrpcTestConstants.cs
@@ -4,7 +4,7 @@ namespace AElf.OS.Network
     {
         public const string FakeListeningPort = "127.0.0.1:666";
         
-        public const string FakePubKey = "0454dcd0afc20d015e328666d8d25f3f28b13ccd9744eb6b153e4a69709aab399";
-        public const string FakePubKey2 = "04f3f28b13ccd9744eb6b153e4a69709aab39954dcd0afc20d015e328666d8d25";
+        public const string FakePubKey = "048f5ced21f8d687cb9ade1c22dc0e183b05f87124c82073f5d82a09b139cc466efbfb6f28494d0a9d7366fcb769fe5436cfb7b5d322a2b0f69c4bcb1c33ac24ad";
+        public const string FakePubKey2 = "040a7bf44d2c79fe5e270943773783a24eed5cda3e71fa49470cdba394a23832d5c831e233cddebea2720c194dffadd656d4dedf84643818ca77edeee17ad4307a";
     }
 }
