0cdc4301875d1a36e567e13ce69865041fd2880a
EanCuznaivy <dansuirenqu@126.com>
2019-06-26 20:27:44 +0800

    [Fix] Tune asc5 test contract.

---
+++ test/AElf.Contracts.TestContract.MethodCallThreshold/MethodCallThresholdContract.cs
@@ -0,0 +1,66 @@
+using Acs5;
+using AElf.Contracts.MultiToken.Messages;
+using AElf.Contracts.TestContract.MethodCallThreshold;
+using AElf.Contracts.TestContract.ProfitSharing;
+using AElf.Contracts.Treasury;
+using AElf.Sdk.CSharp;
+using Google.Protobuf.WellKnownTypes;
+
+namespace AElf.Contracts.TestContract.MethodCallThresholdContract
+{
+    public class MethodCallThresholdContract : MethodCallThresholdContractContainer.MethodCallThresholdContractBase
+    {
+        public override Empty InitializeMethodCallThresholdContract(InitializeMethodCallThresholdContractInput input)
+        {
+            State.TreasuryContract.Value =
+                Context.GetContractAddressByName(SmartContractConstants.TreasuryContractSystemName);
+            State.TreasuryContract.Register.Send(new RegisterInput
+            {
+                TokenSymbol = input.Symbol,
+                TotalSupply =ProfitSharingContractConstants.TotalSupply,
+                ConnectorWeight = "0.2",
+                Decimals = 2,
+                TokenName = "Token of Profit Sharing Contract"
+            });
+
+            State.TokenContract.Value =
+                Context.GetContractAddressByName(SmartContractConstants.TokenContractSystemName);
+            State.TokenContract.Issue.Send(new IssueInput
+            {
+                Symbol = input.Symbol,
+                Amount = ProfitSharingContractConstants.AmountIssueToTokenConverterContract,
+                To = Context.GetContractAddressByName(SmartContractConstants.TokenConverterContractSystemName)
+            });
+
+            State.TokenSymbol.Value = input.Symbol;
+
+            return new Empty();
+        }
+
+        public override Empty SetMethodCallingThreshold(SetMethodCallingThresholdInput input)
+        {
+            AssertPerformedByContractOwner();
+            State.MethodCallingThresholds[input.Method] = new MethodCallingThreshold
+            {
+                SymbolToAmount = {input.SymbolToAmount}
+            };
+            return new Empty();
+        }
+
+        public override MethodCallingThreshold GetMethodCallingThreshold(StringValue input)
+        {
+            return State.MethodCallingThresholds[input.Value];
+        }
+
+        public override Empty SendForFun(Empty input)
+        {
+            return new Empty();
+        }
+        
+        private void AssertPerformedByContractOwner()
+        {
+            var contractInfo = State.Acs0Contract.GetContractInfo.Call(Context.Self);
+            Assert(Context.Sender == contractInfo.Owner, "Only owner are permitted to call this method.");
+        }
+    }
+}
