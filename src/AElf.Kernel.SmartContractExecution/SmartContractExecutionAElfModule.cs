b8493dbddd43fd2d3c187df3adb8a8efeca79dec
EricShu <blackeye286@126.com>
2019-07-01 17:26:36 +0800

    update acs8 test case

--- src/AElf.Kernel.SmartContractExecution/SmartContractExecutionAElfModule.cs
+++ src/AElf.Kernel.SmartContractExecution/SmartContractExecutionAElfModule.cs
@@ -1,17 +1,17 @@
 ﻿using AElf.Kernel.SmartContract;
 using AElf.Kernel.SmartContract.ExecutionPluginForAcs1;
 using AElf.Kernel.SmartContract.ExecutionPluginForAcs5;
+using AElf.Kernel.SmartContract.ExecutionPluginForAcs8;
 using AElf.Kernel.SmartContractExecution.Application;
-using AElf.Kernel.SmartContractExecution.Scheduling;
 using AElf.Modularity;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using Volo.Abp;
 using Volo.Abp.Modularity;
 
 namespace AElf.Kernel.SmartContractExecution
 {
     [DependsOn(typeof(SmartContractAElfModule),
+        typeof(ExecutionPluginForAcs8Module),
         typeof(ExecutionPluginForAcs5Module),
         typeof(ExecutionPluginForAcs1Module))]
     public class SmartContractExecutionAElfModule : AElfModule
