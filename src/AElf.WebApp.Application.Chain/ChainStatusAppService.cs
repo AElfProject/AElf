3abd9b97912e96b2815d6748d1269e60919f9cef
Murphy <lion.zhang@mail.com>
2019-07-02 19:55:03 +0800

    Modify the error caused by merge

--- src/AElf.WebApp.Application.Chain/ChainStatusAppService.cs
+++ src/AElf.WebApp.Application.Chain/ChainStatusAppService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AElf.Kernel.Blockchain.Application;
 using AElf.Kernel.SmartContract.Application;
@@ -27,7 +28,6 @@ namespace AElf.WebApp.Application.Chain
             _blockchainService = blockchainService;
         }
         
-        
         /// <summary>
         /// Get the current status of the block chain.
         /// </summary>
@@ -35,29 +35,19 @@ namespace AElf.WebApp.Application.Chain
         public async Task<ChainStatusDto> GetChainStatusAsync()
         {
             var basicContractZero = _smartContractAddressService.GetZeroSmartContractAddress();
-     
+
             var chain = await _blockchainService.GetChainAsync();
-            var branches = JsonConvert.DeserializeObject<Dictionary<string, long>>(chain.Branches.ToString());
-            var formattedNotLinkedBlocks = new List<NotLinkedBlockDto>();
 
-            foreach (var notLinkedBlock in chain.NotLinkedBlocks)
-            {
-                var block = await _blockchainService.GetBlockAsync(Hash.LoadBase64(notLinkedBlock.Value));
-                formattedNotLinkedBlocks.Add(new NotLinkedBlockDto
-                    {
-                        BlockHash = block.GetHash().ToHex(),
-                        Height = block.Height,
-                        PreviousBlockHash = block.Header.PreviousBlockHash.ToHex()
-                    }
-                );
-            }
+            var branches = chain.Branches.ToDictionary(b => Hash.LoadBase64(b.Key).ToHex(), b => b.Value);
+            var notLinkedBlocks = chain.NotLinkedBlocks.ToDictionary(b => Hash.LoadBase64(b.Key).ToHex(),
+                b => Hash.LoadBase64(b.Value).ToHex());
 
-            return new ChainStatusDto()
+            return new ChainStatusDto
             {
                 ChainId = ChainHelpers.ConvertChainIdToBase58(chain.Id),
                 GenesisContractAddress = basicContractZero?.GetFormatted(),
                 Branches = branches,
-                NotLinkedBlocks = formattedNotLinkedBlocks,
+                NotLinkedBlocks = notLinkedBlocks,
                 LongestChainHeight = chain.LongestChainHeight,
                 LongestChainHash = chain.LongestChainHash?.ToHex(),
                 GenesisBlockHash = chain.GenesisBlockHash.ToHex(),
