b3f5b3ad817deaf5513b3a44d29379ed2499d419
yanhuaViolet <yanhuaviolet@163.com>
2019-07-02 15:32:24 +0800

    Add ClaimTransactionFees Test;

--- test/AElf.Contracts.MultiToken.Tests/MultiTokenContractTestBase.cs
+++ test/AElf.Contracts.MultiToken.Tests/MultiTokenContractTestBase.cs
@@ -1,8 +1,11 @@
 using System.Linq;
 using AElf.Contracts.MultiToken.Messages;
+using AElf.Contracts.Profit;
 using AElf.Contracts.TestKit;
 using AElf.Cryptography.ECDSA;
 using AElf.Types;
+using AElf.Contracts.Treasury;
+using AElf.Contracts.TokenConverter;
 
 namespace AElf.Contracts.MultiToken
 {
@@ -17,7 +20,21 @@ namespace AElf.Contracts.MultiToken
         protected ECKeyPair User1KeyPair { get; } = SampleECKeyPairs.KeyPairs[10];
         protected Address User1Address => Address.FromPublicKey(User1KeyPair.PublicKey);
         protected ECKeyPair User2KeyPair { get; } = SampleECKeyPairs.KeyPairs[11];
+        protected ECKeyPair ManagerKeyPair { get; } = SampleECKeyPairs.KeyPairs[12];
+        protected Address ManagerAddress => Address.FromPublicKey(ManagerKeyPair.PublicKey);
         protected Address User2Address => Address.FromPublicKey(User2KeyPair.PublicKey);
         protected const string DefaultSymbol = "ELF";
+        public byte[] TreasuryContractCode => Codes.Single(kv => kv.Key.Contains("Treasury")).Value;
+        protected Address TreasuryContractAddress { get; set; }
+
+        internal TreasuryContractContainer.TreasuryContractStub TreasuryContractStub;
+        public byte[] ProfitContractCode => Codes.Single(kv => kv.Key.Contains("Profit")).Value;
+        protected Address ProfitContractAddress { get; set; }
+
+        internal ProfitContractContainer.ProfitContractStub ProfitContractStub;
+        public byte[] TokenConverterContractCode => Codes.Single(kv => kv.Key.Contains("TokenConverter")).Value;
+        protected Address TokenConverterContractAddress { get; set; }
+
+        internal TokenConverterContractContainer.TokenConverterContractStub TokenConverterContractStub;
     }
 }
