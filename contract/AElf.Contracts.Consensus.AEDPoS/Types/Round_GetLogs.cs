095280d82a93d297e7baa3ede47bceb09fd3dde5
EanCuznaivy <dansuirenqu@126.com>
2019-07-03 11:30:06 +0800

    [Tune] Round.

--- contract/AElf.Contracts.Consensus.AEDPoS/Types/Round_GetLogs.cs
+++ contract/AElf.Contracts.Consensus.AEDPoS/Types/Round_GetLogs.cs
@@ -1,11 +1,13 @@
+using System;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
 namespace AElf.Contracts.Consensus.AEDPoS
 {
-    public partial class Round
+    public partial class Round : IFormattable
     {
-        public string GetLogs(string publicKey, AElfConsensusBehaviour behaviour)
+        private string GetLogs(string publicKey)
         {
             var logs = new StringBuilder($"\n[Round {RoundNumber}](Round Id: {RoundId})[Term {TermNumber}]");
             foreach (var minerInRound in RealTimeMinersInformation.Values.OrderBy(m => m.Order))
@@ -46,9 +48,27 @@ namespace AElf.Contracts.Consensus.AEDPoS
                 logs.Append(minerInformation);
             }
 
-            logs.AppendLine($"Recent behaviour: {behaviour.ToString()}");
-
             return logs.ToString();
+        }
+
+        public string ToString(string publicKey)
+        {
+            return ToString(publicKey, CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format)) format = "G";
+
+            switch (format)
+            {
+                case "G": return ToString();
+                case "M": 
+                    // Return formatted miner list.
+                    return RealTimeMinersInformation.Keys.Aggregate("\n", (key1, key2) => key1 + "\n" + key2);
+            }
+
+            return GetLogs(format);
         }
     }
 }
