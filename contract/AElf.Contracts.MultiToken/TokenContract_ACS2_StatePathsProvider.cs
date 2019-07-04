14ff23430a5775bb7a90fe7c6ba799ec69f6d2e2
EanCuznaivy <dansuirenqu@126.com>
2019-07-04 21:09:00 +0800

    [Feature] Add more resource infos to GetResourceInfo.

--- contract/AElf.Contracts.MultiToken/TokenContract_ACS2_StatePathsProvider.cs
+++ contract/AElf.Contracts.MultiToken/TokenContract_ACS2_StatePathsProvider.cs
@@ -31,7 +31,8 @@ namespace AElf.Contracts.MultiToken
                     {
                         Reources =
                         {
-                            GetPathHashCode(nameof(TokenContractState.Allowances), args.From.ToString(), txn.From.ToString(),
+                            GetPathHashCode(nameof(TokenContractState.Allowances), args.From.ToString(),
+                                txn.From.ToString(),
                                 args.Symbol),
                             GetPathHashCode(nameof(TokenContractState.Balances), args.From.ToString(), args.Symbol),
                             GetPathHashCode(nameof(TokenContractState.Balances), args.To.ToString(), args.Symbol)
@@ -39,10 +40,50 @@ namespace AElf.Contracts.MultiToken
                     };
                 }
 
+                case nameof(DonateResourceToken):
+                {
+                    return new ResourceInfo
+                    {
+                        Reources =
+                        {
+                            GetPathHashCode(nameof(TokenContractState.ChargedResources), "CPU"),
+                            GetPathHashCode(nameof(TokenContractState.ChargedResources), "STO"),
+                            GetPathHashCode(nameof(TokenContractState.ChargedResources), "NET"),
+                            GetPathHashCode(nameof(TokenContractState.TreasuryContract))
+                        }
+                    };
+                }
+
+                case nameof(ClaimTransactionFees):
+                {
+                    return GetClaimTransactionFessResourceInfo();
+                }
+
                 // TODO: Support more methods
                 default:
                     throw new AssertionException($"invalid method: {txn.MethodName}");
             }
+        }
+
+        private ResourceInfo GetClaimTransactionFessResourceInfo()
+        {
+            var resourceInfo = new ResourceInfo
+            {
+                Reources =
+                {
+                    GetPathHashCode(nameof(TokenContractState.PreviousBlockTransactionFeeTokenSymbolList)),
+                    GetPathHashCode(nameof(TokenContractState.TreasuryContract))
+                }
+            };
+            if (State.PreviousBlockTransactionFeeTokenSymbolList.Value != null)
+            {
+                foreach (var symbol in State.PreviousBlockTransactionFeeTokenSymbolList.Value.SymbolList)
+                {
+                    resourceInfo.Reources.Add(GetPathHashCode(nameof(TokenContractState.ChargedFees), symbol));
+                }
+            }
+
+            return resourceInfo;
         }
 
         private int GetPathHashCode(params string[] parts)
