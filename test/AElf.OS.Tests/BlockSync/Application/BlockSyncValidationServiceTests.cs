89fe19bd9fc78bdefbe8806f1cf00b4671a7b754
zhxymh <13552710076@126.com>
2019-07-09 12:31:02 +0800

    add tests

--- test/AElf.OS.Tests/BlockSync/Application/BlockSyncValidationServiceTests.cs
+++ test/AElf.OS.Tests/BlockSync/Application/BlockSyncValidationServiceTests.cs
@@ -15,6 +15,7 @@ namespace AElf.OS.BlockSync.Application
         private readonly IBlockchainService _blockchainService;
         private readonly IBlockSyncStateProvider _blockSyncStateProvider;
         private readonly IAnnouncementCacheProvider _announcementCacheProvider;
+        private readonly OSTestHelper _osTestHelper;
 
         public BlockSyncValidationServiceTests()
         {
@@ -22,6 +23,7 @@ namespace AElf.OS.BlockSync.Application
             _blockchainService = GetRequiredService<IBlockchainService>();
             _blockSyncStateProvider = GetRequiredService<IBlockSyncStateProvider>();
             _announcementCacheProvider = GetRequiredService<IAnnouncementCacheProvider>();
+            _osTestHelper = GetRequiredService<OSTestHelper>();
         }
 
         [Fact]
@@ -103,6 +105,63 @@ namespace AElf.OS.BlockSync.Application
             var validateResult =
                 await _blockSyncValidationService.ValidateBeforeHandleAnnounceAsync(chain, syncBlockHash,
                     syncBlockHeight);
+
+            validateResult.ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task ValidateBeforeHandleBlock_Success()
+        {
+            var chain = await _blockchainService.GetChainAsync();
+
+            var block = _osTestHelper.GenerateBlockWithTransactions(chain.LastIrreversibleBlockHash,
+                chain.LastIrreversibleBlockHeight);
+
+            var validateResult = await _blockSyncValidationService.ValidateBeforeHandleBlockAsync(chain, block);
+
+            validateResult.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task ValidateBeforeHandleBlock_FetchQueueIsBusy()
+        {
+            var chain = await _blockchainService.GetChainAsync();
+
+            var block = _osTestHelper.GenerateBlockWithTransactions(chain.LastIrreversibleBlockHash,
+                chain.LastIrreversibleBlockHeight);;
+
+            _blockSyncStateProvider.BlockSyncFetchBlockEnqueueTime = TimestampHelper.GetUtcNow()
+                .AddMilliseconds(-(BlockSyncConstants.BlockSyncFetchBlockAgeLimit + 100));
+
+            var validateResult = await _blockSyncValidationService.ValidateBeforeHandleBlockAsync(chain, block);
+
+            validateResult.ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task ValidateBeforeHandleBlock_AlreadySynchronized()
+        {
+            var chain = await _blockchainService.GetChainAsync();
+
+            var block = _osTestHelper.GenerateBlockWithTransactions(chain.LastIrreversibleBlockHash,
+                chain.LastIrreversibleBlockHeight);
+
+            _announcementCacheProvider.TryAddAnnouncementCache(block.GetHash(), block.Height);
+
+            var validateResult = await _blockSyncValidationService.ValidateBeforeHandleBlockAsync(chain, block);
+
+            validateResult.ShouldBeFalse();
+        }
+        
+        [Fact]
+        public async Task ValidateBeforeHandleBlock_LessThenLIBHeight()
+        {
+            var chain = await _blockchainService.GetChainAsync();
+
+            var block = _osTestHelper.GenerateBlockWithTransactions(Hash.FromString("SyncBlockHash"),
+                chain.LastIrreversibleBlockHeight - 1);
+
+            var validateResult = await _blockSyncValidationService.ValidateBeforeHandleBlockAsync(chain, block);
 
             validateResult.ShouldBeFalse();
         }
