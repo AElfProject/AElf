260ce6ce6eee3e32176b543b1fad1664b6251919
zhxymh <13552710076@126.com>
2019-07-10 21:17:14 +0800

    Adjust age limit

--- src/AElf.OS/BlockSync/BlockSyncConstants.cs
+++ src/AElf.OS/BlockSync/BlockSyncConstants.cs
@@ -5,7 +5,7 @@ namespace AElf.OS.BlockSync
     {
         public const long BlockSyncFetchBlockAgeLimit = 1000;
         public const long BlockSyncDownloadBlockAgeLimit = 4000;
-        public const long BlockSyncAttachBlockAgeLimit = 500;
+        public const long BlockSyncAttachBlockAgeLimit = 2000;
         public const long BlockSyncAttachAndExecuteBlockAgeLimit = 500;
 
         public const int BlockSyncModeHeightOffset = 12;
