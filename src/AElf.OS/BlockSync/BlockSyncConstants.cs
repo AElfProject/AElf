6f6e72d405787e8e830b2991e44c4c234e89170a
zhxymh <13552710076@126.com>
2019-07-17 15:34:08 +0800

    change age limit

--- src/AElf.OS/BlockSync/BlockSyncConstants.cs
+++ src/AElf.OS/BlockSync/BlockSyncConstants.cs
@@ -1,13 +1,11 @@
 
-using System.Collections.Concurrent;
-
 namespace AElf.OS.BlockSync
 {
     public class BlockSyncConstants
     {
         public const long BlockSyncFetchBlockAgeLimit = 1000;
         public const long BlockSyncAttachBlockAgeLimit = 2000;
-        public const long BlockSyncAttachAndExecuteBlockAgeLimit = 500;
+        public const long BlockSyncAttachAndExecuteBlockAgeLimit = 4000;
 
         public const int BlockSyncModeHeightOffset = 12;
         public const int FetchBlockRetryTimes = 3;
