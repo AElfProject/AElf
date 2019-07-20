14e7c96a3f6c5abdf0c0da9fbbc389d080fbc1a9
EricShu <blackeye286@126.com>
2019-07-20 15:15:09 +0800

    update kernel consensus aedpos test cases

---
+++ test/AElf.Kernel.Consensus.AEDPoS.Tests/Application/RandomHashCacheServiceTests.cs
@@ -0,0 +1,61 @@
+using AElf.Kernel.Consensus.AEDPoS.Application;
+using AElf.Types;
+using Shouldly;
+using Xunit;
+
+namespace AElf.Kernel.Consensus.DPoS.Tests.Application
+{
+    public class RandomHashCacheServiceTests : AEDPoSTestBase
+    {
+        private readonly IRandomHashCacheService _randomHashCacheService;
+
+        public RandomHashCacheServiceTests()
+        {
+            _randomHashCacheService = GetRequiredService<IRandomHashCacheService>();
+        }
+
+        [Fact]
+        public void RandomHash_Test()
+        {
+            var blockHash = Hash.Generate();
+            var result = _randomHashCacheService.GetRandomHash(blockHash);
+            result.ShouldBe(Hash.Empty);
+
+            var randomHash = Hash.Generate();
+            _randomHashCacheService.SetRandomHash(blockHash, randomHash);
+            
+            var queryResult = _randomHashCacheService.GetRandomHash(blockHash);
+            queryResult.ShouldBe(randomHash);
+        }
+
+        [Fact]
+        public void GetLatestGeneratedBlockRandomHash_Test()
+        {
+            var blockHash = Hash.Generate(); 
+            const long blockHeight = 5L;
+            _randomHashCacheService.SetGeneratedBlockPreviousBlockInformation(blockHash, blockHeight);
+            var queryResult = _randomHashCacheService.GetLatestGeneratedBlockRandomHash();
+            queryResult.ShouldBe(Hash.Empty);
+        }
+
+        [Fact]
+        public void SetGeneratedBlockPreviousBlockInformation()
+        {
+            const long blockHeight = 5L;
+            var randomHash1 = Hash.Generate();
+            var previousHash = Hash.Generate();
+            
+            var blockHash = Hash.Generate();
+            var randomHash2 = Hash.Generate();
+            
+            _randomHashCacheService.SetRandomHash(previousHash, randomHash1);
+            _randomHashCacheService.SetGeneratedBlockPreviousBlockInformation(previousHash, blockHeight);
+            
+            _randomHashCacheService.SetRandomHash(blockHash, randomHash2);
+            _randomHashCacheService.SetGeneratedBlockPreviousBlockInformation(blockHash, blockHeight + 1);
+            
+            var queryResult = _randomHashCacheService.GetLatestGeneratedBlockRandomHash();
+            queryResult.ShouldBe(randomHash1);
+        }
+    }
+}
