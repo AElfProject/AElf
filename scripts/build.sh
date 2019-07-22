7f073035aa9597d9c65b251f7c9df5b3725fbe0a
EricShu <blackeye286@126.com>
2019-07-22 19:49:31 +0800

    clear local nuget package cache

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+dotnet nuget locals all --clear
 dotnet restore -s "https://nuget.cdn.azure.cn/v3/index.json" -s "https://api.nuget.org/v3/index.json" "AElf.sln"
 dotnet build /p:GeneratePackageOnBuild=false "AElf.sln"
 
