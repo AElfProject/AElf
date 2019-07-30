17adae4294f8921a8d02e5da1368c2080cc9ea50
zhxymh <13552710076@126.com>
2019-07-30 15:36:23 +0800

    move handler to network

---
+++ src/AElf.OS/Handlers/NewIrreversibleBlockFoundEventHandler.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using AElf.Kernel.Blockchain.Events;
+using AElf.OS.Network.Application;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus;
+
+namespace AElf.OS.Handlers
+{
+    public class NewIrreversibleBlockFoundEventHandler : ILocalEventHandler<NewIrreversibleBlockFoundEvent>,
+        ITransientDependency
+    {
+        private readonly INetworkService _NetworkService;
+
+        public NewIrreversibleBlockFoundEventHandler(INetworkService networkService)
+        {
+            _NetworkService = networkService;
+        }
+
+        public async Task HandleEventAsync(NewIrreversibleBlockFoundEvent eventData)
+        {
+            var _ = _NetworkService.BroadcastLibAnnounceAsync(eventData.BlockHash, eventData.BlockHeight);
+        }
+    }
+}
