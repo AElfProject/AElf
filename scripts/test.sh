9e1b2810345a655190ff022cdbbbd65e3f90fe76
peng <peng.rong@aelf.io>
2019-07-31 18:34:43 +0800

    Add time measure

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,24 +3,21 @@
 cd test/
 
 for i in *Tests ; do
-  limit=$((${#i}+30))
-
   echo ""
-  echo "Executing Tests for $i"
-  printf '=%.0s' $(seq 1 $limit)
-  echo ""
+  echo "### Executing Tests for $i:"
 
-  dotnet test "$i" --no-build -v quiet /p:CollectCoverage=true /p:CoverletOutputFormat='json%2copencover' \
+  time dotnet test "$i" --no-build -v quiet /p:CollectCoverage=true /p:CoverletOutputFormat='json%2copencover' \
   /p:CoverletOutput="../results/coverage" /p:MergeWith="../results/coverage.json" \
   /p:Exclude="[coverlet.*.tests?]*%2c[xunit.*]*%2c[AElf.Kernel.Consensus.Scheduler.*]*%2c[AElf.Database]AElf.Database.RedisProtocol.*%2c[AElf.Contracts.Authorization]*%2c[AElf.Test.Helpers]*%2c[AElf.CLI]AElf.CLI.Commands.*%2c[*]*Exception%2c[*.Tests]*%2c[AElf.Contracts.TestContract.BasicUpdate]*" \
   /p:ExcludeByFile="../../src/AElf.Runtime.CSharp.Core/Metadata/*.cs%2c../../src/AElf.Kernel.SmartContract/Metadata/*.cs%2c../../src/AElf.Database/RedisDatabase.cs%2c../../test/*.TestBase/*.cs" \
   > /tmp/test.log
-  
+
   if [[ $? -ne 0 ]] ; then
+    echo "Test Run Failed."
     cat /tmp/test.log
     exit 1
   fi
 
-  echo 'Test Run Successful.'
+  echo "Test Run Successful."
 
 done
