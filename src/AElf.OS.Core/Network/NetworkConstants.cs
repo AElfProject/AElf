83a9355292ee4ebb7c0aef45596c1f15a413a4ec
samfrb <samos2x@outlook.com>
2019-08-14 19:20:03 +0800

    Up the transaction queue size.

--- src/AElf.OS.Core/Network/NetworkConstants.cs
+++ src/AElf.OS.Core/Network/NetworkConstants.cs
@@ -22,7 +22,7 @@ namespace AElf.OS.Network
         public const string TransactionBroadcastQueueName = "TransactionBroadcastQueue";
         public const string BlockBroadcastQueueName = "BlockBroadcastQueue";
         
-        public const int DefaultMaxBufferedTransactionCount = 100;
+        public const int DefaultMaxBufferedTransactionCount = 500;
         public const int DefaultMaxBufferedBlockCount = 20;
         public const int DefaultMaxBufferedAnnouncementCount = 100;
     }
