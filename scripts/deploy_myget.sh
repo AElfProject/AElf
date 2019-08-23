b461665d7be1cebe9c7e6edc61f567262638edff
huihui <469854435@qq.com>
2019-08-23 04:19:11 +0000

    20190823

--- scripts/deploy_myget.sh
+++ scripts/deploy_myget.sh
@@ -4,19 +4,29 @@ set -ev
 TAG=$1
 MYGET_API_KEY=$2
 VERSION=`echo ${TAG} | cut -b 2-`
-
-cd src/
-
-for name in `ls -lh | grep ^d | grep AElf | grep -v Tests | awk '{print $NF}'`;
+path1=src/
+path2=contract/
+for path in $path1 $path2 ;
 do
-    if [[ -f ${name}/${name}.csproj ]] && [[ 1 -eq $(grep -c "GeneratePackageOnBuild"  ${name}/${name}.csproj) ]];then
-        dotnet build /clp:ErrorsOnly ${name}/${name}.csproj --configuration Release -P:Version=${VERSION} -P:Authors=AElf -o ../
-    fi
+    cd $path
+    for name in `ls -lh | grep ^d | grep AElf | grep -v Tests | awk '{print $NF}'`;
+    do
+        if [[ -f ${name}/${name}.csproj ]] && [[ 1 -eq $(grep -c "GeneratePackageOnBuild"  ${name}/${name}.csproj) ]];then
+            dotnet build /clp:ErrorsOnly ${name}/${name}.csproj --configuration Release -P:Version=${VERSION} -P:Authors=AElf -o ../
+        fi
+    done
+    sleep 10
+    cd ../
 done
-
 # push
 for name in `ls *.nupkg`;
 do
     echo ${name}
     dotnet nuget push ${name} -k ${MYGET_API_KEY} -s https://www.myget.org/F/aelf-project/api/v3/index.json
+    if [ "$?" != 0 ] ; then
+        echo "重新push"
+        dotnet nuget push ${name} -k ${MYGET_API_KEY} -s https://www.myget.org/F/aelf-project/api/v3/index.json
+    else
+        echo "成功push"
+    fi
 done
--- scripts/deploy_myget_daily.sh
+++ scripts/deploy_myget_daily.sh
@@ -6,19 +6,29 @@ MYGET_API_KEY=$2
 # days since 1970-1-1 as build version
 BUILD_VERSION=`expr $(date +%s) / 86400`
 VERSION=${VERSION_PREFIX}-${BUILD_VERSION}
-
-cd src/
-
-for name in `ls -lh | grep ^d | grep AElf | grep -v Tests| awk '{print $NF}'`;
+path1=src/
+path2=contract/
+for path in $path1 $path2 ;
 do
-    if [[ -f ${name}/${name}.csproj ]] && [[ 1 -eq $(grep -c "GeneratePackageOnBuild" ${name}/${name}.csproj) ]];then
-        dotnet build /clp:ErrorsOnly ${name}/${name}.csproj --configuration Release -P:Version=${VERSION} -P:Authors=AElf -o ../
-    fi
+    cd $path
+    for name in `ls -lh | grep ^d | grep AElf | grep -v Tests| awk '{print $NF}'`;
+    do
+        if [[ -f ${name}/${name}.csproj ]] && [[ 1 -eq $(grep -c "GeneratePackageOnBuild" ${name}/${name}.csproj) ]];then
+            dotnet build /clp:ErrorsOnly ${name}/${name}.csproj --configuration Release -P:Version=${VERSION} -P:Authors=AElf -o ../
+        fi
+    done
+    sleep 10
+    cd ../
 done
-
 # push
 for name in `ls *.nupkg`;
 do
     echo ${name}
     dotnet nuget push ${name} -k ${MYGET_API_KEY} -s https://www.myget.org/F/aelf-project-dev/api/v3/index.json
+    if [ "$?" != 0 ] ; then
+        echo "push失败,重新push"
+        dotnet nuget push ${name} -k ${MYGET_API_KEY} -s https://www.myget.org/F/aelf-project-dev/api/v3/index.json
+    else
+        echo "成功push"
+    fi
 done
