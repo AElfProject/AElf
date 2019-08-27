47744ae7e9333f1c31e74c450ecba6bf92fd89ab
swk <wilkey.shi@aelf.io>
2019-08-27 21:58:18 +0800

    Enable transaction broadcast.

--- src/AElf.OS.Core/Network/Application/NetworkService.cs
+++ src/AElf.OS.Core/Network/Application/NetworkService.cs
@@ -171,7 +171,6 @@ namespace AElf.OS.Network.Application
         
         public Task BroadcastTransactionAsync(Transaction transaction)
         {
-            return Task.CompletedTask;
             var beforeEnqueue = TimestampHelper.GetUtcNow();
             _taskQueueManager.Enqueue(async () =>
             {
