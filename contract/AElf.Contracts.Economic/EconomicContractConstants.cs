f9c10e976890762df8bf7e636ff948c54daa7233
weili <weili.li@aelf.io>
2019-08-30 10:48:29 +0800

    add token parameters for bancor model

--- contract/AElf.Contracts.Economic/EconomicContractConstants.cs
+++ contract/AElf.Contracts.Economic/EconomicContractConstants.cs
@@ -19,11 +19,17 @@ namespace AElf.Contracts.Economic
         public static readonly List<string> ResourceTokenSymbols = new List<string> {"RAM", "CPU", "NET", "STO"};
         public const long ResourceTokenTotalSupply = 1_000_000_000_00000000;
         public const int ResourceTokenDecimals = 8;
-        public const string ResourceTokenConnectorWeight = "0.2";
+        public const string ResourceTokenConnectorWeight = "0.0005";
         public const long ResourceTokenConnectorInitialVirtualBalance = 100_000;
         public const long CpuUnitPrice = 100;
         public const long StoUnitPrice = 100;
         public const long NetUnitPrice = 100;
+        public const long CpuAmount = 100_000_000;
+        public const long StoAmount = 100_000_000;
+        public const long NetAmount = 100_000_000;
+        public const long CpuRelayConnectorInitialVirtualBalance = 1_000_000;
+        public const long StoRelayConnectorInitialVirtualBalance = 1_000_000;
+        public const long NetRelayConnectorInitialVirtualBalance = 1_000_000;
 
         // Election related.
         public const string ElectionTokenSymbol = "VOTE";
