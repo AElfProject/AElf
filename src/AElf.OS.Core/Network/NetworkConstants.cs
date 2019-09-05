63b669f21c2d28d14f6490351484c71e204df06d
samfrb <sam.bassett@aelf.io>
2019-09-05 18:39:52 +0800

    Naming.

--- src/AElf.OS.Core/Network/NetworkConstants.cs
+++ src/AElf.OS.Core/Network/NetworkConstants.cs
@@ -8,7 +8,7 @@ namespace AElf.OS.Network
         public const int DefaultMaxRequestRetryCount = 1;
         public const int DefaultMaxRandomPeersPerRequest = 2;
         public const int DefaultMaxPeers = 25;
-        public const int DefaultMaxPeersPerHost = 1;
+        public const int DefaultMaxPeersPerIpAddress = 1;
 
         public const int DefaultMaxBlockAgeToBroadcastInMinutes = 10;
 
