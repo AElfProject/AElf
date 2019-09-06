eefeb34d619c53de6f992110c8adce1104f7756c
Yiqi Zhao <yiqi.zhao@aelf.io>
2019-09-07 03:09:13 +0800

    [Tune] Dividend pool ratio 0.2 -> 0.12

--- src/AElf.OS.Core/EconomicOptions.cs
+++ src/AElf.OS.Core/EconomicOptions.cs
@@ -7,7 +7,7 @@ namespace AElf.OS
         public long TotalSupply { get; set; } = 1_000_000_000_00000000;
         public int Decimals { get; set; } = 8;
         public bool IsBurnable { get; set; } = true;
-        public double DividendPoolRatio { get; set; } = 0.2;
+        public double DividendPoolRatio { get; set; } = 0.12;
         public long MaximumLockTime { get; set; } = 1080 * 86400;
         public long MinimumLockTime { get; set; } = 90 * 86400;
     }
