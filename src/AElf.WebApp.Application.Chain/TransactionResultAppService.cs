cc9d80c5c6bb65e27beb03407c1f3712819e1e7d
Eric Shu <eric.shu@aelf.io>
2019-09-12 12:11:44 +0800

    remove unused namespace

--- src/AElf.WebApp.Application.Chain/TransactionResultAppService.cs
+++ src/AElf.WebApp.Application.Chain/TransactionResultAppService.cs
@@ -11,7 +11,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AElf.Kernel;
 using Volo.Abp;
 using Volo.Abp.Application.Services;
 
