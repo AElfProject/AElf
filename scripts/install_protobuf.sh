b7fe96e848d8aca6bd0810b7c0e4d2230b661d96
Eric Shu <eric.shu@aelf.io>
2019-09-20 10:58:34 +0800

    update protobuf version

--- scripts/install_protobuf.sh
+++ scripts/install_protobuf.sh
@@ -16,6 +16,8 @@ echo "Install protobuf on ${osn}"
 if [[ ${osn} == "macosx" ]]; then
     HOMEBREW_NO_AUTO_UPDATE=1 brew search protobuf
     HOMEBREW_NO_AUTO_UPDATE=1 brew install protobuf@3.7
+    HOMEBREW_NO_AUTO_UPDATE=1 brew link --force --overwrite protobuf@3.7
+    # echo 'export PATH="/usr/local/opt/protobuf@3.7/bin:$PATH"' >> ~/.bash_profile
 elif [[ ${osn} == "linux" ]]; then
     # Make sure you grab the latest version
     curl -OL https://github.com/google/protobuf/releases/download/v3.7.0/protoc-3.7.0-linux-x86_64.zip
