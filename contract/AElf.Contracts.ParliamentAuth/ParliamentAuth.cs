db8cc751d4a1316a0f6fbceb92f540282ef118b9
swk <wilkey.shi@aelf.io>
2019-10-12 18:27:30 +0800

    Fix test case.

--- contract/AElf.Contracts.ParliamentAuth/ParliamentAuth.cs
+++ contract/AElf.Contracts.ParliamentAuth/ParliamentAuth.cs
@@ -125,7 +125,6 @@ namespace AElf.Contracts.ParliamentAuth
         public override Empty Release(Hash proposalId)
         {
             var proposalInfo = GetValidProposal(proposalId);
-            Assert(proposalInfo != null, "Proposal not found.");
             Assert(Context.Sender.Equals(proposalInfo.Proposer), "Unable to release this proposal.");
             var organization = State.Organisations[proposalInfo.OrganizationAddress];
             var currentParliament = GetCurrentMinerList();
