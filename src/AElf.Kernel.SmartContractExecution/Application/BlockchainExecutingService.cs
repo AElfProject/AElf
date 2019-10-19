1da35f6d2dbe04d4693c43bc32590fac9a03a33f
Jason Wang <jason.wang@aelf.io>
2019-10-19 11:31:21 +0800

    Add log for block execution failed

--- src/AElf.Kernel.SmartContractExecution/Application/BlockchainExecutingService.cs
+++ src/AElf.Kernel.SmartContractExecution/Application/BlockchainExecutingService.cs
@@ -47,7 +47,14 @@ namespace AElf.Kernel.SmartContractExecution.Application
             var transactions = await _blockchainService.GetTransactionsAsync(block.TransactionIds);
             var executedBlock = await _blockExecutingService.ExecuteBlockAsync(block.Header, transactions);
 
-            return executedBlock.GetHashWithoutCache().Equals(blockHash);
+            var blockHashWithoutCache = executedBlock.GetHashWithoutCache();
+            if (blockHashWithoutCache != blockHash)
+            {
+                blockState = await _blockchainStateManager.GetBlockStateSetAsync(blockHashWithoutCache);
+                Logger.LogWarning($"Block execution failed. BlockStateSet: {blockState}");
+                Logger.LogWarning($"Block execution failed. Block: {executedBlock}");
+            }
+            return blockHashWithoutCache.Equals(blockHash);
         }
         
         /// <summary>
