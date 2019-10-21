38a36c0b084b866e6d554e152fb397c678b215a8
samfrb <sam.bassett@aelf.io>
2019-10-21 18:50:10 +0800

    Increase get block timeout.

--- src/AElf.OS.Network.Grpc/GrpcPeer.cs
+++ src/AElf.OS.Network.Grpc/GrpcPeer.cs
@@ -26,7 +26,7 @@ namespace AElf.OS.Network.Grpc
     public class GrpcPeer : IPeer
     {
         private const int MaxMetricsPerMethod = 100;
-        private const int BlockRequestTimeout = 500;
+        private const int BlockRequestTimeout = 700;
         private const int HealthCheckTimeout = 3000;
         private const int BlocksRequestTimeout = 5000;
         private const int GetNodesTimeout = 500;
