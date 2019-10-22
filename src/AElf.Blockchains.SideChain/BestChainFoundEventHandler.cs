12bdf1a2fab33f07f2db5d2532153b776e589808
zhxymh <sean.zhang@aelf.io>
2019-10-22 19:29:10 +0800

    remove unused handler

--- src/AElf.Blockchains.SideChain/BestChainFoundEventHandler.cs
+++ src/AElf.Blockchains.SideChain/BestChainFoundEventHandler.cs
@@ -11,40 +11,40 @@ using Volo.Abp.EventBus;
 
 namespace AElf.Blockchains.SideChain
 {
-    internal class BestChainFoundEventHandler : ILocalEventHandler<BestChainFoundEventData>, ITransientDependency
-    {
-        private readonly ContractEventDiscoveryService<ChainPrimaryTokenSymbolSet> _primaryTokenSymbolDiscoveryService;
-        private readonly IPrimaryTokenSymbolProvider _primaryTokenSymbolProvider;
-        private readonly ISmartContractAddressService _smartContractAddressService;
-
-        public ILogger<BestChainFoundEventHandler> Logger { get; set; }
-
-        public BestChainFoundEventHandler(
-            ContractEventDiscoveryService<ChainPrimaryTokenSymbolSet> primaryTokenSymbolDiscoveryService,
-            IPrimaryTokenSymbolProvider primaryTokenSymbolProvider,
-            ISmartContractAddressService smartContractAddressService)
-        {
-            _primaryTokenSymbolDiscoveryService = primaryTokenSymbolDiscoveryService;
-            _primaryTokenSymbolProvider = primaryTokenSymbolProvider;
-            _smartContractAddressService = smartContractAddressService;
-
-            Logger = NullLogger<BestChainFoundEventHandler>.Instance;
-        }
-
-        public async Task HandleEventAsync(BestChainFoundEventData eventData)
-        {
-            var tokenContractAddress =
-                _smartContractAddressService.GetAddressByContractName(TokenSmartContractAddressNameProvider.Name);
-            if (eventData.BlockHeight == Constants.GenesisBlockHeight)
-            {
-                var symbol =
-                    (await _primaryTokenSymbolDiscoveryService.GetEventMessagesAsync(eventData.BlockHash,
-                        tokenContractAddress))
-                    .FirstOrDefault();
-                _primaryTokenSymbolProvider.SetPrimaryTokenSymbol(symbol?.TokenSymbol);
-
-                Logger.LogInformation($"Primary token symbol for current chain: {symbol}");
-            }
-        }
-    }
+//    internal class BestChainFoundEventHandler : ILocalEventHandler<BestChainFoundEventData>, ITransientDependency
+//    {
+//        private readonly ContractEventDiscoveryService<ChainPrimaryTokenSymbolSet> _primaryTokenSymbolDiscoveryService;
+//        private readonly IPrimaryTokenSymbolProvider _primaryTokenSymbolProvider;
+//        private readonly ISmartContractAddressService _smartContractAddressService;
+//
+//        public ILogger<BestChainFoundEventHandler> Logger { get; set; }
+//
+//        public BestChainFoundEventHandler(
+//            ContractEventDiscoveryService<ChainPrimaryTokenSymbolSet> primaryTokenSymbolDiscoveryService,
+//            IPrimaryTokenSymbolProvider primaryTokenSymbolProvider,
+//            ISmartContractAddressService smartContractAddressService)
+//        {
+//            _primaryTokenSymbolDiscoveryService = primaryTokenSymbolDiscoveryService;
+//            _primaryTokenSymbolProvider = primaryTokenSymbolProvider;
+//            _smartContractAddressService = smartContractAddressService;
+//
+//            Logger = NullLogger<BestChainFoundEventHandler>.Instance;
+//        }
+//
+//        public async Task HandleEventAsync(BestChainFoundEventData eventData)
+//        {
+//            var tokenContractAddress =
+//                _smartContractAddressService.GetAddressByContractName(TokenSmartContractAddressNameProvider.Name);
+//            if (eventData.BlockHeight == Constants.GenesisBlockHeight)
+//            {
+//                var symbol =
+//                    (await _primaryTokenSymbolDiscoveryService.GetEventMessagesAsync(eventData.BlockHash,
+//                        tokenContractAddress))
+//                    .FirstOrDefault();
+//                _primaryTokenSymbolProvider.SetPrimaryTokenSymbol(symbol?.TokenSymbol);
+//
+//                Logger.LogInformation($"Primary token symbol for current chain: {symbol}");
+//            }
+//        }
+//    }
 }
