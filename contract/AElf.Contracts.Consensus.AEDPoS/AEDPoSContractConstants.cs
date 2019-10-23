42e8178680a740eb4007aa2ffbf0e27a08216aa8
Eric Shu <eric.shu@aelf.io>
2019-10-23 18:47:09 +0800

    change initial miner count

--- contract/AElf.Contracts.Consensus.AEDPoS/AEDPoSContractConstants.cs
+++ contract/AElf.Contracts.Consensus.AEDPoS/AEDPoSContractConstants.cs
@@ -7,7 +7,7 @@ namespace AElf.Contracts.Consensus.AEDPoS
         public const int RandomNumberDueRoundCount = 1024;
         public const long InitialMiningRewardPerBlock = 12500000;
         public const long TimeToReduceMiningRewardByHalf = 126144000;// (60 * 60 * 24 * 365 * 4) s
-        public const int InitialMinersCount = 9;
+        public const int InitialMinersCount = 4;
         public const int KeepRounds = 40960;
         public const long TolerableMissedTimeSlotsCount = 10;// update for miss slots test
     }
