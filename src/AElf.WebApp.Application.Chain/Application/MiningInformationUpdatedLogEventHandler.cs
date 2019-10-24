88c6efa6ba2b5cf0f1e973e1dd90ac222ecb2af5
zhxymh <sean.zhang@aelf.io>
2019-10-24 15:14:56 +0800

    use log event listening to process mining information

---
+++ src/AElf.WebApp.Application.Chain/Application/MiningInformationUpdatedLogEventHandler.cs
@@ -0,0 +1,62 @@
+using System.Threading.Tasks;
+using AElf.Contracts.Consensus.AEDPoS;
+using AElf.Kernel;
+using AElf.Kernel.Consensus;
+using AElf.Kernel.Miner.Application;
+using AElf.Kernel.SmartContract.Application;
+using AElf.Sdk.CSharp;
+using AElf.Types;
+using AElf.WebApp.Application.Chain.Dto;
+using Volo.Abp.DependencyInjection;
+
+namespace AElf.WebApp.Application.Chain.Application
+{
+    public class MiningInformationUpdatedLogEventHandler : ILogEventHandler, ISingletonDependency
+    {
+        private readonly ISmartContractAddressService _smartContractAddressService;
+        private readonly IMiningSequenceService _miningSequenceService;
+
+        private LogEvent _interestedEvent;
+
+        public LogEvent InterestedEvent
+        {
+            get
+            {
+                if (_interestedEvent != null) 
+                    return _interestedEvent;
+                
+                var address =
+                    _smartContractAddressService.GetAddressByContractName(
+                        ConsensusSmartContractAddressNameProvider.Name);
+
+                _interestedEvent = new MiningInformationUpdated().ToLogEvent(address);
+
+                return _interestedEvent;
+            }
+        }
+
+        public MiningInformationUpdatedLogEventHandler(ISmartContractAddressService smartContractAddressService,
+            IMiningSequenceService miningSequenceService)
+        {
+            _smartContractAddressService = smartContractAddressService;
+            _miningSequenceService = miningSequenceService;
+        }
+
+        public Task Handle(Block block, TransactionResult result, LogEvent log)
+        {
+            var eventData = new MiningInformationUpdated();
+            eventData.MergeFrom(log);
+
+            _miningSequenceService.AddMiningInformation(new MiningSequenceDto
+            {
+                Pubkey = eventData.Pubkey,
+                Behaviour = eventData.Behaviour,
+                MiningTime = eventData.MiningTime,
+                BlockHeight = eventData.BlockHeight,
+                PreviousBlockHash = eventData.PreviousBlockHash
+            });
+
+            return Task.CompletedTask;
+        }
+    }
+}
