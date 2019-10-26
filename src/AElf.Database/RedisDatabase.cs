104caabffd8e07338a42c5c44a916c65dbac35ef
samfrb <sam.bassett@aelf.io>
2019-10-26 17:26:24 +0800

    Use stackexchange redis.

--- src/AElf.Database/RedisDatabase.cs
+++ src/AElf.Database/RedisDatabase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AElf.Database.RedisProtocol;
+using StackExchange.Redis;
 using Volo.Abp;
 
 #pragma warning disable 1998
@@ -11,43 +13,54 @@ namespace AElf.Database
     public class RedisDatabase<TKeyValueDbContext> : IKeyValueDatabase<TKeyValueDbContext>
         where TKeyValueDbContext : KeyValueDbContext<TKeyValueDbContext>
     {
-        private readonly PooledRedisLite _pooledRedisLite;
+        private readonly ConnectionMultiplexer _connectionMultiplexer;
 
         public RedisDatabase(KeyValueDatabaseOptions<TKeyValueDbContext> options)
         {
             Check.NotNullOrWhiteSpace(options.ConnectionString, nameof(options.ConnectionString));
             var endpoint = options.ConnectionString.ToRedisEndpoint();
-            _pooledRedisLite = new PooledRedisLite(endpoint.Host, endpoint.Port, db: (int) endpoint.Db);
+            var config = new ConfigurationOptions
+            {
+                EndPoints = { { endpoint.Host, endpoint.Port } },
+                DefaultDatabase = (int)endpoint.Db
+            };
+
+            _connectionMultiplexer = ConnectionMultiplexer.Connect(config);
+
         }
 
         public bool IsConnected()
         {
-            return _pooledRedisLite.Ping();
+            return _connectionMultiplexer.IsConnected;
         }
 
         public async Task<byte[]> GetAsync(string key)
         {
             Check.NotNullOrWhiteSpace(key, nameof(key));
-            return await Task.Run(() => _pooledRedisLite.Get(key));
+            return await _connectionMultiplexer.GetDatabase().StringGetAsync(key);
         }
 
         public async Task SetAsync(string key, byte[] bytes)
         {
             Check.NotNullOrWhiteSpace(key, nameof(key));
-            await Task.Run(() => _pooledRedisLite.Set(key, bytes));
+            await _connectionMultiplexer.GetDatabase().StringSetAsync(key, bytes);
         }
 
         public async Task RemoveAsync(string key)
         {
             Check.NotNullOrWhiteSpace(key, nameof(key));
-            await Task.Run(() => _pooledRedisLite.Remove(key));
+            await _connectionMultiplexer.GetDatabase().KeyDeleteAsync(key);
         }
 
         public async Task SetAllAsync(Dictionary<string, byte[]> cache)
         {
             if (cache.Count == 0)
                 return;
-            await Task.Run(() => _pooledRedisLite.SetAll(cache));
+
+            var keyPairs = cache.Select(entry => new KeyValuePair<RedisKey, RedisValue>(entry.Key, entry.Value))
+                .ToArray();
+
+            await _connectionMultiplexer.GetDatabase().StringSetAsync(keyPairs);
         }
     }
 }
