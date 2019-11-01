dc330c9ec286d7e76bcd3f989d5fcba7dfc377a2
xiangxx <buaaxxx1995@gmail.com>
2019-11-01 11:34:25 +0800

    fix test errors

--- test/AElf.OS.TestBase/OSTestBaseAElfModule.cs
+++ test/AElf.OS.TestBase/OSTestBaseAElfModule.cs
@@ -19,6 +19,7 @@ namespace AElf.OS
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             context.Services.AddSingleton<OSTestHelper>();
+            context.Services.AddSingleton<ITxHub, MockTxHub>();
         }
     }
 }
