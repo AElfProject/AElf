82122a593707278068adf8e6c053379fd90297ca
xiangxx <buaaxxx1995@gmail.com>
2019-11-08 21:13:07 +0800

    add symbol length limit

--- contract/AElf.Contracts.TokenConverter/TokenConverterContractConstants.cs
+++ contract/AElf.Contracts.TokenConverter/TokenConverterContractConstants.cs
@@ -2,7 +2,6 @@ namespace AElf.Contracts.TokenConverter
 {
     public class TokenConverterContractConstants
     {
-        public const int SymbolMaxLength = 8;
         public const string TokenSymbol = "AETC";
         public const long TotalSupply = 1_000_000_000;
     }
