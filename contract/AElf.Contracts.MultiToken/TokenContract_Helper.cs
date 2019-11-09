3dee804f12a421b07ce9b53b33a6344d3b9a7667
xiangxx <buaaxxx1995@gmail.com>
2019-11-09 12:08:34 +0800

    add symbol length > 0

--- contract/AElf.Contracts.MultiToken/TokenContract_Helper.cs
+++ contract/AElf.Contracts.MultiToken/TokenContract_Helper.cs
@@ -129,6 +129,7 @@ namespace AElf.Contracts.MultiToken
         private void AssertValidCreateInput(CreateInput input)
         {
             var isValid = input.TokenName.Length <= TokenContractConstants.TokenNameLength
+                          && input.Symbol.Length > 0
                           && input.Symbol.Length <= TokenContractConstants.SymbolMaxLength
                           && input.Decimals >= 0
                           && input.Decimals <= TokenContractConstants.MaxDecimals;
