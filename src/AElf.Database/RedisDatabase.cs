fe98fa647ca857b903a80aeb744cf705d64390f6
Eric Shu <eric.shu@aelf.io>
2019-11-14 20:23:04 +0800

    add stopwatch to monitor performance

--- src/AElf.Database/RedisDatabase.cs
+++ src/AElf.Database/RedisDatabase.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using StackExchange.Redis;
 using Volo.Abp;
@@ -26,39 +28,80 @@ namespace AElf.Database
 
         public bool IsConnected()
         {
-            return _connectionMultiplexer.IsConnected;
+            Stopwatch stopwatch = null;
+            try
+            {
+                stopwatch = Stopwatch.StartNew();
+                return _connectionMultiplexer.IsConnected;
+            }
+            finally
+            {
+                stopwatch.Stop();
+                Console.WriteLine($"## IsConnected: {stopwatch.ElapsedMilliseconds}");
+            }
         }
         
         public async Task<bool> IsExists(string key)
         {
-            Check.NotNullOrWhiteSpace(key, nameof(key));
-            return await _connectionMultiplexer.GetDatabase().KeyExistsAsync(key);
+            Stopwatch stopwatch = null;
+            try
+            {
+                stopwatch = Stopwatch.StartNew();
+                Check.NotNullOrWhiteSpace(key, nameof(key));
+                return await _connectionMultiplexer.GetDatabase().KeyExistsAsync(key);
+            }
+            finally
+            {
+                stopwatch.Stop();
+                Console.WriteLine($"## IsExists: {stopwatch.ElapsedMilliseconds}");
+            }
+            
         }
 
         public async Task<byte[]> GetAsync(string key)
         {
-            Check.NotNullOrWhiteSpace(key, nameof(key));
-            return await _connectionMultiplexer.GetDatabase().StringGetAsync(key);
+            Stopwatch stopwatch = null;
+            try
+            {
+                stopwatch = Stopwatch.StartNew();
+                Check.NotNullOrWhiteSpace(key, nameof(key));
+                return await _connectionMultiplexer.GetDatabase().StringGetAsync(key);
+            }
+            finally
+            {
+                stopwatch.Stop();
+                Console.WriteLine($"## GetAsync: {stopwatch.ElapsedMilliseconds}");
+            }
+            
         }
 
         public async Task SetAsync(string key, byte[] bytes)
         {
+            var stopwatch = Stopwatch.StartNew();
             Check.NotNullOrWhiteSpace(key, nameof(key));
             await _connectionMultiplexer.GetDatabase().StringSetAsync(key, bytes);
+            stopwatch.Stop();
+            Console.WriteLine($"## SetAsync: {stopwatch.ElapsedMilliseconds}");
         }
 
         public async Task RemoveAsync(string key)
         {
+            var stopwatch = Stopwatch.StartNew();
             Check.NotNullOrWhiteSpace(key, nameof(key));
             await _connectionMultiplexer.GetDatabase().KeyDeleteAsync(key);
+            stopwatch.Stop();
+            Console.WriteLine($"## RemoveAsync: {stopwatch.ElapsedMilliseconds}");
         }
 
         public async Task SetAllAsync(Dictionary<string, byte[]> cache)
         {
+            var stopwatch = Stopwatch.StartNew();
             if (cache.Count == 0)
                 return;
             var keyPairs = cache.Select(entry => new KeyValuePair<RedisKey, RedisValue>(entry.Key, entry.Value));
             await _connectionMultiplexer.GetDatabase().StringSetAsync(keyPairs.ToArray());
+            stopwatch.Stop();
+            Console.WriteLine($"## SetAllAsync: {stopwatch.ElapsedMilliseconds}");
         }
     }
 }
