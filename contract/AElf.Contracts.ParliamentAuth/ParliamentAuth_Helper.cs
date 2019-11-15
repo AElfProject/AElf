2388f4f655e3f4fca9380478d5acba3c2b3f4155
swk <wilkey.shi@aelf.io>
2019-11-15 18:57:44 +0800

    Fix parliament proposer authority problem.

--- contract/AElf.Contracts.ParliamentAuth/ParliamentAuth_Helper.cs
+++ contract/AElf.Contracts.ParliamentAuth/ParliamentAuth_Helper.cs
@@ -21,17 +21,18 @@ namespace AElf.Contracts.ParliamentAuth
         {
             // It is a valid proposer if
             // authority check is disable,
-            // or sender is GenesisContract (for new contract deployment)
             // or sender is in proposer white list,
+            // or sender is GenesisContract && origin is in proposer white list (for new contract deployment)
             // or sender is one of miners.
             if (!organization.ProposerAuthorityRequired)
                 return;
-            if (Context.Sender == State.GenesisContract.Value)
-                return;
             if (organization.ProposerWhiteList.Any(p => p == Context.Sender))
                 return;
-            var minerList = GetCurrentMinerList();
-            Assert(minerList.Any(m => m == Context.Sender), "Not authorized to propose.");
+            if (Context.Sender == State.GenesisContract.Value 
+                && organization.ProposerWhiteList.Any(p => p == Context.Origin))
+                return;
+            var isMiner = GetCurrentMinerList().Any(m => m == Context.Sender);
+            Assert(isMiner, "Not authorized to propose.");
         }
 
         private bool IsReleaseThresholdReached(ProposalInfo proposal, Organization organization,
