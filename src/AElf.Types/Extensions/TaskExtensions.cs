ada9c1d2619fccb3632430f60b213dfd576f9b6b
swk <wilkey.shi@aelf.io>
2019-11-15 20:12:12 +0800

    Add test cases for task extension.

--- src/AElf.Types/Extensions/TaskExtensions.cs
+++ src/AElf.Types/Extensions/TaskExtensions.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace AElf.Kernel
+namespace AElf
 {
     public static class TaskExtensions
     {
--- test/AElf.Types.Tests/Extensions/ExtensionTests.cs
+++ test/AElf.Types.Tests/Extensions/ExtensionTests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
 using Google.Protobuf;
 using Shouldly;
 
@@ -59,6 +62,68 @@ namespace AElf.Types.Tests.Extensions
             var bytes = new byte[] {0, 0, 0};
             byteString = ByteString.CopyFrom(bytes);
             byteString.ToPlainBase58().ShouldBe("111");
+        }
+
+        [Fact]
+        public async Task Task_Extensions_Test_One_Cancellation()
+        {
+            var ct = new CancellationTokenSource(100);
+            int times = 2;
+            var task = CountAsync(times, 100);
+            await Assert.ThrowsAsync<OperationCanceledException>(() => task.WithCancellation(ct.Token));
+        }
+        
+        [Fact]
+        public async Task Task_Extensions_Test_One_Cancellation_NotCancelled()
+        {
+            var ct = new CancellationTokenSource(300);
+            int times = 2;
+            var task = CountAsync(times, 100);
+            int res = await task.WithCancellation(ct.Token);
+            Assert.Equal(times, res);
+        }
+
+        [Fact]
+        public async Task Task_Extensions_Test_Two_BothCanceled()
+        {
+            var ct1 = new CancellationTokenSource(100);
+            var ct2 = new CancellationTokenSource(100);
+            int times = 2;
+            var task = CountAsync(times, 100);
+            await Assert.ThrowsAsync<OperationCanceledException>(() => task.WithCancellation(ct1.Token, ct2.Token));
+        }
+        
+        [Fact]
+        public async Task Task_Extensions_Test_Two_Cancellations()
+        {
+            var ct1 = new CancellationTokenSource(300);
+            var ct2 = new CancellationTokenSource(100);
+            int times = 2;
+            var task = CountAsync(times, 100);
+            await Assert.ThrowsAsync<OperationCanceledException>(() => task.WithCancellation(ct1.Token, ct2.Token));
+        }
+        
+        [Fact]
+        public async Task Task_Extensions_Test_Two_Cancellations_NotCanceled()
+        {
+            var ct1 = new CancellationTokenSource(300);
+            var ct2 = new CancellationTokenSource(300);
+            int times = 2;
+            var task = CountAsync(times, 100);
+            var res = await task.WithCancellation(ct1.Token, ct2.Token);
+            Assert.Equal(times, res);
+        }
+        
+        private async Task<int> CountAsync(int times, int waitingPeriodMilliSecond)
+        {
+            int i = 0;
+            while (i < times)
+            {
+                await Task.Delay(waitingPeriodMilliSecond);
+                i++;
+            }
+            
+            return i; 
         }
     }
 }
