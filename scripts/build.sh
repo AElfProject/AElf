3108ab1943879b29858c89e061482ede7a7325c0
aerkol <anil.erkol@aelf.io>
2019-11-20 14:53:13 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 dotnet restore -s "https://nuget.cdn.azure.cn/v3/index.json" -s "https://api.nuget.org/v3/index.json" -v quiet "AElf.sln"
-dotnet build src/AElf.Contracts.Deployer/AElf.Contracts.Deployer.csproj
-mkdir scripts/patcher
-cp src/AElf.Contracts.Deployer/bin/Debug/netcoreapp3.0/*.* scripts/patcher
 dotnet build /p:GeneratePackageOnBuild=false "AElf.sln"
 
 if [[ $? -ne 0 ]] ; then
