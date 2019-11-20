94c8aa449b229636d8c8a4708916e2401732dc1c
aerkol <anil.erkol@aelf.io>
2019-11-20 16:39:25 +0800

    Tune execution duration limit for test cases

--- src/AElf.Kernel.SmartContract/SmartContractConstants.cs
+++ src/AElf.Kernel.SmartContract/SmartContractConstants.cs
@@ -2,6 +2,6 @@ namespace AElf.Kernel.SmartContract
 {
     public class SmartContractConstants
     {
-        public const int TransactionExecutionTimePeriodLimitInMilliSeconds = 50;
+        public const int TransactionExecutionTimePeriodLimitInMilliSeconds = 200;
     }
 }
