a788bdb14077e99e7030a55c52f6fa6564b93b28
xiangxx <buaaxxx1995@gmail.com>
2019-12-04 20:29:33 +0800

    limit memo length

--- contract/AElf.Contracts.MultiToken/TokenContract_Helper.cs
+++ contract/AElf.Contracts.MultiToken/TokenContract_Helper.cs
@@ -33,7 +33,7 @@ namespace AElf.Contracts.MultiToken
 
         private void AssertValidMemo(string memo)
         {
-            Assert(System.Text.Encoding.Unicode.GetByteCount(memo) <= 256, "memo has more than 256 bytes");
+            Assert(memo.Length <= 128, "memo's length is more than 128.");
         }
 
         private void DoTransfer(Address from, Address to, string symbol, long amount, string memo)
