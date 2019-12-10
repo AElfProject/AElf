cdb53511c73d211f036ec6392cbba31aa26389fb
aerkol <anil.erkol@aelf.io>
2019-12-10 16:05:08 +0800

    Add some other mono cecil extensions

--- src/AElf.CSharp.CodeOps/Extensions.cs
+++ src/AElf.CSharp.CodeOps/Extensions.cs
@@ -1,5 +1,7 @@
-using AElf.Sdk.CSharp;
+using System.Linq;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
+using AElf.Sdk.CSharp;
 
 namespace AElf.CSharp.CodeOps
 {
@@ -18,6 +20,60 @@ namespace AElf.CSharp.CodeOps
 
                 typeDefinition = baseType;
             }
+        }
+
+        public static bool HasSameBody(this MethodDefinition sourceMethod, MethodDefinition targetMethod)
+        {
+            // Exclude nop opcodes (compile in debug mode adds nop to be able to place breakpoint, ignore those)
+            var sourceMethodBodyInstructions = sourceMethod.Body.Instructions.Where(i => i.OpCode != OpCodes.Nop).ToArray();
+            var targetMethodBodyInstructions = targetMethod.Body.Instructions.Where(i => i.OpCode != OpCodes.Nop).ToArray();
+
+            // Compare method body
+            for (var i = 0; i < sourceMethodBodyInstructions.Count(); i++)
+            {
+                if (sourceMethodBodyInstructions[i].ToComparableString() == targetMethodBodyInstructions[i].ToComparableString())
+                    continue;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private static string ToComparableString(this Instruction instruction)
+        {
+            string operandStr;
+            
+            if (instruction.Operand is FieldDefinition field)
+            {
+                operandStr = field.Name;
+            }
+            else if (instruction.Operand is Instruction ins) // Is probably branching to another instruction
+            {
+                operandStr = ins.OpCode.ToString() + ins.Operand; // May restrict to branching to ret only
+            }
+            else
+            {
+                operandStr = instruction.Operand?.ToString();
+            }
+
+            return instruction.OpCode + operandStr;
+        }
+
+        public static bool HasSameParameters(this MethodDefinition sourceMethod, MethodDefinition targetMethod)
+        {
+            return sourceMethod.Parameters.Count == targetMethod.Parameters.Count && 
+                   sourceMethod.Parameters.All(sp => 
+                       targetMethod.Parameters.SingleOrDefault(tp => 
+                           tp.Name == sp.Name && tp.ParameterType.FullName == sp.ParameterType.FullName) != null);
+        }
+
+        public static bool HasSameFields(this TypeDefinition sourceType, TypeDefinition targetType)
+        {
+            return sourceType.Fields.Count == targetType.Fields.Count && 
+                   sourceType.Fields.All(sp => 
+                       targetType.Fields.SingleOrDefault(tp => 
+                           tp.Name == sp.Name && tp.FieldType.FullName == sp.FieldType.FullName) != null);
         }
     }
 }
