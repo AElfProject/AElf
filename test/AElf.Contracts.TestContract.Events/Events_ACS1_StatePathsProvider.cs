5e4901f3c5f614a1fd1b2dfe6e9eb73d47044dd4
Eric Shu <eric.shu@aelf.io>
2019-12-14 17:49:20 +0800

    update test contract

--- test/AElf.Contracts.TestContract.Events/Events_ACS1_StatePathsProvider.cs
+++ test/AElf.Contracts.TestContract.Events/Events_ACS1_StatePathsProvider.cs
@@ -1,7 +1,18 @@
+using Acs1;
+using Google.Protobuf.WellKnownTypes;
+
 namespace AElf.Contracts.TestContract.Events
 {
-    public class Events_ACS1_StatePathsProvider
+    public partial class EventsContract
     {
-        
+        public override MethodFees GetMethodFee(StringValue input)
+        {
+            return new MethodFees();
+        }
+
+        public override Empty SetMethodFee(MethodFees input)
+        {
+            return new Empty();
+        }
     }
 }
