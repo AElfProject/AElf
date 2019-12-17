15dc5183bab1272af124209bc8a4464c53054c7f
weili <weili.li@aelf.io>
2019-12-17 20:10:49 +0800

    unit test bug fixed

--- test/AElf.Contracts.TestContract.TransactionFeeCharging/TransactionFeeChargingContract.cs
+++ test/AElf.Contracts.TestContract.TransactionFeeCharging/TransactionFeeChargingContract.cs
@@ -58,5 +58,15 @@ namespace AElf.Contracts.TestContract.TransactionFeeCharging
             Assert(false, "Fate!");
             return new Empty();
         }
+        public override Empty IssueToTokenConvert(IssueAmount input)
+        {
+            State.TokenContract.Issue.Send(new IssueInput
+            {
+                Symbol = input.Symbol,
+                Amount = input.Amount,
+                To = Context.GetContractAddressByName(SmartContractConstants.TokenConverterContractSystemName)
+            });
+            return new Empty();
+        }
     }
 }
