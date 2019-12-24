35f555fa95202f0275fe961ad490a298b44a5e32
xiangxx <levi.xiang@aelf.io>
2019-12-24 20:21:24 +0800

    remove IChainManager

--- src/AElf.Kernel.Core/Blockchain/Domain/IChainManager.cs
+++ src/AElf.Kernel.Core/Blockchain/Domain/IChainManager.cs
@@ -440,17 +440,7 @@ namespace AElf.Kernel.Blockchain.Domain
                     toCleanBranchKeys.Add(branch.Key);
                 }
             }
-            await CollectNotLinkedBlockAsync(chain, toCleanNotLinkedKeys, irreversibleBlockHeight);
-            
-            return new DiscardedBranch
-            {
-                BranchKeys = toCleanBranchKeys,
-                NotLinkedKeys = toCleanNotLinkedKeys
-            };
-        }
 
-        private async Task CollectNotLinkedBlockAsync(Chain chain, List<string> toCleanNotLinkedKeys, long irreversibleBlockHeight)
-        {
             foreach (var notLinkedBlock in chain.NotLinkedBlocks)
             {
                 var blockLink = await GetChainBlockLinkWithCacheAsync(notLinkedBlock.Value);
@@ -465,6 +455,12 @@ namespace AElf.Kernel.Blockchain.Domain
                     toCleanNotLinkedKeys.Add(notLinkedBlock.Key);
                 }
             }
+            
+            return new DiscardedBranch
+            {
+                BranchKeys = toCleanBranchKeys,
+                NotLinkedKeys = toCleanNotLinkedKeys
+            };
         }
 
         public async Task CleanChainBranchAsync(Chain chain, DiscardedBranch discardedBranch)
