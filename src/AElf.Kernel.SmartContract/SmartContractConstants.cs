0fe2b604014732f109666ea076a42710da9a3b77
Eric Shu <eric.shu@aelf.io>
2019-12-27 11:39:28 +0800

    update execution from 30k to 50k

--- src/AElf.Kernel.SmartContract/SmartContractConstants.cs
+++ src/AElf.Kernel.SmartContract/SmartContractConstants.cs
@@ -4,8 +4,8 @@ namespace AElf.Kernel.SmartContract
     {
         public const int TransactionExecutionTimePeriodLimitInMilliSeconds = 50;
         
-        public const int ExecutionCallThreshold = 30000;
+        public const int ExecutionCallThreshold = 50000;
         
-        public const int ExecutionBranchThreshold = 30000;
+        public const int ExecutionBranchThreshold = 50000;
     }
 }
