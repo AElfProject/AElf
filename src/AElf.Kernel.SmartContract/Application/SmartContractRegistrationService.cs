d93149d989b9491c1fe39c93d39b03437a485e3a
Jason Wang <jason.wang@aelf.io>
2020-01-13 15:08:09 +0800

    Move ISmartContractRegistrationService to a new file

--- src/AElf.Kernel.SmartContract/Application/SmartContractRegistrationService.cs
+++ src/AElf.Kernel.SmartContract/Application/SmartContractRegistrationService.cs
@@ -7,18 +7,6 @@ using Volo.Abp.DependencyInjection;
 
 namespace AElf.Kernel.SmartContract.Application
 {
-    public interface ISmartContractRegistrationService
-    {
-        Task AddSmartContractRegistrationAsync(Address address, Hash codeHash, BlockIndex blockIndex);
-        Task<Dictionary<Address, List<Hash>>> RemoveForkCacheAsync(List<BlockIndex> blockIndexes);
-        Dictionary<Address, List<Hash>> SetIrreversedCache(List<BlockIndex> blockIndexes);
-        SmartContractRegistrationCache GetSmartContractRegistrationCacheFromForkCache(
-            IChainContext chainContext, Address address);
-        bool TryGetSmartContractRegistrationLibCache(Address address, out SmartContractRegistrationCache cache);
-        void SetSmartContractRegistrationLibCache(Address address, SmartContractRegistrationCache cache);
-        Task<SmartContractCodeHistory> GetSmartContractCodeHistoryAsync(Address address);
-    }
-    
     public class SmartContractRegistrationService : ISmartContractRegistrationService, ITransientDependency
     {
         private readonly ISmartContractRegistrationCacheProvider _smartContractRegistrationCacheProvider;
