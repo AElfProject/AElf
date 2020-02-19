624e48af669de07b20a99c45a42258beb3ae941f
Eric Shu <eric.shu@aelf.io>
2020-02-19 19:30:05 +0800

    try to fix ci code coverage issue
    1. Update test tool dependency
    2. Update cake script

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,8 +8,8 @@ for i in *Tests ; do
 
   time dotnet test "$i" --no-build /p:CollectCoverage=true /p:CoverletOutputFormat='json%2copencover' \
   /p:CoverletOutput="../results/coverage" /p:MergeWith="../results/coverage.json" \
-  /p:Exclude="[coverlet.*.tests?]*%2c[xunit.*]*%2c[AElf.Kernel.Consensus.Scheduler.*]*%2c[AElf.Database]AElf.Database.RedisProtocol.*%2c[AElf.Contracts.Authorization]*%2c[AElf.Test.Helpers]*%2c[*]*Exception%2c[*.Tests]*%2c[AElf.Contracts.TestContract.BasicFunctionWithParallel]*%2c[AElf.Contracts.GenesisUpdate]*" \
-  /p:ExcludeByFile="../../src/AElf.Runtime.CSharp.Core/Metadata/*.cs%2c../../src/AElf.Kernel.SmartContract/Metadata/*.cs%2c../../src/AElf.Database/RedisDatabase.cs%2c../../test/*.TestBase/*.cs" \
+  /p:Exclude="[coverlet.*.tests?]*%2c[xunit.*]*%2c[AElf.Kernel.Consensus.Scheduler.*]*%2c[AElf.Database]AElf.Database.RedisProtocol.*%2c[AElf.Test.Helpers]*%2c[*]*Exception%2c[*.Tests]*%2c[*.TestContract.*]*%2c[AElf.Contracts.GenesisUpdate]*%2c[AElf.WebApp.Application.Chain]*%2c[AElf.WebApp.Application.Net]*" \
+  /p:ExcludeByFile="../../src/AElf.Runtime.CSharp.Core/Metadata/*.cs%2c../../src/AElf.Kernel.SmartContract/Metadata/*.cs%2c../../src/AElf.Database/RedisDatabase.cs%2c../../test/*.TestBase/*.cs" -m:1 \
   > /tmp/test.log
 
   if [[ $? -ne 0 ]] ; then
