21d3f7ff5abc09532c54d7665fa03c894cfdf056
loning <loning.ma@aelf.io>
2020-02-21 00:09:10 +0800

    reviews for smart contract

--- src/AElf.Kernel.SmartContract/Sdk/AssociationSmartContractAddressNameProvider.cs
+++ src/AElf.Kernel.SmartContract/Sdk/AssociationSmartContractAddressNameProvider.cs
@@ -3,6 +3,7 @@ using Volo.Abp.DependencyInjection;
 
 namespace AElf.Kernel
 {
+    //TODO: move
     public class AssociationSmartContractAddressNameProvider : ISmartContractAddressNameProvider, ISingletonDependency
     {
         public static readonly Hash Name = Hash.FromString("AElf.ContractNames.Association");
