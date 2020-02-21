ce1873603304287feab5dbf98bd054806edd7ddd
loning <loning.ma@aelf.io>
2020-02-21 23:13:33 +0800

    reviews for SmartContract

--- src/AElf.Kernel.SmartContract.ExecutionPluginForAcs8/MethodStubFactory.cs
+++ src/AElf.Kernel.SmartContract.ExecutionPluginForAcs8/MethodStubFactory.cs
@@ -6,6 +6,7 @@ using Google.Protobuf;
 
 namespace AElf.Kernel.SmartContract.ExecutionPluginForAcs8
 {
+    //TODO: base class
     public class MethodStubFactory : IMethodStubFactory
     {
         private readonly IHostSmartContractBridgeContext _context;
