b934152c6452bf6a4b0263501bf12a4ecff61e76
aerkol <anil.erkol@aelf.io>
2020-02-28 20:11:09 +0800

    Check contract code after patching and show errors in IDE

--- src/AElf.Contracts.Deployer/Program.cs
+++ src/AElf.Contracts.Deployer/Program.cs
@@ -1,32 +1,72 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using AElf.CSharp.CodeOps;
 
 namespace AElf.Contracts.Deployer
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var sourcePath = "";
-            var saveAsPath = "";
-            
+            var skipAudit = false;
+            var overwrite = false;
+            var sourcePath = args.Last();
+            string saveAsPath;
+
             // We may later use a proper parser to get arguments if we need to support more
-            if (args[0] != "-overwrite")
+            foreach (var arg in args)
             {
-                sourcePath = args[0];
-                saveAsPath = sourcePath + ".patched";
-                Console.WriteLine($"[SAVING AS] {saveAsPath}");
+                // Set flags
+                if (arg.StartsWith("-"))
+                {
+                    switch (arg)
+                    {
+                        case "-overwrite":
+                            overwrite = true;
+                            break;
+                        
+                        case "-skipaudit":
+                            skipAudit = true;
+                            break;
+                    }
+                }
+            }
+
+            if (overwrite)
+            {
+                saveAsPath = sourcePath;
+                Console.WriteLine($"[CONTRACT-PATCHER] Overwriting {saveAsPath}");
             }
             else
             {
-                sourcePath = args[1];
-                saveAsPath = sourcePath;
-                Console.WriteLine($"[OVERWRITING] {saveAsPath}");
+                saveAsPath = sourcePath + ".patched";
+                Console.WriteLine($"[CONTRACT-PATCHER] Saving as {saveAsPath}");
             }
             
-            var code = File.ReadAllBytes(sourcePath);
-            File.WriteAllBytes(saveAsPath, ContractPatcher.Patch(code));
+            var patchedCode = ContractPatcher.Patch(File.ReadAllBytes(sourcePath));
+
+            if (!skipAudit)
+            {
+                try
+                {
+                    var auditor = new ContractAuditor(null, null);
+                    auditor.Audit(patchedCode, null, false);
+                }
+                catch (InvalidCodeException ex)
+                {
+                    foreach (var finding in ex.Findings)
+                    {
+                        // Print error in parsable format so that it can be shown in IDE
+                        Console.WriteLine($"error: {finding.ToString()}");
+                    }
+                    return 1;
+                }                
+            }
+
+            File.WriteAllBytes(saveAsPath, patchedCode);
+
+            return 0;
         }
     }
 }
