fd6742d166db9084b6ebc4ebc67dc3d9e198874a
levi.xiang <levi.xiang@aelf.io>
2020-02-28 19:41:15 +0800

    enable internal method

--- src/AElf.Kernel.TransactionPool/AssemblyInfo.cs
+++ src/AElf.Kernel.TransactionPool/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("AElf.Kernel.SmartContract.ExecutionPluginForAcsMethodFee.Tests")]
+[assembly: InternalsVisibleTo("AElf.Kernel.SmartContract.ExecutionPluginForMethodFee.Tests")]
 [assembly: InternalsVisibleTo("AElf.Kernel.SmartContract.ExecutionPluginForResourceFee.Tests")]
 [assembly: InternalsVisibleTo("AElf.Contracts.TestContract.Tests")]
 [assembly: InternalsVisibleTo("AElf.WebApp.Application.TestBase")]
