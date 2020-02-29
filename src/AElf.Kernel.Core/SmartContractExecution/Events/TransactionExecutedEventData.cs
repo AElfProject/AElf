5b2119210e2532173f76f2752682925ee9bceb67
yiqi <yiqi.zhao@aelf.io>
2020-02-29 21:51:42 +0800

    Remove DEBUG from TransactionExecutedEventData.

--- src/AElf.Kernel.Core/SmartContractExecution/Events/TransactionExecutedEventData.cs
+++ src/AElf.Kernel.Core/SmartContractExecution/Events/TransactionExecutedEventData.cs
@@ -1,9 +1,7 @@
 namespace AElf.Kernel.SmartContractExecution.Events
 {
-#if DEBUG
     public class TransactionExecutedEventData
     {
         public TransactionTrace TransactionTrace { get; set; }
     }
-#endif
 }
