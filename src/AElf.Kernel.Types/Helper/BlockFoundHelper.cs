5a4b0a5f51677a45fd6235c9e49f6e9bebc62c12
weili <weili.li@aelf.io>
2020-02-29 21:01:29 +0800

    add helper to get pre block

---
+++ src/AElf.Kernel.Types/Helper/BlockFoundHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using AElf.Types;
+using Google.Protobuf;
+
+namespace AElf.Kernel
+{
+    public static class BlockFoundHelper
+    {
+        public static ByteString GetPreBlock(Hash blockHash)
+        {
+            var refBlockPrefix = ByteString.CopyFrom(blockHash.Value.Take(4).ToArray());
+            return refBlockPrefix;
+        }
+    }
+}
