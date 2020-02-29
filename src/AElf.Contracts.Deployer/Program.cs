d83de376e2d4fb73660807dd403f9d0013201dfb
aerkol <anil.erkol@aelf.io>
2020-02-29 19:38:25 +0800

    Use parser to parse command line input parameters.

--- src/AElf.Contracts.Deployer/Program.cs
+++ src/AElf.Contracts.Deployer/Program.cs
@@ -1,52 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using AElf.CSharp.CodeOps;
+using CommandLine;
 
 namespace AElf.Contracts.Deployer
 {
     class Program
     {
-        static int Main(string[] args)
+        class Options
         {
-            var skipAudit = false;
-            var overwrite = false;
-            var sourcePath = args.Last();
+            [Option('s', "skipaudit", Default = false, HelpText = "Skips performing code check on contract code.")]
+            public bool SkipAudit { get; set; }
+            
+            [Option('w', "overwrite", Default = false, HelpText = "Overwrite contract's DLL instead of saving with .patched extension.")]
+            public bool Overwrite { get; set; }
+            
+            [Option('p', "path", Required = true, HelpText = "The path of the contract's DLL.")]
+            public string ContractDllPath { get; set; }
+        }
+        
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(Run)
+                .WithNotParsed(Error);
+        }
+
+        private static void Error(IEnumerable<Error> errors)
+        {
+            Console.WriteLine("error: Problem parsing input parameters to run contract deployer.");
+        }
+
+        private static void Run(Options o)
+        {
             string saveAsPath;
 
-            // We may later use a proper parser to get arguments if we need to support more
-            foreach (var arg in args)
+            if (!File.Exists(o.ContractDllPath))
             {
-                // Set flags
-                if (arg.StartsWith("-"))
-                {
-                    switch (arg)
-                    {
-                        case "-overwrite":
-                            overwrite = true;
-                            break;
-                        
-                        case "-skipaudit":
-                            skipAudit = true;
-                            break;
-                    }
-                }
+                Console.WriteLine($"error: Contract DLL cannot be found in specified path {o.ContractDllPath}");
+                return;
             }
 
-            if (overwrite)
+            if (o.Overwrite)
             {
-                saveAsPath = sourcePath;
+                saveAsPath = o.ContractDllPath;
                 Console.WriteLine($"[CONTRACT-PATCHER] Overwriting {saveAsPath}");
             }
             else
             {
-                saveAsPath = sourcePath + ".patched";
+                saveAsPath = o.ContractDllPath + ".patched";
                 Console.WriteLine($"[CONTRACT-PATCHER] Saving as {saveAsPath}");
             }
             
-            var patchedCode = ContractPatcher.Patch(File.ReadAllBytes(sourcePath));
+            var patchedCode = ContractPatcher.Patch(File.ReadAllBytes(o.ContractDllPath));
 
-            if (!skipAudit)
+            if (!o.SkipAudit)
             {
                 try
                 {
@@ -60,13 +69,10 @@ namespace AElf.Contracts.Deployer
                         // Print error in parsable format so that it can be shown in IDE
                         Console.WriteLine($"error: {finding.ToString()}");
                     }
-                    return 1;
                 }                
             }
 
             File.WriteAllBytes(saveAsPath, patchedCode);
-
-            return 0;
         }
     }
 }
