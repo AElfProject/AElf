b592d89954474b495ef0ccc16f186974d67959ae
yiqi <yiqi.zhao@aelf.io>
2020-03-05 15:51:17 +0800

    Move SymbolListToPayTxFeeUpdatedLogEventProcessor to plugin project.

--- src/AElf.Kernel.SmartContract.ExecutionPluginForMethodFee/SymbolListToPayTxFeeUpdatedLogEventProcessor.cs
+++ src/AElf.Kernel.SmartContract.ExecutionPluginForMethodFee/SymbolListToPayTxFeeUpdatedLogEventProcessor.cs
@@ -1,15 +1,13 @@
 using System.Threading.Tasks;
 using AElf.Contracts.MultiToken;
 using AElf.Kernel.SmartContract.Application;
-using AElf.Kernel.SmartContract.ExecutionPluginForMethodFee;
 using AElf.Kernel.Token;
 using AElf.Sdk.CSharp;
 using AElf.Types;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 
-
-namespace AElf.Kernel.TransactionPool.Application
+namespace AElf.Kernel.SmartContract.ExecutionPluginForMethodFee
 {
     public class SymbolListToPayTxFeeUpdatedLogEventProcessor : IBlockAcceptedLogEventProcessor
     {
