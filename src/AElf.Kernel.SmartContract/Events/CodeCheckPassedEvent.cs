b8b64cb4ac97b9c6dbe8c1e273143abfafebefa2
shiwk <wilkey.shi@aelf.io>
2020-03-21 11:16:22 +0800

    Remove unused event file.

--- src/AElf.Kernel.SmartContract/Events/CodeCheckPassedEvent.cs
+++
@@ -1,9 +0,0 @@
-using AElf.Types;
-
-namespace AElf.Kernel.SmartContract.Events
-{
-    public class CodeCheckPassedEvent
-    {
-        public TransactionResult TransactionResult { get; set; }
-    }
-}
