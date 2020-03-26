265646fbc4d8433279b047c7090462e367b9e74d
samfrb <sam.bassett@aelf.io>
2020-03-26 14:05:47 +0100

    Add core docs.

--- src/AElf.CSharp.Core/Utils/EncodingHelper.cs
+++ src/AElf.CSharp.Core/Utils/EncodingHelper.cs
@@ -2,8 +2,16 @@ using System.Text;
 
 namespace AElf.CSharp.Core.Utils
 {
+    /// <summary>
+    /// Helper class for serializing strings.
+    /// </summary>
     public class EncodingHelper
     {
+        /// <summary>
+        /// Serializes a UTF-8 string to a byte array.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public static byte[] GetBytesFromUtf8String(string str)
         {
             return Encoding.UTF8.GetBytes(str);
