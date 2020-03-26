cad6c4c9d3f8a398046ecfb37eeaf99a2f33aa51
zhxymh <sean.zhang@aelf.io>
2020-03-26 15:46:18 +0800

    add PeerInvalidDataProviderTests

---
+++ test/AElf.OS.Core.Tests/Network/Infrastructure/PeerInvalidDataProviderTests.cs
@@ -0,0 +1,40 @@
+using AElf.OS.Network.Application;
+using AElf.OS.Network.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace AElf.OS.Network
+{
+    public class PeerInvalidDataProviderTests: NetworkInfrastructureTestBase
+    {
+        private readonly IPeerInvalidDataProvider _peerInvalidDataProvider;
+        
+        public PeerInvalidDataProviderTests()
+        {
+            _peerInvalidDataProvider = GetRequiredService<IPeerInvalidDataProvider>();
+        }
+
+        [Fact]
+        public void MarkInvalidData_Test()
+        {
+            var host = "127.0.0.1";
+            bool markResult;
+            for (var i = 0; i < 51; i++)
+            {
+                markResult =_peerInvalidDataProvider.TryMarkInvalidData(host);
+                markResult.ShouldBeTrue();
+            }
+            
+            markResult =_peerInvalidDataProvider.TryMarkInvalidData(host);
+            markResult.ShouldBeFalse();
+            
+            markResult =_peerInvalidDataProvider.TryMarkInvalidData("192.168.1.1");
+            markResult.ShouldBeTrue();
+
+            _peerInvalidDataProvider.TryRemoveInvalidData(host);
+            
+            markResult =_peerInvalidDataProvider.TryMarkInvalidData(host);
+            markResult.ShouldBeTrue();
+        }
+    }
+}
