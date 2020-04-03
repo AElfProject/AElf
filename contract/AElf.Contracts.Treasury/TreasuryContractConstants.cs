68fbe28fcd25cb33d5b7f58aeb23226d3918ff1a
WeiLi <weili.li@aelf.io>
2020-04-03 16:19:17 +0800

    add customize dividend pool

--- contract/AElf.Contracts.Treasury/TreasuryContractConstants.cs
+++ contract/AElf.Contracts.Treasury/TreasuryContractConstants.cs
@@ -2,14 +2,6 @@ namespace AElf.Contracts.Treasury
 {
     public static class TreasuryContractConstants
     {
-        public const int CitizenWelfareWeight = 15;
-        public const int BackupSubsidyWeight = 1;
-        public const int MinerRewardWeight = 4;
-        
-        public const int BasicMinerRewardWeight = 2;
-        public const int VotesWeightRewardWeight = 1;
-        public const int ReElectionRewardWeight = 1;
-
         public const int MaximumReElectionRewardShare = 10;
         public const int DaySec = 86400;
     }
