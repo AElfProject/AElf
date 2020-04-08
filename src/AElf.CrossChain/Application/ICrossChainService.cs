b8719079d2f9920c76eaa29ca794d46c4bc6cbb1
shiwk <wilkey.shi@aelf.io>
2020-04-08 14:35:25 +0800

    Code clean.

--- src/AElf.CrossChain/Application/ICrossChainService.cs
+++ src/AElf.CrossChain/Application/ICrossChainService.cs
@@ -6,12 +6,6 @@ namespace AElf.CrossChain.Application
     public interface ICrossChainService
     {
         Task FinishInitialSyncAsync();
-
-        // Dictionary<int, long> GetNeededChainIdAndHeightPairs();
-        //
-        // Task<Block> GetNonIndexedBlockAsync(long height);
-        //
-        // Task<ChainInitializationData> GetChainInitializationDataAsync(int chainId);
         Task UpdateCrossChainDataWithLibAsync(Hash blockHash, long blockHeight);
     }
 }
