d1a2634aa7c394d0c06af3c2f1629ddba21b81b6
zhxymh <sean.zhang@aelf.io>
2020-04-18 15:56:45 +0800

    add side chain test

---
+++ test/AElf.ContractTestBase.Tests/SideChainTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using AElf.Contracts.MultiToken;
+using AElf.Contracts.TestKit;
+using AElf.Kernel.Token;
+using AElf.Types;
+using Shouldly;
+using Xunit;
+
+namespace AElf.ContractTestBase.Tests
+{
+    public class SideChainTests : SideChainTestBase
+    {
+        [Fact]
+        public async Task Test()
+        {
+           
+        }
+    }
+}
