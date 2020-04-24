2fdd7564e814dc48412996e80d41436487df0788
shiwk <wilkey.shi@aelf.io>
2020-04-24 10:21:04 +0800

    Initialize main chain token contract.

--- src/AElf.Blockchains.MainChain/GenesisSmartContractDtoProvider_Token.cs
+++ src/AElf.Blockchains.MainChain/GenesisSmartContractDtoProvider_Token.cs
@@ -1,11 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using Acs0;
-using AElf.Contracts.MultiToken;
 using AElf.Kernel.Token;
 using AElf.OS.Node.Application;
 using AElf.Types;
-using Google.Protobuf.WellKnownTypes;
 
 namespace AElf.Blockchains.MainChain
 {
@@ -13,14 +11,11 @@ namespace AElf.Blockchains.MainChain
     {
         private IEnumerable<GenesisSmartContractDto> GetGenesisSmartContractDtosForToken()
         {
-            var callList = new SystemContractDeploymentInput.Types.SystemTransactionMethodCallList();
-            callList.Add(nameof(TokenContractContainer.TokenContractStub.InitializeAuthorizedController),
-                new Empty());
             var l = new List<GenesisSmartContractDto>();
             l.AddGenesisSmartContract(
                 GetContractCodeByName("AElf.Contracts.MultiToken"),
                 TokenSmartContractAddressNameProvider.Name,
-                callList);
+                new SystemContractDeploymentInput.Types.SystemTransactionMethodCallList());
             return l;
         }
     }
