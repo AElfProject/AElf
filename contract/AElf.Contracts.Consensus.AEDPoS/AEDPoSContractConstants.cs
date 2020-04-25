d7c031632586a246e22ffb1ec48f3acc1aeee797
yiqi <yiqi.zhao@aelf.io>
2020-04-25 12:37:59 +0800

    Tune some constants values for testing.

--- contract/AElf.Contracts.Consensus.AEDPoS/AEDPoSContractConstants.cs
+++ contract/AElf.Contracts.Consensus.AEDPoS/AEDPoSContractConstants.cs
@@ -7,9 +7,9 @@ namespace AElf.Contracts.Consensus.AEDPoS
         public const int RandomNumberDueRoundCount = 1024;
         public const long InitialMiningRewardPerBlock = 12500000;
         public const long TimeToReduceMiningRewardByHalf = 126144000; // 60 * 60 * 24 * 365 * 4
-        public const int SupposedMinersCount = 17;
+        public const int SupposedMinersCount = 4;
         public const int KeepRounds = 40960;
-        public const long TolerableMissedTimeSlotsCount = 60 * 24 * 3; // one time slot per minute and last 3 days.
+        public const long TolerableMissedTimeSlotsCount = 30;
         public const string SideChainShareProfitsTokenSymbol = "SHARE";
         public const string PayTxFeeSymbolListName = "SymbolListToPayTxFee";
         public const string PayRentalSymbolListName = "SymbolListToPayRental";
