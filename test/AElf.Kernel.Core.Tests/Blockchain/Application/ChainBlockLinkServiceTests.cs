d23e54cb8d2351b45f03a4399df45f465575a388
yonglin zhu <yonglin.zhu@aelf.io>
2020-04-28 14:33:59 +0800

    Add tests

---
+++ test/AElf.Kernel.Core.Tests/Blockchain/Application/ChainBlockLinkServiceTests.cs
@@ -0,0 +1,44 @@
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace AElf.Kernel.Blockchain.Application
+{
+    public class ChainBlockLinkServiceTests: AElfKernelTestBase
+    {
+        private readonly IChainBlockLinkService _chainBlockLinkService;
+
+        public ChainBlockLinkServiceTests()
+        {
+            _chainBlockLinkService = GetRequiredService<IChainBlockLinkService>();
+        }
+
+        [Fact]
+        private void GetCachedChainBlockLinks_Tests()
+        {
+            var chainBlockLinks = _chainBlockLinkService.GetCachedChainBlockLinks();
+            chainBlockLinks.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        private async Task GetNotExecutedChainBlockLinksAsync_Tests()
+        {
+            var l = await _chainBlockLinkService.GetNotExecutedChainBlockLinksAsync(HashHelper.ComputeFromString("test"));
+                l.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        private void CleanCachedChainBlockLinks_Tests()
+        {
+            _chainBlockLinkService.CleanCachedChainBlockLinks(long.MaxValue);
+            var chainBlockLinks = _chainBlockLinkService.GetCachedChainBlockLinks();
+            chainBlockLinks.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        private async Task SetChainBlockLinkExecutionStatusAsync_Tests()
+        {
+            
+        }
+    }
+}
---
+++ test/AElf.Kernel.Core.Tests/Blockchain/Domain/TransactionResultManagerTests.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using AElf.Types;
+using Shouldly;
+using Xunit;
+
+namespace AElf.Kernel.Blockchain.Domain
+{
+    public class TransactionResultManagerTests : AElfKernelTestBase
+    {
+        
+        private readonly ITransactionResultManager _transactionResultManager;
+        private KernelTestHelper _kernelTestHelper;
+
+        public TransactionResultManagerTests()
+        {
+            _transactionResultManager = GetRequiredService<ITransactionResultManager>();
+            _kernelTestHelper = GetRequiredService<KernelTestHelper>();
+
+        }
+        [Fact]
+        private async Task AddTransactionResultAsync_Tests()
+        {
+            
+            var tran = _kernelTestHelper.GenerateTransaction();
+            var tranResult = _kernelTestHelper.GenerateTransactionResult(tran, TransactionResultStatus.Mined);
+            var disambiguationHash1 = HashHelper.ComputeFromString("disambiguationHash1");
+            await _transactionResultManager.AddTransactionResultAsync(tranResult, disambiguationHash1);
+
+            var tranResult2 =
+                await _transactionResultManager.GetTransactionResultAsync(tranResult.TransactionId, disambiguationHash1);
+            tranResult.TransactionId.ShouldBe(tranResult2.TransactionId);
+
+        }
+    }
+}
