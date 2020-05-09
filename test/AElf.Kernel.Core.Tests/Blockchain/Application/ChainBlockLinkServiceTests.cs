ad9e230b791bb56b216f572b73def2bb38319ac6
yonglin zhu <yonglin.zhu@aelf.io>
2020-05-09 19:11:15 +0800

    Modify ChainBlockLinkService unit test

--- test/AElf.Kernel.Core.Tests/Blockchain/Application/ChainBlockLinkServiceTests.cs
+++ test/AElf.Kernel.Core.Tests/Blockchain/Application/ChainBlockLinkServiceTests.cs
@@ -1,34 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AElf.Kernel.Blockchain.Domain;
 using Shouldly;
+using Volo.Abp.Threading;
 using Xunit;
 
 namespace AElf.Kernel.Blockchain.Application
 {
-    public class ChainBlockLinkServiceTests: AElfKernelTestBase
+    public class ChainBlockLinkServiceTests: AElfKernelWithChainTestBase
     {
         private readonly IChainBlockLinkService _chainBlockLinkService;
+        private readonly KernelTestHelper _kernelTestHelper;
+        private readonly IChainManager _chainManager;
 
         public ChainBlockLinkServiceTests()
         {
+            _kernelTestHelper = GetRequiredService<KernelTestHelper>();
             _chainBlockLinkService = GetRequiredService<IChainBlockLinkService>();
+            _chainManager = GetRequiredService<IChainManager>();
+            
         }
 
         [Fact]
-        private void GetCachedChainBlockLinks_Tests()
+        public void GetCachedChainBlockLinks_Tests()
         {
+            var bestCount = _kernelTestHelper.BestBranchBlockList.Count;
+            var longestCount = _kernelTestHelper.LongestBranchBlockList.Count;
+            var forkCount = _kernelTestHelper.ForkBranchBlockList.Count;
+            var unlinkedCount = _kernelTestHelper.UnlinkedBranchBlockList.Count;
             var chainBlockLinks = _chainBlockLinkService.GetCachedChainBlockLinks();
-            chainBlockLinks.Count.ShouldBe(0);
+            chainBlockLinks.Count.ShouldBe(bestCount + longestCount + forkCount + unlinkedCount);
         }
 
         [Fact]
-        private async Task GetNotExecutedChainBlockLinksAsync_Tests()
-        {
-            var l = await _chainBlockLinkService.GetNotExecutedChainBlockLinksAsync(HashHelper.ComputeFrom("test"));
-                l.Count.ShouldBe(0);
+        public async Task GetNotExecutedChainBlockLinksAsync_Tests()
+        { 
+            var chain = await _chainManager.GetAsync();
+            var blockHash = chain.BestChainHash;
+            var count = 0;
+            while (true)
+            {
+                var chainBlockLink = await _chainManager.GetChainBlockLinkAsync(blockHash);
+                if (chainBlockLink != null)
+                {
+                    if (chainBlockLink.ExecutionStatus == ChainBlockLinkExecutionStatus.ExecutionNone)
+                    {
+                        count++;
+                        if (chainBlockLink.PreviousBlockHash != null)
+                            blockHash = chainBlockLink.PreviousBlockHash;
+                        continue;
+                    }
+                    else if (chainBlockLink.ExecutionStatus == ChainBlockLinkExecutionStatus.ExecutionFailed)
+                    {
+                        count = 0;
+                    }
+                }
+
+                break;
+            }
+
+            var chainBlockLinks = await _chainBlockLinkService.GetNotExecutedChainBlockLinksAsync(chain.BestChainHash);
+            chainBlockLinks.Count.ShouldBe(count);
         }
 
         [Fact]
-        private void CleanCachedChainBlockLinks_Tests()
+        public void CleanCachedChainBlockLinks_Tests()
         {
             _chainBlockLinkService.CleanCachedChainBlockLinks(long.MaxValue);
             var chainBlockLinks = _chainBlockLinkService.GetCachedChainBlockLinks();
@@ -36,9 +74,31 @@ namespace AElf.Kernel.Blockchain.Application
         }
 
         [Fact]
-        private async Task SetChainBlockLinkExecutionStatusAsync_Tests()
+        public void SetChainBlockLinkExecutionStatusAsync_Tests()
         {
-            
+            var chainBlockLinks = _chainBlockLinkService.GetCachedChainBlockLinks();
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                AsyncHelper.RunSync(() => _chainBlockLinkService.SetChainBlockLinkExecutionStatusAsync(
+                    chainBlockLinks[0].BlockHash,
+                    ChainBlockLinkExecutionStatus.ExecutionNone));
+            });
+
+            var chainBlockLink1 =
+                chainBlockLinks.First(cbl => cbl.ExecutionStatus != ChainBlockLinkExecutionStatus.ExecutionNone);
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                AsyncHelper.RunSync(() => _chainBlockLinkService.SetChainBlockLinkExecutionStatusAsync(
+                    chainBlockLink1.BlockHash,
+                    ChainBlockLinkExecutionStatus.ExecutionNone));
+            });
+            var chainBlockLink2 =
+                chainBlockLinks.First(cbl => cbl.ExecutionStatus == ChainBlockLinkExecutionStatus.ExecutionNone);
+
+            AsyncHelper.RunSync(() => _chainBlockLinkService.SetChainBlockLinkExecutionStatusAsync(
+                chainBlockLink2.BlockHash,
+                ChainBlockLinkExecutionStatus.ExecutionSuccess));
+            chainBlockLink2.ExecutionStatus.ShouldBe(ChainBlockLinkExecutionStatus.ExecutionSuccess);
         }
     }
 }
