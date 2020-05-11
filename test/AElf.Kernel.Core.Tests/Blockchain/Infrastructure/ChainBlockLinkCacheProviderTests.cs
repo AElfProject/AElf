b48cc34400d78e5a5f0ccc224a8725663b479849
yonglin zhu <yonglin.zhu@aelf.io>
2020-05-11 14:47:10 +0800

    Add ChainBlockLinkCacheProvider tests

--- test/AElf.Kernel.Core.Tests/Blockchain/Infrastructure/ChainBlockLinkCacheProviderTests.cs
+++ test/AElf.Kernel.Core.Tests/Blockchain/Infrastructure/ChainBlockLinkCacheProviderTests.cs
@@ -1,19 +1,64 @@
+using System.Linq;
+using AElf.Kernel.Blockchain.Application;
+using AElf.Kernel.Blockchain.Domain;
+using AElf.Types;
+using Shouldly;
 using Xunit;
 
 namespace AElf.Kernel.Blockchain.Infrastructure
 {
-    public class ChainBlockLinkCacheProviderTests:AElfKernelTestBase
+    public class ChainBlockLinkCacheProviderTests : AElfKernelWithChainTestBase
     {
         private IChainBlockLinkCacheProvider _chainBlockLinkCacheProvider;
+        private readonly IChainBlockLinkService _chainBlockLinkService;
+        private readonly KernelTestHelper _kernelTestHelper;
+        private readonly IChainManager _chainManager;
+
         public ChainBlockLinkCacheProviderTests()
         {
             _chainBlockLinkCacheProvider = GetRequiredService<IChainBlockLinkCacheProvider>();
+            _kernelTestHelper = GetRequiredService<KernelTestHelper>();
+            _chainBlockLinkService = GetRequiredService<IChainBlockLinkService>();
+            _chainManager = GetRequiredService<IChainManager>();
         }
 
         [Fact]
-        private void Get_Set_ChainBlockLink()
+        public void Get_Set_ChainBlockLink_Test()
         {
-            
+            var hash = HashHelper.ComputeFrom("MyBlock");
+            var chainBlockLink1 = new ChainBlockLink()
+            {
+                BlockHash = hash,
+                Height = 20L,
+                PreviousBlockHash = Hash.Empty,
+                IsLinked = false,
+                IsIrreversibleBlock = false
+            };
+            _chainBlockLinkCacheProvider.SetChainBlockLink(chainBlockLink1);
+            var chainBlockLink2 = _chainBlockLinkCacheProvider.GetChainBlockLink(hash);
+            chainBlockLink2.ShouldBe(chainBlockLink1);
+        }
+
+        [Fact]
+        public void GetChainBlockLinks_Test()
+        {
+            var bestCount = _kernelTestHelper.BestBranchBlockList.Count;
+            var longestCount = _kernelTestHelper.LongestBranchBlockList.Count;
+            var forkCount = _kernelTestHelper.ForkBranchBlockList.Count;
+            var unlinkedCount = _kernelTestHelper.UnlinkedBranchBlockList.Count;
+            var chainBlockLinks = _chainBlockLinkCacheProvider.GetChainBlockLinks();
+            chainBlockLinks.Count.ShouldBe(bestCount + longestCount + forkCount + unlinkedCount);
+        }
+
+        [Fact]
+        public void RemoveChainBlockLink()
+        {
+            var chainBlockLinks = _chainBlockLinkCacheProvider.GetChainBlockLinks();
+            if (!chainBlockLinks.Any())
+                return;
+            var hash = chainBlockLinks[^1].BlockHash;
+            _chainBlockLinkCacheProvider.RemoveChainBlockLink(hash);
+            _chainBlockLinkCacheProvider.GetChainBlockLink(hash).ShouldBeNull();
         }
     }
 }
