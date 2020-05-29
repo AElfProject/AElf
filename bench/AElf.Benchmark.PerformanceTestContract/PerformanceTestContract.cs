3c8033645fea753fd441f70d331f041575b548e8
shiwk <wilkey.shi@aelf.io>
2020-05-29 22:06:06 +0800

    Skip audit for performance test contract.

--- bench/AElf.Benchmark.PerformanceTestContract/PerformanceTestContract.cs
+++ bench/AElf.Benchmark.PerformanceTestContract/PerformanceTestContract.cs
@@ -28,7 +28,7 @@ namespace AElf.Benchmark.PerformanceTestContract
             return new Int32Value {Value = input.Seed};
         }
 
-        public override UIntValue LoopDivAdd(DivAddTestInput input)
+        public override DoubleValue LoopDivAdd(DivAddTestInput input)
         {
             var r = input.X;
             for (uint i = 0; i < input.N; i++) 
