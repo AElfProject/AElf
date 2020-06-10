4b14dd8a7677e644eb6934053e85df4abdc9ce85
weili <weili.li@aelf.io>
2020-06-10 15:10:43 +0800

    modify doc

--- docs-sphinx/conf.py
+++ docs-sphinx/conf.py
@@ -315,7 +315,7 @@ html_theme_options = {
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
-    'navigation_depth': 4,
+    'navigation_depth': 2,
     'includehidden': False,
     'titles_only': False,
 }
