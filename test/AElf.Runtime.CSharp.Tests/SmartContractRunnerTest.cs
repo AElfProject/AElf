962e35bb320a92a999027e7b1a0fdd7757b4a23a
zhxymh <sean.zhang@aelf.io>
2020-08-04 20:21:04 +0800

    add tests

--- test/AElf.Runtime.CSharp.Tests/SmartContractRunnerTest.cs
+++ test/AElf.Runtime.CSharp.Tests/SmartContractRunnerTest.cs
@@ -1,36 +1,36 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
-using AElf.Kernel;
-using AElf.Kernel.SmartContract.Infrastructure;
+using AElf.Runtime.CSharp.Tests.TestContract;
 using AElf.Types;
 using Google.Protobuf;
 using Shouldly;
 using Xunit;
 
-namespace AElf.Runtime.CSharp.Tests
+namespace AElf.Runtime.CSharp
 {
     public sealed class SmartContractRunnerTest: CSharpRuntimeTestBase
     {
-        private ISmartContractRunner Runner { get; set; }
-        private SmartContractRegistration Reg { get; set; }
-
-        public SmartContractRunnerTest()
-        {
-            var contractCode = File.ReadAllBytes(typeof(TestContract.TestContract).Assembly.Location);
-            Runner = GetRequiredService<ISmartContractRunner>();
-            Reg = new SmartContractRegistration()
-            {
-                Category = KernelConstants.DefaultRunnerCategory,
-                Code = ByteString.CopyFrom(contractCode),
-                CodeHash = HashHelper.ComputeFrom(contractCode)
-            };
-        }
-
         [Fact]
-        public async Task Get_IExecutive()
+        public async Task Run_Test()
         {
-            var executive = await Runner.RunAsync(Reg);
+            var contractCode = File.ReadAllBytes(typeof(TestContract).Assembly.Location);
+            var smartContractRegistration = new SmartContractRegistration()
+            {
+                Code = ByteString.CopyFrom(contractCode),
+                CodeHash = HashHelper.ComputeFrom(contractCode),
+                IsSystemContract = true
+            };
+            
+            var sdkDir = Path.GetDirectoryName(typeof(CSharpSmartContractRunner).Assembly.Location);
+            var smartContractRunner = new CSharpSmartContractRunner(sdkDir);
+            
+            var executive = await smartContractRunner.RunAsync(smartContractRegistration);
             executive.ShouldNotBe(null);
+            executive.ContractHash.ShouldBe(smartContractRegistration.CodeHash);
+            executive.ContractVersion.ShouldBe("1.0.0.0");
+            executive.IsSystemContract.ShouldBe(smartContractRegistration.IsSystemContract);
+            
+            smartContractRunner.ContractVersion.ShouldBe("1.0.0.0");
         }
     }
 }
