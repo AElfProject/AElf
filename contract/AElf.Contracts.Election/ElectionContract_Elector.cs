cd145613a6b9bbb0b21c42ed86ccaf150b402699
weili <weili.li@aelf.io>
2020-12-03 14:38:16 +0800

    vote 0 unable to add in dataCenter

--- contract/AElf.Contracts.Election/ElectionContract_Elector.cs
+++ contract/AElf.Contracts.Election/ElectionContract_Elector.cs
@@ -366,6 +366,8 @@ namespace AElf.Contracts.Election
             }
             if (amountAfterWithdraw > 0 && maxVoteAmountOutDataCenter <= amountAfterWithdraw)
                 return false;
+            if (maxVoteAmountOutDataCenter == 0)
+                maxVoteOptionOutDataCenter = null;
             rankingList.DataCenters.Remove(member);
             if (maxVoteOptionOutDataCenter != null)
                 rankingList.DataCenters[maxVoteOptionOutDataCenter] = maxVoteAmountOutDataCenter;
