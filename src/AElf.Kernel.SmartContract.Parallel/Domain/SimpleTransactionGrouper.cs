c33b5495ff281055d06b888177d4435c45287cbb
zhxymh <sean.zhang@aelf.io>
2021-04-14 13:53:03 +0800

    add log

---
+++ src/AElf.Kernel.SmartContract.Parallel/Domain/SimpleTransactionGrouper.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AElf.Types;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Volo.Abp.DependencyInjection;
+
+namespace AElf.Kernel.SmartContract.Parallel
+{
+    public class SimpleTransactionGrouper: ITransactionGrouper, ISingletonDependency
+    {
+        public ILogger<SimpleTransactionGrouper> Logger { get; set; }
+
+        public SimpleTransactionGrouper()
+        {
+            Logger = NullLogger<SimpleTransactionGrouper>.Instance;
+        }
+
+        public async Task<GroupedTransactions> GroupAsync(IChainContext chainContext, List<Transaction> transactions)
+        {
+            Logger.LogTrace("Begin SimpleTransactionGrouper.GroupAsync");
+            
+            var groupedTransactions = transactions.GroupBy(t => t.From).Select(g => g.ToList()).ToList();
+
+            var groupCount = string.Join(",", groupedTransactions.Select(p => p.Count));
+            Logger.LogInformation($"Simple GroupCount: {groupCount}");
+            
+            Logger.LogTrace("Begin SimpleTransactionGrouper.GroupAsync");
+            return new GroupedTransactions
+            {
+                Parallelizables = groupedTransactions
+            };
+        }
+    }
+}
