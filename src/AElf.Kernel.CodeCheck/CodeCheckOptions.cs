ef4c8cd5518cf621ad8eba9412fa7a9446a52c9b
zhxymh <sean.zhang@aelf.io>
2023-03-20 20:32:17 +0800

    feat: add code check job

--- src/AElf.Kernel.CodeCheck/CodeCheckOptions.cs
+++ src/AElf.Kernel.CodeCheck/CodeCheckOptions.cs
@@ -3,4 +3,6 @@ namespace AElf.Kernel.CodeCheck;
 public class CodeCheckOptions
 {
     public bool CodeCheckEnabled { get; set; }
+    public int PoolLimit { get; set; } = 5120;
+    public int PoolParallelismDegree { get; set; } = 5;
 }
