89bc0a8023ca022054ad46bd82a59402332da06b
kevin <kaiwen.zheng@aelf.io>
2023-06-29 14:16:18 +0800

    fix: ut

--- test/AElf.Contracts.TokenConverter.Tests/TokenConverterTestBase.cs
+++ test/AElf.Contracts.TokenConverter.Tests/TokenConverterTestBase.cs
@@ -7,11 +7,15 @@ using AElf.Contracts.Parliament;
 using AElf.Contracts.Treasury;
 using AElf.ContractTestKit.AEDPoSExtension;
 using AElf.Cryptography.ECDSA;
+using AElf.CSharp.Core.Extension;
 using AElf.EconomicSystem;
+using AElf.Kernel;
 using AElf.Kernel.Consensus;
 using AElf.Kernel.Proposal;
 using AElf.Kernel.Token;
+using AElf.Standards.ACS3;
 using AElf.Types;
+using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using Volo.Abp.Threading;
 
@@ -30,7 +34,7 @@ public class TokenConverterTestBase : AEDPoSExtensionTestBase
             ParliamentSmartContractAddressNameProvider.Name,
             ConsensusSmartContractAddressNameProvider.Name
         }));
-
+        AsyncHelper.RunSync(InitializeParliamentContractAsync);
         AsyncHelper.RunSync(InitializeTokenAsync);
     }
 
@@ -47,16 +51,17 @@ public class TokenConverterTestBase : AEDPoSExtensionTestBase
 
     private async Task InitializeTokenAsync()
     {
-        await TokenContractStub.Create.SendAsync(new CreateInput
-        {
-            Symbol = "ELF",
-            Decimals = 2,
-            IsBurnable = true,
-            TokenName = "elf token",
-            TotalSupply = 1000_0000_0000L,
-            Issuer = DefaultSender,
-            LockWhiteList = { TokenConverterContractAddress }
-        });
+        await ExecuteProposalForParliamentTransaction(TokenContractAddress, nameof(TokenContractStub.Create),
+            new CreateInput
+            {
+                Symbol = "ELF",
+                Decimals = 2,
+                IsBurnable = true,
+                TokenName = "elf token",
+                TotalSupply = 1000_0000_0000L,
+                Issuer = DefaultSender,
+                LockWhiteList = { TokenContractAddress, TokenConverterContractAddress }
+            });
         await TokenContractStub.Issue.SendAsync(new IssueInput
         {
             Symbol = "ELF",
@@ -92,6 +97,61 @@ public class TokenConverterTestBase : AEDPoSExtensionTestBase
     private void CheckResult(TransactionResult result)
     {
         if (!string.IsNullOrEmpty(result.Error)) throw new Exception(result.Error);
+    }
+
+    protected async Task ExecuteProposalForParliamentTransaction(Address contract,
+        string method, IMessage input, Address parliamentOrganization = null)
+    {
+        if (parliamentOrganization == null)
+            parliamentOrganization =
+                await ParliamentContractStub.GetDefaultOrganizationAddress.CallAsync(new Empty());
+        var proposalHash =
+            await CreateAndApproveProposalForParliament(contract, method, input,
+                parliamentOrganization);
+        await ParliamentContractStub.Release.SendAsync(proposalHash);
+    }
+
+    private async Task<Hash> CreateAndApproveProposalForParliament(Address contract,
+        string method, IMessage input, Address parliamentOrganization = null)
+    {
+        if (parliamentOrganization == null)
+            parliamentOrganization =
+                await ParliamentContractStub.GetDefaultOrganizationAddress.CallAsync(new Empty());
+        var proposal = new CreateProposalInput
+        {
+            OrganizationAddress = parliamentOrganization,
+            ContractMethodName = method,
+            ExpiredTime = TimestampHelper.GetUtcNow().AddHours(1),
+            Params = input.ToByteString(),
+            ToAddress = contract
+        };
+        var createResult = await ParliamentContractStub.CreateProposal.SendAsync(proposal);
+        var proposalHash = createResult.Output;
+        await ApproveByParliamentMembers(proposalHash);
+        return proposalHash;
+    }
+
+    protected async Task<TransactionResult> ExecuteProposalForParliamentTransactionWithException(
+        Address contract,
+        string method, IMessage input, Address parliamentOrganization = null)
+    {
+        if (parliamentOrganization == null)
+            parliamentOrganization =
+                await ParliamentContractStub.GetDefaultOrganizationAddress.CallAsync(new Empty());
+        var proposalHash =
+            await CreateAndApproveProposalForParliament(contract, method, input,
+                parliamentOrganization);
+        var releaseResult = await ParliamentContractStub.Release.SendWithExceptionAsync(proposalHash);
+        return releaseResult.TransactionResult;
+    }
+
+    private async Task ApproveByParliamentMembers(Hash proposalId)
+    {
+        foreach (var bp in InitialCoreDataCenterKeyPairs)
+        {
+            var tester = GetParliamentContractTester(bp);
+            await tester.Approve.SendAsync(proposalId);
+        }
     }
 
     #region Contract Address
