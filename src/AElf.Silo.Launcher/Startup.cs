64facac8106b019f52e7c79e6c10f04e3c0375fe
yann <feng.yan@aelf.io>
2023-11-14 11:50:00 +0800

    produce block success

---
+++ src/AElf.Silo.Launcher/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using AElf.Kernel;
+using AElf.Kernel.SmartContract.Orleans;
+using AElf.Silo.Launcher;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace AElf.Launcher;
+
+public class Startup
+{
+    private const string DefaultCorsPolicyName = "CorsPolicy";
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var chainType = _configuration.GetValue("ChainType", ChainType.MainChain);
+        // switch (chainType)
+        // {
+        //     case ChainType.SideChain:
+        //         AddApplication<SideChainAElfModule>(services);
+        //         break;
+        //     default:
+        //         AddApplication<MainChainAElfModule>(services);
+        //         break;
+        // }
+        AddApplication<AElfSiloLauncherModule>(services);
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy(DefaultCorsPolicyName, builder =>
+            {
+                builder
+                    .WithOrigins(_configuration["CorsOrigins"]
+                        .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(o => o.RemovePostFix("/"))
+                        .ToArray()
+                    )
+                    .WithAbpExposedHeaders()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                if (_configuration["CorsOrigins"] != "*") builder.AllowCredentials();
+            });
+        });
+//        AddApplication<SiloExecutionAElfModule>(services);
+
+    }
+
+    private static void AddApplication<T>(IServiceCollection services) where T : IAbpModule
+    {
+        services.AddApplication<T>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    // ReSharper disable once UnusedMember.Global
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        var cultureInfo = CultureInfo.InvariantCulture;
+        CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+
+        // app.UseRouting();
+        // app.UseAuthentication();
+        // app.UseAuthorization();
+        // app.UseCors(DefaultCorsPolicyName);
+        //
+        app.InitializeApplication();
+    }
+}
