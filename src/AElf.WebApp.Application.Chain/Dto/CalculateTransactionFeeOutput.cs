4a9536670a2f86b91367e8c211a0e94418ec4ddd
ssun0121 <sherry.sun@aelf.io>
2023-11-28 01:29:56 +0800

    feat:change output structure.

--- src/AElf.WebApp.Application.Chain/Dto/CalculateTransactionFeeOutput.cs
+++ src/AElf.WebApp.Application.Chain/Dto/CalculateTransactionFeeOutput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using AElf.Types;
 
@@ -7,11 +8,21 @@ public class CalculateTransactionFeeOutput
 {
     public bool Success { get; set; }
 
+    [Obsolete("ThisField is deprecated, please use NewField instead.")]
     public Dictionary<string, long> TransactionFee { get; set; }
-
+    
+    [Obsolete("ThisField is deprecated, please use NewField instead.")]
     public Dictionary<string, long> ResourceFee { get; set; }
+    
+    public List<FeeDto> TransactionFeeList { get; set; }
+
+    public List<FeeDto> ResourceFeeList { get; set; }
 
     public string Error { get; set; }
-    
-    public string TransactionFeeChargingAddress { get; set; }
+}
+
+public class FeeDto
+{
+    public string ChargingAddress { get; set; }
+    public Dictionary<string, long> Fee { get; set; }
 }
