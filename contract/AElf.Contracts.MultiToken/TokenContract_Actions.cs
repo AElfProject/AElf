242638e12e1c82bb8fbe5b17976dd7933cbfbf33
kevin <kaiwen.zheng@aelf.io>
2023-12-08 10:16:59 +0800

    fix: assert message

--- contract/AElf.Contracts.MultiToken/TokenContract_Actions.cs
+++ contract/AElf.Contracts.MultiToken/TokenContract_Actions.cs
@@ -600,7 +600,7 @@ public partial class TokenContract : TokenContractImplContainer.TokenContractImp
         var tokenInfo = State.TokenInfos[input.Symbol];
 
         Assert(tokenInfo != null, "Token is not found.");
-        Assert(tokenInfo.Owner == null, "Only set token without owner.");
+        Assert(tokenInfo.Owner == null, "Can only set token which does not have owner.");
         Assert(tokenInfo.Issuer == Context.Sender, "Only token issuer can set token issuer and owner.");
 
         tokenInfo.Issuer = input.Issuer;
