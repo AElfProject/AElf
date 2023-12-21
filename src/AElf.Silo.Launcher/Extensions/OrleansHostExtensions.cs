ecef731f7e1e1f516b8fccfe7f34ca1a5f40939f
yann <feng.yan@aelf.io>
2023-12-21 15:40:14 +0800

    add Thread gc config

--- src/AElf.Silo.Launcher/Extensions/OrleansHostExtensions.cs
+++ src/AElf.Silo.Launcher/Extensions/OrleansHostExtensions.cs
@@ -72,7 +72,7 @@ public static class OrleansHostExtensions
                     options.CollectionAge = TimeSpan.FromMinutes(2);
                     options.CollectionQuantum = TimeSpan.FromMinutes(1);
                 })
-                /*.Configure<PerformanceTuningOptions>(opt =>
+                .Configure<PerformanceTuningOptions>(opt =>
                 {
                     opt.MinDotNetThreadPoolSize = 20480;
                     opt.MinIOThreadPoolSize = 200;
@@ -84,7 +84,7 @@ public static class OrleansHostExtensions
                 }).Configure<SiloMessagingOptions>(opt =>
                 {
                     opt.ResponseTimeout = TimeSpan.FromSeconds(10);
-                })*/
+                })
                 // .AddMemoryGrainStorage("PubSubStore")
                 .ConfigureApplicationParts(parts => parts.AddFromApplicationBaseDirectory())
                 .UseDashboard(options =>
