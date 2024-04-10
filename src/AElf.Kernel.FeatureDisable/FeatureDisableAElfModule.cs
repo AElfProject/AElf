66becb98adce1865af0190b4dc8322485884d9c9
eanzhao <yiqi.zhao@aelf.io>
2024-04-10 15:20:38 +0800

    Commit test case for feature disable service.

--- src/AElf.Kernel.FeatureDisable/FeatureDisableAElfModule.cs
+++ src/AElf.Kernel.FeatureDisable/FeatureDisableAElfModule.cs
@@ -1,10 +1,12 @@
-﻿using AElf.Kernel.FeatureDisable.Core;
+﻿using AElf.Kernel.Configuration;
+using AElf.Kernel.FeatureDisable.Core;
 using AElf.Modularity;
 using Volo.Abp.Modularity;
 
 namespace AElf.Kernel.FeatureDisable;
 
-[DependsOn(typeof(FeatureDisableCoreAElfModule))]
+[DependsOn(typeof(FeatureDisableCoreAElfModule),
+    typeof(ConfigurationAElfModule))]
 public class FeatureDisableAElfModule : AElfModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
