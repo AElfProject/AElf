bab222c707068d1de2942ac4ab5bdb0172cb7f75
chopin-fan <chopin.fan@aelf.io>
2024-07-20 11:42:06 +0800

    Resolve issues with thread safety caused by parallel execution

--- src/AElf.CSharp.CodeOps/CSharpContractAuditor.cs
+++ src/AElf.CSharp.CodeOps/CSharpContractAuditor.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 using AElf.CSharp.CodeOps.Policies;
 using AElf.CSharp.CodeOps.Validators;
 using AElf.CSharp.CodeOps.Validators.Assembly;
@@ -12,91 +13,109 @@ using AElf.Kernel.SmartContract;
 using Microsoft.Extensions.Options;
 using Mono.Cecil;
 
-
-namespace AElf.CSharp.CodeOps;
-
-public class CSharpContractAuditor : IContractAuditor
+namespace AElf.CSharp.CodeOps
 {
-    private readonly IAcsValidator _acsValidator;
-
-    public int Category { get; } = 0;
-
-    public IOptionsMonitor<CSharpCodeOpsOptions> CodeOpsOptionsMonitor { get; set; }
-
-    private readonly IPolicy _policy;
-
-    public CSharpContractAuditor(IPolicy policy, IAcsValidator acsValidator)
+    public class CSharpContractAuditor : IContractAuditor
     {
-        _policy = policy;
-        _acsValidator = acsValidator;
-    }
+        private readonly IAcsValidator _acsValidator;
+        private readonly IPolicy _policy;
 
-    public void Audit(byte[] code, RequiredAcs requiredAcs, bool isSystemContract)
-    {
-        var findings = new ConcurrentBag<ValidationResult>();
-        var asm = Assembly.Load(code);
-        var modDef = ModuleDefinition.ReadModule(new MemoryStream(code));
-        var cts = new CancellationTokenSource(CodeOpsOptionsMonitor?.CurrentValue.AuditTimeoutDuration ??
-                                              Constants.DefaultAuditTimeoutDuration);
-        // Run module validators
-        foreach (var finding in Validate(modDef, cts.Token, isSystemContract).AsParallel())
+        public int Category { get; } = 0;
+        public IOptionsMonitor<CSharpCodeOpsOptions> CodeOpsOptionsMonitor { get; set; }
+
+        public CSharpContractAuditor(IPolicy policy, IAcsValidator acsValidator)
         {
-            findings.Add(finding);
+            _policy = policy;
+            _acsValidator = acsValidator;
         }
 
-        // Run assembly validators (run after module validators since we invoke BindService method below)
-        foreach (var finding in Validate(asm, cts.Token, isSystemContract).AsParallel())
+        public void Audit(byte[] code, RequiredAcs requiredAcs, bool isSystemContract)
         {
-            findings.Add(finding);
-        }
+            var findings = new ConcurrentBag<ValidationResult>();
+            var asm = Assembly.Load(code);
+            var modDef = ModuleDefinition.ReadModule(new MemoryStream(code));
+            var cts = new CancellationTokenSource(CodeOpsOptionsMonitor?.CurrentValue.AuditTimeoutDuration ??
+                                                  Constants.DefaultAuditTimeoutDuration);
 
-        // Run method validators
-        foreach (var type in modDef.Types.AsParallel())
-        {
-            foreach (var finding in ValidateMethodsInType(type, cts.Token, isSystemContract).AsParallel())
+            // Run module validators
+            var moduleFindings = Validate(modDef, cts.Token, isSystemContract);
+            foreach (var finding in moduleFindings)
             {
                 findings.Add(finding);
             }
-        }
 
-        // Perform ACS validation
-        if (requiredAcs != null)
-        {
-            foreach (var finding in _acsValidator.Validate(asm, requiredAcs).AsParallel())
+            // Run assembly validators
+            var assemblyFindings = Validate(asm, cts.Token, isSystemContract);
+            foreach (var finding in assemblyFindings)
             {
                 findings.Add(finding);
             }
+
+            // Run method validators
+            var methodFindings = modDef.Types.AsParallel()
+                .SelectMany(type => ValidateMethodsInType(type, cts.Token, isSystemContract));
+            foreach (var finding in methodFindings)
+            {
+                findings.Add(finding);
+            }
+
+            // Perform ACS validation
+            if (requiredAcs != null)
+            {
+                var acsFindings = _acsValidator.Validate(asm, requiredAcs);
+                foreach (var finding in acsFindings)
+                {
+                    findings.Add(finding);
+                }
+            }
+
+            if (findings.Count > 0)
+            {
+                throw new CSharpCodeCheckException(
+                    $"Contract code did not pass audit. Audit failed for contract: {modDef.Assembly.MainModule.Name}\n" +
+                    string.Join("\n", findings), findings.ToList());
+            }
         }
 
-        if (findings.Count > 0)
+        private IEnumerable<ValidationResult> Validate<T>(T t, CancellationToken ct, bool isSystemContract)
         {
-            throw new CSharpCodeCheckException(
-                $"Contract code did not pass audit. Audit failed for contract: {modDef.Assembly.MainModule.Name}\n" +
-                string.Join("\n", findings), findings.ToList());
+            var validators = _policy.GetValidators<T>().AsParallel()
+                .Where(v => !v.SystemContactIgnored || !isSystemContract);
+
+            var results = new ConcurrentBag<ValidationResult>();
+            Parallel.ForEach(validators, v =>
+            {
+                foreach (var result in v.Validate(t, ct))
+                {
+                    results.Add(result);
+                }
+            });
+
+            return results;
         }
-    }
 
-    private IEnumerable<ValidationResult> Validate<T>(T t, CancellationToken ct, bool isSystemContract)
-    {
-        var validators = _policy.GetValidators<T>().AsParallel().Where(v => !v.SystemContactIgnored || !isSystemContract);
-        return validators.AsParallel().SelectMany(v => v.Validate(t, ct));
-    }
-        
-    private IEnumerable<ValidationResult> ValidateMethodsInType(TypeDefinition type,
-        CancellationToken ct, bool isSystemContract)
-    {
-        var findings = new List<ValidationResult>();
-
-        foreach (var method in type.Methods)
+        private IEnumerable<ValidationResult> ValidateMethodsInType(TypeDefinition type,
+            CancellationToken ct, bool isSystemContract)
         {
-            findings.AddRange(Validate(method, ct, isSystemContract));
-        }
+            var findings = new ConcurrentBag<ValidationResult>();
 
-        foreach (var nestedType in type.NestedTypes)
-        {
-            findings.AddRange(ValidateMethodsInType(nestedType, ct, isSystemContract));
-        }
+            Parallel.ForEach(type.Methods, method =>
+            {
+                foreach (var finding in Validate(method, ct, isSystemContract))
+                {
+                    findings.Add(finding);
+                }
+            });
 
-        return findings;
+            Parallel.ForEach(type.NestedTypes, nestedType =>
+            {
+                foreach (var finding in ValidateMethodsInType(nestedType, ct, isSystemContract))
+                {
+                    findings.Add(finding);
+                }
+            });
+
+            return findings;
+        }
     }
 }
