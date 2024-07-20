fe82d78016b4f6f52a7ab33e3a206565433868c3
chopin-fan <chopin.fan@aelf.io>
2024-07-20 14:38:34 +0800

    Change protoc version to 3.19.4 in install_protobuf.sh

--- scripts/install_protobuf.sh
+++ scripts/install_protobuf.sh
@@ -13,18 +13,18 @@ fi
 echo "Install protobuf on ${osn}"
 
 if [[ ${osn} == "macosx" ]]; then
-    curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-osx-x86_64.zip
-    unzip protoc-3.11.4-osx-x86_64.zip -d protoc3
+    curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.19.4/protoc-3.19.4-osx-x86_64.zip
+    unzip protoc-3.19.4-osx-x86_64.zip -d protoc3
     sudo mv protoc3/bin/* /usr/local/bin/
     sudo mv protoc3/include/* /usr/local/include
     sudo chown ${USER} /usr/local/bin/protoc
     sudo chown -R ${USER} /usr/local/include/google
 elif [[ ${osn} == "linux" ]]; then
     # Make sure you grab the latest version
-    curl -OL https://github.com/google/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
+    curl -OL https://github.com/google/protobuf/releases/download/v3.19.4/protoc-3.19.4-linux-x86_64.zip
     
     # Unzip
-    unzip protoc-3.11.4-linux-x86_64.zip -d protoc3
+    unzip protoc-3.19.4-linux-x86_64.zip -d protoc3
     
     # Move protoc to /usr/local/bin/
     sudo mv protoc3/bin/* /usr/local/bin/
