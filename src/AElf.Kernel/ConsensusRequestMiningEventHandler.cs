692c6b2a3072438c1bd0557b11a616378a44ec00
chopin-fan <chopin.fan@aelf.io>
2024-12-19 09:49:16 +0800

    1. Remove log and resolve issue

--- src/AElf.Kernel/ConsensusRequestMiningEventHandler.cs
+++ src/AElf.Kernel/ConsensusRequestMiningEventHandler.cs
@@ -44,9 +44,7 @@ public class ConsensusRequestMiningEventHandler : ILocalEventHandler<ConsensusRe
             var chain = await _blockchainService.GetChainAsync();
             if (eventData.PreviousBlockHash != chain.BestChainHash)
             {
-                Logger.LogDebug($"PreviousBlockHash:{eventData.PreviousBlockHash},BestChainHash：{chain.BestChainHash},BestChainHeight:{chain.BestChainHeight},preHeight:{eventData.PreviousBlockHeight}");
-                await TriggerConsensusEventAsync(chain.BestChainHash, chain.BestChainHeight);
-                Logger.LogDebug("Mining canceled because best chain already updated 1");
+                Logger.LogDebug("Mining canceled because best chain already updated");
                 return;
             }
 
